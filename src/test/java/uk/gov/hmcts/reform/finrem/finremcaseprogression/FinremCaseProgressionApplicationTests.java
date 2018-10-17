4963324d3346a04c1d0014c2856c425bb5ce5086
shaibazkhan <sknizami@gmail.com>
2018-10-17 11:36:13 +0100

    added check for controller not null

--- src/test/java/uk/gov/hmcts/reform/finrem/finremcaseprogression/FinremCaseProgressionApplicationTests.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/finremcaseprogression/FinremCaseProgressionApplicationTests.java
@@ -1,16 +1,24 @@
 package uk.gov.hmcts.reform.finrem.finremcaseprogression;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.finrem.finremcaseprogression.controllers.FRCCDCallbackController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FinremCaseProgressionApplicationTests {
 
+	@Autowired
+	private FRCCDCallbackController frccdCallbackController;
+
 	@Test
 	public void contextLoads() {
+		assertThat(frccdCallbackController).isNotNull();
 	}
 
 }
