115d4e8cf29a6d81ac54290b28bbf31728d7b286
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-10-18 11:33:43 +0100

    FR-365-Setup Jacoco, PMD, Checkstyle, OWASP, SonarCloud for unit tests and coverage reports
    - fixed checkstyle issue

--- src/test/java/uk/gov/hmcts/reform/finrem/finremcaseprogression/smoketest/SmokeTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/finremcaseprogression/smoketest/SmokeTest.java
@@ -1,14 +1,13 @@
 package uk.gov.hmcts.reform.finrem.finremcaseprogression.smoketest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.runner.RunWith;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.finrem.finremcaseprogression.controllers.CcdCallbackController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,7 +17,7 @@ public class SmokeTest {
     private CcdCallbackController ccdCallbackController;
 
     @Test
-    public void contexLoads() {
+    public void contextLoads() {
         assertThat(ccdCallbackController).isNotNull();
     }
 }
