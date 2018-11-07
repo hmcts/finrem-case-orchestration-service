7bec3f07c28e7920a690fea2152e82bb0a2c8fc3
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-07 10:45:53 +0000

    FR-465- set up shared infra and vault
    - updated main.tf and variables.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,58 +1,50 @@
 locals {
-  aseName         = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  local_env       = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  local_ase       = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
-  idam_s2s_url    = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+
+  idam_s2s_url = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+
+
+  previewVaultName = "${var.reform_team}-aat"
+  nonPreviewVaultName = "${var.reform_team}-${var.env}"
+  vaultName = "${var.env == "preview" ? local.previewVaultName : local.nonPreviewVaultName}"
+  vaultUri = "${data.azurerm_key_vault.finrem_key_vault.vault_uri}"
+
+  asp_name = "${var.env == "prod" ? "finrem-cos-prod" : "${var.raw_product}-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "finrem-cos-prod" : "${var.raw_product}-${var.env}"}"
 }
 
-module "finrem-case-orchestration" {
-  source          = "git@github.com:hmcts/moj-module-webapp?ref=master"
-  product         = "${var.product}-${var.app}"
-  location        = "${var.location}"
-  env             = "${var.env}"
-  ilbIp           = "${var.ilbIp}"
-  subscription    = "${var.subscription}"
-  is_frontend     = false
-  capacity        = "${var.capacity}"
-  common_tags     = "${var.common_tags}"
+module "finrem-ns" {
+  source                          = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
+  product                         = "${var.product}-${var.component}"
+  location                        = "${var.location}"
+  env                             = "${var.env}"
+  ilbIp                           = "${var.ilbIp}"
+  subscription                    = "${var.subscription}"
+  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
+  capacity                        = "${var.capacity}"
+  is_frontend                     = false
+  common_tags                     = "${var.common_tags}"
+  asp_name                        = "${local.asp_name}"
+  asp_rg                          = "${local.asp_rg}"
 
   app_settings = {
+    REFORM_SERVICE_NAME                                   = "${var.reform_service_name}"
+    REFORM_TEAM                                           = "${var.reform_team}"
     REFORM_ENVIRONMENT                                    = "${var.env}"
-//    AUTH_PROVIDER_SERVICE_CLIENT_BASEURL                  = "${local.idam_s2s_url}"
-//    AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE             = "${var.auth_provider_service_client_microservice}"
-//    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.vault_generic_secret.auth-provider-service-client-key.data["value"]}"
-//    AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
-//    AUTH_PROVIDER_HEALTH_URI                              = "${local.idam_s2s_url}/health"
+    AUTH_PROVIDER_SERVICE_CLIENT_BASEURL                  = "${local.idam_s2s_url}"
+    AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE             = "${var.auth_provider_service_client_microservice}"
+    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.azurerm_key_vault_secret.finrem-case-orchestration-service-s2s-key.value}"
+    AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
   }
 }
 
-# region save DB details to Azure Key Vault
-module "key-vault" {
-  source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                = "${var.product}-${var.app}-${var.env}"
-  product             = "${var.product}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.finrem-case-orchestration.resource_group_name}"
-
-  # dcd_cc-dev group object ID
-  product_group_object_id = "94ac8962-b614-441b-aa4c-9be878a6bf17"
+data "azurerm_key_vault" "finrem_key_vault" {
+  name                = "${local.vaultName}"
+  resource_group_name = "${local.vaultName}"
 }
 
-provider "vault" {
-  // # tactical vault - for example: use `data "vault_generic_secret" "s2s_secret" {`
-  address = "https://vault.reform.hmcts.net:6200"
+data "azurerm_key_vault_secret" "finrem-case-orchestration-service-s2s-key" {
+  name      = "finrem-case-orchestration-service-s2s-key"
+  vault_uri = "${data.azurerm_key_vault.finrem_key_vault.vault_uri}"
 }
-
-//data "vault_generic_secret" "auth-provider-service-client-key" {
-//  path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/finrem-case-orchestration"
-//}
-//
-//resource "azurerm_key_vault_secret" "auth-provider-service-client-key" {
-//  name      = "auth-provider-service-client-key"
-//  value     = "${data.vault_generic_secret.auth-provider-service-client-key.data["value"]}"
-//  vault_uri = "${module.key-vault.key_vault_uri}"
-//}
-
-
