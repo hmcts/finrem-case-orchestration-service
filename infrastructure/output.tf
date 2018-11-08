a0e99700cbd85313b293aee7cdc10855b4c9f09c
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-08 10:19:49 +0000

    FR-465- set up shared infra and vault
    - updated output variables

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,18 +2,14 @@ output "env" {
   value = "${var.env}"
 }
 
-output "vaultName" {
-  value = "${module.key-vault.key_vault_name}"
-}
-
-output "vaultUri" {
-  value = "${module.key-vault.key_vault_uri}"
-}
-
 output "idam_s2s_url" {
   value = "${local.idam_s2s_url}"
 }
 
 output "test_environment" {
   value = "${local.local_env}"
+}
+
+output "vaultUri" {
+  value = "${data.azurerm_key_vault.finrem_key_vault.vault_uri}"
 }
