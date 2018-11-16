5921b909a0f7b5fbbdef627a1dc38786500dce52
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-16 11:42:54 +0000

    FR_464_integrate_case_orchestration_notification_service
    - Added finrem_notification_service into infrastructure
    - Enabled Swagger documentation
    - health check enabled

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/config/SwaggerConfiguration.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.reform.finrem.caseorchestration.CaseOrchestrationApplication;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Value("${documentation.swagger.enabled}")
+    private boolean swaggerEnabled;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(CaseOrchestrationApplication.class.getPackage().getName()))
+                .build()
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Financial Remedy Case Orchestration Service")
+                .description("Given a case data, This service will orchestrate the financial remedy features " +
+                        "like notifications, fee lookUp and DocumentGenerator")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        if (swaggerEnabled) {
+            registry.addResourceHandler("/swagger-ui.html**")
+                    .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+
+}
