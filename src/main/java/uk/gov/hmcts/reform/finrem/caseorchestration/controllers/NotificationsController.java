4cbfd6d801d195890727af3ed8bd3a7342570ebe
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-11-20 15:36:27 +0000

    FR_381_HWF_SUCCESSFUL_EMAIL
    - modified the call back response
    - docker compose file

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/NotificationsController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/NotificationsController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -27,11 +29,12 @@ public class NotificationsController {
     @ApiOperation(value = "send e-mail for HWF Successful.")
     @ApiResponses(value = {
             @ApiResponse(code = 204, message = "HWFSuccessful e-mail sent successfully")})
-    public ResponseEntity<Void> sendHwfSuccessfulConfirmationEmail(
+    public ResponseEntity<CCDCallbackResponse> sendHwfSuccessfulConfirmationEmail(
             @RequestBody CCDRequest ccdRequest,
             @RequestHeader(value = "Authorization") String userToken) {
         log.info("received notification request for case reference :    ", ccdRequest.getCaseDetails().getCaseId());
         notificationService.sendHWFSuccessfulConfirmationEmail(ccdRequest, userToken);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(CCDCallbackResponse.builder().data(caseData).build());
     }
 }
