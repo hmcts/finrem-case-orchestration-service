e99e934b47e766fc2746c2df6ce2936932ab51d0
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-12-13 15:35:51 +0000

    FR-588 Notification e2e test
    - updated e2e test for notification end points

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/NotificationsTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/NotificationsTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -27,11 +29,15 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDetails;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,6 +59,11 @@ public class NotificationsTest {
             + "/notify/consent-order-not-approved";
     private static final String ASSIGNED_TO_JUDGE_URL = "/case-orchestration"
             + "/notify/assign-to-judge";
+    private static final String NOTIFY_HWF_SUCCESSFUL_CONTEXT_PATH = "/notify/hwf-successful";
+    private static final String NOTIFY_CONSENT_ORDER_MADE_CONTEXT_PATH = "/notify/consent-order-made";
+    private static final String NOTIFY_CONSENT_ORDER_AVAILABLE_CONTEXT_PATH = "/notify/consent-order-available";
+    private static final String NOTIFY_CONSENT_ORDER_NOT_APPROVED_CONTEXT_PATH = "/notify/consent-order-not-approved";
+    private static final String NOTIFY_ASSIGN_TO_JUDGE_CONTEXT_PATH = "/notify/assign-to-judge";
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -74,6 +85,7 @@ public class NotificationsTest {
 
     @Test
     public void notifyHwfSuccessful() throws Exception {
+        stubForNotification(NOTIFY_HWF_SUCCESSFUL_CONTEXT_PATH, HttpStatus.OK.value());
         webClient.perform(MockMvcRequestBuilders.post(HWF_SUCCESSFUL_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
@@ -81,12 +93,13 @@ public class NotificationsTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().json(expectedCaseData()));
-        verify(postRequestedFor(urlEqualTo("/notify/hwf-successful"))
-                .withHeader("Content-Type", equalTo("application/json")));
+        verify(postRequestedFor(urlEqualTo(NOTIFY_HWF_SUCCESSFUL_CONTEXT_PATH))
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_JSON)));
     }
 
     @Test
     public void notifyConsentOrderMade() throws Exception {
+        stubForNotification(NOTIFY_CONSENT_ORDER_MADE_CONTEXT_PATH, HttpStatus.OK.value());
         webClient.perform(MockMvcRequestBuilders.post(CONSENT_ORDER_MADE_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
@@ -94,12 +107,13 @@ public class NotificationsTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().json(expectedCaseData()));
-        verify(postRequestedFor(urlEqualTo("/notify/consent-order-made"))
-                .withHeader("Content-Type", equalTo("application/json")));
+        verify(postRequestedFor(urlEqualTo(NOTIFY_CONSENT_ORDER_MADE_CONTEXT_PATH))
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_JSON)));
     }
 
     @Test
     public void notifyConsentOrderAvailable() throws Exception {
+        stubForNotification(NOTIFY_CONSENT_ORDER_AVAILABLE_CONTEXT_PATH, HttpStatus.OK.value());
         webClient.perform(MockMvcRequestBuilders.post(CONSENT_ORDER_AVAILABLE_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
@@ -107,12 +121,13 @@ public class NotificationsTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().json(expectedCaseData()));
-        verify(postRequestedFor(urlEqualTo("/notify/consent-order-available"))
-                .withHeader("Content-Type", equalTo("application/json")));
+        verify(postRequestedFor(urlEqualTo(NOTIFY_CONSENT_ORDER_AVAILABLE_CONTEXT_PATH))
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_JSON)));
     }
 
     @Test
     public void notifyConsentOrderNotApproved() throws Exception {
+        stubForNotification(NOTIFY_CONSENT_ORDER_NOT_APPROVED_CONTEXT_PATH, HttpStatus.OK.value());
         webClient.perform(MockMvcRequestBuilders.post(CONSENT_ORDER_NOT_APPROVED_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
@@ -120,12 +135,13 @@ public class NotificationsTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().json(expectedCaseData()));
-        verify(postRequestedFor(urlEqualTo("/notify/consent-order-not-approved"))
-                .withHeader("Content-Type", equalTo("application/json")));
+        verify(postRequestedFor(urlEqualTo(NOTIFY_CONSENT_ORDER_NOT_APPROVED_CONTEXT_PATH))
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_JSON)));
     }
 
     @Test
     public void notifyAssignToJudge() throws Exception {
+        stubForNotification(NOTIFY_ASSIGN_TO_JUDGE_CONTEXT_PATH, HttpStatus.OK.value());
         webClient.perform(MockMvcRequestBuilders.post(ASSIGNED_TO_JUDGE_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
@@ -133,8 +149,8 @@ public class NotificationsTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().json(expectedCaseData()));
-        verify(postRequestedFor(urlEqualTo("/notify/assign-to-judge"))
-                .withHeader("Content-Type", equalTo("application/json")));
+        verify(postRequestedFor(urlEqualTo(NOTIFY_ASSIGN_TO_JUDGE_CONTEXT_PATH))
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_JSON)));
     }
 
     private String expectedCaseData() throws JsonProcessingException {
@@ -144,6 +160,14 @@ public class NotificationsTest {
                 null, null));
         System.out.print(response);
         return response;
+    }
+
+    private void stubForNotification(String url, int value) {
+        notificationService.stubFor(post(urlEqualTo(url))
+                .withHeader(AUTHORIZATION, equalTo(AUTH_TOKEN))
+                .withHeader(HttpHeaders.CONTENT_TYPE, equalTo(APPLICATION_JSON))
+                .willReturn(aResponse()
+                        .withStatus(value)));
     }
 
 }
