b8fb8a31b4de9ff45a5dcafcca06a956cf8c801d
shaibazkhan <sknizami@gmail.com>
2018-12-21 00:12:44 +0000

    added e2e test for delete miniform A

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/GenerateMiniFormATest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/GenerateMiniFormATest.java
@@ -23,25 +23,28 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.CaseOrchestrationApplication
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDetails;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.caseDocument;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.document;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = CaseOrchestrationApplication.class)
@@ -54,9 +57,9 @@ public class GenerateMiniFormATest {
     private static final String AUTH_TOKEN = "LIUGBYUFUYVbnvhchvUFJHVJBlBJHBUYCUV";
     private static final String API_URL = "/case-orchestration/generate-mini-form-a";
     private static final String GENERATE_DOCUMENT_CONTEXT_PATH = "/version/1/generatePDF";
-    private static final String URL = "http://dm-store/lhjbyuivu87y989hijbb";
-    private static final String BINARY_URL = "http://dm-store/lhjbyuivu87y989hijbb/binary";
-    private static final String FILE_NAME = "app_docs.pdf";
+    private static final String DELETE_DOCUMENT_CONTEXT_PATH = "/version/1/delete-pdf-document";
+    public static final String TEMP_URL = "url";
+    public static final String TEMP_URL_BINARY = "url_binary";
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -104,20 +107,38 @@ public class GenerateMiniFormATest {
                 .andExpect(status().isInternalServerError());
     }
 
+    @Test
+    public void deleteExistingMiniFormAWithSuccess() throws Exception {
+        doTestDeleteMiniFormA(HttpStatus.OK);
+    }
+
+    @Test
+    public void deleteMiniFormAErrorShouldNotAffectNewMiniFormGeneration() throws Exception {
+        doTestDeleteMiniFormA(HttpStatus.NOT_FOUND);
+    }
+
+    private void doTestDeleteMiniFormA(HttpStatus miniFormAServiceStatus) throws Exception {
+        CaseDetails caseDetails = request.getCaseDetails();
+        caseDetails.getCaseData().setMiniFormA(caseDocument(TEMP_URL, TEMP_URL_BINARY));
+
+        generateDocumentServiceSuccessStub();
+        deleteDocumentServiceStubWith(miniFormAServiceStatus);
+
+        webClient.perform(MockMvcRequestBuilders.post(API_URL)
+                .content(objectMapper.writeValueAsString(request))
+                .header(AUTHORIZATION, AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedCaseData()));
+    }
+
     private String expectedCaseData() throws JsonProcessingException {
         CaseDetails caseDetails = request.getCaseDetails();
         caseDetails.getCaseData().setMiniFormA(caseDocument());
 
         return objectMapper.writeValueAsString(new CCDCallbackResponse(caseDetails.getCaseData(),
                 new ArrayList<>(), new ArrayList<>()));
-    }
-
-    private CaseDocument caseDocument() {
-        CaseDocument caseDocument = new CaseDocument();
-        caseDocument.setDocumentBinaryUrl(BINARY_URL);
-        caseDocument.setDocumentFilename(FILE_NAME);
-        caseDocument.setDocumentUrl(URL);
-        return caseDocument;
     }
 
     private DocumentRequest documentRequest() {
@@ -127,15 +148,11 @@ public class GenerateMiniFormATest {
                 .build();
     }
 
-    private Document document() {
-        Document document = new Document();
-        document.setBinaryUrl(BINARY_URL);
-        document.setCreatedOn("22 Oct 2018");
-        document.setFileName(FILE_NAME);
-        document.setMimeType("application/pdf");
-        document.setUrl(URL);
-
-        return document;
+    private void deleteDocumentServiceStubWith(HttpStatus status) {
+        documentGeneratorService.stubFor(
+                delete(urlMatching(DELETE_DOCUMENT_CONTEXT_PATH.concat("\\?fileUrl=").concat(TEMP_URL_BINARY)))
+                .withHeader(AUTHORIZATION, equalTo(AUTH_TOKEN))
+                .willReturn(aResponse().withStatus(status.value())));
     }
 
     private void generateDocumentServiceSuccessStub() throws JsonProcessingException {
