f524c2cf745a0ce56cb4d417b43e00d0bd36669d
shaibazkhan <sknizami@gmail.com>
2019-02-04 16:34:19 +0000

    added e2e test for rejected generation controller

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/AbstractDocumentTest.java
@@ -0,0 +1,130 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.e2etest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.hmcts.reform.finrem.caseorchestration.CaseOrchestrationApplication;
+import uk.gov.hmcts.reform.finrem.caseorchestration.config.DocumentConfiguration;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.BINARY_URL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.DOC_URL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.FILE_NAME;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CaseOrchestrationApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource(value = "classpath:application.properties")
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public abstract class AbstractDocumentTest {
+
+    private static final String GENERATE_DOCUMENT_CONTEXT_PATH = "/version/1/generatePDF";
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected MockMvc webClient;
+
+    @Autowired
+    protected DocumentConfiguration documentConfiguration;
+
+    @ClassRule
+    public static WireMockClassRule documentGeneratorService = new WireMockClassRule(4009);
+
+    protected CCDRequest request;
+
+    @Before
+    public void setUp() throws IOException {
+        try (InputStream resourceAsStream = getClass().getResourceAsStream("/fixtures/fee-lookup.json")) {
+            request = objectMapper.readValue(resourceAsStream, CCDRequest.class);
+        }
+    }
+
+    protected abstract DocumentRequest documentRequest();
+
+    protected abstract String apiUrl();
+
+
+
+    @Test
+    public void documentGeneratorServiceError() throws Exception {
+        generateDocumentServiceErrorStub();
+
+        webClient.perform(MockMvcRequestBuilders.post(apiUrl())
+                .content(objectMapper.writeValueAsString(request))
+                .header(AUTHORIZATION, AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+    }
+
+    private Document document() {
+        Document document = new Document();
+        document.setBinaryUrl(BINARY_URL);
+        document.setCreatedOn("22 Oct 2018");
+        document.setFileName(FILE_NAME);
+        document.setMimeType("application/pdf");
+        document.setUrl(DOC_URL);
+
+        return document;
+    }
+
+    protected void generateDocumentServiceSuccessStub() throws JsonProcessingException {
+        documentGeneratorService.stubFor(post(urlPathEqualTo(GENERATE_DOCUMENT_CONTEXT_PATH))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(documentRequest()), true, true))
+                .withHeader(AUTHORIZATION, equalTo(AUTH_TOKEN))
+                .withHeader(CONTENT_TYPE, equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
+                        .withBody(objectMapper.writeValueAsString(document()))));
+    }
+
+
+
+    private void generateDocumentServiceErrorStub() throws JsonProcessingException {
+        documentGeneratorService.stubFor(post(urlPathEqualTo(GENERATE_DOCUMENT_CONTEXT_PATH))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(documentRequest()), true, true))
+                .withHeader(AUTHORIZATION, equalTo(AUTH_TOKEN))
+                .withHeader(CONTENT_TYPE, equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/e2etest/RejectionOrderTest.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.e2etest;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentRequest;
+
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.BINARY_URL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.DOC_URL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.FILE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.REJECTED_ORDER_TYPE;
+
+public class RejectionOrderTest extends AbstractDocumentTest {
+
+    private static final String API_URL = "/case-orchestration/documents/consent-order-not-approved";
+
+    @Override
+    protected DocumentRequest documentRequest() {
+        return DocumentRequest.builder()
+                .template(documentConfiguration.getRejectedOrderTemplate())
+                .fileName(documentConfiguration.getRejectedOrderFileName())
+                .values(Collections.singletonMap("caseDetails", request.getCaseDetails()))
+                .build();
+    }
+
+    @Override
+    protected String apiUrl() {
+        return API_URL;
+    }
+
+    @Test
+    public void generateConsentOrder() throws Exception {
+        generateDocumentServiceSuccessStub();
+
+        webClient.perform(MockMvcRequestBuilders.post(apiUrl())
+                .content(objectMapper.writeValueAsString(request))
+                .header(AUTHORIZATION, AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.uploadOrder[0].id", is(notNullValue())))
+                .andExpect(jsonPath("$.data.uploadOrder[0].value.DocumentType", is(REJECTED_ORDER_TYPE)))
+                .andExpect(jsonPath("$.data.uploadOrder[0].value.DocumentDateAdded", is(notNullValue())))
+                .andExpect(jsonPath("$.data.uploadOrder[0].value.DocumentLink.document_url", is(DOC_URL)))
+                .andExpect(
+                        jsonPath("$.data.uploadOrder[0].value.DocumentLink.document_filename", is(FILE_NAME)))
+                .andExpect(
+                        jsonPath("$.data.uploadOrder[0].value.DocumentLink.document_binary_url",
+                                is(BINARY_URL)))
+                .andExpect(jsonPath("$.errors", hasSize(0)))
+                .andExpect(jsonPath("$.warnings", hasSize(0)));
+    }
+}
