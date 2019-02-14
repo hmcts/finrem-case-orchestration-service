0d993097abb9cffaeda5d43ff6b798f8fe161b96
shaibazkhan <sknizami@gmail.com>
2019-02-14 14:54:47 +0000

    fixed failing test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/BaseServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/BaseServiceTest.java
@@ -1,21 +1,11 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration;
 
-import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
 
 @TestPropertySource(locations = "/application.properties")
+@DirtiesContext
 public abstract class BaseServiceTest extends BaseTest {
 
-    @Autowired
-    protected RestTemplate restTemplate;
 
-    protected MockRestServiceServer mockServer;
-
-    @Before
-    public void setUp() {
-        mockServer = MockRestServiceServer.createServer(restTemplate);
-    }
 }
