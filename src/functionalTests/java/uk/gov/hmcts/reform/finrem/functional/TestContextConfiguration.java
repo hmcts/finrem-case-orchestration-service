c027546b8b061e9e645615e53efba0d60fcac656
devadathpt <devadathpt@gmail.com>
2019-02-19 11:43:50 +0000

    refactor

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
@@ -19,9 +19,24 @@ import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 @Configuration
 @ComponentScan("uk.gov.hmcts.reform.finrem.functional")
 @EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
-//@PropertySource(value = {"classpath:application.properties"})
-@PropertySource(value = {"classpath:application-aat.properties"})
+@PropertySource(value = {"classpath:application.properties"})
+@PropertySource(value = {"classpath:application-${env}.properties"})
 public class TestContextConfiguration {
 
-
+    @Bean
+    public ServiceAuthTokenGenerator serviceAuthTokenGenerator(@Value("${idam.s2s-auth.url}")
+                                                                   String s2sUrl,
+                                                               @Value("${idam.oauth2.client.secret}")
+                                                                   String secret,
+                                                               @Value("${idam.s2s-auth.microservice}")
+                                                                       String microservice) {
+        final ServiceAuthorisationApi serviceAuthorisationApi = Feign.builder()
+            .encoder(new JacksonEncoder())
+            .contract(new SpringMvcContract())
+            .target(ServiceAuthorisationApi.class, s2sUrl);
+        log.info("S2S URL: {}", s2sUrl);
+        log.info("service.name: {}", microservice);
+        log.info(": {idam.oauth2.client.secret}", secret);
+        return new ServiceAuthTokenGenerator(secret, microservice, serviceAuthorisationApi);
+    }
 }
