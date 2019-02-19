ebf16de43055ac2a541ba24e179771f5880daa89
devadathpt <devadathpt@gmail.com>
2019-02-19 12:38:09 +0000

    payment tests changes

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/payments/PaymentServiceTests.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/payments/PaymentServiceTests.java
@@ -67,7 +67,6 @@ public class PaymentServiceTests extends IntegrationTestBase {
     }
 
 
-
     public void validatePostSuccessForPBAValidation(String url) {
 
         IntegrationTestBase.setPaymentServiceUrlUrlAsBaseUri();
