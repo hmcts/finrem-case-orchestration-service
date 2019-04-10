f1cf3115c26ba555d4672f1b468a72337c1da9f4
devadathpt <devadathpt@gmail.com>
2019-04-10 11:56:53 +0100

    amountToPay migration functional Tests

---
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/migration/MigrationTests.java
@@ -0,0 +1,81 @@
+package uk.gov.hmcts.reform.finrem.functional.migration;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.jsoup.select.Evaluator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import uk.gov.hmcts.reform.finrem.functional.IntegrationTestBase;
+
+import static org.junit.Assert.*;
+
+@RunWith(SerenityRunner.class)
+public class MigrationTests extends IntegrationTestBase {
+
+    @Value("${migration.api}")
+    private String migrationUrl;
+
+    private static String CCD_MIGRATE_JSON = "ccd-migrate-request.json";
+    private static String CCD_MIGRATE_DONT_JSON = "ccd-migrate-dont-request.json";
+    private static String CCD_MIGRATE_DONT_JSON_NOAMOUNT_NOHWF = "ccd-migrate-dont-request.json";
+
+    @Test
+    public void verifyMigrationIsDone() {
+
+        JsonPath jsonPathEvaluator = validateDoMigration(CCD_MIGRATE_JSON,migrationUrl);
+        assertTrue(jsonPathEvaluator.get("data.amountToPay").toString()
+                .equalsIgnoreCase("5000"));
+        validatePostSuccess(CCD_MIGRATE_JSON,migrationUrl);
+
+    }
+
+    @Test
+    public void verifyMigrationIsNotRequired() {
+
+        validatePostSuccess(CCD_MIGRATE_DONT_JSON,migrationUrl);
+        verifyDontMigrateAssertions(validateDoMigration(CCD_MIGRATE_DONT_JSON,migrationUrl));
+
+    }
+
+    @Test
+    public void verifyMigrationIsNotRequiredWhenNoAmountToPayAndNoHWFInRequest() {
+
+        validatePostSuccess(CCD_MIGRATE_DONT_JSON_NOAMOUNT_NOHWF,migrationUrl);
+        verifyDontMigrateAssertions(validateDoMigration(CCD_MIGRATE_DONT_JSON_NOAMOUNT_NOHWF,migrationUrl));
+
+    }
+
+    private void verifyDontMigrateAssertions(JsonPath jsonPath)
+    {
+        assertNull("Should be null",jsonPath.get("data"));
+        assertNull("Should be null",jsonPath.get("data_classification"));
+        assertNull("Should be null",jsonPath.get("security_classification"));
+        assertNull("Should be null",jsonPath.get("errors"));
+        assertNull("Should be null",jsonPath.get("warnings"));
+    }
+
+    private void validatePostSuccess(String jsonFileName,String url) {
+
+        System.out.println("url " + url);
+        SerenityRest.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .body(utils.getJsonFromFile(jsonFileName))
+                .when().post(url)
+                .then()
+                .assertThat().statusCode(200);
+    }
+
+    private JsonPath validateDoMigration(String jsonFileName,String url) {
+        Response jsonResponse = SerenityRest.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .body(utils.getJsonFromFile(jsonFileName))
+                .when().post(url)
+                .andReturn();
+        return jsonResponse.jsonPath();
+    }
+}
