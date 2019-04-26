034bbc89fe393b0f10e19ed2aea86b825e8fb433
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2019-04-26 16:43:30 +0100

    FR-901
    - removed updating issue date

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseEventController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseEventController.java
@@ -14,13 +14,11 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CcdUpdateService;
 
 import javax.validation.constraints.NotNull;
-import java.time.ZonedDateTime;
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ContestedEvent.ALLOCATE_TO_JUDGE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ContestedEvent.ISSUE_APPLICATION;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ISSUE_DATE;
 
 @RestController
 @RequestMapping("/case-orchestration")
@@ -36,14 +34,14 @@ public class CaseEventController implements BaseController {
             @RequestHeader(value = "Authorization", required = false) String authToken,
             @NotNull @RequestBody @ApiParam("CaseData") CallbackRequest callback) {
         validateCaseData(callback);
-        Map<String, Object> caseData = callback.getCaseDetails().getData();
-        caseData.put(ISSUE_DATE, ZonedDateTime.now().toLocalDate());
+
         log.info(" create event for issue Application called ..");
         ccdUpdateService.createEvent(authToken, callback.getCaseDetails(), ISSUE_APPLICATION.getId(),
                 ISSUE_APPLICATION.getEventSummary(), ISSUE_APPLICATION.getEventDescription());
         log.info(" create event for Allocate to Judge called ..");
         ccdUpdateService.createEvent(authToken, callback.getCaseDetails(), ALLOCATE_TO_JUDGE.getId(),
                 ALLOCATE_TO_JUDGE.getEventSummary(), ALLOCATE_TO_JUDGE.getEventDescription());
+        Map<String, Object> caseData = callback.getCaseDetails().getData();
         return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder().data(caseData).build());
     }
 }
