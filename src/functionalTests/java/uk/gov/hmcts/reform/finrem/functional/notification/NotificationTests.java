65ca5cd8ebb5d09d441eac59e9d4ce763105af08
devadathpt <35432453+devadathpt@users.noreply.github.com>
2019-05-21 10:40:34 +0100

    Case orchestration contested final (#89)
    * contested Amend Application
    
    * Amend application functional tests
    
    * Contested Payment Service functional Tests
    
    * payment services testing
    
    * payment services test success
    
    * payment services test success
    
    * payment services test success
    
    * removed contested pba payment end point
    
    * added document generator tests added
    
    * added document generator tests added
    
    * payment services tests May 17
    
    * payment service tests
    
    * payment service tests
    
    * payment service tests
    
    * Notification  tests refactort
    
    * Notification  tests refactort
    
    * Refactor
    
    * Refactor fee lookup
    
    * Refactor fee lookup
    
    * Refactor fee lookup
    
    * Removed migration tests not needed for contested release
    
    * Removed extra fee lookup test
    
    * FeeAmount refactor for fee code lookup
    
    * testing document generator
    
    * contested document generator tests fix
    
    * refactor, remove printing commands
    
    * refactor appliation properties
    
    * failing test verifyShouldRemovePreviousMiamAttendanceCheckListForContested checking
    
    * failing tests verification
    
    * failing tests verification
    
    * failing tests verification
    
    * added validate hearing dates tests
    
    * added validate hearing dates tests
    
    * Document generator tests check

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/notification/NotificationTests.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/notification/NotificationTests.java
@@ -25,11 +25,16 @@ public class NotificationTests extends IntegrationTestBase {
     @Value("${cos.notification.hwf-success.api}")
     private String hwfSuccessfulApiUri;
 
+    private String contestedDir = "/json/contested/";
+    private String consentedDir = "/json/consented/";
+
+
 
     @Test
     public void verifyNotifyAssignToJudgeTestIsOkay() {
 
-        validatePostSuccessForNotification(notifyAssignToJudge, "ccd-request-with-solicitor-assignedToJudge.json");
+        validatePostSuccessForNotification(notifyAssignToJudge,
+                "ccd-request-with-solicitor-assignedToJudge.json", consentedDir);
 
     }
 
@@ -38,7 +43,7 @@ public class NotificationTests extends IntegrationTestBase {
     public void verifyNotifyConsentOrderAvailableTestIsOkay() {
 
         validatePostSuccessForNotification(consentOrderAvailable,
-                "ccd-request-with-solicitor-consentOrderAvailable.json");
+                "ccd-request-with-solicitor-consentOrderAvailable.json", consentedDir);
 
     }
 
@@ -46,7 +51,8 @@ public class NotificationTests extends IntegrationTestBase {
     @Test
     public void verifyNotifyConsentOrderMadeTestIsOkay() {
 
-        validatePostSuccessForNotification(consentOrderMade, "ccd-request-with-solicitor-consentOrderMade.json");
+        validatePostSuccessForNotification(consentOrderMade,
+                "ccd-request-with-solicitor-consentOrderMade.json", consentedDir);
 
     }
 
@@ -55,18 +61,19 @@ public class NotificationTests extends IntegrationTestBase {
     public void verifyNotifyConsentOrderNotApprovedTestIsOkay() {
 
         validatePostSuccessForNotification(consentOrderNotApproved,
-                "ccd-request-with-solicitor-consentOrderNotApproved.json");
+                "ccd-request-with-solicitor-consentOrderNotApproved.json", consentedDir);
 
     }
 
     @Test
     public void verifyNotifyHwfSuccessfulTestIsOkay() {
 
-        validatePostSuccessForNotification(hwfSuccessfulApiUri, "ccd-request-with-solicitor-hwfSuccessfulEmail.json");
+        validatePostSuccessForNotification(hwfSuccessfulApiUri,
+                "ccd-request-with-solicitor-hwfSuccessfulEmail.json", consentedDir);
 
     }
 
-    private void validatePostSuccessForNotification(String url, String jsonFileName) {
+    private void validatePostSuccessForNotification(String url, String jsonFileName, String journeyType) {
 
         SerenityRest.given()
                 .relaxedHTTPSValidation()
