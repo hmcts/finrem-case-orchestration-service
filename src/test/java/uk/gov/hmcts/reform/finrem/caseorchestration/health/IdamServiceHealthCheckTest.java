854a8239ff362e11fceb17d422b84a14bb379a80
shaibazkhan <sknizami@gmail.com>
2019-05-29 19:19:44 +0100

    added idam health check

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/health/IdamServiceHealthCheckTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.health;
+
+public class IdamServiceHealthCheckTest extends AbstractServiceHealthCheckTest {
+
+    private static final String URI = "http://localhost:4501/health";
+
+    @Override
+    protected String uri() {
+        return URI;
+    }
+
+    @Override
+    protected AbstractServiceHealthCheck healthCheckInstance() {
+        return new IdamServiceHealthCheck(URI, restTemplate);
+    }
+}
