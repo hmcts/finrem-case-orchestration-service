0ffab72293624881138fb4f78a99dd00f27b21d3
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2019-06-13 15:58:49 +0100

    FR_1126_dockerise_Prd_api
    - docker update to include rd-professional-api

---
+++ bin/load-pba-reference-data.sh
@@ -0,0 +1,8 @@
+serviceToken="$(./pba-service-token.sh)"
+
+echo ${serviceToken}
+
+curl -X POST http://localhost:8090/v1/organisations \
+-H "Content-Type: application/json" \
+-H "ServiceAuthorization: Bearer ${serviceToken}" \
+-d '{"name": "TEST ORG5","status": "PENDING","sraId": "1115","sraRegulated": true,"companyNumber": "1110115","companyUrl": "http://testorg5.co.uk","superUser": {"firstName": "Tester5","lastName": "Tester5","email": "henry_fr_harper@yahoo.com"},"paymentAccount": ["PBA0222"]}'
---
+++ bin/pba-service-token.sh
@@ -0,0 +1,1 @@
+curl -X GET -H "Content-Type: application/json"  http://localhost:8089/token
