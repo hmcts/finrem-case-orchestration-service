668e47f02b4395b02711440033235d53ddfeed36
Hasan <hasan.kshaik@gmail.com>
2019-07-18 13:45:14 +0100

    FR-1216: refactoring for bulk print (#113)
    * FR-1216: refactoring for bulk print
    
    * Fixing sonar issues

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenerateCoverSheetServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenerateCoverSheetServiceTest.java
@@ -13,37 +13,35 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentRequest;
 
 import java.io.InputStream;
-import java.util.Map;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.document;
 
-public class BulkPrintServiceTest {
+public class GenerateCoverSheetServiceTest {
 
     private final ObjectMapper mapper = new ObjectMapper();
     private DocumentClient generatorClient;
     private DocumentConfiguration config;
-    private BulkPrintService service;
-    private UUID letterId;
+    private GenerateCoverSheetService coverSheetService;
 
     @Before
     public void setUp() {
-        letterId = UUID.randomUUID();
         config = new DocumentConfiguration();
         config.setBulkPrintFileName("test_file");
         config.setBulkPrintTemplate("test_template");
 
         generatorClient = new TestDocumentClient();
-        service = new BulkPrintService(generatorClient, config, mapper);
+        coverSheetService = new GenerateCoverSheetService(generatorClient, config, mapper);
     }
 
     @Test
     public void sendForBulkPrint() throws Exception {
-        UUID bulkPrintLetterId = service.sendForBulkPrint(new CaseDocument(), caseDetails());
-        assertThat(letterId, is(bulkPrintLetterId));
-
+        CaseDocument caseDocument = coverSheetService.generateCoverSheet(caseDetails(), "AUTH_TOKEN");
+        assertThat(document().getBinaryUrl(), is(caseDocument.getDocumentBinaryUrl()));
+        assertThat(document().getFileName(), is(caseDocument.getDocumentFilename()));
+        assertThat(document().getUrl(), is(caseDocument.getDocumentUrl()));
     }
 
     private CaseDetails caseDetails() throws Exception {
@@ -55,18 +53,16 @@ public class BulkPrintServiceTest {
 
     private class TestDocumentClient implements DocumentClient {
 
-        private Map<String, Object> value;
 
         @Override
         public Document generatePDF(DocumentRequest request, String authorizationToken) {
-            value = request.getValues();
+            assertThat(request.getTemplate(), is("test_template"));
             return document();
         }
 
         @Override
         public UUID bulkPrint(BulkPrintRequest bulkPrintRequest) {
-            assertThat(bulkPrintRequest.getBulkPrintDocuments().size(), is(2));
-            return letterId;
+            throw new UnsupportedOperationException();
         }
 
         @Override
@@ -74,9 +70,6 @@ public class BulkPrintServiceTest {
             throw new UnsupportedOperationException();
         }
 
-        private Map<String, Object> data() {
-            CaseDetails caseDetails = (CaseDetails) value.get("caseDetails");
-            return caseDetails.getData();
-        }
+
     }
 }
