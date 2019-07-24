c8184fbda2616fbc3a1660dddc36a1d1fe46eb41
Harry Henry <H.Henry@kainos.com>
2019-07-24 12:57:03 +0100

    Implemented null check for latest consent order field to force old journeys for upload consent order

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CheckLatestConsentOrderController.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+
+import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.Objects.isNull;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/case-orchestration")
+public class CheckLatestConsentOrderController implements BaseController {
+
+    @PostMapping(path = "/check-latest-consent-order", consumes = APPLICATION_JSON_VALUE,
+        produces = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Validation check for latest consent order field in CCD")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Callback was processed successFully or in case of an error message is "
+            + "attached to the case",
+            response = AboutToStartOrSubmitCallbackResponse.class),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 500, message = "Internal Server Error")
+        })
+
+    public ResponseEntity<AboutToStartOrSubmitCallbackResponse> generateConsentOrderNotApproved(
+        @RequestHeader(value = "Authorization") String authorisationToken,
+        @NotNull @RequestBody @ApiParam("CaseData") CallbackRequest callback) {
+
+        validateCaseData(callback);
+
+        Map<String, Object> caseData = callback.getCaseDetails().getData();
+
+        if (isNull(caseData.get("latestConsentOrder"))) {
+            return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder()
+                .errors(Arrays.asList("Latest Consent Order Field is empty. "
+                        + "Please use the Upload Consent Order Event instead of Send Order"))
+                .build());
+        }
+
+        return ResponseEntity.ok(
+            AboutToStartOrSubmitCallbackResponse.builder()
+                .build());
+    }
+}
