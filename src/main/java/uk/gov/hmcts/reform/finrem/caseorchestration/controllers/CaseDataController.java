5fcf25bf7bcc74bca49e4cb7fe3b8820af3c3482
Hasan <hasan.kshaik@gmail.com>
2019-09-25 10:44:11 +0100

    adding move-collection method (#155)
    * adding move-collection method
    
    * adding move-collection method
    
    * adding move-collection method

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +13,9 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.IdamService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -29,30 +33,57 @@ public class CaseDataController implements BaseController {
     private final IdamService idamService;
 
     @PostMapping(path = "/consented/set-defaults", consumes = APPLICATION_JSON, produces =
-        APPLICATION_JSON)
+                                                                                        APPLICATION_JSON)
     public ResponseEntity<AboutToStartOrSubmitCallbackResponse> setConsentedDefaultValues(
-        @RequestHeader(value = "Authorization", required = false) String authToken,
-        @RequestBody CallbackRequest callbackRequest) {
+            @RequestHeader(value = "Authorization", required = false) final String authToken,
+            @RequestBody final CallbackRequest callbackRequest) {
         log.info("Setting default values for contested journey.");
         validateCaseData(callbackRequest);
-        Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
+        final Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
         setData(authToken, caseData);
         return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder().data(caseData).build());
     }
 
     @PostMapping(path = "/contested/set-defaults", consumes = APPLICATION_JSON, produces =
-        APPLICATION_JSON)
+                                                                                        APPLICATION_JSON)
     public ResponseEntity<AboutToStartOrSubmitCallbackResponse> setContestedDefaultValues(
-        @RequestHeader(value = "Authorization", required = false) String authToken,
-        @RequestBody CallbackRequest callbackRequest) {
+            @RequestHeader(value = "Authorization", required = false) final String authToken,
+            @RequestBody final CallbackRequest callbackRequest) {
         log.info("Setting default values for contested journey.");
         validateCaseData(callbackRequest);
-        Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
+        final Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
         setData(authToken, caseData);
         return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder().data(caseData).build());
     }
 
-    private void setData(String authToken, Map<String, Object> caseData) {
+    @PostMapping(path = "/move-collection/{source}/to/{destination}", consumes = APPLICATION_JSON,
+            produces = APPLICATION_JSON)
+    public ResponseEntity<AboutToStartOrSubmitCallbackResponse> moveValues(
+            @RequestHeader(value = "Authorization", required = false) final String authToken,
+            @RequestBody final CallbackRequest callbackRequest,
+            @PathVariable("source") final String source,
+            @PathVariable("destination") final String destination) {
+
+        log.info("Setting default values for contested journey.");
+        validateCaseData(callbackRequest);
+        final Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
+        if (caseData.get(source) != null && (caseData.get(source) instanceof Collection)) {
+            if (caseData.get(destination) == null || (caseData.get(destination) instanceof Collection)) {
+                final List destinationList = new ArrayList();
+                if (caseData.get(destination) != null) {
+                    destinationList.addAll((List) caseData.get(destination));
+                }
+                destinationList.addAll((List) caseData.get(source));
+                caseData.put(destination, destinationList);
+                caseData.put(source, null);
+            }
+
+        }
+
+        return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder().data(caseData).build());
+    }
+
+    private void setData(final String authToken, final Map<String, Object> caseData) {
         if (idamService.isUserRoleAdmin(authToken)) {
             log.info("Admin users.");
             caseData.put(IS_ADMIN, YES);
