51cd6f615ecea55f55aea6e9eb80c2da482c3d93
Hasan Shaik <khaja.shaikh@hmcts.net>
2019-10-01 16:44:37 +0100

    Migration for courts and judge allocated

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/ccd/datamigration/controller/CcdDataMigrationController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/ccd/datamigration/controller/CcdDataMigrationController.java
@@ -6,15 +6,16 @@ import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -28,35 +29,89 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 @Slf4j
 public class CcdDataMigrationController {
 
+    public static final String ALLOCATED_COURT_LIST = "allocatedCourtList";
+    public static final String ALLOCATED_COURT_LIST_SL = "allocatedCourtListSL";
+    public static final String ALLOCATED_COURT_LIST_GA = "allocatedCourtListGA";
+    public static final String NOTTINGHAM_COURT_LIST_GA = "nottinghamCourtListGA";
+    public static final String CFC_COURT_LIST_GA = "cfcCourtListGA";
+    public static final String NOTTINGHAM_COURT_LIST_SL = "nottinghamCourtListSL";
+    public static final String CFC_COURT_LIST_SL = "cfcCourtListSL";
+    public static final String NOTTINGHAM_COURT_LIST = "nottinghamCourtList";
+    public static final String CFC_COURT_LIST = "cfcCourtList";
+
     @PostMapping(value = "/migrate", consumes = APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Callback was processed successFully or in case of an error message is "
-                    + "attached to the case", response = CallbackResponse.class)})
+                                                       + "attached to the case", response = CallbackResponse.class)})
     public CallbackResponse migrate(
-            @RequestHeader(value = "Authorization") String authorisationToken,
-            @RequestBody @ApiParam("CaseData") CallbackRequest ccdRequest) {
+            @RequestHeader(value = "Authorization") final String authorisationToken,
+            @RequestBody @ApiParam("CaseData") final CallbackRequest ccdRequest) {
         log.info("ccdMigrationRequest >>> authorisationToken {}, ccdRequest {}", authorisationToken, ccdRequest);
-        Map<String, Object> caseData = ccdRequest.getCaseDetails().getData();
-        boolean shouldMigrateCase = shouldMigrateCase(caseData);
-        log.info("shouldMigrateCase >>> {}", shouldMigrateCase);
-        if (shouldMigrateCase) {
-            String value = Objects.toString(caseData.get(JUDGE_ALLOCATED));
-            caseData.put(JUDGE_ALLOCATED, new String[] { value } );
+        final Map<String, Object> caseData = ccdRequest.getCaseDetails().getData();
+        boolean migrationRequired = false;
+        final Object caseId = ccdRequest.getCaseDetails().getId();
+        final Object judgeAllocated = caseData.get(JUDGE_ALLOCATED);
 
+        if (nonNull(judgeAllocated) && !ObjectUtils.isEmpty(judgeAllocated)) {
+            if (judgeAllocated instanceof String) {
+                final String value = Objects.toString(judgeAllocated);
+                caseData.put(JUDGE_ALLOCATED, new String[]{value});
+            }
+            log.info("Migrating value for judgeAllocated  >>> {}", caseId);
+            migrationRequired = true;
+        }
+
+        final Object allocatedCourtList = caseData.get(ALLOCATED_COURT_LIST);
+        if (nonNull(allocatedCourtList) && !ObjectUtils.isEmpty(allocatedCourtList)) {
+            if (allocatedCourtList instanceof String) {
+                courtData(caseData, ALLOCATED_COURT_LIST, NOTTINGHAM_COURT_LIST, CFC_COURT_LIST);
+            }
+            log.info("Migrating value for allocatedCourtList  >>> {}", caseId);
+            migrationRequired = true;
+        }
+
+        final Object allocatedCourtListSL = caseData.get(ALLOCATED_COURT_LIST_SL);
+        if (nonNull(allocatedCourtListSL) && !ObjectUtils.isEmpty(allocatedCourtListSL)) {
+            if (allocatedCourtListSL instanceof String) {
+                courtData(caseData, ALLOCATED_COURT_LIST_SL, NOTTINGHAM_COURT_LIST_SL, CFC_COURT_LIST_SL);
+            }
+            log.info("Migrating value for allocatedCourtListSL  >>> {}", caseId);
+            migrationRequired = true;
+        }
+
+        final Object allocatedCourtListGA = caseData.get(ALLOCATED_COURT_LIST_GA);
+        if (nonNull(allocatedCourtListGA) && !ObjectUtils.isEmpty(allocatedCourtListGA)) {
+            if (allocatedCourtListGA instanceof String) {
+                courtData(caseData, ALLOCATED_COURT_LIST_GA, NOTTINGHAM_COURT_LIST_GA, CFC_COURT_LIST_GA);
+            }
+            log.info("Migrating value for allocatedCourtListGA  >>> {}", caseId);
+            migrationRequired = true;
+        }
+
+        if (migrationRequired) {
             return AboutToStartOrSubmitCallbackResponse.builder().data(caseData).build();
         } else {
             return AboutToStartOrSubmitCallbackResponse.builder().build();
         }
     }
 
-    private boolean shouldMigrateCase(Map<String, Object> caseData) {
-        Object judgeAllocated = caseData.get(JUDGE_ALLOCATED);
-        if (nonNull(judgeAllocated) && !ObjectUtils.isEmpty(judgeAllocated)) {
-            if (judgeAllocated instanceof String) {
-                return true;
-            }
-            return false;
+    private void courtData(final Map<String, Object> caseData, final String allocatedCourtListKey,
+                           final String nottinghamCourtListKey, final String cfcCourtListKey) {
+        final Object allocatedCourtList = caseData.get(allocatedCourtListKey);
+        final String allocatedCourtListStr = Objects.toString(allocatedCourtList);
+        final Map<String, Object> map = new HashMap<>();
+        if (allocatedCourtListStr.equalsIgnoreCase("nottingham")) {
+            map.put("region", "midlands");
+            map.put("midlandsList", "nottingham");
+            map.put("nottinghamCourtList", Objects.toString(caseData.get(nottinghamCourtListKey)));
+            caseData.put(nottinghamCourtListKey, null);
+        } else if (allocatedCourtListStr.equalsIgnoreCase("cfc")) {
+            map.put("region", "london");
+            map.put("londonList", "cfc");
+            map.put("cfcCourtList", Objects.toString(caseData.get(cfcCourtListKey)));
+            caseData.put(cfcCourtListKey, null);
         }
-        return false;
+        caseData.put(allocatedCourtListKey, map);
     }
+
 }
