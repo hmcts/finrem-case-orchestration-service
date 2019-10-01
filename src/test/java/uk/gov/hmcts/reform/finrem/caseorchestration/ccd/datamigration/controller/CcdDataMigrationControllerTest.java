df796bfcc3c4674fc43583c6946b9c3f4193f892
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2019-10-01 14:32:21 +0100

    Revert "Revert "Migration for FR-1249 - judge allocated field (#145)" (#161)"
    This reverts commit 504abf1912adcd60b1206add788c2e730ba82157.

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/ccd/datamigration/controller/CcdDataMigrationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/ccd/datamigration/controller/CcdDataMigrationControllerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.ccd.datamigration.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,24 +10,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.CaseOrchestrationApplication;
 
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 
-import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.STATE;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CcdDataMigrationController.class)
@@ -55,16 +48,17 @@ public class CcdDataMigrationControllerTest {
     private CallbackRequest ccdAlreadyMigratedRequest() throws IOException {
         String alreadyMigratedRequestJson = "/fixtures/ccd-already-migrated-request.json";
         try (InputStream resourceAsStream = getClass().getResourceAsStream(alreadyMigratedRequestJson)) {
-            return objectMapper.readValue(resourceAsStream, CallbackRequest.class);
+            ccdMigrationRequest =  objectMapper.readValue(resourceAsStream, CallbackRequest.class);
         }
+        return ccdMigrationRequest;
     }
 
-    private String expectedCaseData() throws JsonProcessingException {
-        CaseDetails caseDetails = ccdMigrationRequest.getCaseDetails();
-        Map<String, Object> caseData = caseDetails.getData();
-        caseData.remove(STATE);
-        return objectMapper.writeValueAsString(AboutToStartOrSubmitCallbackResponse.builder()
-                .data(caseData).build());
+    private CallbackRequest ccdRequestWithoutField() throws IOException {
+        String withoutFieldJson = "/fixtures/ccd-request-without-field.json";
+        try (InputStream resourceAsStream = getClass().getResourceAsStream(withoutFieldJson)) {
+            ccdMigrationRequest =  objectMapper.readValue(resourceAsStream, CallbackRequest.class);
+        }
+        return ccdMigrationRequest;
     }
 
     private void doMigrateSetup() {
@@ -80,26 +74,35 @@ public class CcdDataMigrationControllerTest {
                 .header("Authorization", BEARER_TOKEN)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json(expectedCaseData()))
-                .andExpect(jsonPath("$.data", not(hasKey(STATE))))
+                .andExpect(jsonPath("$.data.judgeAllocated", contains("Option1")))
                 .andExpect(jsonPath("$.errors", isEmptyOrNullString()))
                 .andExpect(jsonPath("$.warnings", isEmptyOrNullString()));
     }
 
     @Test
-    public void shouldNotDoMigration() throws Exception {
+    public void shouldNotDoMigrationAsAlreadyStringArray() throws Exception {
         doMigrateSetup();
 
         mvc.perform(post(MIGRATE_URL)
                 .content(objectMapper.writeValueAsString(ccdAlreadyMigratedRequest()))
                 .header("Authorization", BEARER_TOKEN)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json("{}"))
                 .andExpect(jsonPath("$.data", isEmptyOrNullString()))
                 .andExpect(jsonPath("$.errors", isEmptyOrNullString()))
                 .andExpect(jsonPath("$.warnings", isEmptyOrNullString()));
     }
 
+    @Test
+    public void shouldNotDoMigrationAsDoesntContainField() throws Exception {
+        doMigrateSetup();
 
+        mvc.perform(post(MIGRATE_URL)
+                .content(objectMapper.writeValueAsString(ccdRequestWithoutField()))
+                .header("Authorization", BEARER_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", isEmptyOrNullString()))
+                .andExpect(jsonPath("$.errors", isEmptyOrNullString()))
+                .andExpect(jsonPath("$.warnings", isEmptyOrNullString()));
+    }
 }
