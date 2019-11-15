b2501d7a4d96b2c79a2020c76683b9c751202d3c
John O'Neill <j.oneill@kainos.com>
2019-11-15 14:33:26 +0000

    Added tests for AuthService (#190)

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AuthServiceTest.java
@@ -0,0 +1,72 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.finrem.caseorchestration.error.UnauthenticatedException;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.AuthService;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    private static final String SERVICE_HEADER = "some-header";
+
+    @Mock
+    private AuthTokenValidator validator;
+
+    private AuthService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new AuthService(validator, asList("allowed_service1", "allowed_service2"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        reset(validator);
+    }
+
+    @Test
+    public void should_throw_missing_header_exception_when_it_is_null() {
+        Throwable exception = catchThrowable(() -> service.authenticate(null));
+
+        assertThat(exception)
+                .isInstanceOf(UnauthenticatedException.class)
+                .hasMessage("Missing ServiceAuthorization header");
+
+        verify(validator, never()).getServiceName(anyString());
+    }
+
+    @Test
+    public void should_track_failure_in_service_dependency_when_invalid_token_received() {
+        willThrow(InvalidTokenException.class).given(validator).getServiceName(anyString());
+
+        Throwable exception = catchThrowable(() -> service.authenticate(SERVICE_HEADER));
+
+        assertThat(exception).isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    public void should_track_successful_service_dependency_when_valid_token_received() {
+        given(validator.getServiceName(SERVICE_HEADER)).willReturn("some-service");
+
+        String serviceName = service.authenticate(SERVICE_HEADER);
+
+        assertThat(serviceName).isEqualTo("some-service");
+    }
+}
