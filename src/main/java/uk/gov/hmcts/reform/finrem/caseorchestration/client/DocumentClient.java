6865913f2782c6d04f10be7ab55263e40a2d31d3
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2019-11-21 10:37:50 +0000

    Pipeline issue

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/client/DocumentClient.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/client/DocumentClient.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.client;
 
-import feign.Body;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -44,7 +43,6 @@ public interface DocumentClient {
             path = "/file-upload-check",
             headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
     )
-//    @Body("%7B%7D")
     DocumentValidationResponse checkUploadedFileType(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationToken,
             @RequestParam("fileBinaryUrl") String fileUrl
