bc206daa035268d9d99015c39ab603d09bcfc561
John O'Neill <j.oneill@kainos.com>
2019-12-09 10:28:39 +0000

    BSP-145: Added update case endpoint for future use by BSP (#206)

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/bulkscan/update/in/transformation/output/SuccessfulUpdateResponse.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.bulkscan.update.in.transformation.output;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.bulkscan.transformation.output.CaseCreationDetails;
+
+import java.util.List;
+
+@Data
+@Builder
+public class SuccessfulUpdateResponse {
+
+    @JsonProperty("case_update_details")
+    public final CaseCreationDetails caseUpdateDetails;
+
+    @JsonProperty("warnings")
+    public final List<String> warnings;
+
+    public SuccessfulUpdateResponse(
+        CaseCreationDetails caseUpdateDetails,
+        List<String> warnings) {
+        this.caseUpdateDetails = caseUpdateDetails;
+        this.warnings = warnings;
+    }
+}
