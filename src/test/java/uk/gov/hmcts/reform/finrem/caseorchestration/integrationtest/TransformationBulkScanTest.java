b9f2ebdad6956abdd5ba64aab896a5030dfc22ab
Patryk Jar <patryk.jar@hmcts.net>
2020-03-10 18:19:20 +0100

    BSP-333 warnings to errors (#241)
    * BSP-333: mapping warnings to errors for transformation
    
    * BSP-333: tests for error handler
    
    * BSP-333: remove unused import
    
    * BSP-333: this test was moved to TransformationBulkScanTest class
    
    * refactored

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/integrationtest/TransformationBulkScanTest.java
@@ -0,0 +1,91 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.integrationtest;
+
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.bsp.common.model.validation.out.OcrValidationResult;
+import uk.gov.hmcts.reform.finrem.caseorchestration.CaseOrchestrationApplication;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.bulkscan.validation.FormAValidator;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.SERVICE_AUTHORISATION_HEADER;
+
+@ContextConfiguration(classes = CaseOrchestrationApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource(value = "classpath:application.yml")
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class TransformationBulkScanTest {
+    private static final String TRANSFORMATION_URL = "/transform-exception-record";
+    private static final String FORMA_JSON_PATH = "fixtures/bulkscan.transformation/simple-formA.json";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    AuthTokenValidator authTokenValidator;
+
+    @MockBean
+    FormAValidator bulkScanFormValidator;
+
+    @Before
+    public void setup() {
+        when(authTokenValidator.getServiceName(any())).thenReturn("bulk_scan_orchestrator");
+    }
+
+    @Test
+    public void shouldReturnErrorResponseForInvalidData() throws Exception {
+        String jsonPayload = loadResourceAsString(FORMA_JSON_PATH);
+        String warningMsg = "warn!";
+
+        when(bulkScanFormValidator.validateBulkScanForm(any()))
+            .thenReturn(OcrValidationResult.builder().addWarning(warningMsg).build());
+
+        mockMvc.perform(
+            post(TRANSFORMATION_URL)
+                .contentType(APPLICATION_JSON)
+                .content(jsonPayload)
+                .header(SERVICE_AUTHORISATION_HEADER, "I'm whitelisted service")
+        )
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().string(hasErrorWithMessageCopiedFromWarning(warningMsg)));
+    }
+
+    private Matcher<String> hasErrorWithMessageCopiedFromWarning(String warningMsg) {
+        return allOf(isJson(), hasJsonPath("$.errors", equalTo(Arrays.asList(warningMsg))));
+    }
+
+    public static String loadResourceAsString(final String filePath) throws Exception {
+        URL url = TransformationBulkScanTest.class.getClassLoader().getResource(filePath);
+
+        if (url == null) {
+            throw new IllegalArgumentException(String.format("Could not find resource in path %s", filePath));
+        }
+
+        return new String(Files.readAllBytes(Paths.get(url.toURI())));
+    }
+}
