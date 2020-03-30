26655e9761f4667ccd3f3c51d74040456b96ef60
John O'Neill <johnoneill546@gmail.com>
2020-03-30 18:26:49 +0100

    Removed dependance on importing file for unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/LetterAddressHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/LetterAddressHelperTest.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.helper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -14,31 +11,29 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstant
 
 public class LetterAddressHelperTest {
 
-    private ObjectMapper mapper = new ObjectMapper();
-
     private LetterAddressHelper letterAddressHelper = new LetterAddressHelper();
-
-    private CaseDetails caseDetails() throws Exception {
-        try (InputStream resourceAsStream =
-                     getClass().getResourceAsStream("/fixtures/bulk-print.json")) {
-            return mapper.readValue(resourceAsStream, CallbackRequest.class).getCaseDetails();
-        }
-    }
 
     @Test
     public void testAddressIsCorrectlyFormatterForLetterPrinting() throws Exception {
 
-        Map<String, Object> testAddress = (Map<String, Object>) caseDetails().getData().get("applicantAddress");
+        Map<String, Object> testAddressMap = new HashMap<>();
+        testAddressMap.put("AddressLine1", "50 Applicant Street");
+        testAddressMap.put("AddressLine2", "Second Address Line");
+        testAddressMap.put("AddressLine3", "Third Address Line");
+        testAddressMap.put("County", "Greater London");
+        testAddressMap.put("Country", "England");
+        testAddressMap.put("PostTown", "London");
+        testAddressMap.put("PostCode", "SW1");
 
-        String formattedAddress = letterAddressHelper.formatAddressForLetterPrinting(testAddress);
+        String formattedAddress = letterAddressHelper.formatAddressForLetterPrinting(testAddressMap);
 
-        String expectedAddress = "1 Victoria Street" + LINE_SEPARATOR
-            + "Westminster" + LINE_SEPARATOR
-            + "" + LINE_SEPARATOR
+        String expectedAddress = "50 Applicant Street" + LINE_SEPARATOR
+            + "Second Address Line" + LINE_SEPARATOR
+            + "Third Address Line" + LINE_SEPARATOR
             + "Greater London" + LINE_SEPARATOR
-            + "UK" + LINE_SEPARATOR
+            + "England" + LINE_SEPARATOR
             + "London" + LINE_SEPARATOR
-            + "SE1";
+            + "SW1";
 
         assertThat(formattedAddress, is(expectedAddress));
     }
