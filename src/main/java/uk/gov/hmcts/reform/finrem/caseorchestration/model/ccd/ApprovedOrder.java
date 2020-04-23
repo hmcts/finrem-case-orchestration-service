b214c12be9570364ab2dd48693903ed41d307787
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2020-04-23 14:17:08 +0100

    Removing getter and setter for ignored field

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ApprovedOrder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ApprovedOrder.java
@@ -3,10 +3,13 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -24,6 +27,8 @@ public class ApprovedOrder {
     private List<PensionCollectionData> pensionDocuments;
 
     @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     @JsonProperty("consentOrderApprovedNotificationLetter")
     private CaseDocument consentOrderApprovedNotificationLetter;
 }
