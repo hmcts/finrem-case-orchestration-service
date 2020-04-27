72931b87935cab8a53e5652d7950508c44d39983
Patryk Jar <jar.patryk@gmail.com>
2020-04-27 13:08:49 +0200

    more unit tests

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/impl/FeatureToggleServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/impl/FeatureToggleServiceImpl.java
@@ -13,6 +13,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.Features.APPROVED_CONSENT_ORDER_NOTIFICATION_LETTER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.Features.ASSIGNED_TO_JUDGE_NOTIFICATION_LETTER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.Features.HWF_SUCCESSFUL_NOTIFICATION_LETTER;
+
 @Service
 @ConfigurationProperties(prefix = "feature-toggle")
 @Configuration
@@ -27,5 +31,17 @@ public class FeatureToggleServiceImpl implements FeatureToggleService {
         return Optional.ofNullable(toggle.get(feature.getName()))
             .map(Boolean::parseBoolean)
             .orElse(false);
+    }
+
+    public boolean isApprovedConsentOrderNotificationLetterEnabled() {
+        return isFeatureEnabled(APPROVED_CONSENT_ORDER_NOTIFICATION_LETTER);
+    }
+
+    public boolean isHwfSuccessfulNotificationLetterEnabled() {
+        return isFeatureEnabled(HWF_SUCCESSFUL_NOTIFICATION_LETTER);
+    }
+
+    public boolean isAssignedToJudgeNotificationLetterEnabled() {
+        return isFeatureEnabled(ASSIGNED_TO_JUDGE_NOTIFICATION_LETTER);
     }
 }
