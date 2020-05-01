bfb1b84113ed83220ba3280d05d8d26fb17baaf2
John O'Neill <johnoneill546@gmail.com>
2020-05-01 17:40:32 +0100

    Moved common methods to AbstractDocumentService

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AbstractDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AbstractDocumentService.java
@@ -1,26 +1,45 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.client.DocumentClient;
 import uk.gov.hmcts.reform.finrem.caseorchestration.config.DocumentConfiguration;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.LetterAddressHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Addressee;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.BulkPrintRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.CtscContactDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentGenerationRequest;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_ADDRESS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_FIRST_MIDDLE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_LAST_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_RESPONDENT_FIRST_MIDDLE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_RESPONDENT_LAST_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_SOLICITOR_ADDRESS_CCD_FIELD;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CommonFunction.addressLineOneAndPostCodeAreBothNotEmpty;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CommonFunction.isApplicantRepresentedByASolicitor;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CommonFunction.nullToEmpty;
+
+@Slf4j
 public abstract class AbstractDocumentService {
     private static final String DOCUMENT_CASE_DETAILS_JSON_KEY = "caseDetails";
 
     protected final DocumentConfiguration config;
     private final DocumentClient documentClient;
     protected final ObjectMapper objectMapper;
+
+    private LetterAddressHelper letterAddressHelper = new LetterAddressHelper();
 
     public AbstractDocumentService(DocumentClient documentClient,
                                    DocumentConfiguration config,
@@ -88,6 +107,51 @@ public abstract class AbstractDocumentService {
         } catch (IOException e) {
             throw new IllegalStateException();
         }
+    }
+
+    CaseDetails prepareNotificationLetter(CaseDetails caseDetails) {
+        Map<String, Object> caseData = caseDetails.getData();
+        Map addressToSendTo;
+
+        String ccdNumber = nullToEmpty((caseDetails.getId()));
+        String reference = "";
+        String addresseeName;
+        String applicantName = nullToEmpty((caseData.get(APPLICANT_FIRST_MIDDLE_NAME)))
+            + " " + nullToEmpty((caseData.get(APPLICANT_LAST_NAME)));
+        String respondentName = nullToEmpty((caseData.get(APP_RESPONDENT_FIRST_MIDDLE_NAME)))
+            + " " + nullToEmpty((caseData.get(APP_RESPONDENT_LAST_NAME)));
+
+        if (isApplicantRepresentedByASolicitor(caseData)) {
+            log.info("Applicant is represented by a solicitor");
+            reference = nullToEmpty((caseData.get(SOLICITOR_REFERENCE)));
+            addresseeName = nullToEmpty((caseData.get(SOLICITOR_NAME)));
+            addressToSendTo = (Map) caseData.get(APP_SOLICITOR_ADDRESS_CCD_FIELD);
+        } else {
+            log.info("Applicant is not represented by a solicitor");
+            addresseeName = applicantName;
+            addressToSendTo = (Map) caseData.get(APPLICANT_ADDRESS);
+        }
+
+        if (addressLineOneAndPostCodeAreBothNotEmpty(addressToSendTo)) {
+            Addressee addressee = Addressee.builder()
+                .name(addresseeName)
+                .formattedAddress(letterAddressHelper.formatAddressForLetterPrinting(addressToSendTo))
+                .build();
+
+            caseData.put("caseNumber", ccdNumber);
+            caseData.put("reference", reference);
+            caseData.put("addressee",  addressee);
+            caseData.put("letterDate", String.valueOf(LocalDate.now()));
+            caseData.put("applicantName", applicantName);
+            caseData.put("respondentName", respondentName);
+            caseData.put("ctscContactDetails", buildCtscContactDetails());
+
+        } else {
+            log.info("Failed to generate Notification Letter as not all required address details were present");
+            throw new IllegalArgumentException(
+                "Mandatory data missing from address when trying to generate Assigned To Judge Notification Letter");
+        }
+        return caseDetails;
     }
 
     CtscContactDetails buildCtscContactDetails() {
