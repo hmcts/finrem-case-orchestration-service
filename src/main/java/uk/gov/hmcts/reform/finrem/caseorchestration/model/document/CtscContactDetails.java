e8bf638a14618985ad065f77f3e2fe3819ae1ac1
vivek reddy <vivek.reddy@hmcts.net>
2020-05-06 12:08:21 +0100

    rebase with master (#323)

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/document/CtscContactDetails.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.document;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@EqualsAndHashCode
+public class CtscContactDetails {
+
+    @JsonProperty("serviceCentre")
+    private String serviceCentre;
+
+    @JsonProperty("careOf")
+    private String careOf;
+
+    @JsonProperty("poBox")
+    private String poBox;
+
+    @JsonProperty("town")
+    private String town;
+
+    @JsonProperty("postcode")
+    private String postcode;
+
+    @JsonProperty("emailAddress")
+    private String emailAddress;
+
+    @JsonProperty("phoneNumber")
+    private String phoneNumber;
+
+    @JsonProperty("openingHours")
+    private String openingHours;
+}
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignedToJudgeDocumentServiceTest.java
@@ -0,0 +1,125 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.BaseServiceTest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.client.DocumentClient;
+import uk.gov.hmcts.reform.finrem.caseorchestration.config.DocumentConfiguration;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Addressee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.YES_VALUE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.assertCaseDocument;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.SetUpUtils.document;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_ADDRESS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_FIRST_MIDDLE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_LAST_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_REPRESENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_RESPONDENT_FIRST_MIDDLE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_RESPONDENT_LAST_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_SOLICITOR_ADDRESS_CCD_FIELD;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SOLICITOR_REFERENCE;
+
+@ActiveProfiles("test-mock-document-client")
+public class AssignedToJudgeDocumentServiceTest extends BaseServiceTest {
+
+    @Autowired
+    private DocumentClient documentClient;
+
+    @Autowired
+    private AssignedToJudgeDocumentService assignedToJudgeDocumentService;
+
+    private CaseDetails caseDetails;
+
+    @Before
+    public void setUp() {
+        DocumentConfiguration config = new DocumentConfiguration();
+        config.setApplicationAssignedToJudgeTemplate("FL-FRM-LET-ENG-00318.docx");
+        config.setApplicationAssignedToJudgeFileName("AssignedToJudgeNotificationLetter.pdf");
+
+        Map<String, Object> applicantAddress = new HashMap<>();
+        applicantAddress.put("AddressLine1", "50 Applicant Street");
+        applicantAddress.put("AddressLine2", "Second Address Line");
+        applicantAddress.put("AddressLine3", "Third Address Line");
+        applicantAddress.put("County", "London");
+        applicantAddress.put("Country", "England");
+        applicantAddress.put("PostTown", "London");
+        applicantAddress.put("PostCode", "SW1");
+
+        Map<String, Object> caseData = new HashMap<>();
+        caseData.put(APPLICANT_FIRST_MIDDLE_NAME, "James");
+        caseData.put(APPLICANT_LAST_NAME, "Joyce");
+        caseData.put(APPLICANT_ADDRESS, applicantAddress);
+        caseData.put(APPLICANT_REPRESENTED, null);
+        caseData.put(APP_RESPONDENT_FIRST_MIDDLE_NAME, "Jane");
+        caseData.put(APP_RESPONDENT_LAST_NAME, "Doe");
+
+        caseDetails = CaseDetails.builder()
+            .id(123456789L)
+            .data(caseData)
+            .build();
+    }
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @Test
+    public void shouldGenerateAssignedToJudgeLetterForApplicant() {
+
+        when(documentClient.generatePdf(any(), anyString())).thenReturn(document());
+
+        CaseDocument generateAssignedToJudgeNotificationLetter
+            = assignedToJudgeDocumentService.generateAssignedToJudgeNotificationLetter(caseDetails, AUTH_TOKEN);
+
+        assertCaseDocument(generateAssignedToJudgeNotificationLetter);
+        verify(documentClient, times(1)).generatePdf(any(), anyString());
+        verifyCtscContactDetails(caseDetails);
+    }
+
+    @Test
+    public void shouldGenerateAssignedToJudgeLetterForApplicantSolicitor() {
+        when(documentClient.generatePdf(any(), anyString())).thenReturn(document());
+
+        Map<String, Object> solicitorAddress = new HashMap<>();
+        solicitorAddress.put("AddressLine1", "123 Applicant Solicitor Street");
+        solicitorAddress.put("AddressLine2", "Second Address Line");
+        solicitorAddress.put("AddressLine3", "Third Address Line");
+        solicitorAddress.put("County", "London");
+        solicitorAddress.put("Country", "England");
+        solicitorAddress.put("PostTown", "London");
+        solicitorAddress.put("PostCode", "SE1");
+
+        Map<String, Object> caseData = caseDetails.getData();
+        caseData.replace(APPLICANT_REPRESENTED, YES_VALUE);
+        caseData.put(SOLICITOR_NAME, TEST_SOLICITOR_NAME);
+        caseData.put(SOLICITOR_REFERENCE, TEST_SOLICITOR_REFERENCE);
+        caseData.put(APP_SOLICITOR_ADDRESS_CCD_FIELD, solicitorAddress);
+
+        CaseDocument generatedAssignedToJudgeNotificationLetter
+            = assignedToJudgeDocumentService.generateAssignedToJudgeNotificationLetter(caseDetails, AUTH_TOKEN);
+
+        assertCaseDocument(generatedAssignedToJudgeNotificationLetter);
+
+        Addressee addressee = Addressee.builder()
+            .name("Saul Goodman")
+            .formattedAddress("123 Applicant Solicitor Street\nSecond Address Line\nThird Address Line\nLondon\nEngland\nLondon\nSE1")
+            .build();
+        assertEquals(addressee, caseDetails.getData().get("addressee"));
+        verifyCtscContactDetails(caseDetails);
+    }
+}
