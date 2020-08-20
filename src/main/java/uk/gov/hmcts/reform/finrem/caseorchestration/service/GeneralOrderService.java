014bbdbbaa8f68b212b7ff2b6fb5bb2fe50f60b6
Dale Cotterill <dalec@kainos.com>
2020-08-20 14:45:49 +0100

    RPET-74 - update general order
    + to differentiate between consent and contested for court name and header

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GeneralOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GeneralOrderService.java
@@ -94,10 +94,14 @@ public class GeneralOrderService {
 
         if (isConsentedApplication(caseDetails)) {
             caseData.put("RespondentName", DocumentHelper.getRespondentFullNameConsented(caseDetails));
-            caseData.put("GeneralOrderCourt", "Courts and Tribunal Service Centre");
+            caseData.put("GeneralOrderCourt", "SITTING in private");
+            caseData.put("GeneralOrderHeader", "Sitting in the Family Court");
         } else {
             caseData.put("RespondentName", DocumentHelper.getRespondentFullNameContested(caseDetails));
-            caseData.put("GeneralOrderCourt", ContestedCourtHelper.getSelectedCourt(caseDetails));
+            caseData.put("GeneralOrderCourt", "SITTING AT the Family Court at the" +
+                ContestedCourtHelper.getSelectedCourt(caseDetails));
+            caseData.put("GeneralOrderHeader", "In the Family Court sitting in the" +
+                ContestedCourtHelper.getSelectedCourt(caseDetails));
         }
 
         caseData.put("GeneralOrderJudgeDetails",
