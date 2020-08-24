06be2f002bf4d30e4ffed538558942c1acce58fc
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2020-08-24 15:21:34 +0100

    Test fix

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/order/ConsentInContestedOrderServiceTest.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.finrem.caseorchestration.service.order;
-
-import uk.gov.hmcts.reform.finrem.caseorchestration.BaseServiceTest;
-
-class ConsentInContestedOrderServiceTest extends BaseServiceTest {
-
-}
