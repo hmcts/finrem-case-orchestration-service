4b4a282003c14c9eb618f4944849bda311302f50
HarryH96 <H.Henry@kainos.com>
2020-08-26 13:58:49 +0100

    Adding test for empty party

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/UploadContestedCaseDocumentsServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/UploadContestedCaseDocumentsServiceTest.java
@@ -44,7 +44,7 @@ public class UploadContestedCaseDocumentsServiceTest extends BaseServiceTest {
         Map<String, Object> caseData = caseDetails.getData();
         service.filterDocumentsToRelevantParty(caseData);
 
-        assertThat(getDocumentCollection(caseData, CONTESTED_UPLOADED_DOCUMENTS), hasSize(0));
+        assertThat(getDocumentCollection(caseData, CONTESTED_UPLOADED_DOCUMENTS), hasSize(1));
         assertThat(getDocumentCollection(caseData, APPLICANT_CORRESPONDENCE_COLLECTION), hasSize(1));
         assertThat(getDocumentCollection(caseData, APPLICANT_FR_FORM_COLLECTION), hasSize(5));
         assertThat(getDocumentCollection(caseData, APPLICANT_EVIDENCE_COLLECTION), hasSize(14));
