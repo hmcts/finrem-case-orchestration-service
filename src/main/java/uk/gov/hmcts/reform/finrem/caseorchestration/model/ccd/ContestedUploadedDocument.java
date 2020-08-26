3d1191ed507ac6ccf96205cc50c9e3ab7d22928d
HarryH96 <H.Henry@kainos.com>
2020-08-26 14:53:35 +0100

    Code cleanup

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ContestedUploadedDocument.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ContestedUploadedDocument.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.microsoft.applicationinsights.core.dependencies.javaxannotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
