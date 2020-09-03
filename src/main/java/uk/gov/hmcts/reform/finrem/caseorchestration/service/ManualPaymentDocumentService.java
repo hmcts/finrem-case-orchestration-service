63e217aaee9327e2c4ebe5a6ecec1a69ece70ad5
HarryH96 <H.Henry@kainos.com>
2020-09-03 12:29:38 +0100

    Fixing test

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/ManualPaymentDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/ManualPaymentDocumentService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,14 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.config.DocumentConfiguration;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseHearingFunctions.buildCourtDetails;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseHearingFunctions.getCourtDetailsString;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseHearingFunctions.getSelectedCourt;
 
 @Service
 @Slf4j
@@ -16,6 +25,7 @@ public class ManualPaymentDocumentService {
     private final GenericDocumentService genericDocumentService;
     private final DocumentConfiguration documentConfiguration;
     private final DocumentHelper documentHelper;
+    private final ObjectMapper objectMapper;
 
     public CaseDocument generateManualPaymentLetter(CaseDetails caseDetails, String authToken) {
         log.info("Generating Manual Payment Letter {} from {} for bulk print",
@@ -40,5 +50,17 @@ public class ManualPaymentDocumentService {
         log.info("Generated Manual Payment Letter: {}", generatedManualPaymentLetter);
 
         return generatedManualPaymentLetter;
+    }
+
+    private CaseDetails addCourtFields(CaseDetails caseDetails) {
+        try {
+            Map<String, Object> courtDetailsMap = objectMapper.readValue(getCourtDetailsString(), HashMap.class);
+            Map<String, Object> data = caseDetails.getData();
+            Map<String, Object> courtDetails = (Map<String, Object>) courtDetailsMap.get(data.get(getSelectedCourt(data)));
+            data.put("courtDetails", buildCourtDetails(courtDetails));
+            return caseDetails;
+        } catch (IOException | NullPointerException e) {
+            return caseDetails;
+        }
     }
 }
