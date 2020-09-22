f91afe5dcf2cacc6887573441e73f7bb7641186c
Owen Carleton <owenc@runaway.home>
2020-09-22 10:23:39 +0100

    removed unused imports

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
@@ -1,12 +1,9 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
-import feign.Feign;
-import feign.jackson.JacksonEncoder;
+
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
