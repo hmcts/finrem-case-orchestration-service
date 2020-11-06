29c7d9133170cc8b0b02d9e2daf0d647534919fb
Owen Carleton <owenc@runaway.home>
2020-11-06 16:28:32 +0000

    test coverage

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/DocumentHelper.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/DocumentHelper.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.helper;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
