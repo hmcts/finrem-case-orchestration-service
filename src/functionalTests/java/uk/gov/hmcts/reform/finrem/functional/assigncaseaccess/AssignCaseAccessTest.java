aa436a257749451b989f9edd9cff073964562352
Shaun Ganley <shaung@Fare.local>
2020-11-11 15:04:41 +0000

    add functional test

---
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/assigncaseaccess/AssignCaseAccessTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.finrem.functional.assigncaseaccess;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.finrem.functional.IntegrationTestBase;
+
+@RunWith(SerenityRunner.class)
+public class AssignCaseAccessTest extends IntegrationTestBase {
+
+    @Autowired private ObjectMapper objectMapper;
+
+    @Value("${cos.aca.applicant-solicitor.api}")
+    private String applicantSolicitorUrl;
+
+    private final String contestedDir = "/json/contested/";
+
+    @Test
+    public void verifyApplicantSolicitor() throws JsonProcessingException {
+        CallbackRequest callbackRequest = objectMapper.readValue(
+            utils.getJsonFromFile("ccd-request-with-solicitor-contestApplicationIssued.json", contestedDir), CallbackRequest.class);
+
+        Response response = utils.getResponseData(applicantSolicitorUrl, callbackRequest);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
