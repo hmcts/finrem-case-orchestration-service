6b01a31cd785c8b59c7dfbf6bedd7a7fedfe88e3
Shaun Ganley <shaung@Fare.local>
2020-11-17 16:30:01 +0000

    logging

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/config/ServiceAuthTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/config/ServiceAuthTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
 @Lazy
+@Slf4j
 @EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class ServiceAuthTokenGeneratorConfiguration {
 
@@ -19,6 +21,9 @@ public class ServiceAuthTokenGeneratorConfiguration {
             @Value("${idam.s2s-auth.totp_secret}") final String secret,
             @Value("${idam.s2s.microservice}") final String microService,
             final ServiceAuthorisationApi serviceAuthorisationApi) {
+        log.info("serviceAuthTokenGenerator - serviceAuthTokenGenerator - secret - {}", secret);
+        log.info("serviceAuthTokenGenerator - serviceAuthTokenGenerator - microService - {}", microService);
+        log.info("serviceAuthTokenGenerator - serviceAuthTokenGenerator - serviceAuthorisationApi - {}", serviceAuthorisationApi.toString());
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 }
