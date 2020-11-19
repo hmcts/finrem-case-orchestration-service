836089b8b89cffcfd2d1bf4f35794463ba26c941
Shaun Ganley <shaung@Fare.local>
2020-11-19 15:50:18 +0000

    update functional tests

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
@@ -26,6 +26,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 
 import javax.annotation.PostConstruct;
@@ -45,6 +47,20 @@ public class TestContextConfiguration {
 
     @Value("${http.proxy:#{null}}")
     private String httpProxy;
+
+    @Bean
+    public ServiceAuthTokenGenerator serviceAuthTokenGenerator(@Value("${idam.s2s-auth.url}")
+                                                                   String s2sUrl,
+                                                               @Value("${idam.auth.secret}")
+                                                                   String secret,
+                                                               @Value("${idam.s2s-auth.microservice}")
+                                                                   String microservice) {
+        ServiceAuthorisationApi serviceAuthorisationApi = Feign.builder()
+            .encoder(new JacksonEncoder())
+            .contract(new SpringMvcContract())
+            .target(ServiceAuthorisationApi.class, s2sUrl);
+        return new ServiceAuthTokenGenerator(secret, microservice, serviceAuthorisationApi);
+    }
 
     @Bean
     public CoreCaseDataApi getCoreCaseDataApi(@Value("${core_case_data.api.url}") String coreCaseDataApiUrl) {
