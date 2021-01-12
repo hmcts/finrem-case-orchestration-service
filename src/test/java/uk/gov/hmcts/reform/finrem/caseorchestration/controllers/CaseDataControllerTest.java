0d1658048727b9d51a91c5b55d5200fd4df1d912
Shaun Ganley <shaung@Fare.local>
2021-01-12 09:38:19 +0000

    test refactor

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataControllerTest.java
@@ -1,19 +1,28 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseDataService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.IdamService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.UpdateSolicitorDetailsService;
 
 import java.util.Map;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,93 +33,41 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstant
 import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.YES_VALUE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_SOLICITOR_POLICY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_SOLICITOR_ADDRESS;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CaseDataControllerTest extends BaseControllerTest {
 
-    private static final String MOVE_VALUES_SAMPLE_JSON = "/fixtures/move-values-sample.json";
     private static final String CONTESTED_HWF_JSON = "/fixtures/contested/hwf.json";
     private static final String CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON = "/fixtures/contested/validate-hearing-successfully.json";
-    private static final String INVALID_CASE_TYPE_JSON = "/fixtures/invalid-case-type.json";
-    private static final String CONTESTED_NO_APPLICANT_SOL_ADDRESS = "/fixtures/contested/contested-upload-case-documents.json";
+
+    @Autowired private CaseDataController caseDataController;
 
     @MockBean private UpdateSolicitorDetailsService updateSolicitorDetailsService;
     @MockBean private IdamService idamService;
     @MockBean private FeatureToggleService featureToggleService;
+    @MockBean private CaseDataService caseDataService;
 
     @Test
     public void shouldSuccessfullyMoveCollection() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(true);
+        String source = "uploadHearingOrder";
+        String destination = "uploadHearingOrderRO";
 
-        loadRequestContentWith(MOVE_VALUES_SAMPLE_JSON);
-        mvc.perform(post("/case-orchestration/move-collection/uploadHearingOrder/to/uploadHearingOrderRO")
+        loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
+        mvc.perform(post(String.format("/case-orchestration/move-collection/%s/to/%s", source, destination))
             .content(requestContent.toString())
             .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
             .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.uploadHearingOrderRO[0]").exists())
-            .andExpect(jsonPath("$.data.uploadHearingOrderRO[1]").exists())
-            .andExpect(jsonPath("$.data.uploadHearingOrderRO[2]").exists())
-            .andExpect(jsonPath("$.data.uploadHearingOrder").isEmpty());
-    }
+            .andExpect(status().isOk());
 
-    @Test
-    public void shouldSuccessfullyMoveValuesToNewCollections() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
-
-        loadRequestContentWith(MOVE_VALUES_SAMPLE_JSON);
-        mvc.perform(post("/case-orchestration/move-collection/uploadHearingOrder/to/uploadHearingOrderNew")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.uploadHearingOrderNew[0]").exists())
-            .andExpect(jsonPath("$.data.uploadHearingOrder").isEmpty());
-    }
-
-    @Test
-    public void shouldDoNothingWithNonArraySourceValueMove() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
-
-        loadRequestContentWith(MOVE_VALUES_SAMPLE_JSON);
-        mvc.perform(post("/case-orchestration/move-collection/someString/to/uploadHearingOrder")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.uploadHearingOrder[0]").exists());
-    }
-
-    @Test
-    public void shouldDoNothingWithNonArrayDestinationValueMove() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
-
-        loadRequestContentWith(MOVE_VALUES_SAMPLE_JSON);
-        mvc.perform(post("/case-orchestration/move-collection/uploadHearingOrder/to/someString")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.uploadHearingOrder[0]").exists());
-    }
-
-    @Test
-    public void shouldDoNothingWhenSourceIsEmptyMove() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
-
-        loadRequestContentWith(MOVE_VALUES_SAMPLE_JSON);
-        mvc.perform(post("/case-orchestration/move-collection/empty/to/uploadHearingOrder")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.uploadHearingOrder[0]").exists());
+        verify(caseDataService, times(1)).moveCollection(any(), eq(source), eq(destination));
     }
 
     @Test
     public void shouldSuccessfullyReturnAsAdminConsented() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_HWF_JSON);
         mvc.perform(post("/case-orchestration/consented/set-defaults")
@@ -123,7 +80,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnNotAsAdminConsented() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/consented/set-defaults")
@@ -137,7 +94,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnAsAdminContested() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_HWF_JSON);
         mvc.perform(post("/case-orchestration/contested/set-defaults")
@@ -150,7 +107,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnNotAsAdminContested() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-defaults")
@@ -164,7 +121,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnAsAdminConsentedPaperCase() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_HWF_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-defaults")
@@ -179,7 +136,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnNotAsAdminConsentedPaperCase() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-defaults")
@@ -195,7 +152,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnAsAdminContestedPaperCase() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_HWF_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-defaults")
@@ -210,7 +167,7 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullyReturnNotAsAdminContestedPaperCase() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-defaults")
@@ -226,8 +183,9 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldSuccessfullySetOrgPolicyIfToggleEnabled() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
         when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-org-policy")
@@ -240,8 +198,9 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldNotSetOrgPolicyIfFeatureDisabled() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
         when(featureToggleService.isShareACaseEnabled()).thenReturn(false);
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
 
         loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-org-policy")
@@ -254,10 +213,12 @@ public class CaseDataControllerTest extends BaseControllerTest {
 
     @Test
     public void shouldNotSetOrgPolicyIfFeatureEnabledButInvalidCaseType() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(false);
         when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+        when(caseDataService.isContestedApplication(any())).thenReturn(false);
+        when(caseDataService.isConsentedApplication(any())).thenReturn(false);
 
-        loadRequestContentWith(INVALID_CASE_TYPE_JSON);
+        loadRequestContentWith(CONTESTED_VALIDATE_HEARING_SUCCESSFULLY_JSON);
         mvc.perform(post("/case-orchestration/contested/set-paper-case-org-policy")
             .content(requestContent.toString())
             .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
@@ -267,63 +228,67 @@ public class CaseDataControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void shouldSuccessfullyPopulateApplicantSolicitorAddress() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
+    public void shouldSuccessfullyPopulateApplicantSolicitorAddress() {
         when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
+        when(caseDataService.isApplicantRepresentedByASolicitor(any())).thenReturn(true);
         when(updateSolicitorDetailsService.getApplicantSolicitorAddressFromPrd(
             isA(String.class))).thenReturn(mockFirmAddress());
 
-        loadRequestContentWith(CONTESTED_NO_APPLICANT_SOL_ADDRESS);
-        mvc.perform(post("/case-orchestration/contested/set-defaults")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.applicantSolicitorAddress", is(notNullValue())));
+        ResponseEntity<AboutToStartOrSubmitCallbackResponse> response =
+            caseDataController.setContestedDefaultValues(AUTH_TOKEN, buildCallbackRequest());
+
+        Assert.assertEquals(response.getBody().getData().get(CONTESTED_SOLICITOR_ADDRESS), mockFirmAddress());
+
+        verify(updateSolicitorDetailsService, times(1)).getApplicantSolicitorAddressFromPrd(AUTH_TOKEN);
     }
 
     @Test
-    public void givenShareCaseIsEnabledAndApplicantIsRepresented_whenCaseInitialised_appSolAddressIsPopulated() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.FALSE);
-        when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+    public void shouldNotPopulateApplicantSolicitorAddress_toggledOff() {
+        when(featureToggleService.isShareACaseEnabled()).thenReturn(false);
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
+        when(caseDataService.isApplicantRepresentedByASolicitor(any())).thenReturn(true);
         when(updateSolicitorDetailsService.getApplicantSolicitorAddressFromPrd(
             isA(String.class))).thenReturn(mockFirmAddress());
 
-        loadRequestContentWith(CONTESTED_NO_APPLICANT_SOL_ADDRESS);
-        mvc.perform(post("/case-orchestration/contested/set-defaults")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.applicantSolicitorAddress", is(notNullValue())));
+        ResponseEntity<AboutToStartOrSubmitCallbackResponse> response =
+            caseDataController.setContestedDefaultValues(AUTH_TOKEN, buildCallbackRequest());
+
+        Assert.assertFalse(response.getBody().getData().containsKey(CONTESTED_SOLICITOR_ADDRESS));
+
+        verify(updateSolicitorDetailsService, never()).getApplicantSolicitorAddressFromPrd(AUTH_TOKEN);
     }
 
     @Test
-    public void shouldNotPopulateApplicantSolicitorAddressAsApplicantIsNotRepresented() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+    public void shouldNotPopulateApplicantSolicitorAddress_consentedApp() {
         when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+        when(caseDataService.isContestedApplication(any())).thenReturn(false);
+        when(caseDataService.isApplicantRepresentedByASolicitor(any())).thenReturn(true);
+        when(updateSolicitorDetailsService.getApplicantSolicitorAddressFromPrd(
+            isA(String.class))).thenReturn(mockFirmAddress());
 
-        loadRequestContentWith(CONTESTED_NO_APPLICANT_SOL_ADDRESS);
-        mvc.perform(post("/case-orchestration/contested/set-defaults")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.applicantSolicitorAddress").doesNotExist());
+        ResponseEntity<AboutToStartOrSubmitCallbackResponse> response =
+            caseDataController.setContestedDefaultValues(AUTH_TOKEN, buildCallbackRequest());
+
+        Assert.assertFalse(response.getBody().getData().containsKey(CONTESTED_SOLICITOR_ADDRESS));
+
+        verify(updateSolicitorDetailsService, never()).getApplicantSolicitorAddressFromPrd(AUTH_TOKEN);
     }
 
     @Test
-    public void shouldNotPopulateApplicantSolicitorAddressAsShareACaseIsDisabled() throws Exception {
-        when(idamService.isUserRoleAdmin(isA(String.class))).thenReturn(Boolean.TRUE);
+    public void shouldNotPopulateApplicantSolicitorAddress_notRepresented() {
         when(featureToggleService.isShareACaseEnabled()).thenReturn(true);
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
+        when(caseDataService.isApplicantRepresentedByASolicitor(any())).thenReturn(false);
+        when(updateSolicitorDetailsService.getApplicantSolicitorAddressFromPrd(
+            isA(String.class))).thenReturn(mockFirmAddress());
 
-        loadRequestContentWith(CONTESTED_NO_APPLICANT_SOL_ADDRESS);
-        mvc.perform(post("/case-orchestration/contested/set-defaults")
-            .content(requestContent.toString())
-            .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
-            .contentType(APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.applicantSolicitorAddress").doesNotExist());
+        ResponseEntity<AboutToStartOrSubmitCallbackResponse> response =
+            caseDataController.setContestedDefaultValues(AUTH_TOKEN, buildCallbackRequest());
+
+        Assert.assertFalse(response.getBody().getData().containsKey(CONTESTED_SOLICITOR_ADDRESS));
+
+        verify(updateSolicitorDetailsService, never()).getApplicantSolicitorAddressFromPrd(AUTH_TOKEN);
     }
 
     private Map<String, Object> mockFirmAddress() {
