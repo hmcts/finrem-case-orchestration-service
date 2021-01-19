7fe02d94d63048a94e93a91e1fc6b8f1fde4be85
HarryH96 <H.Henry@kainos.com>
2021-01-19 13:21:28 +0000

    Adding logs for migrate endpoint

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/migration/RemoveRespondentSolOrg.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/migration/RemoveRespondentSolOrg.java
@@ -7,13 +7,17 @@ import java.util.Map;
 
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ORGANISATION_POLICY_RESPONDENT;
 
+@Slf4j
 @Service
 public class RemoveRespondentSolOrg {
 
     public Map<String, Object> migrateCaseData(Map<String, Object> caseData) {
         if (caseData.containsKey(ORGANISATION_POLICY_RESPONDENT)) {
+            log.info("Attempting to remove respondent organisation policy");
             caseData.remove(ORGANISATION_POLICY_RESPONDENT);
+            log.info("Removed respondent organisation policy");
         }
         return caseData;
     }
 }
+i
