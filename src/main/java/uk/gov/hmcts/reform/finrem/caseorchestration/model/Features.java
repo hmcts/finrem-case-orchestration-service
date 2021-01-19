4be7b1a44ce233c4c2028f21ea5fde958d980e9d
Ravinder Pal Singh <ravinderpal.singh@hmcts.net>
2021-01-19 17:21:24 +0000

    [DO-NOT-MERGE] FR-1924 Remove Share a case toggle (#538)
    * Remove Share a case toggle
    
    * method renaming
    
    * removed unneccessary method
    
    * tests coverage fix
    
    Co-authored-by: John O'Neill <j.oneill@kainos.com>

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/Features.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/Features.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 public enum Features {
 
     RESPONDENT_JOURNEY("respondent_journey"),
-    SHARE_A_CASE("share_a_case"),
     SEND_TO_FRC("send_to_frc");
 
     private final String name;
