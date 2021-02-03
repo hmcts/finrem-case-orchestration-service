5f2a35c5c5397577800e0214fe42adca3012a904
shaunganley <s.ganley@kainos.com>
2021-02-03 12:34:24 +0000

    FR-1932: Pre-populate applicant organisation details (#552)

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/UpdateSolicitorDetailsServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/UpdateSolicitorDetailsServiceTest.java
@@ -13,11 +13,14 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.organisation.Organisat
 import java.util.Arrays;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_NAME;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_SOLICITOR_FIRM;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_SOLICITOR_ADDRESS;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_SOLICITOR_FIRM;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SOLICITOR_REFERENCE;
@@ -34,6 +37,7 @@ public class UpdateSolicitorDetailsServiceTest extends BaseServiceTest {
 
     @Autowired UpdateSolicitorDetailsService updateSolicitorDetailsService;
     @MockBean PrdOrganisationService prdOrganisationService;
+    @MockBean CaseDataService caseDataService;
 
     OrganisationContactInformation organisationContactInformation = OrganisationContactInformation.builder()
         .addressLine1(ADDRESS_LINE_1)
@@ -47,6 +51,7 @@ public class UpdateSolicitorDetailsServiceTest extends BaseServiceTest {
 
     @Before
     public void setUp() {
+        when(caseDataService.isContestedApplication(any())).thenReturn(true);
         when(prdOrganisationService.retrieveOrganisationsData(eq(AUTH_TOKEN))).thenReturn(OrganisationsResponse.builder()
             .contactInformation(Arrays.asList(organisationContactInformation))
             .name(TEST_SOLICITOR_NAME)
@@ -55,7 +60,7 @@ public class UpdateSolicitorDetailsServiceTest extends BaseServiceTest {
     }
 
     @Test
-    public void shouldSuccessfullySetApplicantSolicitorOrganisationDetails() {
+    public void shouldSuccessfullySetApplicantSolicitorOrganisationDetailsContested() {
         CaseDetails caseDetails = buildCaseDetails();
 
         updateSolicitorDetailsService.setApplicantSolicitorOrganisationDetails(AUTH_TOKEN, caseDetails);
@@ -70,6 +75,27 @@ public class UpdateSolicitorDetailsServiceTest extends BaseServiceTest {
         Assert.assertEquals(addressMap.get("PostTown"), organisationContactInformation.getTownCity());
         Assert.assertEquals(addressMap.get("PostCode"), organisationContactInformation.getPostcode());
         Assert.assertEquals(caseDetails.getData().get(CONTESTED_SOLICITOR_FIRM), TEST_SOLICITOR_NAME);
+        Assert.assertEquals(caseDetails.getData().get(SOLICITOR_REFERENCE), TEST_SOLICITOR_REFERENCE);
+    }
+
+    @Test
+    public void shouldSuccessfullySetApplicantSolicitorOrganisationDetailsConsented() {
+        CaseDetails caseDetails = buildCaseDetails();
+
+        when(caseDataService.isContestedApplication(caseDetails)).thenReturn(false);
+
+        updateSolicitorDetailsService.setApplicantSolicitorOrganisationDetails(AUTH_TOKEN, caseDetails);
+
+        Map<String, Object> addressMap = (Map<String, Object>) caseDetails.getData().get(CONSENTED_SOLICITOR_ADDRESS);
+
+        Assert.assertEquals(addressMap.get("AddressLine1"), organisationContactInformation.getAddressLine1());
+        Assert.assertEquals(addressMap.get("AddressLine2"), organisationContactInformation.getAddressLine2());
+        Assert.assertEquals(addressMap.get("AddressLine3"), organisationContactInformation.getAddressLine3());
+        Assert.assertEquals(addressMap.get("County"), organisationContactInformation.getCounty());
+        Assert.assertEquals(addressMap.get("Country"), organisationContactInformation.getCountry());
+        Assert.assertEquals(addressMap.get("PostTown"), organisationContactInformation.getTownCity());
+        Assert.assertEquals(addressMap.get("PostCode"), organisationContactInformation.getPostcode());
+        Assert.assertEquals(caseDetails.getData().get(CONSENTED_SOLICITOR_FIRM), TEST_SOLICITOR_NAME);
         Assert.assertEquals(caseDetails.getData().get(SOLICITOR_REFERENCE), TEST_SOLICITOR_REFERENCE);
     }
 
