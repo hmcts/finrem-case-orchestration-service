3bb474ebbf41101675aa88638b294c7e7a3363a0
rishisharma <rishi.sharma@hmcts.net>
2022-02-09 21:14:08 +0000

    DFR-767: set default value for boolean optional field

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
@@ -1,9 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
