6ba100a3494493b47bc302f202662bcb8b21cb44
rishikrsharma <rishikrsharma@users.noreply.github.com>
2022-03-08 15:20:20 +0000

    DFR-927: fix nightly security scan issue (#642)
    * DFR-927: fix nightly security scan issue
    
    * DFR-927: fix nightly security scan issue
    
    * DFR-927: fix nightly security scan issue

--- security.sh
+++ security.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
-cp /zap/api-report.html functional-output/
+export PYTHONDONTWRITEBYTECODE=1
+
+echo "Run ZAP scan and generate reports"
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL --hook=zap_hooks.py -J report.json -r api-report.html
+echo "Print alerts"
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
+
+echo "LC_ALL: ${LC_ALL}"
+echo "LANG: ${LANG}"
+echo "PYTHONDONTWRITEBYTECODE: ${PYTHONDONTWRITEBYTECODE}"
+echo "Print zap.out logs:"
 cat zap.out
+echo "Copy artifacts for archiving"
+cp zap.out functional-output/
+cp api-report.html functional-output/
