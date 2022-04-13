f0e3a6057db255acc83c3777cdcee0ba5c3d9825
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-04-13 12:31:18 +0100

    add COR and DynamicList

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ChangeOrganisationRequest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ChangeOrganisationRequest {
+    @JsonProperty("NotesReason")
+    private String reason;
+
+    @JsonProperty("CaseRoleId")
+    private DynamicList caseRoleId;
+
+    @JsonProperty("RequestTimestamp")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime requestTimestamp;
+
+    @JsonProperty("ApprovalRejectionTimeStamp")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime approvalRejectionTimestamp;
+
+    @JsonProperty("OrganisationToAdd")
+    private Organisation organisationToAdd;
+
+    @JsonProperty("OrganisationToRemove")
+    private Organisation organisationToRemove;
+
+    @JsonProperty("ApprovalStatus")
+    private int approvalStatus;
+
+}
