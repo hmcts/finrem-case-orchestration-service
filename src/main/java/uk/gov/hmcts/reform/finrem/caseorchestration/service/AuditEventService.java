697e2d39eb6dd40e3d33ca25c77f73b0a5d5c8ac
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-04-20 16:37:55 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AuditEventService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AuditEventService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.client.CaseDataApiV2;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.events.AuditEvent;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.events.AuditEventsResponse;
