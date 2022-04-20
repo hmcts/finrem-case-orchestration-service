127be0af319d6a6eb2a4197b977198397987ddad
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-04-20 16:30:21 +0100

    add authtoken logging

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AuditEventService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AuditEventService.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.client.CaseDataApiV2;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.events.AuditEvent;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.events.AuditEventsResponse;
@@ -12,6 +14,7 @@ import java.util.Comparator;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AuditEventService {
     private final CaseDataApiV2 caseDataApi;
@@ -21,8 +24,10 @@ public class AuditEventService {
     public Optional<AuditEvent> getLatestAuditEventByName(String caseId, String eventName) {
         String userToken = systemUserService.getSysUserToken();
 
+        String authToken = authTokenGenerator.generate();
+        log.info("Generated service auth token {} for caseID {}", authToken, caseId);
         AuditEventsResponse auditEventsResponse
-            = caseDataApi.getAuditEvents(userToken, authTokenGenerator.generate(), false, caseId);
+            = caseDataApi.getAuditEvents(userToken, authToken, false, caseId);
 
         return auditEventsResponse.getAuditEvents().stream()
             .filter(auditEvent -> eventName.equals(auditEvent.getId()))
