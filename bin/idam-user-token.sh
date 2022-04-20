a92955402c5f02d9a93146a53e9fddd8860230db
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-04-20 09:48:33 +0100

    update idam request params

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -3,10 +3,10 @@
 IMPORTER_USERNAME=${1:-fr_system_user@sharklasers.com}
 IMPORTER_PASSWORD=${2:-I87ef91zA!}
 IDAM_URI="https://idam-api.aat.platform.hmcts.net"
-REDIRECT_URI="https://manage-case.aat.platform.hmcts.net/oauth2/callback"
-CLIENT_ID="xuiwebapp"
-CLIENT_SECRET="DBssSOq0KKLNBf2z"
+REDIRECT_URI="https://finrem-frontend-aat.service.core-compute-aat.internal/oauth2/callback"
+CLIENT_ID="finrem"
+CLIENT_SECRET="2QFX-7GOV-GVYN-A5CA"
 
 code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
 
-curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=password" -d "" | jq -r .access_token
+curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
