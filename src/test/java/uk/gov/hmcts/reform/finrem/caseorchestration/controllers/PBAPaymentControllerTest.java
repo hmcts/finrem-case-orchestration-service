f1c133dae562668d491affe33b2a65c79e477348
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-04-29 07:48:29 +0100

    rmv unused import

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/PBAPaymentControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/PBAPaymentControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
