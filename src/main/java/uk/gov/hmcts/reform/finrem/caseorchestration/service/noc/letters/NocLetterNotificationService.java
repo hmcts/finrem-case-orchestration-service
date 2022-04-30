8bc58b754f1c5535ef25e88951d438aec74e51f3
Ruban <ruban72@hotmail.com>
2022-04-30 12:17:03 +0100

    DFR-962: Refactor class with Null pointer checks

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/letters/NocLetterNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/letters/NocLetterNotificationService.java
@@ -7,14 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Element;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.RepresentationUpdate;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.RepresentationUpdateHolder;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.noc.letters.processors.NocSolicitorAddedLettersProcessor;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.REPRESENTATION_UPDATE_HISTORY;
 
@@ -29,34 +28,27 @@ public class NocLetterNotificationService {
     public void sendNoticeOfChangeLetters(CaseDetails caseDetails, String authToken) {
 
         log.info("Send noc letters for case id {}", caseDetails.getId());
-        RepresentationUpdate representationUpdate = getRepresentationUpdate(caseDetails);
-        log.info("Got the representationUpdate");
-        if (representationUpdate.getAdded() != null) {
-            log.info("The representationUpdate is for an Added solicitor");
-            nocSolicitorAddedLettersProcessor.processSolicitorAndLitigantLetters(caseDetails, authToken, representationUpdate);
-        }
-        if (representationUpdate.getRemoved() != null) {
-            log.info("The representationUpdate is for an removed solicitor");
-            nocSolicitorRemovedLettersProcessor.processSolicitorAndLitigantLetters(caseDetails, authToken, representationUpdate);
+        RepresentationUpdate representationUpdate = getLatestRepresentationUpdate(caseDetails);
+        if (representationUpdate != null) {
+            log.info("Got the representationUpdate");
+            if (representationUpdate.getAdded() != null) {
+                log.info("The representationUpdate is for an Added solicitor");
+                nocSolicitorAddedLettersProcessor.processSolicitorAndLitigantLetters(caseDetails, authToken, representationUpdate);
+            }
+            if (representationUpdate.getRemoved() != null) {
+                log.info("The representationUpdate is for a Removed solicitor");
+                nocSolicitorRemovedLettersProcessor.processSolicitorAndLitigantLetters(caseDetails, authToken, representationUpdate);
+            }
         }
     }
 
-    private RepresentationUpdate getRepresentationUpdate(CaseDetails caseDetails) {
-        RepresentationUpdate representationUpdate = Optional.ofNullable(caseDetails.getData().get(REPRESENTATION_UPDATE_HISTORY))
-            .map(this::convertTofRepresentationUpdateHolder)
-            .orElse(new ArrayList<>())
-            .stream()
-            .map(RepresentationUpdateHolder::getRepresentationUpdate)
-            .max(Comparator.comparing(RepresentationUpdate::getDate))
-            .orElse(null);
-        return representationUpdate;
-    }
-
-    private List<RepresentationUpdateHolder> convertTofRepresentationUpdateHolder(Object object) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        return mapper.convertValue(object, new TypeReference<List<RepresentationUpdateHolder>>() {
-        });
+    private RepresentationUpdate getLatestRepresentationUpdate(CaseDetails caseDetails) {
+        log.info("Get the latest Representation Update");
+        List<Element<RepresentationUpdate>> representationUpdates = new ObjectMapper().registerModule(new JavaTimeModule())
+            .convertValue(caseDetails.getData().get(REPRESENTATION_UPDATE_HISTORY), new TypeReference<>() {
+            });
+        return Collections.max(representationUpdates, Comparator.comparing(representationUpdate -> representationUpdate.getValue().getDate()))
+            .getValue();
     }
 
 }
