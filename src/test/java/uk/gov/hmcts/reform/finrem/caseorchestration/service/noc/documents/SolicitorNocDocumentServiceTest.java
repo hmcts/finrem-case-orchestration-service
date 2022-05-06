05af399dce2b00fe645ea96139912051420a1b97
Ruban <ruban72@hotmail.com>
2022-05-06 08:49:00 +0100

    DFR-962: Change placeholdersMap to mirror caseDetails for Document generation call

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/documents/SolicitorNocDocumentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/documents/SolicitorNocDocumentServiceTest.java
@@ -7,6 +7,9 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -17,6 +20,7 @@ public class SolicitorNocDocumentServiceTest extends NocDocumentServiceBaseTest 
 
     @Before
     public void setUpTest() {
+        super.setUpTest();
         solicitorNocDocumentService =
             new SolicitorNocDocumentService(genericDocumentService, objectMapper, documentConfiguration);
     }
@@ -25,10 +29,13 @@ public class SolicitorNocDocumentServiceTest extends NocDocumentServiceBaseTest 
     public void shouldGenerateLitigantSolicitorAddedDocuments() {
         when(documentConfiguration.getNocLetterNotificationSolicitorTemplate()).thenReturn(DOC_TEMPLATE);
         when(documentConfiguration.getNocLetterNotificationSolicitorFileName()).thenReturn(DOC_FILENAME);
-        when(genericDocumentService.generateDocumentFromPlaceholdersMap(AUTH_TOKEN, notiicationLettersDetailsMap, DOC_TEMPLATE,
-            DOC_FILENAME)).thenReturn(new CaseDocument());
+        when(
+            genericDocumentService.generateDocumentFromPlaceholdersMap(eq(AUTH_TOKEN), notiicationLettersDetailsMapCaptor.capture(), eq(DOC_TEMPLATE),
+                eq(DOC_FILENAME))).thenReturn(new CaseDocument());
         CaseDocument caseDocument = solicitorNocDocumentService.generateNoticeOfChangeLetter(AUTH_TOKEN, noticeOfChangeLetterDetails);
 
+        Map placeholdersMap = notiicationLettersDetailsMapCaptor.getValue();
+        assertPlaceHoldersMap(placeholdersMap);
         assertAndVerifyDocumentsAreGenerated(caseDocument);
     }
 }
