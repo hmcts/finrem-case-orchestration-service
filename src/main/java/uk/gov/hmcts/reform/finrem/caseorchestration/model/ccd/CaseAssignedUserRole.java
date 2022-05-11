06207ddcdf23706171ca3279013d11fca8f8ddf2
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-05-11 17:14:51 +0100

    Add annotations to create objects

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseAssignedUserRole.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseAssignedUserRole.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Getter
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class CaseAssignedUserRole {
 
     @JsonProperty("case_id")
