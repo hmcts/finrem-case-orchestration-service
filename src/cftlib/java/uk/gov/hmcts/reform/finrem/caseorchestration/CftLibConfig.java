0e87de29935549e6966ec9f7ce5d5d3085f5a158
Mike Lemos <michael.lemos1@hmcts.net>
2022-05-12 17:36:07 +0100

    DFR-1052 app imports definitions

--- src/cftlib/java/uk/gov/hmcts/reform/finrem/caseorchestration/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/finrem/caseorchestration/CftLibConfig.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
@@ -7,12 +11,21 @@ import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
     @Override
-    public void configure(CFTLib lib) {
+    public void configure(CFTLib lib) throws Exception{
         createCcdRoles(lib);
         createIdamUsers(lib);
+        importDefinitions(lib);
     }
 
-    private void createCcdRoles(CFTLib lib) {
+    private void importDefinitions(CFTLib lib) throws IOException {
+        var consentedDef = Files.readAllBytes(Path.of("build/definitionsToBeImported/ccd-config-local-consented-base.xlsx"));
+        lib.importDefinition(consentedDef);
+
+        var contestedDef = Files.readAllBytes(Path.of("build/definitionsToBeImported/ccd-config-local-contested-base.xlsx"));
+        lib.importDefinition(contestedDef);
+    }
+
+    private void createCcdRoles(CFTLib lib){
         lib.createRoles(
             "citizen",
             "caseworker",
