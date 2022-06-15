107470b103459d2c452a8d5058e89235f5a1d6e3
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-06-15 14:20:04 +0100

    DFR-907: Add enum constant for manage case documents

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
@@ -10,6 +10,7 @@ public enum EventType {
     SEND_ORDER("FR_sendOrder"),
     PREPARE_FOR_HEARING("FR_prepareForHearing"),
     CLOSE("FR_close"),
+    MANAGE_CASE_DOCUMENTS("FR_manageCaseDocument"),
     NONE("");
 
     private final String ccdType;
