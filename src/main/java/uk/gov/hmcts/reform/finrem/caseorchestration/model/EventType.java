28748b8f674246e125ee505dd759b57a190b0998
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-06-21 17:50:11 +0100

    DFR-907: Update event name

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
@@ -10,7 +10,7 @@ public enum EventType {
     SEND_ORDER("FR_sendOrder"),
     PREPARE_FOR_HEARING("FR_prepareForHearing"),
     CLOSE("FR_close"),
-    MANAGE_CASE_DOCUMENTS("FR_manageCaseDocument"),
+    MANAGE_CASE_DOCUMENTS("FR_manageLitigantDocument"),
     NONE("");
 
     private final String ccdType;
