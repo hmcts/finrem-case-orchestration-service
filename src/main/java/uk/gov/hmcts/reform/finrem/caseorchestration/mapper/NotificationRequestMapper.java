f3f39352b0cb0b208ce319dcfe62b4893798efda
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-06-22 10:59:17 +0100

    remove log

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/mapper/NotificationRequestMapper.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/mapper/NotificationRequestMapper.java
@@ -129,7 +129,6 @@ public class NotificationRequestMapper {
 
             log.info("selectedCourt is {} for case ID: {}", selectedCourt, notificationRequest.getCaseReferenceNumber());
         }
-        log.info("Notification request for caseID {}: {}", caseDetails.getId(), notificationRequest);
 
         return notificationRequest;
     }
