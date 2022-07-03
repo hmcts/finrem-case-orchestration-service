24801388dbe18dc2c1e6a38d9ce4360c1bfbeb3a
Michael1142 <michael.johnson9@justice.gov.uk>
2022-07-03 16:54:13 +0100

    made tests async

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentServiceTest.java
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -417,8 +418,10 @@ public class AdditionalHearingDocumentServiceTest extends BaseServiceTest {
         when(notificationService.isContestedApplicantSolicitorEmailCommunicationEnabled(any())).thenReturn(false);
         additionalHearingDocumentService.bulkPrintAdditionalHearingDocuments(caseDetails, AUTH_TOKEN);
 
-        verify(bulkPrintService).printRespondentDocuments(any(), any(), any());
-        verify(bulkPrintService).printApplicantDocuments(any(), any(), any());
+        verify(bulkPrintService, timeout(100).times(1))
+            .printRespondentDocuments(any(), any(), any());
+        verify(bulkPrintService, timeout(100).times(1))
+            .printApplicantDocuments(any(), any(), any());
     }
 
     @Test
