3e9907cf4c22adadef1fdd1487d20542441a6b12
Mike Lemos <michael.lemos1@hmcts.net>
2022-07-05 13:10:48 +0100

    dfr1103 update app insights

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,21 @@ variable "env" {}
 variable "common_tags" {
     type = map(string)
 }
+
+variable "location" {
+    default = "UK South"
+}
+
+variable "appinsights_location" {
+    default     = "West Europe"
+    description = "Location for Application Insights"
+}
+
+variable "jenkins_AAD_objectId" {
+    description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "component" {
+}
+
+variable "tenant_id" {}
