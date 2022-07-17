00413fcf0ed8a302d0b2560d7c95413e41235fc1
Michael1142 <michael.johnson9@justice.gov.uk>
2022-07-17 22:56:03 +0100

    DFR-1191 add method to check if a org is registered with MyHMCTS using regex

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/solicitors/CheckSolicitorIsDigitalServiceBase.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/solicitors/CheckSolicitorIsDigitalServiceBase.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.OrganisationPolicy
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseDataService;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Slf4j
 public abstract class CheckSolicitorIsDigitalServiceBase {
@@ -14,7 +15,7 @@ public abstract class CheckSolicitorIsDigitalServiceBase {
     @Autowired
     protected final CaseDataService caseDataService;
 
-    public CheckSolicitorIsDigitalServiceBase(CaseDataService caseDataService) {
+    protected CheckSolicitorIsDigitalServiceBase(CaseDataService caseDataService) {
         this.caseDataService = caseDataService;
     }
 
@@ -24,4 +25,14 @@ public abstract class CheckSolicitorIsDigitalServiceBase {
     }
 
     public abstract boolean isSolicitorDigital(CaseDetails caseDetails);
+
+    public final boolean isOrganisationIdRegistered(OrganisationPolicy organisationPolicy) {
+        String myHMCTSRegex = "^[A-Z0-9]{7}$";
+
+        if (!isOrganisationEmpty(organisationPolicy)) {
+            String organisationId = organisationPolicy.getOrganisation().getOrganisationID();
+            return Pattern.matches(myHMCTSRegex, organisationId);
+        }
+        return false;
+    }
 }
