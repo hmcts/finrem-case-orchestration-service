5234b100ebaa6700b7c2538318f926074ebf079f
Ruban <ruban72@hotmail.com>
2022-08-02 11:18:49 +0100

    DFR-1183: Merge from master

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/payments/health/FeesServiceHealthCheck.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/payments/health/FeesServiceHealthCheck.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.finrem.caseorchestration.health.AbstractServiceHealthCheck;
 
 @Component
 public class FeesServiceHealthCheck extends AbstractServiceHealthCheck {
--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/health/FeesServiceHealthCheckTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/health/FeesServiceHealthCheckTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.health;
 
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.payments.health.FeesServiceHealthCheck;
+
 public class FeesServiceHealthCheckTest extends AbstractServiceHealthCheckTest {
 
     private static final String URI = "http://fees.test/health";
