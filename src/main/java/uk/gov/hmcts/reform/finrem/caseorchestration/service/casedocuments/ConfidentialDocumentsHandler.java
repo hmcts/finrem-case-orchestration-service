62f9de110b732387ee73bd81d37b1fa3cb3c8def
Barry Weeks <barry.weeks@justice.gov.uk>
2022-08-23 18:12:42 +0100

    DFR-957 Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/ConfidentialDocumentsHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/ConfidentialDocumentsHandler.java
@@ -10,13 +10,10 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConfidentialUpload
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ContestedUploadedDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ContestedUploadedDocumentData;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONFIDENTIAL_DOCS_UPLOADED_COLLECTION;
