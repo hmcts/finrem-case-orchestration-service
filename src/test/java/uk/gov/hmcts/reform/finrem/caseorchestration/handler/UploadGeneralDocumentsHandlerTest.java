cd4f0702efc5f1b35d7009c395e87e4819b95e3a
Barry Weeks <barry.weeks@justice.gov.uk>
2022-08-25 12:30:57 +0100

    DFR-957 Add UploadGeneralDocumentHandlerTest

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UploadGeneralDocumentsHandlerTest.java
@@ -0,0 +1,121 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.UploadedDocumentHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.UploadedGeneralDocumentHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.GeneralUploadedDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.GeneralUploadedDocumentData;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.GENERAL_UPLOADED_DOCUMENTS;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UploadGeneralDocumentsHandlerTest {
+
+    public static final String AUTH_TOKEN = "tokien:)";
+
+    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private UploadGeneralDocumentsAboutToSubmitHandler uploadGeneralDocumentsAboutToSubmitHandler;
+
+    private final List<GeneralUploadedDocumentData> uploadDocumentList = new ArrayList<>();
+    private final List<GeneralUploadedDocumentData> existingDocumentList = new ArrayList<>();
+    private final List<GeneralUploadedDocumentData> combinedDocumentList = new ArrayList<>();
+
+    private final UploadedGeneralDocumentHelper uploadedGeneralDocumentHelper = new UploadedGeneralDocumentHelper(objectMapper);
+
+    protected GeneralUploadedDocumentData createGeneralUploadDocumentItem(String type, String emailContent,
+                                                                          CaseDocument link, String dateAdded, String comment,
+                                                                          String fileName) {
+        int leftLimit = 48;
+        int rightLimit = 122;
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String documentId = random.ints(leftLimit, rightLimit + 1)
+            .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+            .limit(targetStringLength)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
+
+        return GeneralUploadedDocumentData.builder()
+            .id(documentId)
+            .generalUploadedDocument(GeneralUploadedDocument
+                .builder()
+                .documentType(type)
+                .documentEmailContent(emailContent)
+                .documentLink(link)
+                .documentDateAdded(dateAdded)
+                .documentComment(comment)
+                .documentFileName(fileName)
+                .build())
+            .build();
+    }
+
+    @Before
+    public void setUpTest() {
+        uploadGeneralDocumentsAboutToSubmitHandler = new UploadGeneralDocumentsAboutToSubmitHandler(objectMapper, uploadedGeneralDocumentHelper);
+    }
+
+    @Test
+    public void givenACcdCallbackContestedCase_WhenAnAboutToSubmitEventUploadGeneralDocument_thenHandlerCanHandle() {
+        assertThat(uploadGeneralDocumentsAboutToSubmitHandler
+                .canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONTESTED, EventType.UPLOAD_GENERAL_DOCUMENT),
+            is(true));
+    }
+
+    @Test
+    public void givenACcdCallbackConsentedCase_WhenAnAboutToSubmitEventUploadGeneralDocument_thenHandlerCanHandle() {
+        assertThat(uploadGeneralDocumentsAboutToSubmitHandler
+                .canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONSENTED, EventType.UPLOAD_GENERAL_DOCUMENT),
+            is(true));
+    }
+
+    @Test
+    public void givenUploadGeneralDocument_When_IsValid_ThenExecuteHandler() {
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        CaseDocument documentLink = new CaseDocument("/fileUrl", "document.extension", "/binaryUrl");
+
+        // Setup caseDetailsBefore
+        CaseDetails caseDetailsBefore = callbackRequest.getCaseDetailsBefore();
+        existingDocumentList.add(createGeneralUploadDocumentItem("Old", "Old email content", documentLink, "", "Old Example", "oldDocument.filename"));
+        caseDetailsBefore.getData().put(GENERAL_UPLOADED_DOCUMENTS, existingDocumentList);
+
+        // Setup caseDetails
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+        uploadDocumentList.add(createGeneralUploadDocumentItem("Old", "Old email content", documentLink, "", "Old Example", "oldDocument.filename"));
+        uploadDocumentList.add(createGeneralUploadDocumentItem("New", "New email content", documentLink, "", "New Example", "newDocument.filename"));
+        caseDetails.getData().put(GENERAL_UPLOADED_DOCUMENTS, uploadDocumentList);
+
+        // Setup combinedList
+        combinedDocumentList.addAll(uploadDocumentList);
+        combinedDocumentList.addAll(existingDocumentList);
+
+        // VALIDATE RESPONSE FROM uploadGeneralDocumentHandler HERE
+        assertThat(uploadGeneralDocumentsAboutToSubmitHandler.handle(callbackRequest, AUTH_TOKEN).getData().get(GENERAL_UPLOADED_DOCUMENTS).equals(combinedDocumentList), is(true));
+    }
+
+    private CallbackRequest buildCallbackRequest() {
+        Map<String, Object> caseData = new HashMap<>();
+        Map<String, Object> caseDataBefore = new HashMap<>();
+        CaseDetails caseDetails = CaseDetails.builder().id(123L).data(caseData).build();
+        CaseDetails caseDetailsBefore = CaseDetails.builder().id(123L).data(caseDataBefore).build();
+        return CallbackRequest.builder().eventId(EventType.UPLOAD_GENERAL_DOCUMENT.getCcdType())
+            .caseDetails(caseDetails).caseDetailsBefore(caseDetailsBefore).build();
+    }
+}
