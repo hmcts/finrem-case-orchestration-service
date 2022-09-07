8faebfbd1872f26c9bcc90b57a0ddae001bf9fb8
Mike Lemos <michael.lemos1@hmcts.net>
2022-09-07 10:49:21 +0100

    dfr1192 removing unused code

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/CreateCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/CreateCaseService.java
@@ -7,15 +7,11 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.config.CaseFlagsConfiguration;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.YES_VALUE;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_REPRESENTED;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.MINI_FORM_A;
 
 @Service
 @RequiredArgsConstructor
@@ -23,21 +19,9 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 public class CreateCaseService {
 
     private final AuthTokenGenerator authTokenGenerator;
-    private final OnlineFormDocumentService onlineFormDocumentService;
-    private final IdamService idamService;
     private final CoreCaseDataApi coreCaseDataApi;
 
     private final CaseFlagsConfiguration caseFlagsConfiguration;
-
-    public void draftContestedMiniFormA(CallbackRequest callbackRequest, String userAuthorisation) {
-        Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
-        CaseDocument document = onlineFormDocumentService.generateDraftContestedMiniFormA(userAuthorisation, callbackRequest.getCaseDetails());
-        caseData.put(MINI_FORM_A, document);
-        if (!idamService.isUserRoleAdmin(userAuthorisation)) {
-            log.info("other users.");
-            caseData.put(APPLICANT_REPRESENTED, YES_VALUE);
-        }
-    }
 
     public void setSupplementaryData(CallbackRequest callbackRequest, String authorisation) {
         String caseId = String.valueOf(callbackRequest.getCaseDetails().getId());
