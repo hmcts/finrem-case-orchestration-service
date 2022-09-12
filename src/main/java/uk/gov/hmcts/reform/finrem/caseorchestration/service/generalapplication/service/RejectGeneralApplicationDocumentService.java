dc837e716b4b2aed9be10766f79752ace960a8af
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-09-12 11:13:58 +0100

    remove solicitor keyword from logs

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/generalapplication/service/RejectGeneralApplicationDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/generalapplication/service/RejectGeneralApplicationDocumentService.java
@@ -32,7 +32,7 @@ public class RejectGeneralApplicationDocumentService  {
 
     public CaseDocument generateGeneralApplicationRejectionLetter(CaseDetails caseDetails, String authToken,
                                                      DocumentHelper.PaperNotificationRecipient recipient) {
-        log.info("Generating General Application Rejection Letter for {} SOLICITOR for caseId {}", recipient, caseDetails.getId());
+        log.info("Generating General Application Rejection Letter for {} for caseId {}", recipient, caseDetails.getId());
         GeneralApplicationRejectionLetterDetails letterDetails = generalApplicationRejectionLetterGenerator.generate(caseDetails, recipient);
         Map letterDetailsMap = convertGeneralApplicationRejectionLetterDetailsToMap(letterDetails);
         return genericDocumentService.generateDocumentFromPlaceholdersMap(authToken, letterDetailsMap,
