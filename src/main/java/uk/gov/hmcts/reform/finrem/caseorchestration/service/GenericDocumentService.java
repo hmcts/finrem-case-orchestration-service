ac1d6c3d38fea346f668e7bb678d2afc081bebab
Paul Hudson <paul.hudson3@justice.gov.uk>
2022-10-21 16:45:09 +0100

    DFR-1322 Removed unused import after runnung checkstyleMain

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenericDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenericDocumentService.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper.CASE_NUMBER;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.DIVORCE_CASE_NUMBER;
+
 
 @Service
 @RequiredArgsConstructor
