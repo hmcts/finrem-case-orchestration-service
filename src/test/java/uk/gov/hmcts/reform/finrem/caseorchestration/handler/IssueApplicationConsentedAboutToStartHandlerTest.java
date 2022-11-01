dc064254fb7d9ffda852139ea16d4e87a35d9c2b
Ruban <ruban72@hotmail.com>
2022-11-01 09:01:57 +0000

    Dfr-1074: Fix compikle issue after merge

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandlerTest.java
@@ -3,10 +3,10 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import org.junit.Before;
 import org.junit.Test;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
@@ -27,7 +27,7 @@ public class IssueApplicationConsentedAboutToStartHandlerTest {
 
     @Before
     public void setup() {
-        handler =  new IssueApplicationConsentedAboutToStartHandler(new OnStartDefaultValueService());
+        handler = new IssueApplicationConsentedAboutToStartHandler(new OnStartDefaultValueService());
     }
 
     @Test
@@ -47,21 +47,21 @@ public class IssueApplicationConsentedAboutToStartHandlerTest {
     @Test
     public void givenContestedCase_whenUseIssueApplication_thenDefaultIssueDateSetToCurrentDate() {
         CallbackRequest callbackRequest = buildCallbackRequest();
-        AboutToStartOrSubmitCallbackResponse response = handler.handle(callbackRequest, AUTH_TOKEN);
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> response = handler.handle(callbackRequest, AUTH_TOKEN);
         assertNotNull(response.getData().get(ISSUE_DATE));
     }
 
     @Test
     public void givenContestedCase_whenUseIssueApplicationAndIssueDateEnteredManually_thenHandle() {
         CallbackRequest callbackRequest = buildCallbackRequest();
-        callbackRequest.getCaseDetails().getData().put(ISSUE_DATE,"10-10-2000");
-        AboutToStartOrSubmitCallbackResponse response = handler.handle(callbackRequest, AUTH_TOKEN);
+        callbackRequest.getCaseDetails().getData().put(ISSUE_DATE, "10-10-2000");
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> response = handler.handle(callbackRequest, AUTH_TOKEN);
         assertEquals("10-10-2000", response.getData().get(ISSUE_DATE));
     }
 
     private CallbackRequest buildCallbackRequest() {
         Map<String, Object> caseData = new HashMap<>();
-        CaseDetails caseDetails = CaseDetails.builder().id(123L).caseTypeId(CaseType.CONSENTED.getCcdType()). data(caseData).build();
+        CaseDetails caseDetails = CaseDetails.builder().id(123L).caseTypeId(CaseType.CONSENTED.getCcdType()).data(caseData).build();
         return CallbackRequest.builder().eventId(EventType.ISSUE_APPLICATION.getCcdType()).caseDetails(caseDetails).build();
     }
 }
