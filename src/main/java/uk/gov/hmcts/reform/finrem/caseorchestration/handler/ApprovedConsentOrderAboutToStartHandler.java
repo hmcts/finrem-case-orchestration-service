88cc4dbd2c0cf7690a6152232a946b3e91445528
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2022-11-01 09:04:36 +0000

    consented default values

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/ApprovedConsentOrderAboutToStartHandler.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ApprovedConsentOrderAboutToStartHandler implements CallbackHandler {
+
+    private final OnStartDefaultValueService onStartDefaultValueService;
+
+    @Override
+    public boolean canHandle(final CallbackType callbackType, final CaseType caseType,
+                             final EventType eventType) {
+        return CallbackType.ABOUT_TO_START.equals(callbackType)
+            && CaseType.CONSENTED.equals(caseType)
+            && EventType.APPROVE_ORDER.equals(eventType);
+    }
+
+    @Override
+    public AboutToStartOrSubmitCallbackResponse handle(CallbackRequest callbackRequest,
+                                                       String userAuthorisation) {
+        onStartDefaultValueService.defaultJudgeName(callbackRequest, userAuthorisation);
+        onStartDefaultValueService.defaultOrderDate(callbackRequest);
+        return AboutToStartOrSubmitCallbackResponse.builder().data(callbackRequest.getCaseDetails().getData()).build();
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/OnStartDefaultValueService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/OnStartDefaultValueService.java
@@ -7,6 +7,8 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 
 import java.time.LocalDate;
 
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_ORDER_DIRECTION_DATE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_ORDER_DIRECTION_JUDGE_NAME;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ISSUE_DATE;
 
 @Service
@@ -14,7 +16,18 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 @Slf4j
 public class OnStartDefaultValueService {
 
+    private IdamService idamService;
+
     public void defaultIssueDate(CallbackRequest callbackRequest) {
         callbackRequest.getCaseDetails().getData().putIfAbsent(ISSUE_DATE, LocalDate.now());
+    }
+
+    public void defaultJudgeName(CallbackRequest callbackRequest, String userAuthorisation) {
+        callbackRequest.getCaseDetails().getData().put(CONSENTED_ORDER_DIRECTION_JUDGE_NAME,
+            idamService.getIdamFullName(userAuthorisation));
+    }
+
+    public void defaultOrderDate(CallbackRequest callbackRequest) {
+        callbackRequest.getCaseDetails().getData().putIfAbsent(CONSENTED_ORDER_DIRECTION_DATE, LocalDate.now());
     }
 }
