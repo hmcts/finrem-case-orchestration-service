f7c34b1522b95f15f68e05fc2dd8f1ada1d42f7c
rishikrsharma <rishikrsharma@users.noreply.github.com>
2022-11-03 15:59:14 +0000

    Dfr 1366 pdf conversion and additional logging  (#838)
    * DFR-1366: added extended log messages
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging
    
    * DFR-1366: pdf conversion and extra logging

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/TestSetUpUtils.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/TestSetUpUtils.java
@@ -166,9 +166,9 @@ public class TestSetUpUtils {
         return caseDocument;
     }
 
-    public static CaseDocument caseDocument(String documentName, String filename, String binaryUrl) {
+    public static CaseDocument caseDocument(String documentUrl, String filename, String binaryUrl) {
         CaseDocument caseDocument = new CaseDocument();
-        caseDocument.setDocumentUrl(documentName);
+        caseDocument.setDocumentUrl(documentUrl);
         caseDocument.setDocumentFilename(filename);
         caseDocument.setDocumentBinaryUrl(binaryUrl);
 
