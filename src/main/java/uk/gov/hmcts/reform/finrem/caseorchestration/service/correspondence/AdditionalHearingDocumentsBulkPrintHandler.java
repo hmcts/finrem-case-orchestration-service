c1f2aa2fade5618f07e5de4bd8de8abc0d44856e
Ruban <ruban72@hotmail.com>
2022-11-14 12:30:38 +0000

    DFR-1083: Add bulkprint base class

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/AdditionalHearingDocumentsBulkPrintHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/AdditionalHearingDocumentsBulkPrintHandler.java
@@ -1,17 +1,14 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.finrem.caseorchestration.config.DocumentConfiguration;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.AdditionalHearingDocumentData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.BulkPrintDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
-import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseDataService;
-import uk.gov.hmcts.reform.finrem.caseorchestration.service.GenericDocumentService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
 
 import java.util.ArrayList;
@@ -21,40 +18,39 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.HEARING_ADDITIONAL_DOC;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class AdditionalHearingDocumentsLetterHandler {
+public class AdditionalHearingDocumentsBulkPrintHandler  extends BulkPrintDocumentHandler {
 
     private final DocumentHelper documentHelper;
-    private final BulkPrintService bulkPrintService;
-    private final NotificationService notificationService;
 
-    public void bulkPrintAdditionalHearingDocuments(CaseDetails caseDetails, String authorisationToken) {
+    @Autowired
+    public AdditionalHearingDocumentsBulkPrintHandler(BulkPrintService bulkPrintService,
+                                                      NotificationService notificationService,
+                                                      DocumentHelper documentHelper) {
+        super(bulkPrintService, notificationService);
+        this.documentHelper = documentHelper;
+    }
+
+    @Override
+    List<BulkPrintDocument> getDocumentsToPrint(CaseDetails caseDetails) {
         List<AdditionalHearingDocumentData> additionalHearingDocumentData =
             documentHelper.convertToAdditionalHearingDocumentData(
                 caseDetails.getData().get(ADDITIONAL_HEARING_DOCUMENT_COLLECTION));
 
         AdditionalHearingDocumentData additionalHearingDocument = additionalHearingDocumentData.get(additionalHearingDocumentData.size() - 1);
 
-        List<BulkPrintDocument> document = new ArrayList<>();
+        List<BulkPrintDocument> documents = new ArrayList<>();
         if (caseDetails.getData().get(HEARING_ADDITIONAL_DOC) != null) {
             BulkPrintDocument additionalUploadedDoc
                 = documentHelper.getBulkPrintDocumentFromCaseDocument(documentHelper
                 .convertToCaseDocument(caseDetails.getData().get(HEARING_ADDITIONAL_DOC)));
-            document.add(additionalUploadedDoc);
+            documents.add(additionalUploadedDoc);
         }
 
         BulkPrintDocument additionalDoc
             = documentHelper.getBulkPrintDocumentFromCaseDocument(additionalHearingDocument.getAdditionalHearingDocument().getDocument());
 
-        document.add(additionalDoc);
-
-        if (!notificationService.isApplicantSolicitorRegisteredAndEmailCommunicationEnabled(caseDetails)) {
-            bulkPrintService.printApplicantDocuments(caseDetails, authorisationToken, document);
-        }
-        if (!notificationService.isRespondentSolicitorRegisteredAndEmailCommunicationEnabled(caseDetails)) {
-            bulkPrintService.printRespondentDocuments(caseDetails, authorisationToken, document);
-        }
+        documents.add(additionalDoc);
+        return documents;
     }
-
 }
