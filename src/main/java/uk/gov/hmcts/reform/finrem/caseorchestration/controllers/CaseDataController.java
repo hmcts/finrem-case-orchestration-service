81ff1afb450a30a3cdeebc9676aca5e9580ea304
Paul Hudson <paul.hudson3@justice.gov.uk>
2022-12-08 08:18:14 +0000

    DFR-1550: Modified CaseDataController after running checkStyleMain

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/CaseDataController.java
@@ -227,8 +227,7 @@ public class CaseDataController extends BaseController {
 
             Map<String, Object> appSolPolicy = buildOrganisationPolicy(APP_SOLICITOR_POLICY);
             caseDetails.getData().get(ORGANISATION_POLICY_APPLICANT);
-            if (!caseDataService.isNotEmpty(ORGANISATION_POLICY_APPLICANT, caseDetails.getData()))
-            {
+            if (!caseDataService.isNotEmpty(ORGANISATION_POLICY_APPLICANT, caseDetails.getData())) {
 
                 caseDetails.getData().put(ORGANISATION_POLICY_APPLICANT, appSolPolicy);
 
