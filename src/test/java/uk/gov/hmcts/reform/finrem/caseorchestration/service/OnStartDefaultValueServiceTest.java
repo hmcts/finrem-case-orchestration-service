92d66cdb048b20243e39ea90c76d69a00e64d34e
rishisharma <rishi.sharma@hmcts.net>
2022-12-15 16:57:17 +0000

    DFR-1589: Refactor Issue Application Event Handlers to use Pojo representation of FinRem CaseData

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/OnStartDefaultValueServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/OnStartDefaultValueServiceTest.java
@@ -16,12 +16,17 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.NO_VALUE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CIVIL_PARTNERSHIP;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_ORDER_DIRECTION_DATE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_ORDER_DIRECTION_JUDGE_NAME;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_ORDER_APPROVED_DATE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_ORDER_APPROVED_JUDGE_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.TYPE_OF_APPLICATION;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.TYPE_OF_APPLICATION_DEFAULT_TO;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -43,6 +48,30 @@ public class OnStartDefaultValueServiceTest  extends BaseServiceTest {
         FinremCallbackRequest callbackRequest = callbackRequest();
         service.defaultIssueDate(callbackRequest);
         assertNotNull(callbackRequest.getCaseDetails().getData().getIssueDate());
+    }
+
+    @Test
+    public void defaultCivilPartnershipField() {
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        service.defaultCivilPartnershipField(callbackRequest);
+        assertEquals(NO_VALUE,callbackRequest.getCaseDetails().getData().get(CIVIL_PARTNERSHIP));
+    }
+
+
+    @Test
+    public void defaultTypeOfApplication_defaultValue() {
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        service.defaultTypeOfApplication(callbackRequest);
+        assertEquals(TYPE_OF_APPLICATION_DEFAULT_TO,callbackRequest.getCaseDetails().getData().get(TYPE_OF_APPLICATION));
+    }
+
+    @Test
+    public void defaultTypeOfApplication_userChoosen() {
+        var schedule1 = "Under paragraph 1 or 2 of schedule 1 children act 1989";
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        callbackRequest.getCaseDetails().getData().put(TYPE_OF_APPLICATION, schedule1);
+        service.defaultTypeOfApplication(callbackRequest);
+        assertEquals(schedule1,callbackRequest.getCaseDetails().getData().get(TYPE_OF_APPLICATION));
     }
 
     @Test
