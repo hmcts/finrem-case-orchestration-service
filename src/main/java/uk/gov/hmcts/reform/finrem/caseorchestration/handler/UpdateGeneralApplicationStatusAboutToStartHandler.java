7a38512d2846ffc7c93b7feb1557fc979c621d61
rishisharma <rishi.sharma@hmcts.net>
2023-01-05 14:07:12 +0000

    DFR-1464: ga status

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateGeneralApplicationStatusAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateGeneralApplicationStatusAboutToStartHandler.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
