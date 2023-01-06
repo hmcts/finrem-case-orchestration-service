f30024598b5d3e6909d83a464cde372f1444dc8b
rishisharma <rishi.sharma@hmcts.net>
2023-01-06 15:27:51 +0000

    DFR-1394: applicant address

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/RemoveApplicantDetailsControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/RemoveApplicantDetailsControllerTest.java
@@ -16,6 +16,8 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -68,4 +70,98 @@ public class RemoveApplicantDetailsControllerTest extends BaseControllerTest {
             .andExpect(jsonPath("$.data.applicantSolicitorDXnumber").doesNotExist())
             .andExpect(jsonPath("$.data.applicantSolicitorConsentForEmails").doesNotExist());
     }
+
+
+    @Test
+    public void shouldSuccessfullyRemoveApplicantSolicitorDetails_respondentConfidentialAddressNotAmended() throws Exception {
+        requestContent = objectMapper.readTree(new File(getClass()
+            .getResource("/fixtures/contested/amend-applicant-solicitor-details-res-untouched.json").toURI()));
+        when(featureToggleService.isCaseworkerNoCEnabled()).thenReturn(true);
+        when(service.generateContestedMiniFormA(any(), any())).thenReturn(TestSetUpUtils.caseDocument());
+
+        mvc.perform(post(REMOVE_DETAILS_URL)
+                .content(requestContent.toString())
+                .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
+                .contentType(APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.data.applicantRepresented", is(NO_VALUE)))
+            .andExpect(jsonPath("$.data.applicantAddress", is(notNullValue())))
+            .andExpect(jsonPath("$.data.applicantPhone", is("89897876765")))
+            .andExpect(jsonPath("$.data.applicantEmail", is("email01@email.com")))
+            .andExpect(jsonPath("$.data.applicantFMName", is("Poor")))
+            .andExpect(jsonPath("$.data.applicantLName", is("Guy")))
+            .andExpect(jsonPath("$.data.applicantAddressConfidential", is("Yes")))
+            .andExpect(jsonPath("$.data.respondentAddressConfidential").doesNotExist())
+            .andExpect(jsonPath("$.data.miniFormA", isA(LinkedHashMap.class)))
+            .andExpect(jsonPath("$.data.applicantSolicitorName").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorFirm").doesNotExist())
+            .andExpect(jsonPath("$.data.solicitorReference").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorAddress").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorPhone").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorEmail").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorDXnumber").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorConsentForEmails").doesNotExist());
+    }
+
+    @Test
+    public void shouldSuccessfullyRemoveApplicantSolicitorDetails_applicantConfidentialAddressNotAmended() throws Exception {
+        requestContent = objectMapper.readTree(new File(getClass()
+            .getResource("/fixtures/contested/amend-applicant-solicitor-details-app-untouched.json").toURI()));
+        when(featureToggleService.isCaseworkerNoCEnabled()).thenReturn(true);
+        when(service.generateContestedMiniFormA(any(), any())).thenReturn(TestSetUpUtils.caseDocument());
+
+        mvc.perform(post(REMOVE_DETAILS_URL)
+                .content(requestContent.toString())
+                .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
+                .contentType(APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.data.applicantRepresented", is(NO_VALUE)))
+            .andExpect(jsonPath("$.data.applicantAddress", is(notNullValue())))
+            .andExpect(jsonPath("$.data.applicantPhone", is("89897876765")))
+            .andExpect(jsonPath("$.data.applicantEmail", is("email01@email.com")))
+            .andExpect(jsonPath("$.data.applicantFMName", is("Poor")))
+            .andExpect(jsonPath("$.data.applicantLName", is("Guy")))
+            .andExpect(jsonPath("$.data.applicantAddressConfidential").doesNotExist())
+            .andExpect(jsonPath("$.data.respondentAddressConfidential", is("Yes")))
+            .andExpect(jsonPath("$.data.miniFormA", isA(LinkedHashMap.class)))
+            .andExpect(jsonPath("$.data.applicantSolicitorName").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorFirm").doesNotExist())
+            .andExpect(jsonPath("$.data.solicitorReference").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorAddress").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorPhone").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorEmail").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorDXnumber").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorConsentForEmails").doesNotExist());
+    }
+
+    @Test
+    public void shouldSuccessfullyRemoveApplicantSolicitorDetails_bothConfidentialAddressNotAmended() throws Exception {
+        requestContent = objectMapper.readTree(new File(getClass()
+            .getResource("/fixtures/contested/amend-applicant-solicitor-details-both-untouched.json").toURI()));
+        when(featureToggleService.isCaseworkerNoCEnabled()).thenReturn(true);
+        when(service.generateContestedMiniFormA(any(), any())).thenReturn(TestSetUpUtils.caseDocument());
+
+        mvc.perform(post(REMOVE_DETAILS_URL)
+                .content(requestContent.toString())
+                .header(AUTHORIZATION_HEADER, AUTH_TOKEN)
+                .contentType(APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.data.applicantRepresented", is(NO_VALUE)))
+            .andExpect(jsonPath("$.data.applicantAddress", is(notNullValue())))
+            .andExpect(jsonPath("$.data.applicantPhone", is("89897876765")))
+            .andExpect(jsonPath("$.data.applicantEmail", is("email01@email.com")))
+            .andExpect(jsonPath("$.data.applicantFMName", is("Poor")))
+            .andExpect(jsonPath("$.data.applicantLName", is("Guy")))
+            .andExpect(jsonPath("$.data.applicantAddressConfidential").doesNotExist())
+            .andExpect(jsonPath("$.data.respondentAddressConfidential").doesNotExist())
+            .andExpect(jsonPath("$.data.miniFormA", isA(LinkedHashMap.class)))
+            .andExpect(jsonPath("$.data.applicantSolicitorName").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorFirm").doesNotExist())
+            .andExpect(jsonPath("$.data.solicitorReference").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorAddress").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorPhone").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorEmail").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorDXnumber").doesNotExist())
+            .andExpect(jsonPath("$.data.applicantSolicitorConsentForEmails").doesNotExist());
+
+        verify(service, never()).generateDraftContestedMiniFormA(any(), any());
+    }
+
 }
