c3846670fd3ee769790e733ce7f3956d74529e33
sunilrochani <sunil.rochani@hotmail.com>
2023-01-10 12:41:47 +0000

    DFR-1638: High Court

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
@@ -623,7 +623,8 @@ public class FinremCaseData {
             Region.NORTHWEST, getNorthWestCourt(regionWrapper.getNorthWestFrcList(), courtList),
             Region.SOUTHWEST, getSouthWestCourt(regionWrapper.getSouthWestFrcList(), courtList),
             Region.SOUTHEAST, getSouthEastCourt(regionWrapper.getSouthEastFrcList(), courtList),
-            Region.WALES, getWalesCourt(regionWrapper.getWalesFrcList(), courtList)
+            Region.WALES, getWalesCourt(regionWrapper.getWalesFrcList(), courtList),
+            Region.HIGHCOURT, getHighCourt(regionWrapper.getHighCourtFrcList(), courtList)
         ).get(regionWrapper.getRegionList());
     }
 
@@ -639,7 +640,8 @@ public class FinremCaseData {
             Region.NORTHWEST, getNorthWestCourt(interimWrapper.getInterimNorthWestFrcList(), courtList),
             Region.SOUTHWEST, getSouthWestCourt(interimWrapper.getInterimSouthWestFrcList(), courtList),
             Region.SOUTHEAST, getSouthEastCourt(interimWrapper.getInterimSouthEastFrcList(), courtList),
-            Region.WALES, getWalesCourt(interimWrapper.getInterimWalesFrcList(), courtList)
+            Region.WALES, getWalesCourt(interimWrapper.getInterimWalesFrcList(), courtList),
+            Region.HIGHCOURT, getHighCourt(interimWrapper.getInterimHighCourtFrcList(), courtList)
         ).get(interimWrapper.getInterimRegionList());
     }
 
@@ -660,7 +662,8 @@ public class FinremCaseData {
                 courtList),
             Region.SOUTHEAST, getSouthEastCourt(regionWrapper.getGeneralApplicationDirectionsSouthEastFrcList(),
                 courtList),
-            Region.WALES, getWalesCourt(regionWrapper.getGeneralApplicationDirectionsWalesFrcList(), courtList)
+            Region.WALES, getWalesCourt(regionWrapper.getGeneralApplicationDirectionsWalesFrcList(), courtList),
+            Region.HIGHCOURT, getHighCourt(regionWrapper.getGeneralApplicationDirectionsHighCourtFrcList(), courtList)
         ).get(regionWrapper.getGeneralApplicationDirectionsRegionList());
     }
 
@@ -715,6 +718,13 @@ public class FinremCaseData {
             RegionWalesFrc.NORTH_WALES, getCourtListIdOrDefault(courtList.getNorthWalesCourt()),
             RegionWalesFrc.NEWPORT, getCourtListIdOrDefault(courtList.getNewportCourt()),
             RegionWalesFrc.SWANSEA, getCourtListIdOrDefault(courtList.getSwanseaCourt())
+        ).get(frc).getSelectedCourtId();
+    }
+
+    @JsonIgnore
+    private String getHighCourt(RegionHighCourtFrc frc, CourtListWrapper courtList) {
+        return Map.of(
+            RegionHighCourtFrc.HIGHCOURT, getCourtListIdOrDefault(courtList.getHighCourt())
         ).get(frc).getSelectedCourtId();
     }
 
