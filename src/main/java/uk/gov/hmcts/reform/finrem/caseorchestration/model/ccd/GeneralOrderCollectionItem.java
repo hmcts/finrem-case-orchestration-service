d9eeac232c220b761aa7bbbfb2a1a423938800d7
rishisharma <rishi.sharma@hmcts.net>
2023-01-20 10:00:34 +0000

    DFR-1589: issue application

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralOrderCollectionItem.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralOrderCollectionItem.java
@@ -7,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Objects;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @SuperBuilder
@@ -18,25 +16,4 @@ public class GeneralOrderCollectionItem extends CollectionElement<GeneralOrder> 
 
     @JsonProperty("value")
     private GeneralOrder generalOrder;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-
-        GeneralOrderCollectionItem that = (GeneralOrderCollectionItem) o;
-        return Objects.equals(generalOrder, that.generalOrder);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), generalOrder);
-    }
 }
