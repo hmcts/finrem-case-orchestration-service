9c3af53a3b8ac1629b31d7567b9c12f3257f92e9
rishikrsharma <rishikrsharma@users.noreply.github.com>
2023-01-30 10:28:56 +0000

    DFR-1345: General application- Cannot complete Create General Applica… (#909)
    * DFR-1345: General application- Cannot complete Create General Application event if no GA is added.
    
    * DFR-1345: General application- Cannot complete Create General Application event if no GA is added.

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationMidHandler.java
@@ -0,0 +1,65 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.GeneralApplicationHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralApplicationsCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class GeneralApplicationMidHandler extends FinremCallbackHandler {
+
+    private final GeneralApplicationHelper helper;
+
+    public GeneralApplicationMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper, GeneralApplicationHelper helper) {
+        super(finremCaseDetailsMapper);
+        this.helper = helper;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.MID_EVENT.equals(callbackType)
+            && CaseType.CONTESTED.equals(caseType)
+            && (EventType.GENERAL_APPLICATION.equals(eventType));
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+
+        log.info("Mid callback event type {} for case id: {}", EventType.GENERAL_APPLICATION, caseDetails.getId());
+        FinremCaseData caseData = caseDetails.getData();
+        List<String> errors = new ArrayList<>();
+
+        List<GeneralApplicationsCollection> generalApplications = caseData.getGeneralApplicationWrapper().getGeneralApplications();
+        if (generalApplications == null || generalApplications.isEmpty()) {
+            log.info("Please complete the general application for case Id {}", caseDetails.getId());
+            errors.add("Please complete the General Application. No information has been entered for this application.");
+        }
+
+        FinremCaseDetails caseDetailsBefore = callbackRequest.getCaseDetailsBefore();
+        FinremCaseData caseDataBefore = caseDetailsBefore.getData();
+
+        List<GeneralApplicationsCollection> generalApplicationsBefore = caseDataBefore.getGeneralApplicationWrapper().getGeneralApplications();
+
+        if (generalApplicationsBefore != null && generalApplications != null && (generalApplicationsBefore.size() == generalApplications.size())) {
+            log.info("Please complete the general application for case Id {}", caseDetails.getId());
+            errors.add("Any changes to an existing General Applications will not be saved. "
+                + "Please add a new General Application in order to progress.");
+        }
+
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
+            .data(caseData).errors(errors).build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationMidHandlerTest.java
@@ -0,0 +1,125 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.GeneralApplicationHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.GeneralApplicationItems;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralApplicationWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralApplicationsCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.GenericDocumentService;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeneralApplicationMidHandlerTest extends BaseHandlerTest {
+
+    private GeneralApplicationMidHandler handler;
+    @Mock
+    private GenericDocumentService service;
+
+    public static final String AUTH_TOKEN = "tokien:)";
+
+
+    @Before
+    public void setup() {
+        FinremCaseDetailsMapper finremCaseDetailsMapper = new FinremCaseDetailsMapper(new ObjectMapper().registerModule(new JavaTimeModule()));
+        ObjectMapper objectMapper = new ObjectMapper();
+        GeneralApplicationHelper helper = new GeneralApplicationHelper(objectMapper, service);
+        handler = new GeneralApplicationMidHandler(finremCaseDetailsMapper, helper);
+    }
+
+    @Test
+    public void canHandle() {
+        assertThat(handler
+                .canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.GENERAL_APPLICATION),
+            is(true));
+    }
+
+    @Test
+    public void canNotHandle() {
+        assertThat(handler
+                .canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.GENERAL_APPLICATION),
+            is(false));
+    }
+
+    @Test
+    public void canNotHandleWrongEventType() {
+        assertThat(handler
+                .canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.CLOSE),
+            is(false));
+    }
+
+    @Test
+    public void canNotHandleWrongCallbackType() {
+        assertThat(handler
+                .canHandle(CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.GENERAL_APPLICATION),
+            is(false));
+    }
+
+    @Test
+    public void givenContestedCase_whenGeneralApplicationEventStartButNotAddedDetails_thenThrowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle = handler.handle(finremCallbackRequest, AUTH_TOKEN);
+        assertTrue(handle.getErrors().get(0)
+            .contains("Please complete the General Application. No information has been entered for this application."));
+    }
+
+    @Test
+    public void givenContestedCase_whenGeneralApplicationEventStartAndThereIsExistingApplicationButNotAddedNewApplication_thenThrowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildCallbackRequestWithCaseDetailsBefore();
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
+
+        assertTrue(response.getErrors().get(0)
+            .contains("Any changes to an existing General Applications will not be saved."
+                + " Please add a new General Application in order to progress."));
+    }
+
+
+    private FinremCallbackRequest buildCallbackRequest() {
+        return FinremCallbackRequest
+            .builder()
+            .eventType(EventType.GENERAL_APPLICATION)
+            .caseDetailsBefore(FinremCaseDetails.builder().id(123L).caseType(CONTESTED)
+                .data(new FinremCaseData()).build())
+            .caseDetails(FinremCaseDetails.builder().id(123L).caseType(CONTESTED)
+                .data(new FinremCaseData()).build())
+            .build();
+    }
+
+
+    private FinremCallbackRequest buildCallbackRequestWithCaseDetailsBefore() {
+        GeneralApplicationsCollection record1 = GeneralApplicationsCollection.builder().id(UUID.randomUUID())
+            .value(GeneralApplicationItems.builder().generalApplicationCreatedBy("Test1").build()).build();
+        GeneralApplicationsCollection record2 = GeneralApplicationsCollection.builder().id(UUID.randomUUID())
+            .value(GeneralApplicationItems.builder().generalApplicationCreatedBy("Test2").build()).build();
+
+        GeneralApplicationWrapper wrapper1 = GeneralApplicationWrapper.builder().generalApplications(List.of(record1, record2)).build();
+
+        return FinremCallbackRequest
+            .builder()
+            .eventType(EventType.GENERAL_APPLICATION)
+            .caseDetailsBefore(FinremCaseDetails.builder().id(123L).caseType(CONTESTED)
+                .data(FinremCaseData.builder().generalApplicationWrapper(wrapper1).build()).build())
+            .caseDetails(FinremCaseDetails.builder().id(123L).caseType(CONTESTED)
+                .data(FinremCaseData.builder().generalApplicationWrapper(wrapper1).build()).build())
+            .build();
+    }
+}
