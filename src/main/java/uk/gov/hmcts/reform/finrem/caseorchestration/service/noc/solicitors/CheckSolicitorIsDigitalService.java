7f083b832ecdadd5c9378f7c6215eb1dce14d0a2
Ruban <ruban72@hotmail.com>
2023-02-06 15:38:13 +0000

    DFR-1800: Add further logging

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/solicitors/CheckSolicitorIsDigitalService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/noc/solicitors/CheckSolicitorIsDigitalService.java
@@ -26,14 +26,22 @@ public class CheckSolicitorIsDigitalService {
     }
 
     private boolean isSolicitorDigital(String caseId, String caseRole) {
+        log.info("Checking if solicitor is digital for case id: {} and role: {}", caseId, caseRole);
         CaseAssignmentUserRolesResource rolesResource = assignCaseAccessService.getUserRoles(caseId);
 
         if (rolesResource == null || rolesResource.getCaseAssignmentUserRoles() == null) {
+            log.info("No roles found for case id: {} and role: {}", caseId, caseRole);
             return false;
         }
+        log.info("Found {} roles for case id: {} and role: {}", rolesResource.getCaseAssignmentUserRoles().size(), caseId, caseRole);
+        return isRolePresent(caseRole, rolesResource);
+    }
 
-        return rolesResource.getCaseAssignmentUserRoles().stream()
+    private boolean isRolePresent(String caseRole, CaseAssignmentUserRolesResource rolesResource) {
+        boolean foundRole = rolesResource.getCaseAssignmentUserRoles().stream()
             .map(CaseAssignmentUserRole::getCaseRole)
             .anyMatch(caseRole::equals);
+        log.info("Found role: {} for case id: {}", foundRole, caseRole);
+        return foundRole;
     }
 }
