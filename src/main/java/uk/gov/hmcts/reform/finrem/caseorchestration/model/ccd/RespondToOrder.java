8d388b63bac7aa8473e77500a0b41aefb74cf8ad
dawudgovuk <dawud.rahman@justice.gov.uk>
2023-02-16 18:32:42 +0000

    DFR-1833-Consented-Ready-for-Hearing-Error

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RespondToOrder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RespondToOrder.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -9,7 +8,6 @@ import java.time.LocalDate;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RespondToOrder {
     @JsonProperty("DocumentType")
     private String documentType;
