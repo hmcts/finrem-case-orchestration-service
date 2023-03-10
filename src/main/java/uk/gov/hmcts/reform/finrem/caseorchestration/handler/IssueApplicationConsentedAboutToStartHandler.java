554c869c060f21f03e0e12ad83139e90da806950
Mike Lemos <gobii@me.com>
2023-03-10 14:55:00 +0000

    dfr-1199 removing defaults

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandler.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
