c4f201efe641abd4b5ee7e1fe9634c0f42969635
Mike Lemos <gobii@me.com>
2023-03-15 17:42:41 +0000

    DFR-1199 merge fix

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/evidencemanagement/EvidenceManagementDownloadService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/evidencemanagement/EvidenceManagementDownloadService.java
@@ -4,15 +4,20 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
 import uk.gov.hmcts.reform.finrem.caseorchestration.error.InvalidUriException;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.wrapper.IdamToken;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,17 +29,17 @@ import static java.lang.String.format;
 @Slf4j
 public class EvidenceManagementDownloadService {
 
+    private final CaseDocumentClient caseDocumentClient;
     private static final String FINANCIAL_REMEDY_COURT_ADMIN = "caseworker-divorce-financialremedy-courtadmin";
     private static final String USER_ROLES = "user-roles";
     private static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
-
     private final RestTemplate restTemplate;
     private final AuthTokenGenerator authTokenGenerator;
 
     @Value("${document.management.store.baseUrl}")
     private String documentManagementStoreUrl;
 
-    public ResponseEntity<byte[]> download(@NonNull final String binaryFileUrl) {
+    public ResponseEntity<byte[]> download(@NonNull final String binaryFileUrl, String authorizationToken) {
         log.info("Binary url for file download : {} ", binaryFileUrl);
         HttpHeaders headers = new HttpHeaders();
         headers.set(SERVICE_AUTHORIZATION, authTokenGenerator.generate());
@@ -57,6 +62,21 @@ public class EvidenceManagementDownloadService {
 
         log.info("File download status : {} ", response.getStatusCode());
         return response;
+    }
+
+    public byte[] download(String binaryFileUrl, IdamToken idamTokens) throws HttpClientErrorException {
+        ResponseEntity<Resource> responseEntity = downloadResource(binaryFileUrl, idamTokens);
+        ByteArrayResource resource = (ByteArrayResource) responseEntity.getBody();
+
+        return (resource != null) ? resource.getByteArray() : new byte[0];
+    }
+
+    private ResponseEntity<Resource> downloadResource(String binaryFileUrl, IdamToken idamTokens) {
+        String documentHref = URI.create(binaryFileUrl).getPath().replaceFirst("/", "");
+        log.info("EMSDocStore Download file: {} with user: {}", documentHref, idamTokens.getEmail());
+
+        return caseDocumentClient.getDocumentBinary(idamTokens.getIdamOauth2Token(),
+            idamTokens.getServiceAuthorization(), documentHref);
     }
 
     private String getUrl(String binaryFileUrl) throws URISyntaxException {
