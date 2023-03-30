f198d6b4e54c09666c69e1829f1eabb83706c4c0
Jamie Phillips <jamie.phillips@solirius.com>
2023-03-30 09:00:13 +0100

    DFR-1760 Integrate finrem case data test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
@@ -26,7 +26,7 @@ public class FinremCaseDataTest {
 
     @Test
     public void testDefinitionFilesSavedInCorrectLocation() throws IOException {
-        Path dirPath = Paths.get("./definition_files").toAbsolutePath();
+        Path dirPath = Paths.get("./definition_files/definitions").toAbsolutePath();
         File directoryPath = dirPath.toFile();
         String contents[] = directoryPath.list();
         for (int i=0; i<contents.length; i++) {
