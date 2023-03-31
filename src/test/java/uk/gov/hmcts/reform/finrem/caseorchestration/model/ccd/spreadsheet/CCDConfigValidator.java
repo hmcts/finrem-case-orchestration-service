197227bebf1c91fd91c829c99b6346de5bb0c6af
Jamie Phillips <jamie.phillips@solirius.com>
2023-03-31 14:43:05 +0100

    DFR-1825 update tests

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/CCDConfigValidator.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/CCDConfigValidator.java
@@ -41,6 +41,7 @@ public class CCDConfigValidator {
     protected static final int ROW_HEADERS = 2;
     protected static final String STATE_SHEET = "State";
     protected static final String DYNAMIC_LIST = "DynamicList";
+    protected static final String DYNAMIC_RADIO_LIST = "DynamicRadioList";
     private List<String> ccdFieldsToIgnore = Arrays.asList("Label", "OrderSummary", "CaseHistoryViewer", "CasePaymentHistoryViewer");
     private List<String> fixedListValues = Arrays.asList(FIXED_LIST, FIXED_RADIO_LIST);
     private List<String> alreadyProcessedCcdFields = new ArrayList<>();
@@ -60,7 +61,8 @@ public class CCDConfigValidator {
         Map.entry("OrganisationPolicy", "OrganisationPolicy"),
         Map.entry(COLLECTION, "List"),
         Map.entry(MULTI_SELECT_LIST, "List"),
-        Map.entry(DYNAMIC_LIST, "DynamicList")
+        Map.entry(DYNAMIC_LIST, "DynamicList"),
+        Map.entry(DYNAMIC_RADIO_LIST, "DynamicList")
     );
 
     private Map<String, String> specialFieldTypes = Map.ofEntries(
