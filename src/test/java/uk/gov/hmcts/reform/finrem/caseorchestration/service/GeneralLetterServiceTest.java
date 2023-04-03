0b9562bcb2e5c423a79d063df0906ef0cdc7c63d
srochani <sunil.rochani@hotmail.com>
2023-04-03 10:19:17 +0100

    DFR-1787: FR Contested and Consented: General letter add attachment

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GeneralLetterServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GeneralLetterServiceTest.java
@@ -15,6 +15,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.BaseServiceTest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.GeneralLetterData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.BulkPrintDocument;
 
 import java.io.InputStream;
 import java.util.List;
@@ -30,6 +31,9 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -175,7 +179,7 @@ public class GeneralLetterServiceTest extends BaseServiceTest {
     public void whenGeneralLetterIsCreated_thenItGetsSentToBulkPrint() {
         CaseDetails caseDetails = TestSetUpUtils.caseDetailsFromResource("/fixtures/general-letter.json", mapper);
         generalLetterService.createGeneralLetter(AUTH_TOKEN, caseDetails);
-        verify(bulkPrintService, times(1)).sendDocumentForPrint(any(CaseDocument.class), any(CaseDetails.class));
+        verify(bulkPrintService, times(1)).bulkPrintFinancialRemedyLetterPack(anyLong(), any());
     }
 
     private void assertNameUsedForGeneralLetterAddressTo(int invocation, String generalLetterAddressTo, String expectedName) {
