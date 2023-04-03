775ab8812563077fddb9848ec33e369003b3e548
Jamie Phillips <jamie.phillips@solirius.com>
2023-04-03 10:59:20 +0100

    DFR-1825 update tests

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
@@ -36,7 +36,7 @@ public class FinremCaseDataTest {
           }
           System.out.println(consentedFileNameWithPath);
           System.out.println(contestedFileNameWithPath);
-          Path dirPath = Paths.get("./definition_files/definitions").toAbsolutePath();
+          Path dirPath = Paths.get("./definition_files/definitions/consented").toAbsolutePath();
           File directoryPath = dirPath.toFile();
           String contents[] = directoryPath.list();
           for (int i=0; i<contents.length; i++) {
