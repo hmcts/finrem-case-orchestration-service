3783feb3da33986c2dbeeda45bf03dfd72accc27
rishikrsharma <rishikrsharma@users.noreply.github.com>
2023-04-21 14:10:29 +0100

    Dfr 1991 order refusal date (#998)
    * DFR-1991: FR Consent:INC5502354 Date not going into Order
    
    * DFR-1991: FR Consent:INC5502354 Date not going into Order

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/OrderRefusal.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/OrderRefusal.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -24,7 +27,8 @@ public class OrderRefusal {
     private String orderRefusalJudge;
     @JsonProperty("orderRefusalJudgeName")
     private String orderRefusalJudgeName;
-    @JsonProperty("orderRefusalDate")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate orderRefusalDate;
     @JsonProperty("orderRefusalAddComments")
     private String orderRefusalAddComments;
