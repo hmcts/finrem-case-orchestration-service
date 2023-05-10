c73ee559db0ed97f913c24b2cdc5978e4b26861d
rishisharma <rishi.sharma@hmcts.net>
2023-05-10 15:23:50 +0100

    DFR-1730: Manage order

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SendOrderContestedAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SendOrderContestedAboutToStartHandlerTest.java
@@ -11,11 +11,14 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseRole;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicMultiSelectList;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicMultiSelectListElement;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Organisation;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.OrganisationPolicy;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.GeneralOrderService;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -74,6 +77,48 @@ class SendOrderContestedAboutToStartHandlerTest {
     }
 
     @Test
+    void givenACcdCallbackContestedCase_whenStartEventCalledAndAllPartiesAredigitalAndOneSelected_thenPrepareOrderList() {
+        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
+        FinremCaseDetails caseDetails = finremCallbackRequest.getCaseDetails();
+        FinremCaseData data = caseDetails.getData();
+
+        data.setApplicantOrganisationPolicy(getOrganisation("ORGAPP","applicant",
+            CaseRole.APP_SOLICITOR.getValue()));
+        data.setRespondentOrganisationPolicy(getOrganisation("ORGRESP","respondent",
+            CaseRole.RESP_SOLICITOR.getValue()));
+        data.getIntervenerOneWrapper().setIntervener1Organisation(getOrganisation("ORGINTV1","intervener1",
+            CaseRole.INTVR_SOLICITOR_1.getValue()));
+        data.getIntervenerTwoWrapper().setIntervener2Organisation(getOrganisation("ORGINTV2","intervener2",
+            CaseRole.INTVR_SOLICITOR_2.getValue()));
+        data.getIntervenerThreeWrapper().setIntervener3Organisation(getOrganisation("ORGINTV3","intervener3",
+            CaseRole.INTVR_SOLICITOR_3.getValue()));
+        data.getIntervenerFourWrapper().setIntervener4Organisation(getOrganisation("ORGINTV4","intervener4",
+            CaseRole.INTVR_SOLICITOR_4.getValue()));
+
+
+        List<DynamicMultiSelectListElement> dynamicElementList = List.of(getDynamicElementList(CaseRole.APP_SOLICITOR.getValue()),
+            getDynamicElementList(CaseRole.RESP_SOLICITOR.getValue()),
+            getDynamicElementList(CaseRole.INTVR_SOLICITOR_1.getValue()),
+            getDynamicElementList(CaseRole.INTVR_SOLICITOR_2.getValue()),
+            getDynamicElementList(CaseRole.INTVR_SOLICITOR_3.getValue()),
+            getDynamicElementList(CaseRole.INTVR_SOLICITOR_4.getValue()));
+
+        DynamicMultiSelectList parties = DynamicMultiSelectList.builder()
+            .value(dynamicElementList)
+            .listItems(dynamicElementList)
+            .build();
+
+        data.setPartiesInCase(parties);
+
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> resp = handler.handle(finremCallbackRequest, AUTH_TOKEN);
+
+        DynamicMultiSelectList partiesInCase = resp.getData().getPartiesInCase();
+        assertEquals(6, partiesInCase.getListItems().size(), "available parties");
+        assertEquals(6, partiesInCase.getValue().size(), "selected parties");
+        verify(generalOrderService).setOrderList(caseDetails);
+    }
+
+    @Test
     void givenACcdCallbackContestedCase_whenStartEventCalledAndAllPartiesAreNotdigital_thenPrepareOrderList() {
         FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
         FinremCaseDetails caseDetails = finremCallbackRequest.getCaseDetails();
@@ -111,6 +156,15 @@ class SendOrderContestedAboutToStartHandlerTest {
         assertEquals(6, partiesInCase.getListItems().size(), "available parties");
         verify(generalOrderService).setOrderList(caseDetails);
     }
+
+
+    private DynamicMultiSelectListElement getDynamicElementList(String role) {
+        return DynamicMultiSelectListElement.builder()
+            .code(role)
+            .label(role)
+            .build();
+    }
+
 
     private OrganisationPolicy getOrganisation(String orgId, String orgName, String role) {
         Organisation organisation = Organisation.builder()
