0a2041f6e1ed1944217b04c30a9d686cafbda9de
srochani <sunil.rochani@hotmail.com>
2023-05-11 16:14:58 +0100

    Dfr 615 cdam into 1199

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenericDocumentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/GenericDocumentServiceTest.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
