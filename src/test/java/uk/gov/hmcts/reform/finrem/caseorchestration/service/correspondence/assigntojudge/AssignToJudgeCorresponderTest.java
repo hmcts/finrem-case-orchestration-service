1fe11b50bca093497cc1a1d1ad4d5bb6e630b4b3
srochani <sunil.rochani@hotmail.com>
2023-05-18 18:44:47 +0100

    DFR-1734: All email notifications-Intervener solicitor

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/AssignToJudgeCorresponderTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/AssignToJudgeCorresponderTest.java
@@ -16,8 +16,6 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
