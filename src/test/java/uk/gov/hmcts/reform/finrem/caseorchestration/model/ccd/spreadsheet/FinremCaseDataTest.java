bd311aae1520779b28ba294252d880187d92e4fe
jamiephillips <jamie.phillips@justice.gov.uk>
2023-06-03 14:05:01 +0100

    DFR-1647-Update Intervener Letter Notifications

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/spreadsheet/FinremCaseDataTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.spreadsheet;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 
