91a3d9785dffae5c049f1357898fa04fc1473854
srochani <sunil.rochani@hotmail.com>
2023-06-09 10:40:30 +0100

    DFR-2041: Rerun in bulk the 'Send Order' event for identified cases

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/SendOrderTask.java
@@ -0,0 +1,98 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.scheduler;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.service.dataextract.DataExtractDateValidator;
+import uk.gov.hmcts.probate.service.dataextract.SmeeAndFordDataExtractService;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.BulkPrintDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
+import uk.gov.hmcts.reform.probate.model.client.ApiClientException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_ORDER_APPROVED_COVER_LETTER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.LATEST_DRAFT_HEARING_ORDER;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SendOrderTask implements Runnable {
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private final NotificationService notificationService;
+    private final BulkPrintService bulkPrintService;
+    private final DocumentHelper documentHelper;
+
+    @Override
+    public void run() {
+        log.info("Scheduled task SendOrderTask started to run for selected cases");
+        String date = DATE_FORMAT.format(LocalDate.now().minusDays(1L));
+        log.info("Calling perform SendOrderTask for selected cases {}", date);
+        try {
+            printAndMailHearingDocuments();
+        } catch (ApiClientException e) {
+            log.error(e.getMessage());
+        } catch (FeignException e) {
+            log.error("Error on calling BackOfficeAPI {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Error on SmeeAndFordExtractTask Scheduler {}", e.getMessage());
+        }
+    }
+
+    private void printAndMailHearingDocuments(CaseDetails caseDetails, String authorisationToken) {
+
+        String caseId = String.valueOf(caseDetails.getId());
+        log.info("In request to send hearing pack for case {}:", caseId);
+        Map<String, Object> caseData = caseDetails.getData();
+
+        List<BulkPrintDocument> hearingDocumentPack = createHearingDocumentPack(caseData, authorisationToken, caseId);
+        if (!hearingDocumentPack.isEmpty()) {
+            if (!notificationService.isApplicantSolicitorDigitalAndEmailPopulated(caseDetails)) {
+                log.info("Received request to send hearing pack for applicant for case {}:", caseId);
+                bulkPrintService.printApplicantDocuments(caseDetails, authorisationToken, hearingDocumentPack);
+            }
+
+            if (!notificationService.isRespondentSolicitorDigitalAndEmailPopulated(caseDetails)) {
+                log.info("Received request to send hearing pack for respondent for case {}:", caseId);
+                bulkPrintService.printRespondentDocuments(caseDetails, authorisationToken, hearingDocumentPack);
+            }
+        }
+    }
+
+    private List<BulkPrintDocument> createHearingDocumentPack(Map<String, Object> caseData, String authorisationToken, String caseId) {
+        List<BulkPrintDocument> hearingDocumentPack = new ArrayList<>();
+
+        documentHelper.getDocumentLinkAsBulkPrintDocument(caseData, CONTESTED_ORDER_APPROVED_COVER_LETTER).ifPresent(hearingDocumentPack::add);
+        documentHelper.getDocumentLinkAsBulkPrintDocument(caseData, LATEST_DRAFT_HEARING_ORDER).ifPresent(hearingDocumentPack::add);
+
+        if (documentHelper.hasAnotherHearing(caseData)) {
+            Optional<CaseDocument> latestAdditionalHearingDocument = documentHelper.getLatestAdditionalHearingDocument(caseData);
+            latestAdditionalHearingDocument.ifPresent(
+                caseDocument -> hearingDocumentPack.add(documentHelper.getCaseDocumentAsBulkPrintDocument(caseDocument)));
+        }
+
+        List<BulkPrintDocument> otherHearingDocuments = documentHelper.getHearingDocumentsAsBulkPrintDocuments(
+            caseData, authorisationToken, caseId);
+
+        if (otherHearingDocuments != null) {
+            hearingDocumentPack.addAll(otherHearingDocuments);
+        }
+
+        return hearingDocumentPack;
+    }
+
+}
