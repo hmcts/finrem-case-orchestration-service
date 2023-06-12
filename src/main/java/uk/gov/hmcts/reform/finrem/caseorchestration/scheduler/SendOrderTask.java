a8f2e96c7d50ddb03b983bda6afc07cfa717963b
srochani <sunil.rochani@hotmail.com>
2023-06-12 10:39:57 +0100

    DFR-2041: Rerun in bulk the 'Send Order' event for identified cases

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/SendOrderTask.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/SendOrderTask.java
@@ -1,22 +1,28 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.scheduler;
 
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.BulkPrintDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.CcdService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.SystemUserService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.csv.CaseReference;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.csv.CaseReferenceCsvLoader;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_ORDER_APPROVED_COVER_LETTER;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.LATEST_DRAFT_HEARING_ORDER;
@@ -26,25 +32,50 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 @RequiredArgsConstructor
 public class SendOrderTask implements Runnable {
 
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
     private final NotificationService notificationService;
     private final BulkPrintService bulkPrintService;
     private final DocumentHelper documentHelper;
+    private final CaseReferenceCsvLoader csvLoader;
+    private final CcdService ccdService;
+    private final SystemUserService systemUserService;
+    @Value("${cron.batchsize:500}")
+    private int bulkPrintBatchSize;
+    @Value("${cron.wait-time-mins:10}")
+    private int bulkPrintWaitTime;
+
+    @Value("${cron.sendorder.enabled:false}")
+    private boolean isSendOrderTaskEnabled;
 
     @Override
     public void run() {
-        log.info("Scheduled task SendOrderTask started to run for selected cases");
-        String date = DATE_FORMAT.format(LocalDate.now().minusDays(1L));
-        log.info("Calling perform SendOrderTask for selected cases {}", date);
-        try {
-            printAndMailHearingDocuments();
-        } catch (ApiClientException e) {
-            log.error(e.getMessage());
-        } catch (FeignException e) {
-            log.error("Error on calling BackOfficeAPI {}", e.getMessage());
-        } catch (Exception e) {
-            log.error("Error on SmeeAndFordExtractTask Scheduler {}", e.getMessage());
+        log.info("Scheduled task SendOrderTask isEnabled {}", isSendOrderTaskEnabled);
+        if (isSendOrderTaskEnabled) {
+            log.info("Scheduled task SendOrderTask started to run for selected cases");
+            List<CaseReference> caseReferences = csvLoader.loadCaseReferenceList("printAosPackCaseReferenceList.csv");
+            int count = 0;
+            int batchCount = 1;
+            for (CaseReference caseReference : caseReferences) {
+                count++;
+                try {
+                    if (count == bulkPrintBatchSize) {
+                        log.info("Batch {} limit reached {}, pausing for {} minutes", batchCount, bulkPrintBatchSize, bulkPrintWaitTime);
+                        TimeUnit.MINUTES.sleep(bulkPrintWaitTime);
+                        count = 0;
+                        batchCount++;
+                    }
+
+                    log.info("Process case reference {}, batch {}, count {}", caseReference.getCaseReference(), batchCount, count);
+                    SearchResult searchResult =
+                        ccdService.getCaseByCaseId(caseReference.getCaseReference(), CaseType.CONTESTED, systemUserService.getSysUserToken());
+                    if (CollectionUtils.isNotEmpty(searchResult.getCases())) {
+                        CaseDetails caseDetails = searchResult.getCases().get(0);
+                        printAndMailHearingDocuments(caseDetails, systemUserService.getSysUserToken());
+                    }
+
+                } catch (InterruptedException | RuntimeException e) {
+                    log.error("Error processing caseRef {} ", caseReference.getCaseReference());
+                }
+            }
         }
     }
 
