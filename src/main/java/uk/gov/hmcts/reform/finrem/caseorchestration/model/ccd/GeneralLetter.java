dc6cd68a2b34a6a6193570faadaafc79da3ffdd3
srochani <sunil.rochani@hotmail.com>
2023-06-29 09:21:14 +0100

    DFR-2102 FR Contested: General email and letter error

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
