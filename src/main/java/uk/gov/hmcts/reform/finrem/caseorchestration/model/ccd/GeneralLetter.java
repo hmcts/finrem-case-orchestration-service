7d3349f675ea5d843291d90c7b93e571e5da721d
srochani <sunil.rochani@hotmail.com>
2023-06-29 09:13:01 +0100

    DFR-2102 FR Contested: General email and letter error

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
@@ -1,19 +1,23 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 @Builder
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GeneralLetter {
     @JsonProperty("generatedLetter")
     private CaseDocument generatedLetter;
+    @JsonProperty("generalLetterUploadedDocument")
     private CaseDocument generalLetterUploadedDocument;
 }
