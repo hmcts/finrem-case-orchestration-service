f4a64f815af7bea8cd64d084fa5da9cd4fd53550
AbuSalam22 <abu.sufian.salam@hotmail.com>
2023-07-10 17:09:53 +0100

    DFR-1033 Remove feature flag FEATURE_USE_CASE_TOKEN

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/Features.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/Features.java
@@ -11,7 +11,6 @@ public enum Features {
     CASEWORKER_NOTICE_OF_CHANGE("caseworker-notice-of-change"),
     SEND_TO_FRC("send_to_frc"),
     PAYMENT_REQUEST_USING_CASE_TYPE("pba_case_type"),
-    USE_USER_TOKEN("use_user_token"),
     MANAGE_BUNDLE("manage_bundle"),
     SOLICITOR_NOTICE_OF_CHANGE("solicitor_notice_of_change"),
     SEND_LETTER_RECIPIENT_CHECK("send_letter_recipient_check"),
