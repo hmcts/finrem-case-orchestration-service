99eae92ebc02705db72c509aa1abc4eb83daa910
rishikrsharma <rishikrsharma@users.noreply.github.com>
2023-07-12 18:59:34 +0100

    DFR 1731 share selected document with others (#985)
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: Share selected document
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    * DFR-1731: share selected document with other parties
    
    ---------
    
    Co-authored-by: GEvans-HMCTS <106392984+GEvans-HMCTS@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/DynamicMultiSelectList.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class DynamicMultiSelectList {
+    /**
+     * The selected value for the dropdown.
+     */
+    @JsonProperty("value")
+    private List<DynamicMultiSelectListElement> value;
+
+    /**
+     * List of options for the dropdown.
+     */
+    @JsonProperty("list_items")
+    private List<DynamicMultiSelectListElement> listItems;
+
+    public List<DynamicMultiSelectListElement> getValue() {
+        return value != null ? value : null;
+    }
+}
