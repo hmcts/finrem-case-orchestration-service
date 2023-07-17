4992d04444ce44d870eb7b61f4bc3a446ad7a75e
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2023-07-17 11:52:06 +0100

    DFR-1768 Merge in latest changes

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/applicant/ApplicantExpertEvidenceHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/applicant/ApplicantExpertEvidenceHandler.java
@@ -1,17 +1,17 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service.casedocuments.applicant;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocumentParty;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.CaseDocumentCollectionType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.casedocuments.ExpertEvidenceHandler;
 
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_EXPERT_EVIDENCE_COLLECTION;
-
-@Component
+@Service
 public class ApplicantExpertEvidenceHandler extends ExpertEvidenceHandler {
 
     @Autowired
-    public ApplicantExpertEvidenceHandler(ObjectMapper mapper) {
-        super(APP_EXPERT_EVIDENCE_COLLECTION, APPLICANT, mapper);
+    public ApplicantExpertEvidenceHandler() {
+        super(CaseDocumentCollectionType.APP_EXPERT_EVIDENCE_COLLECTION,
+            CaseDocumentParty.APPLICANT);
     }
 }
