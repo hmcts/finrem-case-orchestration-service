a90dfdf0e91cfd538b6dd5b089d510cdefb3f034
AbuSalam22 <abu.sufian.salam@hotmail.com>
2023-08-24 15:17:33 +0100

    DFR-1039 Deactivate FEATURE_SEND_TO_FRC flag from config in COS

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/NotificationServiceTest.java
@@ -576,17 +576,6 @@ public class NotificationServiceTest extends BaseServiceTest {
     }
 
     @Test
-    public void sendContestedGeneralApplicationOutcomeNotificationEmailWhenSendToFRCToggleTrue() throws IOException {
-        when(featureToggleService.isSendToFRCEnabled()).thenReturn(true);
-
-        callbackRequest = getContestedCallbackRequest();
-        notificationService.sendContestedGeneralApplicationOutcomeEmail(callbackRequest.getCaseDetails());
-
-        verify(notificationRequestMapper).getNotificationRequestForApplicantSolicitor(callbackRequest.getCaseDetails());
-        verify(emailService).sendConfirmationEmail(notificationRequest, FR_CONTESTED_GENERAL_APPLICATION_OUTCOME);
-    }
-
-    @Test
     public void sendContestedConsentGeneralOrderNotificationEmailApplicantSolicitor() {
         notificationService.sendContestedConsentGeneralOrderEmailApplicantSolicitor(callbackRequest.getCaseDetails());
 
@@ -1003,7 +992,6 @@ public class NotificationServiceTest extends BaseServiceTest {
 
     @Test
     public void sendUpdateFrcInformationEmailToCourt() throws JsonProcessingException {
-        when(featureToggleService.isSendToFRCEnabled()).thenReturn(true);
         callbackRequest = getContestedCallbackRequest();
 
         notificationService.sendUpdateFrcInformationEmailToCourt(callbackRequest.getCaseDetails());
