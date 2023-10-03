312ef6e0d391e65c896929c93a3d5d1e8d3070f7
rishisharma <rishi.sharma@hmcts.net>
2023-10-03 12:16:38 +0100

    DFR-2358: Prevent protected file uploads - FR_uploadApprovedOrde

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderContestedMidHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderContestedMidHandler.java
@@ -1,16 +1,18 @@
-package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.uploadapprovedorder;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DirectionOrderCollection;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DocumentCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadAdditionalDocumentCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintDocumentService;
 
 import java.util.ArrayList;
@@ -18,11 +20,11 @@ import java.util.List;
 
 @Slf4j
 @Service
-public class DirectionUploadOrderMidHandler extends FinremCallbackHandler {
+public class UploadApprovedOrderContestedMidHandler extends FinremCallbackHandler {
 
     private final BulkPrintDocumentService service;
 
-    public DirectionUploadOrderMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+    public UploadApprovedOrderContestedMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
                                           BulkPrintDocumentService service) {
         super(finremCaseDetailsMapper);
         this.service = service;
@@ -32,7 +34,7 @@ public class DirectionUploadOrderMidHandler extends FinremCallbackHandler {
     public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
         return CallbackType.MID_EVENT.equals(callbackType)
             && CaseType.CONTESTED.equals(caseType)
-            && (EventType.DIRECTION_UPLOAD_ORDER.equals(eventType));
+            && (EventType.UPLOAD_APPROVED_ORDER.equals(eventType));
     }
 
     @Override
@@ -41,9 +43,9 @@ public class DirectionUploadOrderMidHandler extends FinremCallbackHandler {
         FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
         String caseId = String.valueOf(caseDetails.getId());
         log.info("Invoking contested event {} mid callback for case id: {}",
-            EventType.DIRECTION_UPLOAD_ORDER, caseId);
-        FinremCaseData caseData = caseDetails.getData();
+            EventType.UPLOAD_APPROVED_ORDER, caseId);
 
+        FinremCaseData caseData = caseDetails.getData();
         List<String> errors = new ArrayList<>();
 
         List<DirectionOrderCollection> uploadHearingOrders = caseData.getUploadHearingOrder();
@@ -53,14 +55,14 @@ public class DirectionUploadOrderMidHandler extends FinremCallbackHandler {
                     caseId, errors, userAuthorisation)
             );
         }
-        List<DocumentCollection> hearingOrderOtherDocuments = caseData.getHearingOrderOtherDocuments();
-        if (hearingOrderOtherDocuments != null) {
-            hearingOrderOtherDocuments.forEach(doc ->
-                service.validateEncryptionOnUploadedDocument(doc.getValue(),
+
+        List<UploadAdditionalDocumentCollection> uploadAdditionalDocument = caseData.getUploadAdditionalDocument();
+        if (uploadAdditionalDocument != null) {
+            uploadAdditionalDocument.forEach(doc ->
+                service.validateEncryptionOnUploadedDocument(doc.getValue().getAdditionalDocuments(),
                     caseId, errors, userAuthorisation)
             );
         }
-
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
             .data(caseData).errors(errors).build();
     }
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderContestedMidHandlerTest.java
@@ -0,0 +1,103 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.uploadapprovedorder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.BaseHandlerTestSetup;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DirectionOrder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DirectionOrderCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadAdditionalDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadAdditionalDocumentCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintDocumentService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UploadApprovedOrderContestedMidHandlerTest extends BaseHandlerTestSetup {
+
+    private UploadApprovedOrderContestedMidHandler handler;
+    @Mock
+    private BulkPrintDocumentService service;
+    private static final String FILE_URL = "http://dm:80/documents/kbjh87y8y9JHVKKKJVJ";
+    private static final String FILE_BINARY_URL = "http://dm:80/documents/kbjh87y8y9JHVKKKJVJ/binary";
+    private static final String FILE_NAME = "abc.pdf";
+    public static final String AUTH_TOKEN = "tokien:)";
+
+
+    @BeforeEach
+    void setup() {
+        FinremCaseDetailsMapper finremCaseDetailsMapper = new FinremCaseDetailsMapper(new ObjectMapper().registerModule(new JavaTimeModule()));
+        handler = new UploadApprovedOrderContestedMidHandler(finremCaseDetailsMapper, service);
+    }
+
+    @Test
+    void canHandle() {
+        assertTrue(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.UPLOAD_APPROVED_ORDER));
+    }
+
+    @Test
+    void canNotHandle() {
+        assertFalse(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.UPLOAD_APPROVED_ORDER));
+    }
+
+    @Test
+    void canNotHandleWrongEventType() {
+        assertFalse(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.CLOSE));
+    }
+
+    @Test
+    void canNotHandleWrongCallbackType() {
+        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.UPLOAD_APPROVED_ORDER));
+    }
+
+    @Test
+    void canNotHandleWhenAllParameterWrong() {
+        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.CLOSE));
+    }
+
+    @Test
+    void givenContestedCase_whenApprovedUploadOrderButNonEncryptedFileShouldNotGetError() {
+        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest(EventType.DIRECTION_UPLOAD_ORDER);
+        FinremCaseData caseData = finremCallbackRequest.getCaseDetails().getData();
+
+
+        CaseDocument caseDocument = TestSetUpUtils.caseDocument(FILE_URL, FILE_NAME, FILE_BINARY_URL);
+
+        DirectionOrder order = DirectionOrder.builder().uploadDraftDocument(caseDocument).build();
+        DirectionOrderCollection orderCollection = DirectionOrderCollection.builder().value(order).build();
+        List<DirectionOrderCollection> uploadHearingOrders = new ArrayList<>();
+        uploadHearingOrders.add(orderCollection);
+        caseData.setUploadHearingOrder(uploadHearingOrders);
+
+        UploadAdditionalDocumentCollection documentCollection = UploadAdditionalDocumentCollection.builder()
+            .value(UploadAdditionalDocument.builder().additionalDocuments(caseDocument).build()).build();
+        List<UploadAdditionalDocumentCollection> uploadAdditionalDocument = new ArrayList<>();
+        uploadAdditionalDocument.add(documentCollection);
+        caseData.setUploadAdditionalDocument(uploadAdditionalDocument);
+
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
+
+        assertTrue(response.getErrors().isEmpty());
+        verify(service, times(2)).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+    }
+}
