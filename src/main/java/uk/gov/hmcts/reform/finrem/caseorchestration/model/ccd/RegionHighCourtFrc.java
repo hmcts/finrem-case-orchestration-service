4808ee2fbef7a0f1daf034bd854b45207c57d44c
rishisharma <rishi.sharma@hmcts.net>
2023-11-08 14:56:59 +0000

    DFR-2397: Process Order

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RegionHighCourtFrc.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RegionHighCourtFrc.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @RequiredArgsConstructor
 public enum RegionHighCourtFrc {
     HIGHCOURT("highcourt");
