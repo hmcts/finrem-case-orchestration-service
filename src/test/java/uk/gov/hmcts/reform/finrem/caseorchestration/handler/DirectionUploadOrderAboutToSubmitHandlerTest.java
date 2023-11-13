d2d53a8bd2f6f7846628e2665123072e48bbe7b9
rishisharma <rishi.sharma@hmcts.net>
2023-11-13 14:55:44 +0000

    DFR-2397: Upload Approve Order

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/DirectionUploadOrderAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/DirectionUploadOrderAboutToSubmitHandlerTest.java
@@ -7,14 +7,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.error.CourtDetailsParseException;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.AdditionalHearingDocumentService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 
@@ -52,6 +56,20 @@ class DirectionUploadOrderAboutToSubmitHandlerTest {
     void handlerCanHandleWhenExpectedInputReceived() {
         assertTrue(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONTESTED, EventType.DIRECTION_UPLOAD_ORDER));
     }
+
+    @Test
+    void createAndStoreAdditionalHearingDocumentsHandleException() throws JsonProcessingException {
+        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
+        FinremCaseDetails finremCaseDetails = finremCallbackRequest.getCaseDetails();
+        doThrow(new CourtDetailsParseException()).when(service)
+            .createAndStoreAdditionalHearingDocuments(finremCaseDetails, AUTH_TOKEN);
+
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> res = handler.handle(finremCallbackRequest, AUTH_TOKEN);
+        assertEquals(1, res.getErrors().size());
+        assertEquals("Failed to parse court details.", res.getErrors().get(0));
+        verify(service).createAndStoreAdditionalHearingDocuments(finremCallbackRequest.getCaseDetails(), AUTH_TOKEN);
+    }
+
 
     @Test
     void createAndStoreAdditionalHearingDocuments() throws JsonProcessingException {
