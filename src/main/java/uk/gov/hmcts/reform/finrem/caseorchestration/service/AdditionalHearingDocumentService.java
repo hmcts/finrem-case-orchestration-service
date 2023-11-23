67c3bad9fa8e8767648dcb4c856b529c7f9bc78c
Mike Lemos <gobii@me.com>
2023-11-23 13:52:21 +0000

    DFR-2548 fixing date issue on additional hearing

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentService.java
@@ -81,7 +81,7 @@ public class AdditionalHearingDocumentService {
         CaseDetails caseDetailsCopy = documentHelper.deepCopy(caseDetails, CaseDetails.class);
 
         prepareHearingCaseDetails(caseDetailsCopy, courtDetails, caseData.get(HEARING_TYPE),
-            caseData.get(HEARING_DATE).toString(),
+            caseData.get(HEARING_DATE),
             caseData.get(HEARING_TIME), caseData.get(TIME_ESTIMATE));
         caseDetailsCopy.getData().put("AnyOtherDirections", caseData.get(HEARING_ADDITIONAL_INFO));
 
