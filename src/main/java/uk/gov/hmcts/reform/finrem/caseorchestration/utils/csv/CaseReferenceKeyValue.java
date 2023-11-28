a4a67b93c86138780ca346f6068bb4e0bf0dce0c
srochani <sunil.rochani@hotmail.com>
2023-11-28 14:28:29 +0000

    DFR-2485: INC5562612: FR contested Exception: There must be exactly one court selected in case data

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/csv/CaseReferenceKeyValue.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.utils.csv;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.*;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@EqualsAndHashCode
+@JsonPropertyOrder(value = {"caseReference"})
+@AllArgsConstructor
+@NoArgsConstructor
+@SuppressWarnings("java:S1700")
+public class CaseReferenceKeyValue {
+
+    @JsonProperty
+    private String caseReference;
+
+    @JsonProperty
+    private String previousFRCKey;
+
+    @JsonProperty
+    private String previousFRCValue;
+
+    @JsonProperty
+    private String previousCourtListKey;
+
+    @JsonProperty
+    private String previousCourtListValue;
+
+}
