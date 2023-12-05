b9a33f87ea9c810b7e3d88cd642050b2b6e93f70
srochani <sunil.rochani@hotmail.com>
2023-12-05 13:05:15 +0000

    DFR-2461: send order event

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcatergory/SendOrdersCategoriserTest.java
@@ -0,0 +1,96 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.documentcatergory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.BaseHandlerTestSetup;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApproveOrder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApprovedOrderCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.OrderWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentCategory;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SendOrdersCategoriserTest extends BaseHandlerTestSetup {
+    private SendOrdersCategoriser sendOrdersCategoriser;
+
+    @Mock
+    FeatureToggleService featureToggleService;
+
+    @BeforeEach
+    public void setUp() {
+        sendOrdersCategoriser = new SendOrdersCategoriser(featureToggleService);
+        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
+    }
+
+    @Test
+    void testCategorizeDocuments() {
+        FinremCaseData finremCaseData = buildFinremCaseData();
+        sendOrdersCategoriser.categorise(finremCaseData);
+        OrderWrapper orderWrapper = finremCaseData.getOrderWrapper();
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_APPLICANT.getDocumentCategoryId(),
+            orderWrapper.getAppOrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_RESPONDENT.getDocumentCategoryId(),
+            orderWrapper.getRespOrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_INTERVENER_1.getDocumentCategoryId(),
+            orderWrapper.getIntv1OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_INTERVENER_2.getDocumentCategoryId(),
+            orderWrapper.getIntv2OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_INTERVENER_3.getDocumentCategoryId(),
+            orderWrapper.getIntv3OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+        assertEquals(DocumentCategory.APPROVED_ORDERS_SEND_ORDERS_INTERVENER_4.getDocumentCategoryId(),
+            orderWrapper.getIntv4OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+    }
+
+    @Test
+    void testCategorizeDocumentsWhenCollectionIsnull() {
+        FinremCaseData finremCaseData = FinremCaseData.builder().orderWrapper(OrderWrapper.builder()
+            .build()).build();
+        sendOrdersCategoriser.categorise(finremCaseData);
+        assertNull(finremCaseData.getOrderWrapper().getAppOrderCollection());
+        assertNull(finremCaseData.getOrderWrapper().getRespOrderCollection());
+        assertNull(finremCaseData.getOrderWrapper().getIntv1OrderCollection());
+        assertNull(finremCaseData.getOrderWrapper().getIntv2OrderCollection());
+        assertNull(finremCaseData.getOrderWrapper().getIntv3OrderCollection());
+        assertNull(finremCaseData.getOrderWrapper().getIntv4OrderCollection());
+    }
+
+    protected FinremCaseData buildFinremCaseData() {
+
+        ApprovedOrderCollection appApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderCollection repApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderCollection intv1ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderCollection intv2ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderCollection intv3ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderCollection intv4ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
+            .caseDocument(CaseDocument.builder().build()).build()).build();
+
+
+        OrderWrapper orderWrapper = OrderWrapper.builder()
+            .appOrderCollection(List.of(appApprovedOrderCollection))
+            .respOrderCollection(List.of(repApprovedOrderCollection))
+            .intv1OrderCollection(List.of(intv1ApprovedOrderCollection))
+            .intv2OrderCollection(List.of(intv2ApprovedOrderCollection))
+            .intv3OrderCollection(List.of(intv3ApprovedOrderCollection))
+            .intv4OrderCollection(List.of(intv4ApprovedOrderCollection))
+            .build();
+
+        return FinremCaseData.builder().orderWrapper(orderWrapper).build();
+
+    }
+}
