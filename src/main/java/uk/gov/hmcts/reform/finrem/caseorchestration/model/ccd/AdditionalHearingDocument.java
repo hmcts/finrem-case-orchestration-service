e78b67aeb1ce8ac53748a2b53ab82681111a3c73
srochani <sunil.rochani@hotmail.com>
2023-12-07 11:33:02 +0000

    DFR-1897: Contested - Upload order event should send notifications if a hearing is listed

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/AdditionalHearingDocument.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/AdditionalHearingDocument.java
@@ -7,7 +7,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
