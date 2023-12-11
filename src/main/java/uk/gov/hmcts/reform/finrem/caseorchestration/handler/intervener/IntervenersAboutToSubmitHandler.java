d5607e5dde5b46f572524180c365a2bc141ce0a3
AbuSalam22 <abu.sufian.salam@hotmail.com>
2023-12-11 09:17:01 +0000

    DFR-2559: Tidy up handler logs fix

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/intervener/IntervenersAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/intervener/IntervenersAboutToSubmitHandler.java
@@ -63,7 +63,7 @@ public class IntervenersAboutToSubmitHandler extends FinremCallbackHandler {
             case DEL_INTERVENER_TWO_CODE -> service.removeIntervenerDetails(caseData.getIntervenerTwoWrapper(), errors, caseData, caseId);
             case DEL_INTERVENER_THREE_CODE -> service.removeIntervenerDetails(caseData.getIntervenerThreeWrapper(), errors, caseData, caseId);
             case DEL_INTERVENER_FOUR_CODE -> service.removeIntervenerDetails(caseData.getIntervenerFourWrapper(), errors, caseData, caseId);
-            default -> throw new IllegalArgumentException("Invalid option received for Case ID: " + caseId);
+            default -> throw new IllegalArgumentException("Invalid option received for case " + caseId);
         }
 
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
