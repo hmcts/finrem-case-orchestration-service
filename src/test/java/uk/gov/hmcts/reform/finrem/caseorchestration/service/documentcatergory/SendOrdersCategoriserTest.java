0362ac43288a1f914bafee15a8c8f43767ac1c00
srochani <sunil.rochani@hotmail.com>
2023-12-12 16:30:34 +0000

    DFR-2461: send order event

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcatergory/SendOrdersCategoriserTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcatergory/SendOrdersCategoriserTest.java
@@ -7,9 +7,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.BaseHandlerTestSetup;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApproveOrder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApproveOrdersHolder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApprovedOrder;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApprovedOrderCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ApprovedOrderConsolidateCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConsentInContestedApprovedOrder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConsentInContestedApprovedOrderCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ConsentOrderWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.OrderWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentCategory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
@@ -39,17 +45,44 @@ class SendOrdersCategoriserTest extends BaseHandlerTestSetup {
         sendOrdersCategoriser.categorise(finremCaseData);
         OrderWrapper orderWrapper = finremCaseData.getOrderWrapper();
         assertEquals(DocumentCategory.APPLICANT_DOCUMENTS_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getAppOrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getAppOrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
         assertEquals(DocumentCategory.RESPONDENT_DOCUMENTS_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getRespOrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getRespOrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
         assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_1_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getIntv1OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getIntv1OrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
         assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_2_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getIntv2OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getIntv2OrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
         assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_3_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getIntv3OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getIntv3OrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
         assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_4_SEND_ORDERS.getDocumentCategoryId(),
-            orderWrapper.getIntv4OrderCollection().get(0).getValue().getCaseDocument().getCategoryId());
+            orderWrapper.getIntv4OrderCollections().get(0).getValue().getApproveOrders()
+                .get(0).getValue().getCaseDocument().getCategoryId());
+
+        ConsentOrderWrapper consentOrderWrapper = finremCaseData.getConsentOrderWrapper();
+        assertEquals(DocumentCategory.APPLICANT_DOCUMENTS_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getAppConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+        assertEquals(DocumentCategory.RESPONDENT_DOCUMENTS_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getRespConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+        assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_1_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getIntv1ConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+        assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_2_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getIntv2ConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+        assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_3_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getIntv3ConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+        assertEquals(DocumentCategory.INTERVENER_DOCUMENTS_INTERVENER_4_SEND_ORDERS.getDocumentCategoryId(),
+            consentOrderWrapper.getIntv4ConsentApprovedOrders().get(0).getApprovedOrder().getConsentOrder()
+                .getCategoryId());
+
     }
 
     @Test
@@ -57,40 +90,98 @@ class SendOrdersCategoriserTest extends BaseHandlerTestSetup {
         FinremCaseData finremCaseData = FinremCaseData.builder().orderWrapper(OrderWrapper.builder()
             .build()).build();
         sendOrdersCategoriser.categorise(finremCaseData);
-        assertNull(finremCaseData.getOrderWrapper().getAppOrderCollection());
-        assertNull(finremCaseData.getOrderWrapper().getRespOrderCollection());
-        assertNull(finremCaseData.getOrderWrapper().getIntv1OrderCollection());
-        assertNull(finremCaseData.getOrderWrapper().getIntv2OrderCollection());
-        assertNull(finremCaseData.getOrderWrapper().getIntv3OrderCollection());
-        assertNull(finremCaseData.getOrderWrapper().getIntv4OrderCollection());
+        assertNull(finremCaseData.getOrdersSentToPartiesCollection());
+
+        assertNull(finremCaseData.getOrderWrapper().getAppOrderCollections());
+        assertNull(finremCaseData.getOrderWrapper().getRespOrderCollections());
+        assertNull(finremCaseData.getOrderWrapper().getIntv1OrderCollections());
+        assertNull(finremCaseData.getOrderWrapper().getIntv2OrderCollections());
+        assertNull(finremCaseData.getOrderWrapper().getIntv3OrderCollections());
+        assertNull(finremCaseData.getOrderWrapper().getIntv4OrderCollections());
+
+        assertNull(finremCaseData.getConsentOrderWrapper().getAppConsentApprovedOrders());
+        assertNull(finremCaseData.getConsentOrderWrapper().getRespConsentApprovedOrders());
+        assertNull(finremCaseData.getConsentOrderWrapper().getIntv1ConsentApprovedOrders());
+        assertNull(finremCaseData.getConsentOrderWrapper().getIntv2ConsentApprovedOrders());
+        assertNull(finremCaseData.getConsentOrderWrapper().getIntv3ConsentApprovedOrders());
+        assertNull(finremCaseData.getConsentOrderWrapper().getIntv4ConsentApprovedOrders());
+
+
     }
 
     protected FinremCaseData buildFinremCaseData() {
 
-        ApprovedOrderCollection appApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
-        ApprovedOrderCollection repApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
-        ApprovedOrderCollection intv1ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
-        ApprovedOrderCollection intv2ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
-        ApprovedOrderCollection intv3ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
-        ApprovedOrderCollection intv4ApprovedOrderCollection = ApprovedOrderCollection.builder().value(ApproveOrder.builder()
-            .caseDocument(CaseDocument.builder().build()).build()).build();
+        ApprovedOrderConsolidateCollection appApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
+        ApprovedOrderConsolidateCollection repApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
+        ApprovedOrderConsolidateCollection intv1ApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
+        ApprovedOrderConsolidateCollection intv2ApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
+        ApprovedOrderConsolidateCollection intv3ApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
+        ApprovedOrderConsolidateCollection intv4ApprovedOrderConsolidateCollection = ApprovedOrderConsolidateCollection.builder()
+            .value(ApproveOrdersHolder.builder().approveOrders(List.of(ApprovedOrderCollection.builder()
+                    .value(ApproveOrder.builder().caseDocument(CaseDocument.builder().build()).build()).build()))
+                .build()).build();
 
 
         OrderWrapper orderWrapper = OrderWrapper.builder()
-            .appOrderCollection(List.of(appApprovedOrderCollection))
-            .respOrderCollection(List.of(repApprovedOrderCollection))
-            .intv1OrderCollection(List.of(intv1ApprovedOrderCollection))
-            .intv2OrderCollection(List.of(intv2ApprovedOrderCollection))
-            .intv3OrderCollection(List.of(intv3ApprovedOrderCollection))
-            .intv4OrderCollection(List.of(intv4ApprovedOrderCollection))
+            .appOrderCollections(List.of(appApprovedOrderConsolidateCollection))
+            .respOrderCollections(List.of(repApprovedOrderConsolidateCollection))
+            .intv1OrderCollections(List.of(intv1ApprovedOrderConsolidateCollection))
+            .intv2OrderCollections(List.of(intv2ApprovedOrderConsolidateCollection))
+            .intv3OrderCollections(List.of(intv3ApprovedOrderConsolidateCollection))
+            .intv4OrderCollections(List.of(intv4ApprovedOrderConsolidateCollection))
             .build();
 
-        return FinremCaseData.builder().orderWrapper(orderWrapper).build();
+        ConsentInContestedApprovedOrderCollection appConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+
+        ConsentInContestedApprovedOrderCollection repsConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+        ConsentInContestedApprovedOrderCollection intv1ConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+        ConsentInContestedApprovedOrderCollection intv2ConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+        ConsentInContestedApprovedOrderCollection intv3ConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+        ConsentInContestedApprovedOrderCollection intv4ConsentOrderCollection = ConsentInContestedApprovedOrderCollection
+            .builder().approvedOrder(ConsentInContestedApprovedOrder.builder()
+                .consentOrder(CaseDocument.builder().build()).build())
+            .build();
+
+        ConsentOrderWrapper consentOrderWrapper = ConsentOrderWrapper.builder()
+            .appConsentApprovedOrders(List.of(appConsentOrderCollection))
+            .respConsentApprovedOrders(List.of(repsConsentOrderCollection))
+            .intv1ConsentApprovedOrders(List.of(intv1ConsentOrderCollection))
+            .intv2ConsentApprovedOrders(List.of(intv2ConsentOrderCollection))
+            .intv3ConsentApprovedOrders(List.of(intv3ConsentOrderCollection))
+            .intv4ConsentApprovedOrders(List.of(intv4ConsentOrderCollection))
+            .build();
+
+        return FinremCaseData.builder().orderWrapper(orderWrapper).consentOrderWrapper(consentOrderWrapper).build();
 
     }
 }
