9bae4693ad27aa94cb1fbb7dfc47f93e75693651
Mike Lemos <gobii@me.com>
2024-01-23 14:04:47 +0000

    DFR-2636 fixing interim hearing

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/InterimHearingContestedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/InterimHearingContestedAboutToSubmitHandler.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/InterimHearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/InterimHearingServiceTest.java
@@ -36,7 +36,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
