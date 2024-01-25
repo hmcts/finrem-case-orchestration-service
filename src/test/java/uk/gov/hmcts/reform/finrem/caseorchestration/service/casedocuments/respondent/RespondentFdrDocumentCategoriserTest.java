d3496270d498ea4c96f0731e85bc15452ed44e96
Ruban <ruban72@hotmail.com>
2024-01-25 15:58:40 +0000

    DFR-2284: Fix WPOD issue

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/respondent/RespondentFdrDocumentCategoriserTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.casedocuments.respondent;
+
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocumentType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.DocumentCategory;
+
+import static org.junit.Assert.assertEquals;
+
+public class RespondentFdrDocumentCategoriserTest {
+    RespondentFdrDocumentCategoriser categoriser = new RespondentFdrDocumentCategoriser();
+
+    @org.junit.Test
+    public void getDocumentCategory() {
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_POSITION_STATEMENTS,
+            categoriser.getDocumentCategory(CaseDocumentType.POSITION_STATEMENT));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_ES1,
+            categoriser.getDocumentCategory(CaseDocumentType.ES1));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_ES2,
+            categoriser.getDocumentCategory(CaseDocumentType.ES2));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_WITHOUT_PREJUDICE_OFFERS,
+            categoriser.getDocumentCategory(CaseDocumentType.WITHOUT_PREJUDICE_OFFERS));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_CHRONOLOGY,
+            categoriser.getDocumentCategory(CaseDocumentType.CHRONOLOGY));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_QUESTIONNAIRES,
+            categoriser.getDocumentCategory(CaseDocumentType.QUESTIONNAIRE));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_FAMILY_HOME_VALUATION,
+            categoriser.getDocumentCategory(CaseDocumentType.FAMILY_HOME_VALUATION));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_PRE_HEARING_DRAFT_ORDER,
+            categoriser.getDocumentCategory(CaseDocumentType.PRE_HEARING_DRAFT_ORDER));
+
+        assertEquals(DocumentCategory.FDR_DOCUMENTS_AND_FDR_BUNDLE_RESPONDENT_SKELETON_ARGUMENT,
+            categoriser.getDocumentCategory(CaseDocumentType.SKELETON_ARGUMENT));
+
+    }
+
+}
