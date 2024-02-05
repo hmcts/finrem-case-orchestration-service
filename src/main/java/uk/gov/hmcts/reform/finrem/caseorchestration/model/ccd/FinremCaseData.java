b4ac17edd0d4c752678a8ce0309d9e6e812d018f
Mike Lemos <gobii@me.com>
2024-02-05 10:39:49 +0000

    DFR-1688 estimated asset to single select

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
@@ -205,7 +205,7 @@ public class FinremCaseData {
     private List<FastTrackReason> fastTrackDecisionReason;
     private Complexity addToComplexityListOfCourts;
     private List<EstimatedAsset> estimatedAssetsChecklist;
-    private EstimatedAssetV2 estimatedAssetsChecklist2;
+    private EstimatedAssetV2 estimatedAssetsChecklistV2;
     private String netValueOfHome;
     private List<PotentialAllegation> potentialAllegationChecklist;
     private String detailPotentialAllegation;
