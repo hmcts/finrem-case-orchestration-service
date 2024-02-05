f41adedf793028e2e4fca959a9b0472972e05344
srochani <sunil.rochani@hotmail.com>
2024-02-05 16:01:44 +0000

    DFR-2653: FR Consent- Unable to create listing for hearing

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/CourtListUpdateTask.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/CourtListUpdateTask.java
@@ -1,17 +1,25 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ConsentedHearingHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.CourtDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConsentedHearingDataWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CcdService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.SystemUserService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.csv.CaseReferenceKeyValue;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.csv.CaseReferenceKeyValueCsvLoader;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.LIST_FOR_HEARING_COLLECTION_CONSENTED;
 
 @Component
 @Slf4j
@@ -20,13 +28,17 @@ public class CourtListUpdateTask extends SpecializedBaseTask {
     @Value("${cron.courtListUpdate.enabled:false}")
     private boolean isCourtListUpdateTaskEnabled;
 
+    private ConsentedHearingHelper consentedHearingHelper;
+
     @Autowired
     protected CourtListUpdateTask(CaseReferenceKeyValueCsvLoader csvLoader,
                                   CcdService ccdService,
                                   SystemUserService systemUserService,
                                   FinremCaseDetailsMapper finremCaseDetailsMapper,
-                                  CourtDetailsMapper courtDetailsMapper) {
+                                  CourtDetailsMapper courtDetailsMapper,
+                                  ConsentedHearingHelper consentedHearingHelper) {
         super(csvLoader, ccdService, systemUserService, finremCaseDetailsMapper);
+        this.consentedHearingHelper = consentedHearingHelper;
     }
 
     @Override
@@ -46,23 +58,31 @@ public class CourtListUpdateTask extends SpecializedBaseTask {
 
     @Override
     protected CaseType getCaseType() {
-        return CaseType.CONTESTED;
+        return CaseType.CONSENTED;
     }
 
     @Override
     protected String getSummary() {
-        return "Update Court list DFR-2488";
+        return "Update Court list DFR-2653";
     }
 
     @Override
     protected void executeTask(CaseDetails caseDetails, CaseReferenceKeyValue caseReferenceKeyValue) {
-        String frcValue = (String) caseDetails.getData().get(caseReferenceKeyValue.getPreviousFRCKey());
-        String courtValue = (String) caseDetails.getData().get(caseReferenceKeyValue.getPreviousCourtListKey());
+        List<ConsentedHearingDataWrapper> listOfHearings = consentedHearingHelper.getHearings(caseDetails.getData());
 
-        if (frcValue != null && courtValue != null) {
-            log.info("Setting null values court and frc for case {}", caseDetails.getId());
-            caseDetails.getData().put(caseReferenceKeyValue.getPreviousFRCKey(), null);
-            caseDetails.getData().put(caseReferenceKeyValue.getPreviousCourtListKey(), null);
+        ConsentedHearingDataWrapper hearing = listOfHearings.stream().filter(ch -> caseReferenceKeyValue
+                .getPreviousCourtListKey().equals(ch.getId())).findFirst().orElse(null);
+
+        if (hearing != null) {
+            log.info("Found matching hearing id {} for case id ()", hearing.getId(), caseDetails.getId());
+            try {
+                Field field = hearing.getValue().getClass().getDeclaredField(caseReferenceKeyValue.getPreviousFRCKey());
+                FieldUtils.writeField(hearing.getValue(), field.getName(), null, true);
+                caseDetails.getData().put(LIST_FOR_HEARING_COLLECTION_CONSENTED, listOfHearings);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                log.error("No field found error", e.getMessage());
+                e.printStackTrace();
+            }
         }
 
     }
