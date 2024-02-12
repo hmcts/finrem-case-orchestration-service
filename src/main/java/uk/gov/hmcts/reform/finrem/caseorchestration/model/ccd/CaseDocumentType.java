e55a80133c6befbaad68fae3183b31a76599750b
srochani <sunil.rochani@hotmail.com>
2024-02-12 12:32:26 +0000

    DFR-2616 Remove redundant Events and Drop Down Options (#1522)
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options
    
    * DFR-2616 Remove redundant Events and Drop Down Options

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseDocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseDocumentType.java
@@ -9,9 +9,7 @@ import java.util.Arrays;
 public enum CaseDocumentType {
     STATEMENT_OF_ISSUES("Statement of Issues"),
     CHRONOLOGY("Chronology"),
-    FORM_B("Form B"),
     APPLICANT_FORM_E("Applicant - Form E"),
-    FORM_F("Form F"),
     FORM_G("Form G"),
     FORM_H("Form H"),
     LETTER_FROM_APPLICANT("Letter from Applicant"),
@@ -24,7 +22,6 @@ public enum CaseDocumentType {
     EXPERT_EVIDENCE("Expert Evidence"),
     STATEMENT_AFFIDAVIT("Statement/Affidavit"),
     WITNESS_STATEMENT_AFFIDAVIT("Witness Statement/Affidavit"),
-    CARE_PLAN("Care Plan"),
     OFFERS("Offers"),
     TRIAL_BUNDLE("Trial Bundle"),
     CONDITIONAL_ORDER("Conditional order"),
