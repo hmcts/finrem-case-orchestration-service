0033d77d5be28b6be608be1bf9f433306f92ca3f
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2024-03-12 16:09:15 +0000

    DFR-2821-Update logging to work out issue

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignCaseAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignCaseAccessService.java
@@ -91,8 +91,6 @@ public class AssignCaseAccessService {
     public AboutToStartOrSubmitCallbackResponse applyDecision(String authToken, CaseDetails caseDetails) {
         if (serviceAuthTokenGenerator.generate() != null) {
             log.info("{} serviceAuth {}", caseDetails.getId(), serviceAuthTokenGenerator.generate());
-        } else {
-            log.info("{} serviceAuth null", caseDetails.getId());
         }
         if (DecisionRequest.decisionRequest(caseDetails) != null) {
             log.info("{} decisionRequest {}", caseDetails.getId(), DecisionRequest.decisionRequest(caseDetails));
