2d8824cd18197160bc23d23b63dabd2ccb576b30
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-03-14 11:13:21 +0000

    Added CaseSubmissionPbaValidateMidEventHandler class

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/casesubmission/CaseSubmissionPbaValidateMidEventHandler.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.casesubmission;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+
+@Service
+@Slf4j
+public class CaseSubmissionPbaValidateMidEventHandler extends FinremCallbackHandler {
+
+}
