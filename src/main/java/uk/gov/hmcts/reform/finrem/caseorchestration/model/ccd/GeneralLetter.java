60a858b9001ae1b6cbf3abfe4d8aabaf29d57750
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2024-03-19 10:48:39 +0000

    DFR-2248-Update general letter consented side

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/GeneralLetter.java
@@ -19,6 +19,8 @@ import java.util.List;
 public class GeneralLetter {
     @JsonProperty("generatedLetter")
     private CaseDocument generatedLetter;
+    @JsonProperty("generalLetterUploadedDocument")
+    private CaseDocument generalLetterUploadedDocument;
     @JsonProperty("generalLetterUploadedDocuments")
     private List<DocumentCollection> generalLetterUploadedDocuments;
 }
