f8675572be3d937b1585a8b8194c1b2baa16975b
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-03-19 12:24:34 +0000

    Test for handle method.

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/casesubmission/CaseSubmissionPbaValidateMidEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/casesubmission/CaseSubmissionPbaValidateMidEventHandlerTest.java
@@ -4,14 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.PBAValidationService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo.YES;
 
 @ExtendWith(MockitoExtension.class)
 class CaseSubmissionPbaValidateMidEventHandlerTest {
@@ -27,6 +36,30 @@ class CaseSubmissionPbaValidateMidEventHandlerTest {
         assertTrue(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.APPLICATION_PAYMENT_SUBMISSION));
         assertTrue(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.APPLICATION_PAYMENT_SUBMISSION));
         assertFalse(handler.canHandle(CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.APPLICATION_PAYMENT_SUBMISSION));
+    }
+
+    @Test
+    void testHandle() {
+        // Mock data for caseDetails
+        FinremCaseDetails caseDetails = new FinremCaseDetails();
+        FinremCaseData caseData = new FinremCaseData();
+        caseData.setHelpWithFeesQuestion(YES);
+        caseData.setPbaNumber("ValidPbaNumber");
+        caseDetails.setData(caseData);
+
+        // Mock behavior
+        Mockito.lenient().when(pbaValidationService.isValidPBA(any(), eq("ValidPbaNumber"))).thenReturn(true);
+
+        // Execute the method under test
+        FinremCallbackRequest request = FinremCallbackRequest.builder()
+            .caseDetails(caseDetails)
+            .build();
+        var response = handler.handle(request, "userAuthorisation");
+
+        // Verify the response
+        assertThat(response).isNotNull();
+        assertEquals(0, response.getErrors().size());
+        assertEquals(caseData, response.getData());
     }
 
 }
