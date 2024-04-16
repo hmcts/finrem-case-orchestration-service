3b462bb34c9adfd1142b51c6cf0b9e7c52808980
jamiephillipshmcts <123951690+jamiephillipshmcts@users.noreply.github.com>
2024-04-16 17:29:19 +0100

    Update UploadCaseDocument.java

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/UploadCaseDocument.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/UploadCaseDocument.java
@@ -35,8 +35,6 @@ public class UploadCaseDocument {
     private String exceptionRecordReference;
     private YesOrNo selectForUpdate;
 
-    private YesOrNo selectForUpdate;
-
     public static UploadCaseDocument from(ScannedDocumentCollection scannedDocumentCollection) {
         ScannedDocument scannedDocument = scannedDocumentCollection.getValue();
         return UploadCaseDocument.builder()
