8738f8bb6034fd1d89ec53f28db26e3bb3cd6549
Adam <adam.lee2@justice.gov.uk>
2024-05-31 13:48:46 +0100

    Remove unused import

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/mapper/FinremNotificationRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/mapper/FinremNotificationRequestMapperTest.java
@@ -27,7 +27,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_DIVORCE_CASE_NUMBER;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_EMAIL;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_NAME;
