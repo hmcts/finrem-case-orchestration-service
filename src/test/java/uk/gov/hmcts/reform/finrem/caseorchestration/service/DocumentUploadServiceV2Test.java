9227760f4a5e7ed7df5fdb0d7ec2d3ec8c07581f
Ashley Wong <ashley.wong@justice.gov.uk>
2024-06-13 13:26:48 +0100

    decoupling the test case

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentUploadServiceV2Test.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentUploadServiceV2Test.java
@@ -3,18 +3,23 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.BaseServiceTest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocumentCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadGeneralDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadGeneralDocumentCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.util.TestResource;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 class DocumentUploadServiceV2Test extends BaseServiceTest {
@@ -22,38 +27,51 @@ class DocumentUploadServiceV2Test extends BaseServiceTest {
     @Autowired
     private DocumentUploadServiceV2 documentUploadService;
 
-    @Test
-    void givenContestedCaseWithExistingGeneralDocument_whenNewGeneralDocumentUploaded_thenReturnNewGeneralDocument() {
+    private static Stream<Arguments> provideDocumentModifier() {
         final List<UploadGeneralDocumentCollection> existingGeneralDocument = List.of(
             UploadGeneralDocumentCollection.builder()
                 .value(UploadGeneralDocument.builder()
-                    .documentLink(buildCaseDocument("url1", "binaryUrl1", "filename1"))
+                    .documentLink(TestResource.buildCaseDocument("url1", "binaryUrl1", "filename1"))
                     .build())
                 .build()
         );
 
-        FinremCaseData caseDataBefore = FinremCaseData.builder()
-            .uploadGeneralDocuments(existingGeneralDocument)
-            .build();
-        FinremCaseData caseData = caseDataBefore.toBuilder()
-            .uploadGeneralDocuments(Stream.concat(existingGeneralDocument.stream(), Stream.of(
-                UploadGeneralDocumentCollection.builder()
+        return Stream.of(
+            // uloadGeneralDocuments
+            Arguments.of((Function<FinremCaseData.FinremCaseDataBuilder, FinremCaseData.FinremCaseDataBuilder>) finremCaseDataBuilder -> {
+                    finremCaseDataBuilder.uploadGeneralDocuments(existingGeneralDocument);
+                    return finremCaseDataBuilder;
+                },
+                (Function<FinremCaseData.FinremCaseDataBuilder, FinremCaseData.FinremCaseDataBuilder>) finremCaseDataBuilder -> {
+                    finremCaseDataBuilder.uploadGeneralDocuments(Stream.concat(existingGeneralDocument.stream(), Stream.of(
+                        UploadGeneralDocumentCollection.builder()
+                            .value(UploadGeneralDocument.builder()
+                                .documentLink(TestResource.buildCaseDocument("newUrl", "newBinaryUrl1", "newFilename"))
+                                .build())
+                            .build()
+                    )).toList());
+                    return finremCaseDataBuilder;
+                },
+                (Function<FinremCaseData, ?>) FinremCaseData::getUploadGeneralDocuments,
+                List.of(UploadGeneralDocumentCollection.builder()
                     .value(UploadGeneralDocument.builder()
-                        .documentLink(buildCaseDocument("newUrl", "newBinaryUrl1", "newFilename"))
+                        .documentLink(TestResource.buildCaseDocument("newUrl", "newBinaryUrl1", "newFilename"))
                         .build())
-                    .build()
-            )).toList())
-            .build();
+                    .build())
+            )
+        );
+    }
 
-        List<UploadGeneralDocumentCollection> actual = documentUploadService.getNewUploadDocuments(caseData, caseDataBefore,
-            FinremCaseData::getUploadGeneralDocuments);
-        List<UploadGeneralDocumentCollection> expected = List.of(UploadGeneralDocumentCollection.builder()
-            .value(UploadGeneralDocument.builder()
-                .documentLink(buildCaseDocument("newUrl", "newBinaryUrl1", "newFilename"))
-                .build())
-            .build());
-
-        assertEquals(expected, actual);
+    @ParameterizedTest
+    @MethodSource("provideDocumentModifier")
+    void givenCaseWithExistingDocument_whenNewDocumentUploaded_thenReturnNewDocument(
+        Function<FinremCaseData.FinremCaseDataBuilder, FinremCaseData.FinremCaseDataBuilder> caseDataBeforeModifier,
+        Function<FinremCaseData.FinremCaseDataBuilder, FinremCaseData.FinremCaseDataBuilder> caseDataModifier,
+        Function<FinremCaseData, List<CaseDocumentCollection<?>>> accessor,
+        List<CaseDocumentCollection<?>> expectedReturn) {
+        FinremCaseData caseDataBefore = caseDataBeforeModifier.apply(FinremCaseData.builder()).build();
+        FinremCaseData caseData = caseDataModifier.apply(caseDataBefore.toBuilder()).build();
+        assertEquals(expectedReturn, documentUploadService.getNewUploadDocuments(caseData, caseDataBefore, accessor));
     }
 
     @Test
