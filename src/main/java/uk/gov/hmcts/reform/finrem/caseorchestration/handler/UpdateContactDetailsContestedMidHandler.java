02e7521b999d266f4d0c2a750f37145763d44fd3
dawudgovuk <dawud.rahman@justice.gov.uk>
2024-06-28 03:22:48 +0100

    DFR-2980-Mandatory-Postcode-Field

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedMidHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedMidHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
@@ -9,13 +10,20 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 public class UpdateContactDetailsContestedMidHandler extends FinremCallbackHandler {
 
     private final InternationalPostalService postalService;
+
+    private static final String APPLICANT_POSTCODE_ERROR = "Postcode field is required for applicant address.";
+    private static final String RESPONDENT_POSTCODE_ERROR = "Postcode field is required for respondent address.";
 
     public UpdateContactDetailsContestedMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
                                                    InternationalPostalService postalService) {
@@ -37,8 +45,46 @@ public class UpdateContactDetailsContestedMidHandler extends FinremCallbackHandl
         log.info("Invoking contested event {} mid event callback for case id {}", EventType.UPDATE_CONTACT_DETAILS,
             caseDetails.getId());
 
+        List<String> errors = new ArrayList<>();
+
         FinremCaseData caseData = caseDetails.getData();
+
+        errors.addAll(postalService.validate(caseData));
+
+        validateCaseData(caseData, errors);
+
+        if (!errors.isEmpty()) {
+            return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).errors(errors).build();
+        }
+
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
-            .data(caseData).errors(postalService.validate(caseData)).build();
+            .data(caseData).errors(errors).build();
+    }
+
+    private void validateCaseData(FinremCaseData caseData, List<String> errors) {
+
+        if (caseData.isContestedApplication()) {
+            ContactDetailsWrapper wrapper = caseData.getContactDetailsWrapper();
+
+            if (caseData.isApplicantRepresentedByASolicitor()) {
+                if (wrapper.getApplicantSolicitorAddress() != null
+                    && ObjectUtils.isEmpty(wrapper.getApplicantSolicitorAddress().getPostCode()))
+                    errors.add(APPLICANT_POSTCODE_ERROR);
+            } else {
+                if (wrapper.getApplicantAddress() != null
+                    && ObjectUtils.isEmpty(wrapper.getApplicantAddress().getPostCode()))
+                    errors.add(APPLICANT_POSTCODE_ERROR);
+            }
+
+            if (caseData.isRespondentRepresentedByASolicitor()) {
+                if (wrapper.getRespondentSolicitorAddress() != null
+                    && ObjectUtils.isEmpty(wrapper.getRespondentSolicitorAddress().getPostCode()))
+                    errors.add(RESPONDENT_POSTCODE_ERROR);
+            } else {
+                if (wrapper.getApplicantAddress() != null
+                    && ObjectUtils.isEmpty(wrapper.getRespondentAddress().getPostCode()))
+                    errors.add(RESPONDENT_POSTCODE_ERROR);
+            }
+        }
     }
 }
