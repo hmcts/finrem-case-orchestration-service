88fd07b034f62eac8cc8da0d7aa469a9b2beadf3
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-07-09 13:40:41 +0100

    add postmap for /amend-application-app-sol

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/AmendApplicationContestedController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/AmendApplicationContestedController.java
@@ -1,14 +1,42 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.AUTHORIZATION_HEADER;
 
 @RestController
 @RequestMapping(value = "/case-orchestration")
 @Slf4j
 @RequiredArgsConstructor
 public class AmendApplicationContestedController extends BaseController {
+
+    @PostMapping(path = "/amend-application-app-sol", consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Add empty org policies for both parties")
+    public ResponseEntity<AboutToStartOrSubmitCallbackResponse> amendApplicationAppSolicitor(
+        @RequestBody CallbackRequest callbackRequest,
+        @RequestHeader(value = AUTHORIZATION_HEADER, required = false) final String authToken
+    ) {
+        Map<String, Object> caseData = callbackRequest.getCaseDetails().getData();
+
+        List<String> errors = new ArrayList<>();
+
+        return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder().data(caseData).errors(errors).build());
+    }
 
 }
