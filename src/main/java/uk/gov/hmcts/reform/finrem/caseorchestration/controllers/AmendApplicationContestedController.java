dc4227c0a4b6bfc6334b8813ba77ba6865838b1d
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-07-09 13:40:01 +0100

    extend basecontroller

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/AmendApplicationContestedController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/AmendApplicationContestedController.java
@@ -1,4 +1,14 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
 
-public class AmendApplicationContestedController {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/case-orchestration")
+@Slf4j
+@RequiredArgsConstructor
+public class AmendApplicationContestedController extends BaseController {
+
 }
