3cf84beb57db7a64803b2ba6348dca733994adb0
Adam <adam.lee2@justice.gov.uk>
2024-07-26 10:50:03 +0100

    Add event and AboutToStartHandler

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/documentremoval/DocumentRemovalAboutToStartHandler.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.documentremoval;
+
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+
+public class DocumentRemovalAboutToStartHandler extends FinremCallbackHandler {
+    public DocumentRemovalAboutToStartHandler(FinremCaseDetailsMapper mapper) {
+        super(mapper);
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.ABOUT_TO_START.equals(callbackType)
+            && (EventType.REMOVE_CASE_DOCUMENT.equals(eventType));
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequestWithFinremCaseDetails, String userAuthorisation) {
+        // TODO: Logic to retrieve list of documents and populate DynamicList to display on CCD, Store selection and prep for document removal
+        
+        return null;
+    }
+
+}
