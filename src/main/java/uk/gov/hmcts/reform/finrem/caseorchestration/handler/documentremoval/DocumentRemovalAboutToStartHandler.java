e548534ff37faa162887ef479fb268c690a98af8
Adam <adam.lee2@justice.gov.uk>
2024-07-26 14:53:12 +0100

    Setup handler cont.

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/documentremoval/DocumentRemovalAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/documentremoval/DocumentRemovalAboutToStartHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler.documentremoval;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
@@ -8,8 +10,12 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapp
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 
+@Slf4j
+@Service
 public class DocumentRemovalAboutToStartHandler extends FinremCallbackHandler {
+
     public DocumentRemovalAboutToStartHandler(FinremCaseDetailsMapper mapper) {
         super(mapper);
     }
@@ -21,9 +27,10 @@ public class DocumentRemovalAboutToStartHandler extends FinremCallbackHandler {
     }
 
     @Override
-    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequestWithFinremCaseDetails, String userAuthorisation) {
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest, String userAuthorisation) {
         // TODO: Logic to retrieve list of documents and populate DynamicList to display on CCD, Store selection and prep for document removal
-        
+        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+        log.info("Retieving documents to delete");
         return null;
     }
 
