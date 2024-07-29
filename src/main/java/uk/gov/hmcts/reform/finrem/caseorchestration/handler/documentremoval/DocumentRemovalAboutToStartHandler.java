9e765ec151e6ca8ea3168ea98f1006c7079bb4ad
Adam <adam.lee2@justice.gov.uk>
2024-07-29 16:04:25 +0100

    WIP: Iterate logic

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/documentremoval/DocumentRemovalAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/documentremoval/DocumentRemovalAboutToStartHandler.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler.documentremoval;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
@@ -11,6 +15,9 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 @Slf4j
 @Service
@@ -28,10 +35,42 @@ public class DocumentRemovalAboutToStartHandler extends FinremCallbackHandler {
 
     @Override
     public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest, String userAuthorisation) {
-        // TODO: Logic to retrieve list of documents and populate DynamicList to display on CCD, Store selection and prep for document removal
         FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
-        log.info("Retieving documents to delete");
-        return null;
+        FinremCaseData caseData = caseDetails.getData();
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+        try {
+            String json = ow.writeValueAsString(caseData);
+            JsonNode rootNode = objectMapper.readTree(json);
+            traverse(rootNode);
+        } catch (Exception e) {
+            log.error("Exception occurred while converting case data to JSON", e);
+        }
+
+
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).build();
+    }
+
+    public void traverse(JsonNode root) {
+
+        if (root.isObject()) {
+            Iterator<String> fieldNames = root.fieldNames();
+
+            while (fieldNames.hasNext()) {
+                String fieldName = fieldNames.next();
+                JsonNode fieldValue = root.get(fieldName);
+                traverse(fieldValue);
+            }
+        } else if (root.isArray()) {
+            ArrayNode arrayNode = (ArrayNode) root;
+            for (int i = 0; i < arrayNode.size(); i++) {
+                JsonNode arrayElement = arrayNode.get(i);
+                traverse(arrayElement);
+            }
+        } else {
+            // JsonNode root represents a single value field - do something with it.
+//   if root.get("documentUrl")
+        }
     }
 
 }
