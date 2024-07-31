6cfb3b17cecd526702f1f559c77149ea4038692e
dawudgovuk <dawud.rahman@justice.gov.uk>
2024-07-31 16:24:15 +0100

    DFR-3141-UpdateContactDetailsContested

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedSubmittedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedSubmittedHandlerTest.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.solicitorcreatecase.SolicitorCreateContestedAboutToSubmitHandler;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
@@ -22,9 +25,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdateContactDetailsContestedSubmittedHandlerTest extends BaseHandlerTestSetup {
@@ -47,6 +54,16 @@ public class UpdateContactDetailsContestedSubmittedHandlerTest extends BaseHandl
     @Mock
     private FinremCaseDetailsMapper finremCaseDetailsMapper;
 
+
+    @Before
+    public void setup() {
+        handler = new UpdateContactDetailsContestedSubmittedHandler(
+            finremCaseDetailsMapper,
+            updateRepresentationWorkflowService,
+            service);
+    }
+
+
     @Test
     void givenACcdCallbackUpdateContactDetailsContestCase_WhenCanHandleCalled_thenHandlerCanHandle() {
         assertThat(handler
@@ -56,49 +73,53 @@ public class UpdateContactDetailsContestedSubmittedHandlerTest extends BaseHandl
 
     @Test
     void shouldSuccessfullyRemoveApplicantSolicitorDetails() {
+        when(service.generateDraftContestedMiniFormA(anyString(),
+            any(FinremCaseDetails.class))).thenReturn(caseDocument());
+
         FinremCallbackRequest finremCallbackRequest = buildCaseDetailsWithPath(FIXTURES_CONTESTED_AMEND_APPLICANT_SOLICITOR_DETAILS_JSON);
+
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        assertEquals(response.getData().isApplicantRepresentedByASolicitor(), false);
+        assertEquals(false, response.getData().isApplicantRepresentedByASolicitor());
         assertNotNull(response.getData().getContactDetailsWrapper().getApplicantAddress());
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantPhone(), "89897876765");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantEmail(), "email01@email.com");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantFmName(), "Poor");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantLname(), "Guy");
-        assertEquals(response.getData().isAppAddressConfidential(), "Yes");
-        assertEquals(response.getData().isRespAddressConfidential(), "No");
-        assertEquals(response.getData().getMiniFormA().getClass(), LinkedHashMap.class);
-        assertEquals(response.getData().getAppSolicitorName(), null);
-        assertEquals(response.getData().getAppSolicitorFirm(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorReference(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorAddress(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails(), null);
+        assertEquals("89897876765", response.getData().getContactDetailsWrapper().getApplicantPhone());
+        assertEquals("email01@email.com", response.getData().getContactDetailsWrapper().getApplicantEmail());
+        assertEquals("Poor", response.getData().getContactDetailsWrapper().getApplicantFmName());
+        assertEquals("Guy", response.getData().getContactDetailsWrapper().getApplicantLname());
+        assertEquals(true, response.getData().isAppAddressConfidential());
+        assertEquals(false, response.getData().isRespAddressConfidential());
+        assertEquals(caseDocument(), response.getData().getMiniFormA());
+        assertNull(response.getData().getAppSolicitorName());
+        assertNull(response.getData().getAppSolicitorFirm());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorReference());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorAddress());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails());
         verify(service).generateContestedMiniFormA(any(), any());
     }
 
     @Test
-    void shouldSuccessfullyRemoveApplicantSolicitorDetails_respondentConfidentialAddressNotAmended() throws Exception {
+    void shouldSuccessfullyRemoveApplicantSolicitorDetails_respondentConfidentialAddressNotAmended() {
         FinremCallbackRequest finremCallbackRequest = buildCaseDetailsWithPath(FIXTURES_CONTESTED_AMEND_APPLICANT_SOLICITOR_DETAILS_RES_UNTOUCHED_JSON);
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        assertEquals(response.getData().isApplicantRepresentedByASolicitor(), "No");
+        assertEquals(false, response.getData().isApplicantRepresentedByASolicitor());
         assertNotNull(response.getData().getContactDetailsWrapper().getApplicantAddress());
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantPhone(), "89897876765");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantEmail(), "email01@email.com");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantFmName(), "Poor");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantLname(), "Guy");
-        assertEquals(response.getData().isAppAddressConfidential(), "Yes");
-        assertEquals(response.getData().isRespAddressConfidential(), null);
-        assertEquals(response.getData().getMiniFormA().getClass(), LinkedHashMap.class);
-        assertEquals(response.getData().getAppSolicitorName(), null);
-        assertEquals(response.getData().getAppSolicitorFirm(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorReference(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorAddress(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails(), null);
+        assertEquals("89897876765", response.getData().getContactDetailsWrapper().getApplicantPhone());
+        assertEquals("email01@email.com", response.getData().getContactDetailsWrapper().getApplicantEmail());
+        assertEquals("Poor", response.getData().getContactDetailsWrapper().getApplicantFmName());
+        assertEquals("Guy", response.getData().getContactDetailsWrapper().getApplicantLname());
+        assertEquals(true, response.getData().isAppAddressConfidential());
+        assertEquals(false, response.getData().isRespAddressConfidential());
+        assertEquals(LinkedHashMap.class, response.getData().getMiniFormA());
+        assertNull(response.getData().getAppSolicitorName());
+        assertNull(response.getData().getAppSolicitorFirm());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorReference());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorAddress());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails());
         verify(service).generateContestedMiniFormA(any(), any());
     }
 
@@ -106,23 +127,23 @@ public class UpdateContactDetailsContestedSubmittedHandlerTest extends BaseHandl
     void shouldSuccessfullyRemoveApplicantSolicitorDetails_applicantConfidentialAddressNotAmended() throws Exception {
         FinremCallbackRequest finremCallbackRequest = buildCaseDetailsWithPath(FIXTURES_CONTESTED_AMEND_APPLICANT_SOLICITOR_DETAILS_APP_UNTOUCHED_JSON);
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        assertEquals(response.getData().isApplicantRepresentedByASolicitor(), "No");
+        assertEquals(false, response.getData().isApplicantRepresentedByASolicitor());
         assertNotNull(response.getData().getContactDetailsWrapper().getApplicantAddress());
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantPhone(), "89897876765");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantEmail(), "email01@email.com");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantFmName(), "Poor");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantLname(), "Guy");
-        assertEquals(response.getData().isAppAddressConfidential(), null);
-        assertEquals(response.getData().isRespAddressConfidential(), "Yes");
-        assertEquals(response.getData().getMiniFormA().getClass(), LinkedHashMap.class);
-        assertEquals(response.getData().getAppSolicitorName(), null);
-        assertEquals(response.getData().getAppSolicitorFirm(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorReference(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorAddress(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails(), null);
+        assertEquals("89897876765", response.getData().getContactDetailsWrapper().getApplicantPhone());
+        assertEquals("email01@email.com", response.getData().getContactDetailsWrapper().getApplicantEmail());
+        assertEquals("Poor", response.getData().getContactDetailsWrapper().getApplicantFmName());
+        assertEquals("Guy", response.getData().getContactDetailsWrapper().getApplicantLname());
+        assertNull(response.getData().isAppAddressConfidential());
+        assertEquals(false, response.getData().isRespAddressConfidential());
+        assertEquals(LinkedHashMap.class, response.getData().getMiniFormA());
+        assertNull(response.getData().getAppSolicitorName());
+        assertNull(response.getData().getAppSolicitorFirm());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorReference());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorAddress());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails());
         verify(service).generateContestedMiniFormA(any(), any());
     }
 
@@ -130,29 +151,31 @@ public class UpdateContactDetailsContestedSubmittedHandlerTest extends BaseHandl
     void shouldSuccessfullyRemoveApplicantSolicitorDetails_bothConfidentialAddressNotAmended() throws Exception {
         FinremCallbackRequest finremCallbackRequest = buildCaseDetailsWithPath(FIXTURES_CONTESTED_AMEND_APPLICANT_SOLICITOR_DETAILS_BOTH_UNTOUCHED_JSON);
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        assertEquals(response.getData().isApplicantRepresentedByASolicitor(), "No");
+        assertEquals(false, response.getData().isApplicantRepresentedByASolicitor());
         assertNotNull(response.getData().getContactDetailsWrapper().getApplicantAddress());
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantPhone(), "89897876765");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantEmail(), "email01@email.com");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantFmName(), "Poor");
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantLname(), "Guy");
-        assertEquals(response.getData().isAppAddressConfidential(), null);
-        assertEquals(response.getData().isRespAddressConfidential(), null);
-        assertEquals(response.getData().getAppSolicitorName(), null);
-        assertEquals(response.getData().getAppSolicitorFirm(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorReference(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getSolicitorAddress(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber(), null);
-        assertEquals(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails(), null);
-        verify(service, never()).generateContestedMiniFormA(any(), any());
+        assertEquals("89897876765", response.getData().getContactDetailsWrapper().getApplicantPhone());
+        assertEquals("email01@email.com", response.getData().getContactDetailsWrapper().getApplicantEmail());
+        assertEquals("Poor", response.getData().getContactDetailsWrapper().getApplicantFmName());
+        assertEquals("Guy", response.getData().getContactDetailsWrapper().getApplicantLname());
+        assertNull(response.getData().isAppAddressConfidential());
+        assertEquals(false, response.getData().isRespAddressConfidential());
+        assertEquals(LinkedHashMap.class, response.getData().getMiniFormA());
+        assertNull(response.getData().getAppSolicitorName());
+        assertNull(response.getData().getAppSolicitorFirm());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorReference());
+        assertNull(response.getData().getContactDetailsWrapper().getSolicitorAddress());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorPhone());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorEmail());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorDxNumber());
+        assertNull(response.getData().getContactDetailsWrapper().getApplicantSolicitorConsentForEmails());
+        verify(service).generateContestedMiniFormA(any(), any());
     }
 
     private FinremCallbackRequest buildCaseDetailsWithPath(String path) {
         try (InputStream resourceAsStream = getClass().getResourceAsStream(path)) {
             FinremCaseDetails caseDetails =
                 objectMapper.readValue(resourceAsStream, FinremCallbackRequest.class).getCaseDetails();
+
             return FinremCallbackRequest
                 .builder()
                 .eventType(EventType.UPDATE_CONTACT_DETAILS)
