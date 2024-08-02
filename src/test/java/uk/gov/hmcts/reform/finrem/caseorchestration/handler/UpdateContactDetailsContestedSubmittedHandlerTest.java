e37683628ae0cff960090f8095792ca38a1eb349
dawudgovuk <dawud.rahman@justice.gov.uk>
2024-08-03 00:14:26 +0100

    DFR-3141-UpdateContactDetailsContested

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedSubmittedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/UpdateContactDetailsContestedSubmittedHandlerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
