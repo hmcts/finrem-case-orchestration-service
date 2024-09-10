106fdd1338cdbfdab433e8900fae1bab2a75776f
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-09-10 13:46:37 +0100

    Create common base class for update contact details event

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/UpdateContactDetailsController.java
@@ -0,0 +1,94 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.controllers;
+
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.NO_VALUE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.YES_VALUE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_ORGANISATION_POLICY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APPLICANT_REPRESENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_RESPONDENT_REPRESENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONTESTED_RESPONDENT_REPRESENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.INCLUDES_REPRESENTATION_CHANGE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.NOC_PARTY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT_ADDRESS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT_ORGANISATION_POLICY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT_PHONE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESPONDENT_RESIDE_OUTSIDE_UK;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_DX_NUMBER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_FIRM;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_NOTIFICATIONS_EMAIL_CONSENT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_PHONE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseDataService.nullToEmpty;
+
+public class UpdateContactDetailsController extends BaseController {
+
+    void persistOrgPolicies(Map<String, Object> caseData, CaseDetails originalDetails) {
+        caseData.put(APPLICANT_ORGANISATION_POLICY, originalDetails.getData().get(APPLICANT_ORGANISATION_POLICY));
+        caseData.put(RESPONDENT_ORGANISATION_POLICY, originalDetails.getData().get(RESPONDENT_ORGANISATION_POLICY));
+    }
+
+    boolean isIncludesRepresentationChange(Map<String, Object> caseData) {
+        return YES_VALUE.equals(caseData.get(INCLUDES_REPRESENTATION_CHANGE));
+    }
+
+
+    void handleApplicantRepresentationChange(Map<String, Object> caseData) {
+        String nocParty = (String) caseData.get(NOC_PARTY);
+        if (APPLICANT.equalsIgnoreCase(nocParty)) {
+            removeApplicantSolicitorDetails(caseData);
+        }
+    }
+
+    void handleRespondentRepresentationChange(CaseDetails caseDetails) {
+        Map<String, Object> caseData = caseDetails.getData();
+        String nocParty = (String) caseData.get(NOC_PARTY);
+        if (RESPONDENT.equalsIgnoreCase(nocParty)) {
+            removeRespondentDetails(caseData, caseDetails.getCaseTypeId());
+        }
+    }
+
+    void removeApplicantSolicitorDetails(Map<String, Object> caseData) {
+        String applicantRepresented = nullToEmpty(caseData.get(APPLICANT_REPRESENTED));
+        if (applicantRepresented.equals(NO_VALUE)) {
+            caseData.remove("applicantSolicitorName");
+            caseData.remove("applicantSolicitorFirm");
+            caseData.remove("applicantSolicitorAddress");
+            caseData.remove("applicantSolicitorPhone");
+            caseData.remove("applicantSolicitorEmail");
+            caseData.remove("applicantSolicitorDXnumber");
+            caseData.remove("applicantSolicitorConsentForEmails");
+            caseData.remove(APPLICANT_ORGANISATION_POLICY);
+        }
+    }
+
+    void removeRespondentDetails(Map<String, Object> caseData, String caseTypeId) {
+        boolean isContested = caseTypeId.equalsIgnoreCase(CaseType.CONTESTED.getCcdType());
+        String respondentRepresented = isContested
+            ? (String) caseData.get(CONTESTED_RESPONDENT_REPRESENTED)
+            : (String) caseData.get(CONSENTED_RESPONDENT_REPRESENTED);
+        if (respondentRepresented.equals(YES_VALUE)) {
+            caseData.remove(RESPONDENT_ADDRESS);
+            caseData.remove(RESPONDENT_PHONE);
+            caseData.remove(RESPONDENT_EMAIL);
+            caseData.put(RESPONDENT_RESIDE_OUTSIDE_UK, NO_VALUE);
+        } else {
+            caseData.remove(RESP_SOLICITOR_NAME);
+            caseData.remove(RESP_SOLICITOR_FIRM);
+            caseData.remove(RESP_SOLICITOR_ADDRESS);
+            caseData.remove(RESP_SOLICITOR_PHONE);
+            caseData.remove(RESP_SOLICITOR_EMAIL);
+            caseData.remove(RESP_SOLICITOR_DX_NUMBER);
+            caseData.remove(RESP_SOLICITOR_NOTIFICATIONS_EMAIL_CONSENT);
+            caseData.remove(RESPONDENT_ORGANISATION_POLICY);
+        }
+    }
+}
