1e62496e6bbbc5de1a1000c285374f651785bc8f
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-10-01 12:12:54 +0100

    checkstyle fix again

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/SuggestedDraftOrder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/SuggestedDraftOrder.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-import lombok.Builder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
