c2350bc7884a9b2c156a57025cbe43f15ba57a09
Ashley Wong <ashley.wong@justice.gov.uk>
2024-10-01 15:13:48 +0100

    Fix TestConstants

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/TestData.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/TestData.java
@@ -8,9 +8,17 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NottinghamCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Region;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.RegionMidlandsFrc;
 
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_DIVORCE_CASE_NUMBER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_JUDGE_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_REFERENCE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.NOTTINGHAM;
 
-public abstract class TestData implements TestConstants {
+public abstract class TestData {
 
     public static FinremCaseDetails getContestedFinremCaseDetails() {
         return getContestedFinremCaseDetails(getDefaultContestedFinremCaseData());
