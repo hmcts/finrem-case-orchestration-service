2b9c54d0f813748172893e108ee03a73833761c5
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-10-01 12:59:05 +0100

    Dfr 3278 initial wrapper setup (#1891)
    * draftOrdersWrapper and collections
    
    * Add collection values.
    
    * Remove builder for now
    
    * implement HasCaseDocument on the collections
    
    * Remove noArgsConstructor annotation
    
    * checkstyle fix
    
    * checkstyle fix again

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/SuggestedDraftOrder.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode
+public class SuggestedDraftOrder implements HasCaseDocument {
+//fields here
+}
