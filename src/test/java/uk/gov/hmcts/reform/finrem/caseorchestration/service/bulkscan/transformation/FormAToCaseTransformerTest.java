57982849defc26c46d5ef91f828258d22aee9687
Adam <adam.lee2@justice.gov.uk>
2024-10-02 11:22:22 +0100

    Update existing test to account for null instead of empty strings

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/bulkscan/transformation/FormAToCaseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/bulkscan/transformation/FormAToCaseTransformerTest.java
@@ -372,7 +372,7 @@ public class FormAToCaseTransformerTest {
         assertThat(transformedCaseData, allOf(
             hasEntry(BULK_SCAN_CASE_REFERENCE, TEST_CASE_ID),
             hasEntry(PAPER_APPLICATION, YES_VALUE),
-            hasEntry(RESP_SOLICITOR_NAME, ""),
+            hasEntry(RESP_SOLICITOR_NAME, null),
             hasEntry(CONSENTED_RESPONDENT_REPRESENTED, NO_VALUE)
         ));
     }
