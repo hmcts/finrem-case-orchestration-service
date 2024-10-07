09a635f17e03d90b0279aa63c69584b744783df8
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-10-07 10:37:49 +0100

    setup for categoriser

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SuggestedDraftOrdersAboutToSubmitHandler.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.documentcatergory.SuggestedDraftOrdersCategoriser;
+
+@Slf4j
+@Service
+public class SuggestedDraftOrdersAboutToSubmitHandler extends FinremCallbackHandler {
+
+    private final SuggestedDraftOrdersCategoriser suggestedDraftOrdersCategoriser;
+
+
+    public SuggestedDraftOrdersAboutToSubmitHandler(FinremCaseDetailsMapper finremCaseDetailsMapper, SuggestedDraftOrdersCategoriser suggestedDraftOrdersCategoriser) {
+        super(finremCaseDetailsMapper);
+        this.suggestedDraftOrdersCategoriser = suggestedDraftOrdersCategoriser;
+
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.ABOUT_TO_SUBMIT.equals(callbackType)
+            && CaseType.CONTESTED.equals(caseType)
+            && EventType.DRAFT_ORDERS.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+        log.info("Invoking contested {} about to start callback for Case ID: {}",
+            callbackRequest.getEventType(), caseDetails.getId());
+        FinremCaseData finremCaseData = caseDetails.getData();
+
+        suggestedDraftOrdersCategoriser.categorise(finremCaseData);
+
+
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
+            .data(finremCaseData).build();
+    }
+
+}
