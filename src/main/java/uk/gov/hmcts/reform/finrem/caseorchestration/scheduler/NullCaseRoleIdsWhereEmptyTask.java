e5a2beb3045134c511bf1fb315ac4eaf972612d7
Adam <adam.lee2@justice.gov.uk>
2024-10-14 13:56:59 +0100

    Set flag for NOC bug fix batch as appropriate

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/NullCaseRoleIdsWhereEmptyTask.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/scheduler/NullCaseRoleIdsWhereEmptyTask.java
@@ -37,7 +37,7 @@ import java.util.Optional;
 public class NullCaseRoleIdsWhereEmptyTask extends BaseTask {
 
     public static final String NOC_FIX_APPLIED_FLAG_FIELD = "isNocFixAppliedFlag";
-    private static final String CASE_DATA_NOC_FIX_APPLIED__FLAG = String.format("data.%s", NOC_FIX_APPLIED_FLAG_FIELD);
+    private static final String CASE_DATA_NOC_FIX_APPLIED_FLAG = String.format("data.%s", NOC_FIX_APPLIED_FLAG_FIELD);
 
     private static final String TASK_NAME = "NullCaseRoleIdsWhereEmptyTask";
     private static final String SUMMARY = "DFR-3351";
@@ -57,7 +57,7 @@ public class NullCaseRoleIdsWhereEmptyTask extends BaseTask {
 
     @Override
     protected List<CaseReference> getCaseReferences() {
-        log.info("Getting case references for CFV migration");
+        log.info("Getting case references for NOC migration");
         String searchQuery = getSearchQuery();
 
         String systemUserToken = getSystemUserToken();
@@ -91,37 +91,30 @@ public class NullCaseRoleIdsWhereEmptyTask extends BaseTask {
         return SUMMARY;
     }
 
-    // Needs tweaking.  Acts on caseRoleId = null - should skip those.  Easy refactor.
     @Override
     protected void executeTask(FinremCaseDetails finremCaseDetails) {
         FinremCaseData caseData = finremCaseDetails.getData();
+        ChangeOrganisationRequest noc = caseData.getChangeOrganisationRequestField();
 
-        if (caseData.getChangeOrganisationRequestField() != null
-                && caseData.getChangeOrganisationRequestField().getCaseRoleId() != null
-                && caseData.getChangeOrganisationRequestField().getCaseRoleId().getValue() == null) {
-
+        if (noc != null && noc.getCaseRoleId() != null && noc.getCaseRoleId().getValue() == null) {
             log.info("Case {} will have caseRoleId set to null", finremCaseDetails.getId());
             caseData.setCcdCaseId(String.valueOf(finremCaseDetails.getId()));
             caseData.getChangeOrganisationRequestField().setCaseRoleId(null);
             caseData.setIsNocFixAppliedFlag(YesOrNo.YES);
-        }
-
-        // This optional code needs enhancement.
-        // when the caseRoleId or is null (which doesn't need attention) get value stills run so expression is true.
-        // same if the ChangeOrganisationRequest is null.
-        if (Optional.ofNullable(caseData.getChangeOrganisationRequestField())
-                .map(ChangeOrganisationRequest::getCaseRoleId)
-                .map(DynamicList::getValue)
-                .isEmpty()) {
-            log.info("gone in - optional");
+        } else {
+            log.info("Case {} not affected by caseRoleId NOC bug", finremCaseDetails.getId());
+            caseData.setIsNocFixAppliedFlag(YesOrNo.NO);
         }
     }
 
     private String getSearchQuery() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime nocBugReleaseDateTime = LocalDateTime.parse("30-07-2024 23:59", formatter);
 
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery()
-            .mustNot(QueryBuilders.existsQuery(CASE_DATA_NOC_FIX_APPLIED__FLAG))
-            .mustNot(new TermQueryBuilder("state.keyword", "close"));
+            .mustNot(QueryBuilders.existsQuery(CASE_DATA_NOC_FIX_APPLIED_FLAG))
+            .mustNot(new TermQueryBuilder("state.keyword", "close"))
+            .filter(QueryBuilders.rangeQuery("last_modified").from(nocBugReleaseDateTime));
 
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
                 .query(boolQueryBuilder);
