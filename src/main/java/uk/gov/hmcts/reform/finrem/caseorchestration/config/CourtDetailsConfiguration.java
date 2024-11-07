e15d6b685f4d097d5f402e4b2827fffcac643797
Paul Trelease <paul.trelease@justice.gov.uk>
2024-11-07 10:13:12 +0000

    Explicitly close InputStream to resolve Fortify potential memory leak issue

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/config/CourtDetailsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/config/CourtDetailsConfiguration.java
@@ -3,22 +3,39 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.config;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+@Slf4j
 @Component
 @Getter
 public class CourtDetailsConfiguration {
     private final Map<String, CourtDetails> courts;
 
     public CourtDetailsConfiguration(ObjectMapper objectMapper) throws IOException {
-        try (InputStream inputStream = CourtDetailsConfiguration.class
-            .getResourceAsStream("/json/court-details.json")) {
+        InputStream inputStream = CourtDetailsConfiguration.class
+                .getResourceAsStream("/json/court-details.json");
+        try {
             courts = objectMapper.readValue(inputStream, new TypeReference<>() {
             });
+        } finally {
+            if (inputStream != null) {
+                safeClose(inputStream);
+            }
+        }
+    }
+
+    public static void safeClose(InputStream inputStream) {
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                log.error("Failed to close inputStream in CourtDetailsConfiguration.java: {}", e.getMessage());
+            }
         }
     }
 }
