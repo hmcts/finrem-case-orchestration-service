0012a8e733cfed2e019bfc07b57dc7a9c8db67dc
Ashley Wong <ashley.wong@justice.gov.uk>
2024-11-07 17:17:27 +0000

    Add ApproveDraftOrdersMidEventHandlerTest

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/approvedraftorders/ApproveDraftOrdersMidEventHandlerTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.approvedraftorders;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
+
+@ExtendWith(MockitoExtension.class)
+class ApproveDraftOrdersMidEventHandlerTest {
+
+    @InjectMocks
+    private ApproveDraftOrdersMidEventHandler handler;
+
+    @Test
+    void canHandle() {
+        assertCanHandle(handler, CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.APPROVE_ORDERS);
+    }
+
+}
