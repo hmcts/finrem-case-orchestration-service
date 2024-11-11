994305abcc1ae57b96052a11d4cf604ae8c6b616
dawudgovuk <dawud.rahman@justice.gov.uk>
2024-11-11 09:02:30 +0000

    DFR-2689-Auto-date-pension-sharing-annex-review-points

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/ApprovedConsentOrderAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/ApprovedConsentOrderAboutToSubmitHandler.java
@@ -93,7 +93,7 @@ public class ApprovedConsentOrderAboutToSubmitHandler implements CallbackHandler
         if (Boolean.FALSE.equals(isPensionDocumentsEmpty(caseData))) {
             log.info("Pension Documents not empty for case - stamping Pension Documents and adding to approvedOrder for Case ID: {}",
                 caseId);
-            LocalDate approvalDate = getLocalDateFromMap(caseData, CONTESTED_ORDER_DIRECTION_DATE);
+            LocalDate approvalDate = getApprovalDate(caseData);
             List<PensionTypeCollection> stampedPensionDocs = consentOrderApprovedDocumentService.stampPensionDocuments(
                 documentHelper.getPensionDocuments(caseData), authToken, stampType, approvalDate, caseId);
             log.info("Generated StampedPensionDocs = {} for Case ID: {}", stampedPensionDocs, caseDetails.getId());
@@ -134,8 +134,8 @@ public class ApprovedConsentOrderAboutToSubmitHandler implements CallbackHandler
         return pensionDocumentsData.isEmpty();
     }
 
-    private static LocalDate getLocalDateFromMap(Map<String, Object> map, String key) {
-        return Optional.ofNullable(map.get(key))
+    private static LocalDate getApprovalDate(Map<String, Object> caseData) {
+        return Optional.ofNullable(caseData.get(CONTESTED_ORDER_DIRECTION_DATE))
             .filter(value -> value instanceof LocalDate || value instanceof String)
             .map(value -> {
                 if (value instanceof LocalDate) {
@@ -144,13 +144,13 @@ public class ApprovedConsentOrderAboutToSubmitHandler implements CallbackHandler
                     try {
                         return LocalDate.parse((String) value);
                     } catch (Exception e) {
-                        log.info("Invalid Approved date of order for key '" + key + "': " + e.getMessage());
+                        log.info("Invalid Approved date of order for key '" + caseData + "': " + e.getMessage());
                         return null;
                     }
                 }
             })
             .orElseGet(() -> {
-                log.info("Approved Date of Order for key '" + key + "' is null or invalid");
+                log.info("Approved Date of Order for key '" + caseData + "' is null or invalid");
                 return null;
             });
     }
