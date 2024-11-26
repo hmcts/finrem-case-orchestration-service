e95113acd1ae777ac356b253c6358844fadffcfa
Ashley Wong <ashley.wong@justice.gov.uk>
2024-11-26 18:12:27 +0000

    WIP - populating status (applying status to agreedDraftOrderCollection)

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/judgeapproval/ApproveOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/judgeapproval/ApproveOrderService.java
@@ -7,18 +7,25 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicList;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicListElement;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.agreed.AgreedDraftOrder;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeApproval;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeApprovalDocType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeDecision;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.review.DraftOrderDocReviewCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.review.DraftOrderDocumentReview;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.review.OrderStatus;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.review.PsaDocReviewCollection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.review.PsaDocumentReview;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.DraftOrdersWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.IdamService;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.String.format;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeDecision.JUDGE_NEEDS_TO_MAKE_CHANGES;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeDecision.READY_TO_BE_SEALED;
 
 @Service
 @Slf4j
@@ -27,33 +34,100 @@ public class ApproveOrderService {
 
     private final IdamService idamService;
 
+    /**
+     * Populates judge decisions for draft orders by iterating through a predefined range of indexes (1 to 5),
+     * resolving judge approvals, and updating the corresponding draft orders and PSA documents.
+     *
+     * <p>For each index, the method:
+     * <ol>
+     *   <li>Resolves the {@link JudgeApproval} object for the specified index.</li>
+     *   <li>Validates the judge approval and ensures it contains a valid document.</li>
+     *   <li>Updates the draft orders and PSA documents in the provided {@link DraftOrdersWrapper} based on the judge's decision.</li>
+     * </ol>
+     *
+     * @param draftOrdersWrapper the wrapper object containing draft orders and PSA document collections to be updated
+     * @param userAuthorisation  the authorisation token of the user, used to fetch the approving judge's details
+     * @throws IllegalArgumentException if a judge approval is found with a null document at a given index
+     */
     public void populateJudgeDecisions(DraftOrdersWrapper draftOrdersWrapper, String userAuthorisation) {
         for (int i = 1; i <= 5; i++) {
             JudgeApproval judgeApproval = resolveJudgeApproval(draftOrdersWrapper, i);
-            if (judgeApproval != null && judgeApproval.getJudgeDecision() == JudgeDecision.READY_TO_BE_SEALED) {
-                CaseDocument doc = judgeApproval.getDocument();
-                if (doc == null) {
-                    throw new IllegalArgumentException(format("Document is null for JudgeApproval at index %d with decision READY_TO_BE_SEALED. "
-                        + "Please check the data integrity.", i));
-                }
-                draftOrdersWrapper.getDraftOrdersReviewCollection().forEach(d -> {
-                    d.getValue().getDraftOrderDocReviewCollection().forEach(d1 -> {
-                        if (doc.equals(d1.getValue().getDraftOrderDocument())) {
-                            d1.getValue().setOrderStatus(OrderStatus.APPROVED_BY_JUDGE);
-                            d1.getValue().setApprovalDate(LocalDate.now());
-                            d1.getValue().setApprovalJudge(idamService.getIdamFullName(userAuthorisation));
-                        }
-                    });
-                    d.getValue().getPsaDocReviewCollection().forEach(d1 -> {
-                        if (doc.equals(d1.getValue().getPsaDocument())) {
-                            d1.getValue().setOrderStatus(OrderStatus.APPROVED_BY_JUDGE);
-                            d1.getValue().setApprovalDate(LocalDate.now());
-                            d1.getValue().setApprovalJudge(idamService.getIdamFullName(userAuthorisation));
-                        }
-                    });
-                });
+            if (isJudgeApprovalValid(judgeApproval)) {
+                CaseDocument targetDoc = validateJudgeApprovalDocument(judgeApproval, i);
+                updateDraftOrders(draftOrdersWrapper, targetDoc, judgeApproval, userAuthorisation);
             }
         }
+    }
+
+    private boolean isJudgeApprovalValid(JudgeApproval judgeApproval) {
+        return judgeApproval != null && Arrays.asList(READY_TO_BE_SEALED, JUDGE_NEEDS_TO_MAKE_CHANGES).contains(judgeApproval.getJudgeDecision());
+    }
+
+    private CaseDocument validateJudgeApprovalDocument(JudgeApproval judgeApproval, int index) {
+        CaseDocument doc = judgeApproval.getDocument();
+        if (doc == null) {
+            throw new IllegalArgumentException(format(
+                "Document is null for JudgeApproval at index %d. Please check the data integrity.", index));
+        }
+        return doc;
+    }
+
+    private void updateDraftOrders(DraftOrdersWrapper draftOrdersWrapper, CaseDocument targetDoc, JudgeApproval judgeApproval,
+                                   String userAuthorisation) {
+        draftOrdersWrapper.getDraftOrdersReviewCollection().forEach(el -> {
+            if (el.getValue() != null) {
+                processDraftOrderDocReviewCollection(el.getValue().getDraftOrderDocReviewCollection(), targetDoc, judgeApproval, userAuthorisation);
+                processPsaDocReviewCollection(el.getValue().getPsaDocReviewCollection(), targetDoc, userAuthorisation);
+            }
+        });
+        draftOrdersWrapper.getAgreedDraftOrderCollection().forEach(el -> {
+            if (el.getValue() != null) {
+                processAgreedDraftOrderCollection(el.getValue(), targetDoc);
+            }
+        });
+    }
+
+    private void processDraftOrderDocReviewCollection(List<DraftOrderDocReviewCollection> docReviews, CaseDocument targetDoc,
+                                                      JudgeApproval judgeApproval, String userAuthorisation) {
+        if (docReviews != null) {
+            docReviews.forEach(el -> {
+                if (targetDoc.equals(el.getValue().getDraftOrderDocument())) {
+                    handleDraftOrderDocumentUpdate(el.getValue(), judgeApproval, userAuthorisation);
+                }
+            });
+        }
+    }
+
+    private void processPsaDocReviewCollection(List<PsaDocReviewCollection> psaReviews, CaseDocument targetDoc, String userAuthorisation) {
+        if (psaReviews != null) {
+            psaReviews.forEach(el -> {
+                if (targetDoc.equals(el.getValue().getPsaDocument())) {
+                    handlePsaDocumentUpdate(el.getValue(), userAuthorisation);
+                }
+            });
+        }
+    }
+
+    private void processAgreedDraftOrderCollection(AgreedDraftOrder agreedDraftOrder, CaseDocument targetDoc) {
+        if (agreedDraftOrder != null
+            && (targetDoc.equals(agreedDraftOrder.getDraftOrder()) || targetDoc.equals(agreedDraftOrder.getPensionSharingAnnex()))) {
+            agreedDraftOrder.setOrderStatus(OrderStatus.APPROVED_BY_JUDGE);
+        }
+    }
+
+    private void handleDraftOrderDocumentUpdate(DraftOrderDocumentReview review, JudgeApproval judgeApproval, String userAuthorisation) {
+        if (judgeApproval.getJudgeDecision() == JUDGE_NEEDS_TO_MAKE_CHANGES) {
+            review.setDraftOrderDocument(judgeApproval.getAmendedDocument());
+        }
+        review.setOrderStatus(OrderStatus.APPROVED_BY_JUDGE);
+        review.setApprovalDate(LocalDate.now());
+        review.setApprovalJudge(idamService.getIdamFullName(userAuthorisation));
+    }
+
+    private void handlePsaDocumentUpdate(PsaDocumentReview review, String userAuthorisation) {
+        review.setOrderStatus(OrderStatus.APPROVED_BY_JUDGE);
+        review.setApprovalDate(LocalDate.now());
+        review.setApprovalJudge(idamService.getIdamFullName(userAuthorisation));
     }
 
     /**
@@ -100,11 +174,11 @@ public class ApproveOrderService {
 
     private static String getDocumentFileName(JudgeApproval judgeApproval) {
         String filename = null;
-        if (JudgeDecision.JUDGE_NEEDS_TO_MAKE_CHANGES == judgeApproval.getJudgeDecision()) {
+        if (JUDGE_NEEDS_TO_MAKE_CHANGES == judgeApproval.getJudgeDecision()) {
             filename = judgeApproval.getAmendedDocument() != null
                 ? judgeApproval.getAmendedDocument().getDocumentFilename()
                 : "Unknown Filename";
-        } else if (JudgeDecision.READY_TO_BE_SEALED == judgeApproval.getJudgeDecision()) {
+        } else if (READY_TO_BE_SEALED == judgeApproval.getJudgeDecision()) {
             filename = judgeApproval.getDocument() != null
                 ? judgeApproval.getDocument().getDocumentFilename()
                 : "Unknown Filename";
