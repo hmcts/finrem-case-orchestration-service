b2a71f3ad1339471a7d2fdb7634039a4d9a61430
Ashley Wong <ashley.wong@justice.gov.uk>
2024-12-10 18:28:29 +0000

    Java docs

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/HasApprovable.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/HasApprovable.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
+/**
+ * Interface representing a container for an {@link Approvable} object.
+ */
 public interface HasApprovable {
     Approvable getValue();
 }
--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RefusalOrderConvertible.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/RefusalOrderConvertible.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import java.time.LocalDateTime;
 
+/**
+ * Interface defining the methods required to convert
+ * a refusal order from draft orders or pension sharing annexes.
+ */
 public interface RefusalOrderConvertible extends Approvable {
 
     String getApprovalJudge();
