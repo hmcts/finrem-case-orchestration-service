f0a7677f21411838bb501fa51653dada017848b9
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2024-12-13 08:34:10 +0000

    Refactor CaseDocument object creation (#2029)

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseDocumentTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/CaseDocumentTest.java
@@ -1,16 +1,16 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class CaseDocumentTest {
+class CaseDocumentTest {
     CaseDocument document;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         String json = "{"
             + " \"document_url\" : \"http://doc1\", "
@@ -22,9 +22,24 @@ public class CaseDocumentTest {
     }
 
     @Test
-    public void shouldCreateCaseDocumentFromJson() {
+    void shouldCreateCaseDocumentFromJson() {
         assertThat(document.getDocumentUrl(), is("http://doc1"));
         assertThat(document.getDocumentFilename(), is("doc1"));
         assertThat(document.getDocumentBinaryUrl(), is("http//doc1.binary"));
+    }
+
+    @Test
+    void shouldCreateCaseDocumentFromDocument() {
+        uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document document =
+            uk.gov.hmcts.reform.finrem.caseorchestration.model.document.Document.builder()
+            .binaryUrl("http//doc1.binary")
+            .fileName("doc1")
+            .url("http://doc1")
+            .build();
+
+        CaseDocument caseDocument = CaseDocument.from(document);
+        assertThat(caseDocument.getDocumentUrl(), is("http://doc1"));
+        assertThat(caseDocument.getDocumentFilename(), is("doc1"));
+        assertThat(caseDocument.getDocumentBinaryUrl(), is("http//doc1.binary"));
     }
 }
