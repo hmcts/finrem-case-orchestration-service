73af83674ab5cabbfc7a95d20e2d56e26345ceae
Paul Trelease <paul.trelease@justice.gov.uk>
2024-12-17 11:20:44 +0000

    Dfr 3082 (#2024)
    * DFR-3082: Corrected invalid data in unit test fixtures
    
    * DFR-3082: Initial commit for amend application details callback work.  Requires DFR-3081 changes scheduled for release Dec 11th
    
    * DFR-3082: Corrected errors in functional tests
    
    * DFR-3082: Moved UpdateContestedCaseControllerTest.java from JUnit4 to 5
    
    * DFR-3082: AmendApplicationContestedAboutToStartHandler refactored to temp store when the user is admin
    
    * linting changes
    
    * Removed redundant test code
    
    ---------
    
    Co-authored-by: Adam Lee <166380454+al-hmcts@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendPaperApplicationContestedAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendPaperApplicationContestedAboutToStartHandlerTest.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
@@ -11,56 +11,57 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.refuge.RefugeWrapperUtils;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Schedule1OrMatrimonialAndCpList.MATRIMONIAL_AND_CIVIL_PARTNERSHIP_PROCEEDINGS;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo.NO;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 public class AmendPaperApplicationContestedAboutToStartHandlerTest {
 
     public static final String AUTH_TOKEN = "tokien:)";
     private AmendPaperApplicationContestedAboutToStartHandler handler;
 
-    @Before
+    @BeforeEach
     public void setup() {
-        FinremCaseDetailsMapper finremCaseDetailsMapper = Mockito.mock(FinremCaseDetailsMapper.class);
+        FinremCaseDetailsMapper finremCaseDetailsMapper = mock(FinremCaseDetailsMapper.class);
         handler =  new AmendPaperApplicationContestedAboutToStartHandler(finremCaseDetailsMapper,
             new OnStartDefaultValueService());
     }
 
     @Test
-    public void givenContestedCase_whenEventIsAmendAndCallbackIsSubmitted_thenHandlerCanNotHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.SUBMITTED, CaseType.CONTESTED, EventType.AMEND_CONTESTED_PAPER_APP_DETAILS),
-            is(false));
+    void testHandlerCanHandle() {
+        assertCanHandle(handler, CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.AMEND_CONTESTED_PAPER_APP_DETAILS);
     }
 
     @Test
-    public void givenConsentedCase_whenEventIsAmendAndCallbackIsSubmitted_thenHandlerCanNotHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.SUBMITTED, CaseType.CONSENTED, EventType.AMEND_CONTESTED_PAPER_APP_DETAILS),
-            is(false));
-    }
-
-    @Test
-    public void givenContestedCase_whenEventIsAmend_thenHandlerCanHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.AMEND_CONTESTED_PAPER_APP_DETAILS),
-            is(true));
-    }
-
-
-    @Test
-    public void handle() {
+    void handle() {
         FinremCallbackRequest callbackRequest = buildCallbackRequest();
         var response = handler.handle(callbackRequest, AUTH_TOKEN);
         assertEquals(NO, response.getData().getCivilPartnership());
         assertEquals(NO, response.getData().getPromptForUrgentCaseQuestion());
         assertEquals(MATRIMONIAL_AND_CIVIL_PARTNERSHIP_PROCEEDINGS,
             response.getData().getScheduleOneWrapper().getTypeOfApplication());
+    }
+
+    @Test
+    void testPopulateInRefugeQuestionsCalled() {
+        FinremCallbackRequest callbackRequest = buildCallbackRequest();
+        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+
+        // MockedStatic is closed after the try resources block
+        try (MockedStatic<RefugeWrapperUtils> mockedStatic = mockStatic(RefugeWrapperUtils.class)) {
+
+            handler.handle(callbackRequest, AUTH_TOKEN);
+            // Check that updateRespondentInRefugeTab is called with our case details instance
+            mockedStatic.verify(() -> RefugeWrapperUtils.populateApplicantInRefugeQuestion(caseDetails), times(1));
+            mockedStatic.verify(() -> RefugeWrapperUtils.populateRespondentInRefugeQuestion(caseDetails), times(1));
+        }
     }
 
     private FinremCallbackRequest buildCallbackRequest() {
