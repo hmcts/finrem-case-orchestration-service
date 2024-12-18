e88fb6a58c0bc41fbefdd86a998314f5d51d6677
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2024-12-18 15:19:39 +0000

    Use StringBuilder instead

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/judgeapproval/JudgeApprovalInfoCapturer.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/judgeapproval/JudgeApprovalInfoCapturer.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service.judgeapproval;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders.judgeapproval.JudgeApproval;
@@ -9,15 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 class JudgeApprovalInfoCapturer {
 
-    private static final List<String> ordersApproved = new ArrayList<>();
-
-    private static final List<String> ordersRepresentativeChanges = new ArrayList<>();
-
-    private static final List<String> ordersChanged = new ArrayList<>();
-
-    private static final List<String> ordersReviewLater = new ArrayList<>();
+    private final JudgeApprovalHelper judgeApprovalHelper;
 
     private static final String ORDERS_APPROVED_CONFIRMATION_BODY_FORMAT =
         "<br>The draft orders (%s) for the case have been approved. "
@@ -34,38 +30,46 @@ class JudgeApprovalInfoCapturer {
         "<br>You have said you will review draft orders (%s) later. These will remain on the "
             + "['Draft Orders' tab](/cases/case-details/%s#Draft%%20orders).";
 
-
-    void fileNameCaptors(JudgeApproval judgeApproval) {
-        String fileName = judgeApproval.getDocument().getDocumentFilename();
-        switch (judgeApproval.getJudgeDecision()) {
-            case READY_TO_BE_SEALED -> ordersApproved.add(fileName);
-            case LEGAL_REP_NEEDS_TO_MAKE_CHANGE -> ordersRepresentativeChanges.add(fileName);
-            case JUDGE_NEEDS_TO_MAKE_CHANGES -> ordersChanged.add(fileName);
-            case REVIEW_LATER -> ordersReviewLater.add(fileName);
-            default -> throw new IllegalStateException("Unhandled judge decision for document:" + fileName);
-        }
-    }
-
     void buildConfirmationBody(FinremCaseDetails caseDetails, DraftOrdersWrapper draftOrdersWrapper) {
-        List<String> body = new ArrayList<>();
 
+        final List<String> ordersApproved = new ArrayList<>();
+        final List<String> ordersRepresentativeChanges = new ArrayList<>();
+        final List<String> ordersChanged = new ArrayList<>();
+        final List<String> ordersReviewLater = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            JudgeApproval judgeApproval = judgeApprovalHelper.resolveJudgeApproval(draftOrdersWrapper, i);
+            String fileName = judgeApproval.getDocument().getDocumentFilename();
+            switch (judgeApproval.getJudgeDecision()) {
+                case READY_TO_BE_SEALED -> ordersApproved.add(fileName);
+                case LEGAL_REP_NEEDS_TO_MAKE_CHANGE -> ordersRepresentativeChanges.add(fileName);
+                case JUDGE_NEEDS_TO_MAKE_CHANGES -> ordersChanged.add(fileName);
+                case REVIEW_LATER -> ordersReviewLater.add(fileName);
+                default -> throw new IllegalStateException("Unhandled judge decision for document:" + fileName);
+            }
+        }
+
+        StringBuilder body = new StringBuilder();
         if (!ordersApproved.isEmpty()) {
-            body.add(String.format(ORDERS_APPROVED_CONFIRMATION_BODY_FORMAT,
-                String.join(", ", ordersApproved)));
+            body.append(String.format(ORDERS_APPROVED_CONFIRMATION_BODY_FORMAT,
+                    String.join(", ", ordersApproved)))
+                .append("\n\n");
         }
         if (!ordersRepresentativeChanges.isEmpty()) {
-            body.add(String.format(ORDERS_REQUIRE_REPRESENTATIVE_CHANGE_CONFIRMATION_BODY_FORMAT,
-                String.join(", ", ordersRepresentativeChanges)));
+            body.append(String.format(ORDERS_REQUIRE_REPRESENTATIVE_CHANGE_CONFIRMATION_BODY_FORMAT,
+                    String.join(", ", ordersRepresentativeChanges)))
+                .append("\n\n");
         }
         if (!ordersChanged.isEmpty()) {
-            body.add(String.format(ORDERS_AMENDED_CHANGE_CONFIRMATION_BODY_FORMAT,
-                String.join(", ", ordersChanged)));
+            body.append(String.format(ORDERS_AMENDED_CHANGE_CONFIRMATION_BODY_FORMAT,
+                    String.join(", ", ordersChanged)))
+                .append("\n\n");
         }
         if (!ordersReviewLater.isEmpty()) {
-            body.add(String.format(ORDERS_REVIEW_LATER_CONFIRMATION_BODY_FORMAT,
-                String.join(", ", ordersReviewLater), caseDetails.getId()));
+            body.append(String.format(ORDERS_REVIEW_LATER_CONFIRMATION_BODY_FORMAT,
+                    String.join(", ", ordersReviewLater), caseDetails.getId()));
         }
 
-        draftOrdersWrapper.setApproveOrdersConfirmationBody(String.join("\n\n", body));
+        draftOrdersWrapper.setApproveOrdersConfirmationBody(body.toString());
     }
 }
