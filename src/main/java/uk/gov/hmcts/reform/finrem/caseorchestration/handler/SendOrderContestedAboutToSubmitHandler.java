ca8748ba8319a1d5a3e639e3763359be849ef68f
Ashley Wong <ashley.wong@justice.gov.uk>
2025-01-08 11:45:05 +0000

    remove extra line.

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SendOrderContestedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SendOrderContestedAboutToSubmitHandler.java
@@ -32,7 +32,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
-
 @Slf4j
 @Service
 public class SendOrderContestedAboutToSubmitHandler extends FinremCallbackHandler {
