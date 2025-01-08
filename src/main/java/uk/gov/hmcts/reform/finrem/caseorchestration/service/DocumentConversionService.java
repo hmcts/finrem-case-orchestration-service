529e36a31c967631861e8795768256c249911c82
dawudgovuk <dawud.rahman@justice.gov.uk>
2025-01-08 16:36:03 +0000

    DFR-3327-PDF-Documents-missing-signatures-Bulk-Print

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentConversionService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentConversionService.java
@@ -8,7 +8,12 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.util.Matrix;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -26,6 +31,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.evidencemanagement.E
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -73,6 +79,7 @@ public class DocumentConversionService {
 
             if (acroForm.isPresent()) {
                 acroForm.get().flatten();
+                flattenAnnotations(doc);
                 doc.save(bos);
                 doc.close();
                 return bos.toByteArray();
@@ -83,6 +90,34 @@ public class DocumentConversionService {
             IOUtils.closeQuietly(doc);
         }
         return document;
+    }
+
+    private void flattenAnnotations(PDDocument doc) throws IOException {
+        for (PDPage page : doc.getPages()) {
+            List<PDAnnotation> annotations = page.getAnnotations();
+            for (PDAnnotation annotation : annotations) {
+                PDAppearanceStream appearanceStream = annotation.getNormalAppearanceStream();
+                if (appearanceStream != null) {
+                    float appearanceWidth = (float) appearanceStream.getBBox().getWidth();
+                    float appearanceHeight = (float) appearanceStream.getBBox().getHeight();
+
+                    if (appearanceWidth > 0 && appearanceHeight > 0) {
+                        try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true)) {
+                            contentStream.saveGraphicsState();
+                            float x = (float) annotation.getRectangle().getLowerLeftX();
+                            float y = (float) annotation.getRectangle().getLowerLeftY();
+                            float width = (float) annotation.getRectangle().getWidth();
+                            float height = (float) annotation.getRectangle().getHeight();
+                            Matrix transformation = new Matrix(width / appearanceWidth, 0, 0, height / appearanceHeight, x, y);
+                            contentStream.transform(transformation);
+                            contentStream.drawForm(appearanceStream);
+                            contentStream.restoreGraphicsState();
+                        }
+                    }
+                }
+            }
+            annotations.clear();
+        }
     }
 
     private byte[] convert(Document sourceDocument, String auth) {
