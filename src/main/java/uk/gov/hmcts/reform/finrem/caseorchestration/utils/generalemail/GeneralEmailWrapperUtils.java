05cff8cebb1196b367be2ba2619de055961074e0
Paul Trelease <paul.trelease@justice.gov.uk>
2025-01-17 13:16:49 +0000

    Added full stop to JavaDoc to pass linting

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/generalemail/GeneralEmailWrapperUtils.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/generalemail/GeneralEmailWrapperUtils.java
@@ -5,7 +5,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 
 /**
- * Utility class for GeneralEmailWrapper related static methods
+ * Utility class for GeneralEmailWrapper related static methods.
  */
 @Slf4j
 public class GeneralEmailWrapperUtils {
