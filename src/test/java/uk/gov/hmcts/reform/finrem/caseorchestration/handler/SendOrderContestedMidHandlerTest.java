51416f8b1cb75a5ba8dcdf549769fe4d20f74402
Ashley Wong <ashley.wong@justice.gov.uk>
2025-01-24 23:16:35 +0000

    Add MidEvent Handler test

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SendOrderContestedMidHandlerTest.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.SEND_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
+
+@ExtendWith(MockitoExtension.class)
+class SendOrderContestedMidHandlerTest {
+
+    @InjectMocks
+    private SendOrderContestedMidHandler underTest;
+
+    @Test
+    void testCanHandle() {
+        assertCanHandle(underTest, MID_EVENT, CONTESTED, SEND_ORDER);
+    }
+}
