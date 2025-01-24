7c70922637ef861cd5cdfcd7c6f274e0a6b53094
Ashley Wong <ashley.wong@justice.gov.uk>
2025-01-24 23:10:57 +0000

    Fix test case.

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/SendOrderWrapper.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/SendOrderWrapper.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.SendOrderEventPostStateOption;
@@ -22,12 +20,11 @@ public class SendOrderWrapper {
 
     private CaseDocument additionalDocument;
 
-    @Getter(AccessLevel.NONE)
-    @JsonProperty("ordersToSend")
     private OrdersToSend ordersToSend;
 
     private SendOrderEventPostStateOption sendOrderPostStateOption;
 
+    @JsonIgnore
     public OrdersToSend getOrdersToSend() {
         if (this.ordersToSend == null) {
             this.ordersToSend = new OrdersToSend();
