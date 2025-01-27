91df8caaf90a120c8369eeaf3e2921046449d26b
Ashley Wong <ashley.wong@justice.gov.uk>
2025-01-27 12:05:07 +0000

    Java doc

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/draftorders/WithAttachmentsCollection.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/draftorders/WithAttachmentsCollection.java
@@ -1,6 +1,15 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.draftorders;
 
+/**
+ * Represents an entity that contains an attachments collection.
+ * This interface provides a method to retrieve the attachment-related value.
+ */
 public interface WithAttachmentsCollection {
 
+    /**
+     * Retrieves the value containing attachment details.
+     *
+     * @return an instance of {@link WithAttahments} containing attachment data.
+     */
     WithAttahments getValue();
 }
