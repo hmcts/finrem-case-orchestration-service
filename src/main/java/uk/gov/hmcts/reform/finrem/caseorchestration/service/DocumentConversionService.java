074f98333a825d1c5a33bf563e0691ab957a41fb
Dawud Rahman <109285380+dawudgovuk@users.noreply.github.com>
2025-01-27 09:56:58 +0000

    DFR-3327-PDF-Documents-missing-signatures-Bulk-Print (#2072)
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-2961-CS-Postcode-field
    
    * DFR-2961-CS-Postcode-field
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    * Update DocumentConversionService.java
    
    Added reason for code
    
    * DFR-3327-PDF-Documents-missing-signatures-Bulk-Print
    
    ---------
    
    Co-authored-by: Paul Trelease <paul.trelease@justice.gov.uk>
    Co-authored-by: Adam Lee <166380454+al-hmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentConversionService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/DocumentConversionService.java
@@ -8,7 +8,12 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.util.Matrix;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -73,6 +78,7 @@ public class DocumentConversionService {
 
             if (acroForm.isPresent()) {
                 acroForm.get().flatten();
+                flattenAnnotations(doc);
                 doc.save(bos);
                 doc.close();
                 return bos.toByteArray();
@@ -83,6 +89,57 @@ public class DocumentConversionService {
             IOUtils.closeQuietly(doc);
         }
         return document;
+    }
+
+    /**
+     * This method iterates over all pages in the document and removes all annotations from each page.
+     * This was specifically introduced to flatten unflattened signatures which caused issues printing later.
+     * @param doc - The document to be flattened
+     * @throws IOException - If the annotations cannot be removed
+     */
+    private void flattenAnnotations(PDDocument doc) throws IOException {
+        for (PDPage page : doc.getPages()) {
+            for (PDAnnotation annotation : page.getAnnotations()) {
+                PDAppearanceStream appearanceStream = annotation.getNormalAppearanceStream();
+                if (appearanceStream != null) {
+                    transformAnnotationsToContentStream(doc, page, annotation, appearanceStream);
+                }
+            }
+            page.getAnnotations().clear();
+        }
+    }
+
+    /**
+     * The transformation matrix needs to be set correctly by considering both the position and the size of the
+     * annotation's bounding box, along with the appearance stream's bounding box. Once the transformation matrix
+     * is applied, the drawForm() method is used to draw the appearance stream directly onto the page at the
+     * correct position.
+     * @param doc - The document to which the annotations belong
+     * @param page - The page to which the annotations belong
+     * @param annotation - The annotation to be transformed
+     * @param appearanceStream - The appearance stream of the annotation
+     * @throws IOException - If the content stream cannot be created
+     */
+    private void transformAnnotationsToContentStream(PDDocument doc, PDPage page, PDAnnotation annotation,
+                                                     PDAppearanceStream appearanceStream) throws IOException {
+        float appearanceWidth = appearanceStream.getBBox().getWidth();
+        float appearanceHeight = appearanceStream.getBBox().getHeight();
+
+        if (appearanceWidth > 0 && appearanceHeight > 0) {
+            try (PDPageContentStream contentStream =
+                     new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND,
+                         true, true)) {
+                contentStream.saveGraphicsState();
+                float x = annotation.getRectangle().getLowerLeftX();
+                float y =  annotation.getRectangle().getLowerLeftY();
+                float width = annotation.getRectangle().getWidth();
+                float height = annotation.getRectangle().getHeight();
+                Matrix transformation = new Matrix(width / appearanceWidth, 0, 0, height / appearanceHeight, x, y);
+                contentStream.transform(transformation);
+                contentStream.drawForm(appearanceStream);
+                contentStream.restoreGraphicsState();
+            }
+        }
     }
 
     private byte[] convert(Document sourceDocument, String auth) {
