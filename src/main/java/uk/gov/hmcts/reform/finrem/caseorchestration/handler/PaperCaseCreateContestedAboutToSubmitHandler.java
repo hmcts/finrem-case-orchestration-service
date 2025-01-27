20a3614f5d381b9dd44386f4bf75ace8806e00e3
Adam <adam.lee2@justice.gov.uk>
2025-01-27 13:41:32 +0000

    Add call to caseDataService to set powerBI fields

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/PaperCaseCreateContestedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/PaperCaseCreateContestedAboutToSubmitHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.OrganisationPolicy;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseDataService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseFlagsService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.IdamService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.refuge.RefugeWrapperUtils;
@@ -26,13 +28,16 @@ public class PaperCaseCreateContestedAboutToSubmitHandler extends FinremCallback
 
     private final CaseFlagsService caseFlagsService;
     private final IdamService idamService;
+    private final CaseDataService caseDataService;
 
     public PaperCaseCreateContestedAboutToSubmitHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
                                                         CaseFlagsService caseFlagsService,
-                                                        IdamService idamService) {
+                                                        IdamService idamService,
+                                                        CaseDataService caseDataService) {
         super(finremCaseDetailsMapper);
         this.caseFlagsService = caseFlagsService;
         this.idamService = idamService;
+        this.caseDataService = caseDataService;
     }
 
     @Override
@@ -80,6 +85,10 @@ public class PaperCaseCreateContestedAboutToSubmitHandler extends FinremCallback
 
         RefugeWrapperUtils.updateRespondentInRefugeTab(caseDetails);
         RefugeWrapperUtils.updateApplicantInRefugeTab(caseDetails);
+
+        CaseDetails oldCaseDetails = finremCaseDetailsMapper.mapToCaseDetails(caseDetails);
+        caseDataService.setFinancialRemediesCourtDetails(oldCaseDetails);
+        caseData = finremCaseDetailsMapper.mapToFinremCaseData(oldCaseDetails.getData());
 
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).build();
     }
