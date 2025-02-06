b26cb50f5eb78589b952aa2c20f4fd053908ade4
Paul Trelease <paul.trelease@justice.gov.uk>
2025-02-06 09:40:54 +0000

    src

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/GeneralEmailWrapper.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/GeneralEmailWrapper.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
