4ac5433b3d0f9766785153fd9288362256f15b3a
Ashley Wong <ashley.wong@justice.gov.uk>
2025-02-07 12:22:25 +0000

    simplify

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentServiceTest.java
@@ -162,7 +162,7 @@ class AdditionalHearingDocumentServiceTest {
         assertThat(data).extracting("CourtPhone").isEqualTo("0115 910 3504");
         assertThat(data).extracting("CourtEmail").isEqualTo("FRCNottingham@justice.gov.uk");
 
-        assertThat(data.get(ADDITIONAL_HEARING_DOCUMENT_COLLECTION)).isNotNull();
+        assertThat(data).extracting(ADDITIONAL_HEARING_DOCUMENT_COLLECTION).isNotNull();
     }
 
     @Test
