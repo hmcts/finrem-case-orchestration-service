7e689faa1deea8f3b2716b23a0739398f753ea9d
Ashley Wong <ashley.wong@justice.gov.uk>
2025-02-12 16:15:07 +0000

    DFR-3595 populate hearing infos from hearing details created from process order event..

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/HearingService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/HearingService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DirectionDetailCollection;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicList;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicListElement;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
@@ -82,34 +83,40 @@ public class HearingService {
         return DynamicListElement.builder().code(code).label(label).build();
     }
 
+    DynamicListElement buildDirectionDetailDynamicListElement(DirectionDetailCollection directionDetailCollection) {
+        String code = directionDetailCollection.getId().toString();
+        LocalDate hearingDate = directionDetailCollection.getValue().getDateOfHearing();
+        String hearingTime = directionDetailCollection.getValue().getHearingTime();
+        HearingTypeDirection hearingType = directionDetailCollection.getValue().getTypeOfHearing();
+
+        String label = formatDynamicListElementLabel(hearingType == null ? "" : hearingType.getId(), hearingDate, hearingTime);
+        return DynamicListElement.builder().code(code).label(label).build();
+    }
+
+    /**
+     * Generates a {@link DynamicList} containing selectable hearings for a given case.
+     *
+     * <p>
+     * This method retrieves the hearing type, date, and time from the case details and constructs
+     * a list of {@link DynamicListElement} objects representing the available hearings.
+     * It includes both the top-level hearing, any interim hearings and hearings created from
+     * Process Order event if present.
+     *
+     * <p>
+     * The list is sorted based on a {@link HearingSortingKey}, which orders elements
+     * by hearing date, time, and type.
+     *
+     * @param caseDetails the {@link FinremCaseDetails} containing case data with hearing details
+     * @return a {@link DynamicList} of selectable hearings sorted by hearing attributes
+     */
     public DynamicList generateSelectableHearingsAsDynamicList(FinremCaseDetails caseDetails) {
+        FinremCaseData caseData = caseDetails.getData();
         List<DynamicListElement> dynamicListElements = new ArrayList<>();
         Map<DynamicListElement, HearingSortingKey> elementToSortingKeyMap = new HashMap<>();
 
-        FinremCaseData caseData = caseDetails.getData();
-        HearingTypeDirection hearingType = caseData.getListForHearingWrapper().getHearingType();
-        LocalDate hearingDate = caseData.getListForHearingWrapper().getHearingDate();
-        String hearingTime = caseData.getListForHearingWrapper().getHearingTime();
-
-        DynamicListElement topLevelDynamicListElement = buildTopLevelHearingDynamicListElement(hearingType, hearingDate, hearingTime);
-        if (topLevelDynamicListElement != null) {
-            dynamicListElements.add(topLevelDynamicListElement);
-            elementToSortingKeyMap.put(topLevelDynamicListElement, new HearingSortingKey(hearingDate, hearingTime, hearingType == null
-                ? null : hearingType.getId()));
-        }
-
-        ofNullable(caseData.getInterimWrapper().getInterimHearings()).orElse(List.of()).forEach(ihc -> {
-            DynamicListElement interimDynamicListElement = buildInterimHearingDynamicListElement(ihc);
-            if (interimDynamicListElement != null) {
-                LocalDate interimHearingDate = ihc.getValue().getInterimHearingDate();
-                String interimHearingTime = ihc.getValue().getInterimHearingTime();
-                String interimHearingType = ihc.getValue().getInterimHearingType() != null ? ihc.getValue().getInterimHearingType().getId() : null;
-
-                dynamicListElements.add(interimDynamicListElement);
-                elementToSortingKeyMap.put(interimDynamicListElement, new HearingSortingKey(interimHearingDate, interimHearingTime,
-                    interimHearingType));
-            }
-        });
+        populateTopLevelHearings(caseData, dynamicListElements, elementToSortingKeyMap);
+        populateInterimHearings(caseData, dynamicListElements, elementToSortingKeyMap);
+        populateHearingsCreatedFromProcessOrder(caseData, dynamicListElements, elementToSortingKeyMap);
 
         // Sort the dynamicListElements using the sorting keys from the map
         dynamicListElements.sort(Comparator.comparing(elementToSortingKeyMap::get));
@@ -166,6 +173,52 @@ public class HearingService {
                 .orElse("N/A"),
             Optional.ofNullable(hearingTime).orElse("N/A")
         );
+    }
+
+    private void populateTopLevelHearings(FinremCaseData caseData, List<DynamicListElement> dynamicListElements,
+                                          Map<DynamicListElement, HearingSortingKey> elementToSortingKeyMap) {
+        HearingTypeDirection hearingType = caseData.getListForHearingWrapper().getHearingType();
+        LocalDate hearingDate = caseData.getListForHearingWrapper().getHearingDate();
+        String hearingTime = caseData.getListForHearingWrapper().getHearingTime();
+
+        DynamicListElement topLevelDynamicListElement = buildTopLevelHearingDynamicListElement(hearingType, hearingDate, hearingTime);
+        if (topLevelDynamicListElement != null) {
+            dynamicListElements.add(topLevelDynamicListElement);
+            elementToSortingKeyMap.put(topLevelDynamicListElement, new HearingSortingKey(hearingDate, hearingTime, hearingType == null
+                ? null : hearingType.getId()));
+        }
+    }
+
+    private void populateInterimHearings(FinremCaseData caseData, List<DynamicListElement> dynamicListElements,
+                                         Map<DynamicListElement, HearingSortingKey> elementToSortingKeyMap) {
+        ofNullable(caseData.getInterimWrapper().getInterimHearings()).orElse(List.of()).forEach(interimHearingCollection -> {
+            DynamicListElement dynamicListElement = buildInterimHearingDynamicListElement(interimHearingCollection);
+            if (dynamicListElement != null) {
+                LocalDate hearingDate = interimHearingCollection.getValue().getInterimHearingDate();
+                String hearingTime = interimHearingCollection.getValue().getInterimHearingTime();
+                String hearingType = interimHearingCollection.getValue().getInterimHearingType() != null
+                    ? interimHearingCollection.getValue().getInterimHearingType().getId() : null;
+
+                dynamicListElements.add(dynamicListElement);
+                elementToSortingKeyMap.put(dynamicListElement, new HearingSortingKey(hearingDate, hearingTime, hearingType));
+            }
+        });
+    }
+
+    private void populateHearingsCreatedFromProcessOrder(FinremCaseData caseData, List<DynamicListElement> dynamicListElements,
+                                                         Map<DynamicListElement, HearingSortingKey> elementToSortingKeyMap) {
+        ofNullable(caseData.getDirectionDetailsCollection()).orElse(List.of()).forEach(directionDetailCollection -> {
+            DynamicListElement dynamicListElement = buildDirectionDetailDynamicListElement(directionDetailCollection);
+            if (dynamicListElement != null) {
+                LocalDate hearingDate = directionDetailCollection.getValue().getDateOfHearing();
+                String hearingTime = directionDetailCollection.getValue().getHearingTime();
+                String hearingType = directionDetailCollection.getValue().getTypeOfHearing() != null
+                    ? directionDetailCollection.getValue().getTypeOfHearing().getId() : null;
+
+                dynamicListElements.add(dynamicListElement);
+                elementToSortingKeyMap.put(dynamicListElement, new HearingSortingKey(hearingDate, hearingTime, hearingType));
+            }
+        });
     }
 
 }
