132ad45f1b41ee10758c6c85f6e243b203003074
Adam <adam.lee2@justice.gov.uk>
2025-02-17 14:25:36 +0000

    Add pilot participation to FinRemCaseData

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/FinremCaseData.java
@@ -73,6 +73,7 @@ public class FinremCaseData implements HasCaseDocument {
     @JsonIgnore
     private CaseType ccdCaseType;
 
+    private ExpressPilotParticipation expressPilotParticipation;
     private String divorceCaseNumber;
     private StageReached divorceStageReached;
     private CaseDocument divorceUploadEvidence1;
