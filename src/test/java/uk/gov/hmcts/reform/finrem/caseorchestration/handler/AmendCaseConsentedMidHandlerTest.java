6b11d86bce72097190d801bd9868c888333444b3
dawudgovuk <dawud.rahman@justice.gov.uk>
2025-02-19 17:56:34 +0000

    DFR-3162 Java 21 update

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendCaseConsentedMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendCaseConsentedMidHandlerTest.java
@@ -1,17 +1,18 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderService;
-import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
 
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
@@ -22,29 +23,26 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.asser
 @ExtendWith(MockitoExtension.class)
 class AmendCaseConsentedMidHandlerTest {
 
-    @InjectMocks
-    private AmendApplicationConsentedMidHandler handler;
+    private AmendCaseConsentedMidHandler handler;
     @Mock
     private ConsentOrderService consentOrderService;
-    @Mock
-    private InternationalPostalService postalService;
+
     @Mock
     private ObjectMapper objectMapper;
 
-    @Test
-    void testCanHandle() {
-        assertCanHandle(handler, CallbackType.MID_EVENT, CONSENTED, AMEND_CASE);
+    @BeforeEach
+    void setUp() {
+        FinremCaseDetailsMapper finremCaseDetailsMapper = new FinremCaseDetailsMapper(objectMapper);
+        handler = new AmendCaseConsentedMidHandler(finremCaseDetailsMapper, consentOrderService, objectMapper);
     }
 
     @Test
-    void testHandle() {
-        FinremCallbackRequest callbackRequest = buildCallbackRequest();
-        handler.handle(callbackRequest, AUTH_TOKEN);
-        verify(postalService).validate(callbackRequest.getCaseDetails().getData());
+    void canHandle() {
+        assertCanHandle(handler, CallbackType.MID_EVENT, CONSENTED, EventType.AMEND_CASE);
     }
 
     @Test
-    void handle() {
+    void givenCase_whenCaseNatureOfApplicaftionUpdated_handleMidEvent() {
         FinremCallbackRequest callbackRequest = buildCallbackRequest();
         handler.handle(callbackRequest, AUTH_TOKEN);
         verify(consentOrderService).performCheck(objectMapper.convertValue(callbackRequest, CallbackRequest.class), AUTH_TOKEN);
