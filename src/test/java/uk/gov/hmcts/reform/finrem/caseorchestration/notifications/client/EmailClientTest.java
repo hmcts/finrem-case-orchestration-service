2620ad444780cf95f752a00117897c0283f1dbbf
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-02-24 10:04:16 +0000

    Remove unnecessary @DirtiesContext (#2170)

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/client/EmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/client/EmailClientTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.finrem.caseorchestration.BaseTest;
@@ -23,7 +22,6 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @PropertySource(value = "classpath:application.properties")
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Slf4j
 @Category(IntegrationTest.class)
 public class EmailClientTest extends BaseTest {
--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/service/EmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/service/EmailServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -77,7 +76,6 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.notifications.domain.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @PropertySource(value = "classpath:application.properties")
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Slf4j
 @Category(IntegrationTest.class)
 public class EmailServiceTest {
