02d073989afd042be910d39fa3cd5d3b459b1f32
Adam Lee <166380454+al-hmcts@users.noreply.github.com>
2025-02-24 14:28:59 +0000

    DFR-3617 Express Pilot tracking field  (#2158)
    * Add pilot participation to FinRemCaseData
    
    * Add express pilot case data field and set in FR_Sol_create
    
    * Unit test
    
    * EOF
    
    * Remove not needed expects in amend test
    
    * Add express pilot frcs to functional test application properties
    
    * Handle getSelectedAllocatedCourt returning null
    
    * Revert optional on FRC return and turn off ExpressPilot for functional tests
    
    * Add missing court wrapper fields to functional test data
    
    * Add missing court details for functional test data
    
    * Add missing FRC data for functional test and log for isFeatureEnabled
    
    * Rename express pilot -> express case and disable in probs by default
    
    * Remove feature toggle log
    
    * PR Comments
    
    * PR comments
    
    * Remove unused import
    
    * PR Comments
    
    * Unused imports
    
    * Use ReflectionUtils for private property testing
    
    * Import order

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/express/ExpressCaseServiceTest.java
@@ -0,0 +1,127 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.express;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.RegionMidlandsFrc;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.AllocatedRegionWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.DefaultCourtListWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.NatureApplicationWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.RegionWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ScheduleOneWrapper;
+
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.EstimatedAssetV2.UNABLE_TO_QUANTIFY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.EstimatedAssetV2.UNDER_TWO_HUNDRED_AND_FIFTY_THOUSAND_POUNDS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ExpressCaseParticipation.DOES_NOT_QUALIFY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ExpressCaseParticipation.ENROLLED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.A_SETTLEMENT_OR_A_TRANSFER_OF_PROPERTY;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.CONTESTED_VARIATION_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.LUMP_SUM_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.MAINTENANCE_PENDING_SUIT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PENSION_ATTACHMENT_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PENSION_COMPENSATION_ATTACHMENT_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PENSION_COMPENSATION_SHARING_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PENSION_SHARING_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PERIODICAL_PAYMENT_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NatureApplication.PROPERTY_ADJUSTMENT_ORDER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NottinghamCourt.BOSTON_COUNTY_COURT_AND_FAMILY_COURT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NottinghamCourt.NOTTINGHAM_COUNTY_COURT_AND_FAMILY_COURT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Region.MIDLANDS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Schedule1OrMatrimonialAndCpList.MATRIMONIAL_AND_CIVIL_PARTNERSHIP_PROCEEDINGS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Schedule1OrMatrimonialAndCpList.SCHEDULE_1_CHILDREN_ACT_1989;
+
+@ExtendWith(MockitoExtension.class)
+class ExpressCaseServiceTest {
+
+    private ExpressCaseService expressCaseService;
+
+    @BeforeEach
+    public void setUp() {
+        expressCaseService = new ExpressCaseService();
+        ReflectionTestUtils.setField(expressCaseService, "expressCaseFrcs", List.of("FR_s_NottinghamList_1", "FR_s_NottinghamList_2"));
+    }
+
+    @Test
+    void shouldEnrolledInExpressPilot_WhenCaseDataMeetsRequirements() {
+        FinremCaseData caseData = createCaseData();
+        expressCaseService.setExpressCaseEnrollmentStatus(caseData);
+        assertEquals(ENROLLED, caseData.getExpressCaseParticipation());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidCaseData")
+    void shouldNotQualify_WhenCaseDataDoesNotMeetCriteria(FinremCaseData caseData) {
+        expressCaseService.setExpressCaseEnrollmentStatus(caseData);
+        assertEquals(DOES_NOT_QUALIFY, caseData.getExpressCaseParticipation());
+    }
+
+    private static Stream<FinremCaseData> provideInvalidCaseData() {
+        FinremCaseData hasVariationOrder = createCaseData();
+        hasVariationOrder.getNatureApplicationWrapper()
+            .setNatureOfApplicationChecklist(List.of(CONTESTED_VARIATION_ORDER));
+
+        FinremCaseData isNotInParticipatingFRC = createCaseData();
+        isNotInParticipatingFRC.getRegionWrapper().getAllocatedRegionWrapper()
+            .getDefaultCourtListWrapper().setNottinghamCourtList(BOSTON_COUNTY_COURT_AND_FAMILY_COURT);
+
+        FinremCaseData isNotUnder250k = createCaseData();
+        isNotUnder250k.setEstimatedAssetsChecklistV2(UNABLE_TO_QUANTIFY);
+
+        FinremCaseData isFastTrackApplication = createCaseData();
+        isFastTrackApplication.setFastTrackDecision(YesOrNo.YES);
+
+        FinremCaseData isNotMatrimonialApplication = createCaseData();
+        isNotMatrimonialApplication.getScheduleOneWrapper().setTypeOfApplication(SCHEDULE_1_CHILDREN_ACT_1989);
+
+        return Stream.of(
+            hasVariationOrder,
+            isNotInParticipatingFRC,
+            isNotUnder250k,
+            isFastTrackApplication,
+            isNotMatrimonialApplication
+        );
+    }
+
+    private static FinremCaseData createCaseData() {
+        return FinremCaseData.builder()
+            .scheduleOneWrapper(ScheduleOneWrapper.builder()
+                .typeOfApplication(MATRIMONIAL_AND_CIVIL_PARTNERSHIP_PROCEEDINGS)
+                .build())
+            .regionWrapper(RegionWrapper.builder()
+                .allocatedRegionWrapper(AllocatedRegionWrapper.builder()
+                    .courtListWrapper(DefaultCourtListWrapper.builder()
+                        .nottinghamCourtList(NOTTINGHAM_COUNTY_COURT_AND_FAMILY_COURT)
+                        .build())
+                    .regionList(MIDLANDS)
+                    .midlandsFrcList(RegionMidlandsFrc.NOTTINGHAM)
+                    .build())
+                .build())
+            .natureApplicationWrapper(NatureApplicationWrapper.builder()
+                .natureOfApplicationChecklist(List.of(
+                    MAINTENANCE_PENDING_SUIT,
+                    LUMP_SUM_ORDER,
+                    PROPERTY_ADJUSTMENT_ORDER,
+                    A_SETTLEMENT_OR_A_TRANSFER_OF_PROPERTY,
+                    PERIODICAL_PAYMENT_ORDER,
+                    PENSION_SHARING_ORDER,
+                    PENSION_COMPENSATION_SHARING_ORDER,
+                    PENSION_ATTACHMENT_ORDER,
+                    PENSION_COMPENSATION_ATTACHMENT_ORDER
+                ))
+                .build())
+            .fastTrackDecision(YesOrNo.NO)
+            .estimatedAssetsChecklistV2(UNDER_TWO_HUNDRED_AND_FIFTY_THOUSAND_POUNDS)
+            .build();
+    }
+}
