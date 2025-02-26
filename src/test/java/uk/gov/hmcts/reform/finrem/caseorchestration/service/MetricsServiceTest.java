e7abb4e39d5e4a9ec7f987a96da6ccbb5fc6847c
Paul Trelease <paul.trelease@justice.gov.uk>
2025-02-26 09:03:15 +0000

    wip, switching branch

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/MetricsServiceTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.config.CourtDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.config.CourtDetailsConfiguration;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ConsentOrderWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MetricsServiceTest {
+
+    @Mock
+    private CourtDetailsConfiguration courtDetailsConfiguration;
+    private MetricsService metricsService;
+
+    @BeforeEach
+    void setUp() {
+        metricsService = new MetricsService(courtDetailsConfiguration);
+    }
+
+    @Test
+    void shouldSetCourtMetricsWhenCourtExists() {
+
+
+    }
+}
