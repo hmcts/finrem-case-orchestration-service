3a27cb26df26db8e344144414ba17e716b191db2
Ashley Wong <ashley.wong@justice.gov.uk>
2025-02-27 15:22:16 +0000

    fix

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralEmailMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralEmailMidHandlerTest.java
@@ -3,65 +3,63 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.provider.Arguments;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
-import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralEmailWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintDocumentService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.evidencemanagement.EvidenceManagementDownloadService;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.CREATE_GENERAL_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
-class CreateGeneralEmailConsentMidHandlerTest {
+class GeneralEmailMidHandlerTest {
 
-    private CreateGeneralEmailConsentMidHandler handler;
+    private GeneralEmailMidHandler handler;
 
     @Mock
-    private BulkPrintDocumentService service;
+    private BulkPrintDocumentService bulkPrintDocumentService;
     @Mock
     private FinremCaseDetailsMapper finremCaseDetailsMapper;
+    @Mock
+    private EvidenceManagementDownloadService evidenceManagementDownloadService;
 
     @BeforeEach
     void setup() {
-        handler =  new CreateGeneralEmailConsentMidHandler(finremCaseDetailsMapper, service);
+        handler =  new GeneralEmailMidHandler(finremCaseDetailsMapper, evidenceManagementDownloadService, bulkPrintDocumentService);
     }
 
     @Test
-    void givenACcdCallbackCreateGeneralLetterAboutToSubmitHandler_WhenCanHandleCalled_thenHandlerCanHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.CREATE_GENERAL_EMAIL),
-            is(true));
-    }
-
-    @Test
-    void givenACcdCallbackCreateGeneralLetterAboutToSubmitHandlerJudge_WhenCanHandleCalled_thenHandlerCanHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.CLOSE),
-            is(false));
-    }
-
-    @Test
-    void givenACcdCallbackAboutToSubmit_WhenCanHandleCalled_thenHandlerCanNotHandle() {
-        assertThat(handler
-                .canHandle(CallbackType.SUBMITTED, CaseType.CONTESTED, EventType.CREATE_GENERAL_EMAIL),
-            is(false));
+    void testCanHandle() {
+        assertCanHandle(handler,
+            Arguments.of(MID_EVENT, CONTESTED, CREATE_GENERAL_EMAIL),
+            Arguments.of(MID_EVENT, CONSENTED, CREATE_GENERAL_EMAIL)
+        );
     }
 
     @Test
     void givenACcdCallbackCallbackCreateGeneralEmailMidHandler() {
         FinremCallbackRequest callbackRequest = buildFinremCallbackRequest();
+
+        when(evidenceManagementDownloadService.getByteArray(any(CaseDocument.class), eq(AUTH_TOKEN))).thenReturn(new byte[1]);
+
         handler.handle(callbackRequest, AUTH_TOKEN);
-        verify(service).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
     }
 
     private FinremCallbackRequest buildFinremCallbackRequest() {
@@ -74,7 +72,6 @@ class CreateGeneralEmailConsentMidHandlerTest {
                 .build())
             .build();
         FinremCaseDetails caseDetails = FinremCaseDetails.builder().id(123L).caseType(CaseType.CONSENTED).data(caseData).build();
-        return FinremCallbackRequest.builder().eventType(EventType.CREATE_GENERAL_EMAIL).caseDetails(caseDetails).build();
+        return FinremCallbackRequest.builder().eventType(CREATE_GENERAL_EMAIL).caseDetails(caseDetails).build();
     }
-
 }
