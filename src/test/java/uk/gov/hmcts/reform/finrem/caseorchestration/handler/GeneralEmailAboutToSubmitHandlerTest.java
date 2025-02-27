e8303496978de1b5483f12fd0d76b649fc4a236e
Ashley Wong <ashley.wong@justice.gov.uk>
2025-02-27 16:55:37 +0000

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralEmailAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralEmailAboutToSubmitHandlerTest.java
@@ -32,7 +32,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
