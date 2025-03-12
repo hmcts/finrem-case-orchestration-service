ea5ca2aacaed2b94cee39632e86dff7a925c96d4
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2025-03-12 12:40:14 +0000

    unit tests for SolicitorCreateConsentedMidHandlerTest

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/SolicitorCreateConsentedMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/SolicitorCreateConsentedMidHandlerTest.java
@@ -8,17 +8,30 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 
 @ExtendWith(MockitoExtension.class)
 class SolicitorCreateConsentedMidHandlerTest {
@@ -32,6 +45,14 @@ class SolicitorCreateConsentedMidHandlerTest {
 
     @Mock
     private InternationalPostalService postalService;
+
+    @Mock
+    private FinremCaseDetailsMapper finremCaseDetailsMapper;
+
+    private static final String APPLICANT_POSTCODE_ERROR = "Postcode field is required for applicant address.";
+    private static final String RESPONDENT_POSTCODE_ERROR = "Postcode field is required for respondent address.";
+    private static final String APPLICANT_SOLICITOR_POSTCODE_ERROR = "Postcode field is required for applicant solicitor address.";
+    private static final String RESPONDENT_SOLICITOR_POSTCODE_ERROR = "Postcode field is required for respondent solicitor address.";
 
     @Test
     void given_case_whenEvent_type_is_amendApp_thenCanHandle() {
@@ -56,9 +77,16 @@ class SolicitorCreateConsentedMidHandlerTest {
     @Test
     void handle() {
         CallbackRequest callbackRequest = buildCallbackRequest();
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
         handler.handle(callbackRequest, AUTH_TOKEN);
+
         verify(consentOrderService).performCheck(callbackRequest, AUTH_TOKEN);
         verify(postalService).validate(callbackRequest.getCaseDetails().getData());
+
     }
 
     private CallbackRequest buildCallbackRequest() {
@@ -68,5 +96,168 @@ class SolicitorCreateConsentedMidHandlerTest {
         caseDetails.setData(caseData);
         return CallbackRequest.builder().eventId(EventType.SOLICITOR_CREATE.getCcdType())
             .caseDetails(caseDetails).caseDetailsBefore(caseDetailsBefore).build();
+    }
+
+
+    @Test
+    void givenConsentedCase_WhenNotEmptyPostCode_thenHandlerWillShowNoErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+        FinremCaseData data = finremCallbackRequest.getCaseDetails().getData();
+
+        data.getContactDetailsWrapper().setSolicitorAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            "SW1A 1AA"
+        ));
+
+        data.getContactDetailsWrapper().setApplicantAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            "SW1A 1AA"
+        ));
+
+        data.getContactDetailsWrapper().setRespondentSolicitorAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            "SW1A 2AA"
+        ));
+
+        data.getContactDetailsWrapper().setRespondentAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            "SW1A 2AA"
+        ));
+
+        data.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.YES);
+        data.getContactDetailsWrapper().setContestedRespondentRepresented(YesOrNo.YES);
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> handle = handler.handle(callbackRequest, AUTH_TOKEN);
+
+
+        assertEquals(0, handle.getErrors().size());
+    }
+
+    @Test
+    void givenConsentedCase_WhenEmptyApplicantSolicitorPostCode_thenHandlerWillShowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+        FinremCaseData data = finremCallbackRequest.getCaseDetails().getData();
+
+        data.getContactDetailsWrapper().setApplicantSolicitorAddress(new Address());
+
+        data.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.YES);
+        data.getContactDetailsWrapper().setContestedRespondentRepresented(YesOrNo.YES);
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> handle = handler.handle(callbackRequest, AUTH_TOKEN);
+
+        assertThat(handle.getErrors())
+            .containsExactly(APPLICANT_SOLICITOR_POSTCODE_ERROR);
+    }
+
+    @Test
+    void givenConsentedCase_WhenEmptyApplicantPostCode_thenHandlerWillShowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+        FinremCaseData data = finremCallbackRequest.getCaseDetails().getData();
+
+        data.getContactDetailsWrapper().setApplicantAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            null));
+
+        data.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.NO);
+        data.getContactDetailsWrapper().setContestedRespondentRepresented(YesOrNo.YES);
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> handle = handler.handle(callbackRequest, AUTH_TOKEN);
+
+        assertThat(handle.getErrors())
+            .containsExactly(APPLICANT_POSTCODE_ERROR);
+    }
+
+    @Test
+    void givenConsentedCase_WhenEmptyRespondentSolicitorPostCode_thenHandlerWillShowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+        FinremCaseData data = finremCallbackRequest.getCaseDetails().getData();
+
+        data.getContactDetailsWrapper().setRespondentSolicitorAddress(new Address());
+
+        data.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.YES);
+        data.getContactDetailsWrapper().setContestedRespondentRepresented(YesOrNo.YES);
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> handle = handler.handle(callbackRequest, AUTH_TOKEN);
+
+        assertThat(handle.getErrors())
+            .containsExactly(RESPONDENT_SOLICITOR_POSTCODE_ERROR);
+    }
+
+    @Test
+    void givenConsentedCase_WhenEmptyRespondentPostCode_thenHandlerWillShowErrorMessage() {
+        FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
+        FinremCaseData data = finremCallbackRequest.getCaseDetails().getData();
+
+        data.getContactDetailsWrapper().setRespondentAddress(new Address(
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "County",
+            "Country",
+            "Town",
+            null
+        ));
+
+        data.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.YES);
+        data.getContactDetailsWrapper().setContestedRespondentRepresented(YesOrNo.YES);
+
+        when(finremCaseDetailsMapper.mapToFinremCaseDetails(any(CaseDetails.class)))
+            .thenReturn(finremCallbackRequest.getCaseDetails());
+
+        CallbackRequest callbackRequest = buildCallbackRequest();
+        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>> handle = handler.handle(callbackRequest, AUTH_TOKEN);
+
+        assertThat(handle.getErrors())
+            .containsExactlyInAnyOrder(RESPONDENT_POSTCODE_ERROR);
+    }
+
+    private FinremCallbackRequest buildFinremCallbackRequest() {
+        return FinremCallbackRequest
+            .builder()
+            .eventType(EventType.SOLICITOR_CREATE)
+            .caseDetails(FinremCaseDetails.builder().id(123L).caseType(CONSENTED)
+                .data(FinremCaseData.builder().ccdCaseType(CONTESTED).build()).build())
+            .build();
     }
 }
