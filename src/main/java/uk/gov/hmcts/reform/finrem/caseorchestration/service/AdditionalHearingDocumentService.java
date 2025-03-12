65ca0a7d557ffc98283923de60cfd8b3ee145bab
Ashley Wong <ashley.wong@justice.gov.uk>
2025-03-12 17:44:33 +0000

    Bug fix

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AdditionalHearingDocumentService.java
@@ -196,11 +196,11 @@ public class AdditionalHearingDocumentService {
                     CaseDocument stampedDocs = getStampedDocs(authorisationToken, caseData, caseId, uploadDraftDocument);
                     log.info("Stamped Documents = {} for Case ID: {}", stampedDocs, caseId);
                     if (!finalOrderCollection.isEmpty()) {
-                        caseData.getFinalOrderCollection().add(documentHelper.prepareFinalOrder(stampedDocs));
+                        finalOrderCollection.add(documentHelper.prepareFinalOrder(stampedDocs));
                     } else {
                         List<DirectionOrderCollection> orderList = new ArrayList<>();
                         orderList.add(documentHelper.prepareFinalOrder(stampedDocs));
-                        caseData.setFinalOrderCollection(orderList);
+                        finalOrderCollection.addAll(orderList);
                     }
                     return getDirectionOrderCollection(doc.getValue(), stampedDocs, orderDateTime);
                 }
