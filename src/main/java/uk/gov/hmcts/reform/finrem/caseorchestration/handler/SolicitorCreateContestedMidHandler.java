011300d518de96a339e02e2336a612b056560d1b
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2025-03-12 11:10:38 +0000

    Call validateCaseDataAddresses in SolicitorCreateContestedMidHandler

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SolicitorCreateContestedMidHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/SolicitorCreateContestedMidHandler.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ContactDetailsValidator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -34,10 +37,14 @@ public class SolicitorCreateContestedMidHandler extends FinremCallbackHandler {
     public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
                                                                               String userAuthorisation) {
         FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+        FinremCaseData caseData = caseDetails.getData();
         log.info("Invoking contested event {} mid event callback", EventType.SOLICITOR_CREATE);
 
-        FinremCaseData caseData = caseDetails.getData();
+        List<String> validate = postalService.validate(callbackRequest.getCaseDetails().getData());
+        List<String> errors = ContactDetailsValidator.validateCaseDataAddresses(caseData);
+        errors.addAll(validate);
+
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
-            .data(caseData).errors(postalService.validate(caseData)).build();
+            .data(caseData).errors(errors).build();
     }
 }
