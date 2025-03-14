14cc00189bc79cff458becc50a36c9585a17e468
James Turnbull <james.turnbull@justice.gov.uk>
2025-03-14 16:40:01 +0000

    Add logging

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationAboutToSubmitHandler.java
@@ -12,6 +12,9 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.GeneralApplicationService;
 
+import java.util.Collections;
+import java.util.Optional;
+
 @Slf4j
 @Service
 public class GeneralApplicationAboutToSubmitHandler extends FinremCallbackHandler {
@@ -37,15 +40,44 @@ public class GeneralApplicationAboutToSubmitHandler extends FinremCallbackHandle
     public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
                                                                                    String userAuthorisation) {
         FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+        String caseId = String.valueOf(caseDetails.getId());
         log.info("About to Submit callback event type {} for Case ID: {}",
-            EventType.GENERAL_APPLICATION, caseDetails.getId());
+            EventType.GENERAL_APPLICATION, caseId);
+
+        logAppRespCollection(caseId, caseDetails.getData(), "1");
 
         FinremCaseData caseData = service.updateGeneralApplications(callbackRequest, userAuthorisation);
 
+        logAppRespCollection(caseId, caseData, "2");
+
         helper.deleteNonCollectionGeneralApplication(caseData);
+
+        logAppRespCollection(caseId, caseData, "3");
+        logNoChangeInAppRespCollection(caseId, callbackRequest.getCaseDetailsBefore().getData(), caseData);
 
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).build();
     }
 
+    private void logAppRespCollection(String caseId, FinremCaseData caseData, String message) {
+        int size = Optional.ofNullable(caseData.getGeneralApplicationWrapper().getAppRespGeneralApplications())
+            .orElse(Collections.emptyList())
+            .size();
+
+        log.info("Case ID: {} AppResp GA {} {}", caseId, message, size);
+    }
+
+    private void logNoChangeInAppRespCollection(String caseId, FinremCaseData caseDataBefore, FinremCaseData caseData) {
+        int size = Optional.ofNullable(caseData.getGeneralApplicationWrapper().getAppRespGeneralApplications())
+            .orElse(Collections.emptyList())
+            .size();
+
+        int sizeBefore = Optional.ofNullable(caseDataBefore.getGeneralApplicationWrapper().getAppRespGeneralApplications())
+            .orElse(Collections.emptyList())
+            .size();
+
+        if (size == sizeBefore) {
+            log.error("Case ID: {} DFR-2698 AppResp GA no change {}", caseId, size);
+        }
+    }
 
 }
