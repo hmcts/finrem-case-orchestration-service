c35490d85b637833d0c844c6634d2b697d661be9
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-05-12 09:52:06 +0100

    Remove logging 400 errors and sensitive data (#2293)

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/PBAValidationService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/PBAValidationService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.pba.validation.PBAValidationResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.payments.client.PBAValidationClient;
 
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -14,9 +13,7 @@ public class PBAValidationService {
     private final PBAValidationClient pbaValidationClient;
 
     public boolean isValidPBA(String authToken, String pbaNumber) {
-        log.info("Inside isValidPBA, authToken : {}, pbaNumber : {}", authToken, pbaNumber);
         PBAValidationResponse pbaResponse = pbaValidationClient.isPBAValid(authToken, pbaNumber);
-        log.info("pbaResponse : {}", pbaResponse);
         return pbaResponse.isPbaNumberValid();
     }
 }
