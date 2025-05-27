55a5aa432d6054e3374bf199da73e0c395280a48
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-05-27 11:22:40 +0100

    Javadocs for Dynamiclist constructor

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/DynamicList.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/DynamicList.java
@@ -19,6 +19,18 @@ import java.util.List;
 @Builder
 public class DynamicList {
 
+    /**
+     * Constructs a new {@link DynamicList} instance from a given string value.
+     * <p>
+     * This constructor is primarily used for deserialization purposes, allowing
+     * Jackson to create a {@link DynamicList} object from a string representation.
+     * The provided string value is used to initialize the {@code code} property
+     * of the {@link DynamicListElement} that represents the selected value.
+     * </p>
+     *
+     * @param value the string value used to set the {@code code} property of the
+     *              {@link DynamicListElement} representing the selected value.
+     */
     public DynamicList(String value) {
         this.value = new DynamicListElement();
         this.value.setCode(value);
