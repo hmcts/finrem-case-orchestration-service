aebc3536def1e928e2d5f9417ac3851b29609508
James Turnbull <james.turnbull@justice.gov.uk>
2025-06-09 07:44:21 +0100

    Update ObjectMapper configuration

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/ReadyForHearingConsentedAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/ReadyForHearingConsentedAboutToStartHandler.java
@@ -1,8 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
@@ -10,6 +9,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ConsentedHearingHelpe
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConsentedHearingDataElement;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ConsentedHearingDataWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
@@ -20,10 +20,12 @@ import java.util.List;
 @Service
 public class ReadyForHearingConsentedAboutToStartHandler extends FinremCallbackHandler {
 
+    private final ConsentedHearingHelper consentedHearingHelper;
 
-    @Autowired
-    public ReadyForHearingConsentedAboutToStartHandler(FinremCaseDetailsMapper mapper) {
+    public ReadyForHearingConsentedAboutToStartHandler(FinremCaseDetailsMapper mapper,
+                                                       ConsentedHearingHelper consentedHearingHelper) {
         super(mapper);
+        this.consentedHearingHelper = consentedHearingHelper;
     }
 
     @Override
@@ -36,9 +38,10 @@ public class ReadyForHearingConsentedAboutToStartHandler extends FinremCallbackH
     @Override
     public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
                                                                               String userAuthorisation) {
+        CallbackHandlerLogger.aboutToStart(callbackRequest);
+
         FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
         FinremCaseData caseData = caseDetails.getData();
-        log.info("Received request to invoke event {} for Case ID: {}", EventType.READY_FOR_HEARING, caseDetails.getId());
 
         if (!isHearingDatePresent(caseData)) {
             return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData)
@@ -47,13 +50,10 @@ public class ReadyForHearingConsentedAboutToStartHandler extends FinremCallbackH
         return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).build();
     }
 
-    private static boolean isHearingDatePresent(FinremCaseData caseData) {
-
-        ConsentedHearingHelper helper = new ConsentedHearingHelper(new ObjectMapper());
-        ConsentedHearingDataWrapper listForHearings = helper.getHearings(caseData).stream()
-            .filter(hearing -> !hearing.getValue().getHearingDate().isEmpty())
-            .findAny().orElse(null);
-
-        return listForHearings != null;
+    private boolean isHearingDatePresent(FinremCaseData caseData) {
+        return consentedHearingHelper.getHearings(caseData).stream()
+            .map(ConsentedHearingDataWrapper::getValue)
+            .map(ConsentedHearingDataElement::getHearingDate)
+            .anyMatch(StringUtils::isNotBlank);
     }
 }
