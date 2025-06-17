5ba7450e878cfb0a18b9c735deb4948a68e95e46
dawudgovuk <dawud.rahman@justice.gov.uk>
2025-06-17 12:38:48 +0100

    Fix-Fortify-Vulnerability-ImageUtils

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/ImageUtils.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/ImageUtils.java
@@ -10,20 +10,24 @@ import java.io.InputStream;
  */
 public class ImageUtils {
 
+    private ImageUtils() {
+    }
+
     /**
      * Reads an image file from the classpath and returns its contents as a byte array.
      *
      * @param fileName the name or path of the image file relative to the classpath (e.g., "/images/sample.png")
      * @return a byte array representing the image file content
-     * @throws IOException if an I/O error occurs while reading the file
+     * @throws IOException           if an I/O error occurs while reading the file
      * @throws IllegalStateException if the file is not found on the classpath
      */
     public static byte[] imageAsBytes(String fileName) throws IOException {
-        try (InputStream inputStream = ImageUtils.class.getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                throw new IllegalStateException("File not found: " + fileName);
-            }
-            return IOUtils.toByteArray(inputStream);
+        InputStream inputStream = ImageUtils.class.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalStateException("File not found: " + fileName);
+        }
+        try (InputStream is = inputStream) {
+            return IOUtils.toByteArray(is);
         }
     }
 }
