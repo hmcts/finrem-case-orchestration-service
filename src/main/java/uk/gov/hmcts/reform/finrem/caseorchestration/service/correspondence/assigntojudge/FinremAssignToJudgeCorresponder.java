f3836f86b19768df926d7d5080af66187402b0fa
Ashley Wong <im@so99y.com>
2025-06-23 11:37:19 +0100

    Java Doc

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/FinremAssignToJudgeCorresponder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/FinremAssignToJudgeCorresponder.java
@@ -7,6 +7,26 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignedToJudgeDocum
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
 
+/**
+ * Corresponder for the "Assign to Judge" event in financial remedy cases.
+ * <p>
+ * This class extends {@link AbstractAssignToJudgeCorresponder} and inherits all default behaviour for sending
+ * assign-to-judge notifications, including sending letters to all parties (applicant, respondent, and interveners),
+ * and sending emails to solicitors where applicable.
+ * </p>
+ *
+ * <p>
+ * Unlike {@link IssueApplicationConsentCorresponder}, this implementation does not override any behaviour,
+ * meaning it sends letters to all recipients regardless of whether they reside outside the UK.
+ * </p>
+ *
+ * <p>
+ * It is suitable for general use in events where international filtering is not required.
+ * </p>
+ *
+ * @see AbstractAssignToJudgeCorresponder
+ * @see IssueApplicationConsentCorresponder
+ */
 @Slf4j
 @Component
 public class FinremAssignToJudgeCorresponder extends AbstractAssignToJudgeCorresponder {
