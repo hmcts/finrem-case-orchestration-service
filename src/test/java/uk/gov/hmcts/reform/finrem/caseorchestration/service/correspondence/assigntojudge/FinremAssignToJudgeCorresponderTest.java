a2961d168f38471020e074505896d4e9fb919ab1
Ashley Wong <im@so99y.com>
2025-06-23 16:31:04 +0100

    tidy up

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/FinremAssignToJudgeCorresponderTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/FinremAssignToJudgeCorresponderTest.java
@@ -338,7 +338,6 @@ class FinremAssignToJudgeCorresponderTest {
         verify(notificationService, never()).sendAssignToJudgeConfirmationEmailToRespondentSolicitor(caseDetails);
         verify(bulkPrintService, never()).sendDocumentForPrint(expectedRespondentCaseDocument, caseDetails, CCDConfigConstant.RESPONDENT, AUTH_TOKEN);
         assertThat(logs.getInfos()).contains(format("Nothing is sent to respondent for Case ID: %s", CASE_ID));
-        logs.reset();
     }
 
     @Test
