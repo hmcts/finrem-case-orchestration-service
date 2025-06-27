8a7bb737ce7a0c3b63fb523c35c2ce4c1cc70ac4
Ashley Wong <im@so99y.com>
2025-06-27 12:38:17 +0100

    Add an event for migration

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/managehearings/ManageHearingsMigrationAboutToSubmitHandler.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.managehearings;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.managehearings.ManageHearingsMigrationService;
+
+@Slf4j
+@Service
+public class ManageHearingsMigrationAboutToSubmitHandler extends FinremCallbackHandler {
+
+    private final ManageHearingsMigrationService manageHearingsMigrationService;
+
+    public ManageHearingsMigrationAboutToSubmitHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                                       ManageHearingsMigrationService manageHearingsMigrationService) {
+        super(finremCaseDetailsMapper);
+        this.manageHearingsMigrationService = manageHearingsMigrationService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.ABOUT_TO_SUBMIT.equals(callbackType)
+            && CaseType.CONTESTED.equals(caseType)
+            && EventType.MANAGE_HEARINGS_MIGRATION.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.aboutToSubmit(callbackRequest));
+        FinremCaseDetails finremCaseDetails = callbackRequest.getCaseDetails();
+
+        FinremCaseData finremCaseData = finremCaseDetails.getData();
+
+        manageHearingsMigrationService.populateListForHearingWrapper(finremCaseData);
+
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(finremCaseData).build();
+    }
+}
