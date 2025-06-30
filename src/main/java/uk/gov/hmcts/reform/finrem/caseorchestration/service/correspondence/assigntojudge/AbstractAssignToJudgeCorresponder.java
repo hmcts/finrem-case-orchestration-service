546266b4ffc2d162f906e3f2702b49b57763b449
Ashley Wong <50691452+so99y@users.noreply.github.com>
2025-06-30 10:36:09 +0100

    DFR-3625 CS: Prevent Issue Application to send letter to international respondents (#2355)
    * DFR-3674 Check resideOutsideUK before checking post code nullity
    
    * Fix existing test case
    
    * Add test scenarios
    
    * tidy up
    
    * update test case and checkstyle count
    
    * revert test case
    
    * Fix test cases
    
    * update logic and java doc
    
    * fix test case
    
    * Tidy up
    
    * Fix
    
    * code ordering
    
    * fine-tuning
    
    * not calling code when null
    
    * revert
    
    * fix
    
    * fix
    
    * Tidy up
    
    * check null
    
    * formatting
    
    * Fix
    
    * update checkstyle count
    
    * refactoring
    
    * add edge cases.
    
    * add edge cases.
    
    * checkestyle
    
    * Fix
    
    * checkstyle
    
    * edge case
    
    * typo
    
    * Fixed regression test on Amend Application Details
    
    * Fixed regression test on Amend Application Details
    
    * tidy up
    
    * tidy up
    
    * update checkstyle warnings count
    
    * update checkstyle warnings count
    
    * JavaDoc
    
    * Fix test case
    
    * add contested path
    
    * update JavaDoc
    
    * Applying CallbackHandlerLogger
    
    * update checkstyle maxWarnings
    
    * Applying CallbackHandlerLogger
    
    * Skip sending letter to respondent's international address.
    
    * remove validateCaseData
    
    * update
    
    * public to protected
    
    * typo
    
    * Test case for AssignedToJudgeSkipRespIntlPostCorresponder
    
    * intervener cases
    
    * checkstyle
    
    * Intervener test case.
    
    * typo
    
    * revert
    
    * Skip postcode checking for non-uk respondent.
    
    * Tidy up
    
    * Remove unused methods.
    
    * Tidy up
    
    * conflict bug
    
    * update maxWarnings
    
    * checkstyle fix
    
    * update maxWarnings
    
    * refactoring
    
    * Fix Sonar cloud
    
    * checkstyle
    
    * introducing AbstractAssignToJudgeCorresponder.java
    
    * refactoring
    
    * Remove test cases for contested cases as it only handles consented cases.
    
    * Remove test cases for contested cases as it only handles consented cases.
    
    * wrong package
    
    * move common functions to CorresponderBase
    
    * checkstyle
    
    * checkstyle
    
    * update maxWarnings
    
    * Java Doc
    
    * Java Doc
    
    * tidy up
    
    * checkstyle
    
    * update test case
    
    * update test cases
    
    * Rename
    
    * remove unused code
    
    * remove extra line
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>
    Co-authored-by: tomfairclough1 <167980396+tomfairclough1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/AbstractAssignToJudgeCorresponder.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.assigntojudge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.intevener.IntervenerWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignedToJudgeDocumentService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.FinremSingleLetterOrEmailAllPartiesCorresponder;
+
+/**
+ * Abstract base class for sending "Assign to Judge" notifications to all relevant parties in a financial remedy case.
+ *
+ * <p>
+ * This class handles the orchestration of notifications and letter generation for the Assign to Judge event,
+ * including emails to solicitors and letters for bulk print. Subclasses can specialise behaviour such as skipping
+ * letters to international respondents (e.g. {@code FinremAssignToJudgeCorresponder} was previously used for Issue Application,
+ * but that use case avoids sending letters to international respondents).
+ * </p>
+ *
+ * <p>
+ * This class extends {@link FinremSingleLetterOrEmailAllPartiesCorresponder}, and uses the
+ * {@link AssignedToJudgeDocumentService} to generate the relevant documents.
+ * </p>
+ *
+ * @see uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.FinremSingleLetterOrEmailAllPartiesCorresponder
+ * @see uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignedToJudgeDocumentService
+ */
+@Component
+@Slf4j
+public abstract class AbstractAssignToJudgeCorresponder extends FinremSingleLetterOrEmailAllPartiesCorresponder {
+
+    private final AssignedToJudgeDocumentService assignedToJudgeDocumentService;
+
+    @Autowired
+    public AbstractAssignToJudgeCorresponder(NotificationService notificationService,
+                                             BulkPrintService bulkPrintService,
+                                             AssignedToJudgeDocumentService assignedToJudgeDocumentService) {
+        super(notificationService, bulkPrintService);
+        this.assignedToJudgeDocumentService = assignedToJudgeDocumentService;
+    }
+
+    @Override
+    protected boolean shouldSendIntervenerLetter(IntervenerWrapper intervenerWrapper) {
+        return intervenerWrapper.getIntervenerName() != null && !intervenerWrapper.getIntervenerName().isEmpty();
+    }
+
+    @Override
+    protected boolean shouldSendRespondentSolicitorEmail(FinremCaseDetails caseDetails) {
+        return notificationService.isRespondentSolicitorEmailPopulated(caseDetails);
+    }
+
+    @Override
+    protected boolean shouldSendApplicantSolicitorEmail(FinremCaseDetails caseDetails) {
+        return notificationService.isApplicantSolicitorEmailPopulated(caseDetails);
+    }
+
+    @Override
+    public CaseDocument getDocumentToPrint(FinremCaseDetails caseDetails, String authorisationToken,
+                                           DocumentHelper.PaperNotificationRecipient recipient) {
+        return assignedToJudgeDocumentService.generateAssignedToJudgeNotificationLetter(
+            caseDetails, authorisationToken, recipient);
+    }
+
+    @Override
+    protected void emailApplicantSolicitor(FinremCaseDetails caseDetails) {
+        notificationService.sendAssignToJudgeConfirmationEmailToApplicantSolicitor(caseDetails);
+    }
+
+    @Override
+    protected void emailRespondentSolicitor(FinremCaseDetails caseDetails) {
+        notificationService.sendAssignToJudgeConfirmationEmailToRespondentSolicitor(caseDetails);
+    }
+
+    @Override
+    protected void emailIntervenerSolicitor(IntervenerWrapper intervenerWrapper, FinremCaseDetails caseDetails) {
+        notificationService.sendAssignToJudgeConfirmationEmailToIntervenerSolicitor(caseDetails,
+            notificationService.getCaseDataKeysForIntervenerSolicitor(intervenerWrapper));
+    }
+}
