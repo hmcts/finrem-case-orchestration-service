395fbffc6120d30a3231196a29813022e668146e
Ashley Wong <im@so99y.com>
2025-07-04 13:05:41 +0100

    Add unit test for interim hearings

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/migration/HearingsAppenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/migration/HearingsAppenderTest.java
@@ -9,6 +9,8 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Court;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DocumentCollectionItem;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HearingTypeDirection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.InterimHearingItem;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.InterimTypeOfHearing;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.Hearing;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
@@ -23,6 +25,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -88,7 +91,6 @@ public class HearingsAppenderTest {
             .hearingRegionWrapper(hearingRegionWrapper)
             .additionalListOfHearingDocuments(additionalDoc)
             .build();
-        FinremCaseData caseData = FinremCaseData.builder().listForHearingWrapper(listForHearingWrapper).build();
 
         // Act
         Hearing result = underTest.toHearing(listForHearingWrapper);
@@ -104,5 +106,45 @@ public class HearingsAppenderTest {
         assertThat(result.getAdditionalHearingDocs())
             .extracting(DocumentCollectionItem::getValue)
             .containsExactly(additionalDoc);
+    }
+
+    @Test
+    void shouldConvertInterimHearingItemToHearing() {
+        // Arrange
+        LocalDate hearingDate = LocalDate.of(2025, 7, 3);
+        String hearingTime = "10:30 AM";
+        String timeEstimate = "1 hour";
+        String additionalInfo = "Judge prefers early hearing";
+        InterimTypeOfHearing typeOfHearing = InterimTypeOfHearing.FH;
+
+        HearingType expectedHearingType = HearingType.FH;
+        Court expectedCourt = mock(Court.class);
+
+        CaseDocument additionalDoc = mock(CaseDocument.class);
+
+        InterimHearingItem interimHearingItem = spy(InterimHearingItem.builder()
+            .interimHearingDate(hearingDate)
+            .interimHearingTime(hearingTime)
+            .interimHearingTimeEstimate(timeEstimate)
+            .interimAdditionalInformationAboutHearing(additionalInfo)
+            .interimHearingType(typeOfHearing)
+            .interimUploadAdditionalDocument(additionalDoc)
+            .build());
+        when(interimHearingItem.toCourt()).thenReturn(expectedCourt);
+
+        // Act
+        Hearing result = underTest.toHearing(interimHearingItem);
+
+        // Assert
+        assertEquals(hearingDate, result.getHearingDate());
+        assertEquals(expectedHearingType, result.getHearingType());
+        assertEquals(hearingTime, result.getHearingTime());
+        assertEquals(timeEstimate, result.getHearingTimeEstimate());
+        assertEquals(additionalInfo, result.getAdditionalHearingInformation());
+        assertEquals(expectedCourt, result.getHearingCourtSelection());
+        assertEquals(YesOrNo.YES, result.getWasMigrated());
+        assertThat(result.getAdditionalHearingDocs())
+                .extracting(DocumentCollectionItem::getValue)
+                .containsExactly(additionalDoc);
     }
 }
