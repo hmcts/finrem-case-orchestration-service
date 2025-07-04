375dee4c1f69a47c5147920ed30cfc422b25dd5b
Ashley Wong <im@so99y.com>
2025-07-04 09:08:04 +0100

    Split to HearingsAppenderTest

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/migration/HearingTabItemsAppender.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/migration/HearingTabItemsAppender.java
@@ -7,10 +7,6 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DocumentCollectionItem;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HearingTypeDirection;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.Hearing;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.ManageHearingsCollectionItem;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.tabs.HearingTabCollectionItem;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.tabs.HearingTabItem;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.HearingRegionWrapper;
@@ -41,39 +37,6 @@ public class HearingTabItemsAppender {
         addItemToList(caseData.getManageHearingsWrapper()::getRespondentHHearingTabItems,
                 caseData.getManageHearingsWrapper()::setRespondentHHearingTabItems, item);
          */
-    }
-
-    public void appendToHearings(FinremCaseData caseData, ManageHearingsCollectionItem item) {
-        addItemToList(caseData.getManageHearingsWrapper()::getHearings,
-            caseData.getManageHearingsWrapper()::setHearings, item);
-    }
-
-    public Hearing toHearing(ListForHearingWrapper listForHearingWrapper) {
-        // Type of Hearing
-        HearingTypeDirection hearingType = listForHearingWrapper.getHearingType();
-        // Hearing Date
-        LocalDate hearingDate = listForHearingWrapper.getHearingDate();
-        // Hearing Time
-        String hearingTime = listForHearingWrapper.getHearingTime();
-        // Time Estimate
-        String timeEstimate = listForHearingWrapper.getTimeEstimate();
-        // Additional information about the hearing
-        String additionalInformationAboutHearing = listForHearingWrapper.getAdditionalInformationAboutHearing();
-        // Hearing Court - Please state in which Financial Remedies Court Zone the applicant resides
-        HearingRegionWrapper hearingRegionWrapper = listForHearingWrapper.getHearingRegionWrapper();
-
-        return Hearing.builder()
-            .hearingDate(hearingDate)
-            .hearingType(HearingType.valueOf(hearingType.name()))
-            .hearingTimeEstimate(timeEstimate)
-            .hearingTime(hearingTime)
-            .hearingCourtSelection(hearingRegionWrapper.toCourt())
-            //.hearingMode(null) // Ignore it because existing List for Hearing doesn't capture hearing mode
-            .additionalHearingInformation(additionalInformationAboutHearing)
-            .additionalHearingDocs(toAdditionalHearingDocs(listForHearingWrapper))
-            //.partiesOnCaseMultiSelectList() // Unknown as partiesOnCase is updated by multiple events.
-            .wasMigrated(YesOrNo.YES)
-            .build();
     }
 
     public HearingTabItem toHearingTabItem(ListForHearingWrapper listForHearingWrapper) {
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/migration/HearingsAppenderTest.java
@@ -0,0 +1,108 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.managehearings.migration;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Court;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DocumentCollectionItem;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HearingTypeDirection;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.Hearing;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.ManageHearingsCollectionItem;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.HearingRegionWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ListForHearingWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ManageHearingsWrapper;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class HearingsAppenderTest {
+
+    @InjectMocks
+    private HearingsAppender underTest;
+
+    @Test
+    void shouldAppendToHearings() {
+        // Arrange
+        ManageHearingsCollectionItem item = mock(ManageHearingsCollectionItem.class);
+        FinremCaseData caseData = FinremCaseData.builder().build();
+
+        // Act
+        underTest.appendToHearings(caseData, item);
+
+        // Assert
+        assertThat(caseData.getManageHearingsWrapper().getHearings()).containsExactly(item);
+    }
+
+    @Test
+    void shouldAppendToHearingsWithExistingHearingExists() {
+        // Arrange
+        ManageHearingsCollectionItem existing = mock(ManageHearingsCollectionItem.class);
+        ManageHearingsCollectionItem item = mock(ManageHearingsCollectionItem.class);
+        FinremCaseData caseData = FinremCaseData.builder()
+            .manageHearingsWrapper(ManageHearingsWrapper.builder()
+                .hearings(List.of(existing))
+                .build())
+            .build();
+
+        // Act
+        underTest.appendToHearings(caseData, item);
+
+        // Assert
+        assertThat(caseData.getManageHearingsWrapper().getHearings()).containsExactly(existing, item);
+    }
+
+    @Test
+    void shouldConvertListForHearingWrapperToHearing() {
+        // Arrange
+        LocalDate hearingDate = LocalDate.of(2025, 7, 3);
+        String hearingTime = "10:30 AM";
+        String timeEstimate = "1 hour";
+        String additionalInfo = "Judge prefers early hearing";
+        HearingTypeDirection hearingTypeDirection = HearingTypeDirection.FDA;
+
+        HearingType expectedHearingType = HearingType.FDA;
+        Court expectedCourt = mock(Court.class);
+
+        HearingRegionWrapper hearingRegionWrapper = mock(HearingRegionWrapper.class);
+        when(hearingRegionWrapper.toCourt()).thenReturn(expectedCourt);
+
+        CaseDocument additionalDoc = mock(CaseDocument.class);
+
+        ListForHearingWrapper listForHearingWrapper = ListForHearingWrapper.builder()
+            .hearingDate(hearingDate)
+            .hearingTime(hearingTime)
+            .timeEstimate(timeEstimate)
+            .additionalInformationAboutHearing(additionalInfo)
+            .hearingType(hearingTypeDirection)
+            .hearingRegionWrapper(hearingRegionWrapper)
+            .additionalListOfHearingDocuments(additionalDoc)
+            .build();
+        FinremCaseData caseData = FinremCaseData.builder().listForHearingWrapper(listForHearingWrapper).build();
+
+        // Act
+        Hearing result = underTest.toHearing(caseData);
+
+        // Assert
+        assertEquals(hearingDate, result.getHearingDate());
+        assertEquals(expectedHearingType, result.getHearingType());
+        assertEquals(hearingTime, result.getHearingTime());
+        assertEquals(timeEstimate, result.getHearingTimeEstimate());
+        assertEquals(additionalInfo, result.getAdditionalHearingInformation());
+        assertEquals(expectedCourt, result.getHearingCourtSelection());
+        assertEquals(YesOrNo.YES, result.getWasMigrated());
+        assertThat(result.getAdditionalHearingDocs())
+            .extracting(DocumentCollectionItem::getValue)
+            .containsExactly(additionalDoc);
+    }
+}
