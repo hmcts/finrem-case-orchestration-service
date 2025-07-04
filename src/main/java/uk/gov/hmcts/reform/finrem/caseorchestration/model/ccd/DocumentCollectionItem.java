4e0d64bc0ba17b9f66faab50b86098a9cf439c4a
Ashley Wong <im@so99y.com>
2025-07-04 10:58:24 +0100

    unit test

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/DocumentCollectionItem.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/DocumentCollectionItem.java
@@ -17,6 +17,13 @@ public class DocumentCollectionItem implements HasCaseDocument {
     private UUID id;
     private CaseDocument value;
 
+    /**
+     * Converts a {@link CaseDocument} into a {@link DocumentCollectionItem}.
+     *
+     * @param caseDocument the case document to convert; may be {@code null}
+     * @return a {@link DocumentCollectionItem} wrapping the given case document,
+     *         or {@code null} if the input is {@code null}
+     */
     public static DocumentCollectionItem fromCaseDocument(CaseDocument caseDocument) {
         return caseDocument == null ? null : DocumentCollectionItem.builder().value(caseDocument).build();
     }
