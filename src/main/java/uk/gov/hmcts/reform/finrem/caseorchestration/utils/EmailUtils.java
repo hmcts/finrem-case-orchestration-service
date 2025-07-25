a238a20e6d19908d797285e9bb118f408a540b2f
Ashley Wong <im@so99y.com>
2025-07-25 15:55:07 +0100

    Add EmailUtils

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/EmailUtils.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EmailUtils {
+
+    private EmailUtils() {
+    }
+
+    /**
+     * Validates whether the given email address is in a valid format.
+     *
+     * <p>
+     * This method attempts to create and validate a {@link javax.mail.internet.InternetAddress}
+     * using the provided email string. If the email format is valid, it returns {@code true};
+     * otherwise, it returns {@code false}.
+     * </p>
+     *
+     * @param email the email address to validate
+     * @return {@code true} if the email address is valid; {@code false} otherwise
+     */
+    public static boolean isValidEmailAddress(final String email) {
+        return org.apache.commons.validator.routines.EmailValidator.getInstance().isValid(email);
+    }
+}
