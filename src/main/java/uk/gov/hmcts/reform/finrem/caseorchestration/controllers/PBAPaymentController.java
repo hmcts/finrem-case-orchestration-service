2729f20e8d522ae6142d12ef9b7d504fae774560
dawudgovuk <dawud.rahman@justice.gov.uk>
2025-07-28 12:30:23 +0100

    DFR-3895-Fix-Same_Solicitor_For_App_And_Resp

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/PBAPaymentController.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/controllers/PBAPaymentController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.organisation.OrganisationsResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignCaseAccessService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CcdDataStoreService;
@@ -30,9 +31,11 @@ import java.util.Map;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.AUTHORIZATION_HEADER;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_RESPONDENT_REPRESENTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ORGANISATION_POLICY_APPLICANT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ORGANISATION_POLICY_ORGANISATION;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ORGANISATION_POLICY_ORGANISATION_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.ORGANISATION_POLICY_RESPONDENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.SUBMIT_CASE_DATE;
 
 @RestController
@@ -73,9 +76,16 @@ public class PBAPaymentController extends BaseController {
         if (assignCaseAccessService.isCreatorRoleActiveOnCase(caseDetails)) {
             try {
                 String applicantOrgId = getApplicantOrgId(caseDetails);
+                String respondentOrgId = getRespondentOrgId(caseDetails);
 
                 if (applicantOrgId != null) {
                     OrganisationsResponse prdOrganisation = prdOrganisationService.retrieveOrganisationsData(authToken);
+
+                    // Check if the applicant organisation and respondent organisation are the same if the respondent is represented
+                    if(isRespondentRepresented(caseDetails) && (respondentOrgId != null && applicantOrgId.equals(respondentOrgId))) {
+                        String errorMessage = "Applicant organisation cannot be the same as respondent organisation";
+                        return assignCaseAccessFailure(caseDetails, singletonList(errorMessage));
+                    }
 
                     if (prdOrganisation.getOrganisationIdentifier().equals(applicantOrgId)) {
                         log.info("Assigning case access for Case ID: {}", caseDetails.getId());
@@ -121,6 +131,23 @@ public class PBAPaymentController extends BaseController {
         }
 
         return null;
+    }
+
+    private String getRespondentOrgId(CaseDetails caseDetails) {
+        Map<String, Object> respondentOrgPolicy = (Map<String, Object>) caseDetails.getData().get(ORGANISATION_POLICY_RESPONDENT);
+        if (respondentOrgPolicy != null) {
+            Map<String, Object> respondentOrganisation = (Map<String, Object>) respondentOrgPolicy.get(ORGANISATION_POLICY_ORGANISATION);
+
+            if (respondentOrganisation != null) {
+                return (String) respondentOrganisation.get(ORGANISATION_POLICY_ORGANISATION_ID);
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isRespondentRepresented(CaseDetails caseDetails) {
+        return (YesOrNo.isYes(caseDetails.getData().get(CONSENTED_RESPONDENT_REPRESENTED).toString()));
     }
 
     private ResponseEntity<AboutToStartOrSubmitCallbackResponse> assignCaseAccessFailure(CaseDetails caseDetails, List<String> errorDetails) {
