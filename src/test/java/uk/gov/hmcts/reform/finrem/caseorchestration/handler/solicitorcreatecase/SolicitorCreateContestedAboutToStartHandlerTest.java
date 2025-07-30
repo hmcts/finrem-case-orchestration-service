8981e19c3af0806ba1e7efc45d9c69609ac7947b
Ashley Wong <im@so99y.com>
2025-07-30 11:47:38 +0100

    tidy up

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/SolicitorCreateContestedAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/SolicitorCreateContestedAboutToStartHandlerTest.java
@@ -5,23 +5,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
 
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.SOLICITOR_CREATE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
 class SolicitorCreateContestedAboutToStartHandlerTest {
 
-    private SolicitorCreateContestedAboutToStartHandler handler;
+    private SolicitorCreateContestedAboutToStartHandler underTest;
 
     @Mock
     private FinremCaseDetailsMapper finremCaseDetailsMapper;
@@ -31,20 +32,20 @@ class SolicitorCreateContestedAboutToStartHandlerTest {
 
     @BeforeEach
     void setup() {
-        handler =  new SolicitorCreateContestedAboutToStartHandler(finremCaseDetailsMapper,
+        underTest =  new SolicitorCreateContestedAboutToStartHandler(finremCaseDetailsMapper,
             onStartDefaultValueService);
     }
 
     @Test
     void testHandlerCanHandle() {
-        assertCanHandle(handler, CallbackType.ABOUT_TO_START, CaseType.CONTESTED, EventType.SOLICITOR_CREATE);
+        assertCanHandle(underTest, ABOUT_TO_START, CONTESTED, SOLICITOR_CREATE);
     }
 
     @Test
     void handle() {
         FinremCallbackRequest finremCallbackRequest = buildFinremCallbackRequest();
 
-        handler.handle(finremCallbackRequest, AUTH_TOKEN);
+        underTest.handle(finremCallbackRequest, AUTH_TOKEN);
 
         verify(onStartDefaultValueService).defaultCivilPartnershipField(finremCallbackRequest);
         verify(onStartDefaultValueService).defaultTypeOfApplication(finremCallbackRequest);
@@ -53,7 +54,7 @@ class SolicitorCreateContestedAboutToStartHandlerTest {
 
     private FinremCallbackRequest buildFinremCallbackRequest() {
         FinremCaseData caseData = FinremCaseData.builder().build();
-        FinremCaseDetails caseDetails = FinremCaseDetails.builder().id(123L).data(caseData).build();
-        return FinremCallbackRequest.builder().eventType(EventType.SOLICITOR_CREATE).caseDetails(caseDetails).build();
+        FinremCaseDetails caseDetails = FinremCaseDetails.builder().id(Long.valueOf(CASE_ID)).data(caseData).build();
+        return FinremCallbackRequest.builder().eventType(SOLICITOR_CREATE).caseDetails(caseDetails).build();
     }
 }
