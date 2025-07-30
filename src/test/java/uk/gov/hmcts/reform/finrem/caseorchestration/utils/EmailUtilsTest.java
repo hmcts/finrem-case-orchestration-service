7545145ef1a79648c2d7eeffeb0be94afc7086f2
Ashley Wong <im@so99y.com>
2025-07-30 13:21:25 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/EmailUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/EmailUtilsTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EmailUtilsTest {
 
