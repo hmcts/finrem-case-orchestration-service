8bd48662d20b42820a0ecd1b0e459e9f7c7a96b9
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-07-31 14:04:47 +0100

    Submitted handler unit tests for new General Application Directions event

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/generalapplicationdirections/GeneralApplicationDirectionsSubmittedHandlerTest.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.generalapplicationdirections;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.GeneralApplicationDirectionsSubmittedHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.*;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.Hearing;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingMode;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.ManageHearingsCollectionItem;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.DefaultCourtListWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ManageHearingsWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.managehearing.ManageHearingsCorresponder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+
+@ExtendWith(MockitoExtension.class)
+class GeneralApplicationDirectionsSubmittedHandlerTest {
+
+    @InjectMocks
+    private GeneralApplicationDirectionsSubmittedHandler generalApplicationDirectionsSubmittedHandler;
+
+    @Mock
+    private ManageHearingsCorresponder manageHearingsCorresponder;
+
+    @Test
+    void testCanHandle() {
+        Assertions.assertCanHandle(generalApplicationDirectionsSubmittedHandler, CallbackType.SUBMITTED, CaseType.CONTESTED,
+            EventType.GENERAL_APPLICATION_DIRECTIONS_MH);
+    }
+
+    @Test
+    void shouldHandleSubmittedCallback() {
+        // Arrange
+        UUID hearingID = UUID.randomUUID();
+        FinremCallbackRequest callbackRequest = buildCallbackRequest(hearingID, hearingID);
+
+        // Act
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response =
+            generalApplicationDirectionsSubmittedHandler.handle(callbackRequest, AUTH_TOKEN);
+
+        // Assert
+        assertThat(response.getData()).isNotNull();
+        assertThat(response.getErrors()).isNullOrEmpty();
+        verify(manageHearingsCorresponder).sendHearingCorrespondence(callbackRequest, AUTH_TOKEN);
+    }
+
+    private FinremCallbackRequest buildCallbackRequest(UUID hearingID, UUID hearingItemId) {
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .manageHearingsWrapper(ManageHearingsWrapper
+                .builder()
+                .workingHearingId(hearingID)
+                .hearings(List.of(
+                    ManageHearingsCollectionItem
+                        .builder()
+                        .id(hearingItemId)
+                        .value(Hearing
+                            .builder()
+                            .hearingType(HearingType.APPEAL_HEARING)
+                            .hearingDate(LocalDate.now())
+                            .hearingTime("10:00 AM")
+                            .hearingTimeEstimate("2 hours")
+                            .hearingMode(HearingMode.IN_PERSON)
+                            .additionalHearingInformation("Additional Info")
+                            .hearingCourtSelection(Court.builder()
+                                .region(Region.LONDON)
+                                .londonList(RegionLondonFrc.LONDON)
+                                .courtListWrapper(DefaultCourtListWrapper.builder()
+                                    .cfcCourtList(CfcCourt.BROMLEY_COUNTY_COURT_AND_FAMILY_COURT)
+                                    .build())
+                                .build())
+                            .build())
+                        .build()
+                ))
+                .build())
+            .build();
+
+        FinremCaseDetails caseDetails = FinremCaseDetails.builder()
+            .data(finremCaseData)
+            .build();
+
+        return FinremCallbackRequest.builder()
+            .caseDetails(caseDetails)
+            .build();
+    }
+
+}
