7938c2af5ccf7b255c2878188ef4add2fb499346
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-07-31 10:07:44 +0100

    Add Submitted Event for sending notifications on the GAD MH event

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationDirectionsSubmittedHandler.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.managehearing.ManageHearingsCorresponder;
+
+@Slf4j
+@Service
+public class GeneralApplicationDirectionsSubmittedHandler extends FinremCallbackHandler {
+
+    private final ManageHearingsCorresponder manageHearingsCorresponder;
+
+    public GeneralApplicationDirectionsSubmittedHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                                        ManageHearingsCorresponder manageHearingsCorresponder) {
+        super(finremCaseDetailsMapper);
+        this.manageHearingsCorresponder = manageHearingsCorresponder;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.SUBMITTED.equals(callbackType)
+            && CaseType.CONTESTED.equals(caseType)
+            && EventType.GENERAL_APPLICATION_DIRECTIONS_MH.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.submitted(callbackRequest));
+
+        // Send Notifications
+        manageHearingsCorresponder.sendHearingCorrespondence(callbackRequest, userAuthorisation);
+
+        // Where appropriate, send generated documents, this is upcoming work.
+
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
+            .data(callbackRequest.getCaseDetails().getData())
+            .build();
+    }
+}
