e1c1373117ede0975b13836c730f5c0fcc9efda4
Adam Lee <adam.lee2@justice.gov.uk>
2025-08-05 14:30:24 +0100

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/ManageHearingActionService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/managehearings/ManageHearingActionService.java
@@ -94,7 +94,8 @@ public class ManageHearingActionService {
             Map<String, CaseDocument> pfdDocs = manageHearingsDocumentService.generatePfdNcdrDocuments(finremCaseDetails, authToken);
 
             if (pfdDocs.get(PFD_NCDR_COVER_LETTER) != null) {
-                documentMap.put(PFD_NCDR_COVER_LETTER, new DocumentRecord(pfdDocs.get(PFD_NCDR_COVER_LETTER), CaseDocumentType.PFD_NCDR_COVER_LETTER));
+                documentMap.put(PFD_NCDR_COVER_LETTER, new DocumentRecord(pfdDocs.get(PFD_NCDR_COVER_LETTER),
+                    CaseDocumentType.PFD_NCDR_COVER_LETTER));
             }
 
             documentMap.put(PFD_NCDR_COMPLIANCE_LETTER, new DocumentRecord(pfdDocs.get(PFD_NCDR_COMPLIANCE_LETTER),
