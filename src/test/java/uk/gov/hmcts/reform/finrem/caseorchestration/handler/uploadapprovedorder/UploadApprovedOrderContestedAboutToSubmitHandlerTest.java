d47d4e75494a62e3ab23eab7930ed2e454b40cf2
Ashley Wong <im@so99y.com>
2025-08-12 10:19:18 +0100

    update unit tests.

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderContestedAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderContestedAboutToSubmitHandlerTest.java
@@ -13,9 +13,15 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.UploadApprovedOrderService;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
@@ -41,11 +47,25 @@ class UploadApprovedOrderContestedAboutToSubmitHandlerTest {
     }
 
     @Test
-    public void givenContestedCase_whenAboutToSubmitUploadApprovedOrderAndNoErrors_thenHandle() {
+    void givenContestedCase_whenAboutToSubmitUploadApprovedOrderAndNoErrors_thenHandle() {
         FinremCallbackRequest finremCallbackRequest = FinremCallbackRequestFactory
             .from(Long.valueOf(CASE_ID), CONTESTED, UPLOAD_APPROVED_ORDER);
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = underTest.handle(finremCallbackRequest, AUTH_TOKEN);
         assertTrue(response.getErrors().isEmpty());
         verify(uploadApprovedOrderService).processApprovedOrders(finremCallbackRequest, new ArrayList<>(), AUTH_TOKEN);
+    }
+
+    @Test
+    void givenContestedCase_whenUploadDocumentWithWarnings_thenReturnWarnings() {
+        FinremCallbackRequest finremCallbackRequest = FinremCallbackRequestFactory
+            .from(Long.valueOf(CASE_ID), CONTESTED, UPLOAD_APPROVED_ORDER);
+        when(documentWarningsHelper.getDocumentWarnings(eq(finremCallbackRequest), any(Function.class), eq(AUTH_TOKEN)))
+            .thenReturn(List.of("warning 1"));
+
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = underTest.handle(finremCallbackRequest, AUTH_TOKEN);
+        assertThat(response.getWarnings()).containsExactly("warning 1");
+
+        verify(uploadApprovedOrderService).processApprovedOrders(finremCallbackRequest, new ArrayList<>(), AUTH_TOKEN);
+        verify(documentWarningsHelper).getDocumentWarnings(eq(finremCallbackRequest), any(Function.class), eq(AUTH_TOKEN));
     }
 }
