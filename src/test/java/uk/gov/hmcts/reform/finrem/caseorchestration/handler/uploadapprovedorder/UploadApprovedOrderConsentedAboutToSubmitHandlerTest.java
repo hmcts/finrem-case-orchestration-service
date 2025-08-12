bf63ccbb4dc2b138e20ae9316a5a8b6150d5a848
Ashley Wong <im@so99y.com>
2025-08-12 13:09:20 +0100

    unit tests

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderConsentedAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadapprovedorder/UploadApprovedOrderConsentedAboutToSubmitHandlerTest.java
@@ -2,21 +2,28 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler.uploadapprovedorder
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.helper.DocumentWarningsHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HasUploadingDocuments;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ConsentOrderWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderApprovedDocumentService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.GenericDocumentService;
 
+import java.util.List;
+import java.util.function.Function;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
@@ -69,5 +76,36 @@ class UploadApprovedOrderConsentedAboutToSubmitHandlerTest {
         assertThat(response.getData().getLatestConsentOrder()).isEqualTo(convertedCaseDocument);
         verify(consentOrderApprovedDocumentService).addGeneratedApprovedConsentOrderDocumentsToCase(any(), any());
         verify(genericDocumentService).convertDocumentIfNotPdfAlready(any(), any(), any());
+    }
+
+    @Test
+    void givenUploadConsentedApproveOrder_whenDocumentWarningExists_thenReturnWarnings() {
+        CaseDocument uploadApproveOrder = caseDocument(DOC_URL, DOC_FILE_NAME, BINARY_URL);
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .consentOrderWrapper(
+                ConsentOrderWrapper.builder().uploadApprovedConsentOrder(uploadApproveOrder).build()
+            )
+            .build();
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(Long.valueOf(CASE_ID), finremCaseData);
+
+        List<String> expectedWarnings = List.of("warning1", "warning2");
+        when(documentWarningsHelper.getDocumentWarnings(eq(callbackRequest), any(), eq(AUTH_TOKEN)))
+            .thenReturn(expectedWarnings);
+        CaseDocument convertedCaseDocument = caseDocument("convertedUrl", "converted.pdf");
+        when(genericDocumentService.convertDocumentIfNotPdfAlready(any(), any(), any())).thenReturn(convertedCaseDocument);
+
+        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = underTest.handle(callbackRequest, AUTH_TOKEN);
+
+        // Verify
+        assertThat(response.getWarnings()).containsExactly("warning1", "warning2");
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Function<FinremCaseData, List<HasUploadingDocuments>>> lambdaCaptor =
+            ArgumentCaptor.forClass(Function.class);
+
+        verify(documentWarningsHelper).getDocumentWarnings(eq(callbackRequest), lambdaCaptor.capture(), eq(AUTH_TOKEN));
+        List<? extends HasUploadingDocuments> docs = lambdaCaptor.getValue().apply(finremCaseData);
+        assertThat(docs)
+            .extracting(HasUploadingDocuments::getUploadingDocuments)
+            .containsExactly(List.of(convertedCaseDocument));
     }
 }
