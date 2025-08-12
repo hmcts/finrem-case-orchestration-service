8bc1918d48291353fff3d66c989b505b9c099869
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-08-12 13:58:47 +0100

    AboutToStartHandler for new GAD event to populate working hearing data

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/GeneralApplicationDirectionsMHAboutToStartHandler.java
@@ -0,0 +1,111 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.GeneralApplicationHelper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.*;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.WorkingHearing;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignCaseAccessService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.GeneralApplicationDirectionsService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.PartyService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Service
+public class GeneralApplicationDirectionsMHAboutToStartHandler extends FinremCallbackHandler implements GeneralApplicationHandler {
+
+    private final AssignCaseAccessService assignCaseAccessService;
+    private final GeneralApplicationHelper helper;
+    private final GeneralApplicationDirectionsService service;
+    private final PartyService partyService;
+
+    public GeneralApplicationDirectionsMHAboutToStartHandler(AssignCaseAccessService assignCaseAccessService,
+                                                             FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                                             GeneralApplicationHelper helper,
+                                                             GeneralApplicationDirectionsService service, PartyService partyService) {
+        super(finremCaseDetailsMapper);
+        this.helper = helper;
+        this.service = service;
+        this.assignCaseAccessService = assignCaseAccessService;
+        this.partyService = partyService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.ABOUT_TO_START.equals(callbackType)
+            && CaseType.CONTESTED.equals(caseType)
+            && EventType.GENERAL_APPLICATION_DIRECTIONS_MH.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.aboutToStart(callbackRequest));
+        FinremCaseDetails finremCaseDetails = callbackRequest.getCaseDetails();
+
+        String caseId = finremCaseDetails.getId().toString();
+
+        FinremCaseData caseData = finremCaseDetails.getData();
+
+        // Initialize the working hearing for general application directions (MH)
+        if (EventType.GENERAL_APPLICATION_DIRECTIONS_MH.equals(callbackRequest.getEventType())) {
+            caseData.getManageHearingsWrapper().setWorkingHearing(
+                WorkingHearing.builder()
+                    .partiesOnCaseMultiSelectList(partyService.getAllActivePartyList(finremCaseDetails))
+                    .withHearingTypes(HearingType.APPLICATION_HEARING)
+                    .build());
+        }
+
+        String loggedInUserCaseRole = assignCaseAccessService.getActiveUser(caseId, userAuthorisation);
+        log.info("Logged in user case role type {} on Case ID: {}", loggedInUserCaseRole, caseId);
+        caseData.setCurrentUserCaseRoleType(loggedInUserCaseRole);
+
+        service.resetGeneralApplicationDirectionsFields(caseData);
+
+        helper.populateGeneralApplicationSender(caseData, caseData.getGeneralApplicationWrapper().getGeneralApplications());
+
+        List<GeneralApplicationCollectionData> outcomeList = helper.getOutcomeList(caseData);
+        AtomicInteger index = new AtomicInteger(0);
+        if (outcomeList.isEmpty() && caseData.getGeneralApplicationWrapper().getGeneralApplicationCreatedBy() != null) {
+            log.info("Setting direction list if existing general application not moved to collection for Case ID: {}", caseId);
+            setDirectionListForNonCollectionGeneralApplication(caseData, index, userAuthorisation, caseId);
+        } else {
+            if (outcomeList.isEmpty()) {
+                log.info("The user cannot carry out the directions as there are no general applications in the outcome state"
+                    + " for Case ID: {}", caseId);
+                return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData)
+                    .errors(List.of("There are no general application available for issue direction.")).build();
+            }
+            List<DynamicListElement> dynamicListElements = outcomeList.stream()
+                .map(ga -> getDynamicListElements(ga.getId() + "#" + ga.getGeneralApplicationItems().getGeneralApplicationStatus(),
+                    getLabel(ga.getGeneralApplicationItems(), index.incrementAndGet())))
+                .toList();
+
+            DynamicList dynamicList = generateAvailableGeneralApplicationAsDynamicList(dynamicListElements);
+            caseData.getGeneralApplicationWrapper().setGeneralApplicationDirectionsList(dynamicList);
+        }
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).build();
+    }
+
+    private void setDirectionListForNonCollectionGeneralApplication(FinremCaseData caseData,
+                                                                    AtomicInteger index,
+                                                                    String userAuthorisation, String caseId) {
+        GeneralApplicationItems applicationItems = helper.getApplicationItems(caseData, userAuthorisation, caseId);
+        DynamicListElement dynamicListElements
+            = getDynamicListElements(applicationItems.getGeneralApplicationCreatedBy(), getLabel(applicationItems, index.incrementAndGet()));
+
+        List<DynamicListElement> dynamicListElementsList = new ArrayList<>();
+        dynamicListElementsList.add(dynamicListElements);
+
+        DynamicList dynamicList = generateAvailableGeneralApplicationAsDynamicList(dynamicListElementsList);
+        caseData.getGeneralApplicationWrapper().setGeneralApplicationDirectionsList(dynamicList);
+    }
+}
