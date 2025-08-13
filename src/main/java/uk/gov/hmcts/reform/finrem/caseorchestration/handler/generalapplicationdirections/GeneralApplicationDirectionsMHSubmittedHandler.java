02ee2adc0687cf0785751947c1fb41b7aa264a21
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-08-13 10:06:19 +0100

    rename package for handlers

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/generalapplicationdirections/GeneralApplicationDirectionsMHSubmittedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/generalapplicationdirections/GeneralApplicationDirectionsMHSubmittedHandler.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.finrem.caseorchestration.handler.generalapplicationdirection;
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.generalapplicationdirections;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
