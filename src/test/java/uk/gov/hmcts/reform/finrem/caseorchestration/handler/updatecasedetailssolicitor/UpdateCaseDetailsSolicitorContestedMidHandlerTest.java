13e42897e22ae6c0b0c533933cf929e38d49afae
Ashley Wong <50691452+so99y@users.noreply.github.com>
2025-08-13 14:19:39 +0100

    DFR-3942 CS & CT: Case creation allows invalid email addresses to be entered (#2433)
    * CallbackHandlerLogger
    
    * Add EmailUtils
    
    * refactor
    
    * tidy up
    
    * update comment
    
    * update comment
    
    * JavaDoc
    
    * unit tests
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * simplify test case
    
    * Test case
    
    * simplify test case
    
    * test case
    
    * tidy up
    
    * refactoring
    
    * Remove unused methods.
    
    * Removed
    
    * checkstyle
    
    * tidy up
    
    * refactor
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>
    Co-authored-by: tomfairclough1 <167980396+tomfairclough1@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/updatecasedetailssolicitor/UpdateCaseDetailsSolicitorContestedMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/updatecasedetailssolicitor/UpdateCaseDetailsSolicitorContestedMidHandlerTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.finrem.caseorchestration.handler.updatecontactdetails;
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.updatecasedetailssolicitor;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
@@ -14,7 +13,6 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToSt
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ContactDetailsValidator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
-import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,27 +21,22 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.UPDATE_CONTACT_DETAILS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.UPDATE_CASE_DETAILS_SOLICITOR;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
-class UpdateContactDetailsContestedMidHandlerTest {
+class UpdateCaseDetailsSolicitorContestedMidHandlerTest {
 
     @InjectMocks
-    private UpdateContactDetailsContestedMidHandler underTest;
-
-    @Mock
-    private InternationalPostalService internationalPostalService;
+    private UpdateCaseDetailsSolicitorContestedMidHandler underTest;
 
     @Test
     void testCanHandle() {
-        assertCanHandle(underTest, MID_EVENT, CONTESTED, UPDATE_CONTACT_DETAILS);
+        assertCanHandle(underTest, MID_EVENT, CONTESTED, UPDATE_CASE_DETAILS_SOLICITOR);
     }
 
     static Stream<Arguments> errorScenarios() {
@@ -51,18 +44,15 @@ class UpdateContactDetailsContestedMidHandlerTest {
             Arguments.of(
                 List.of("address error 1"),
                 List.of("email error 1"),
-                List.of("postal error 1"),
-                List.of("postal error 1", "address error 1", "email error 1")
+                List.of("address error 1", "email error 1")
             ),
             Arguments.of(
-                List.of(),
                 List.of(),
                 List.of(),
                 List.of()
             ),
             Arguments.of(
                 List.of("address only"),
-                List.of(),
                 List.of(),
                 List.of("address only")
             )
@@ -73,27 +63,22 @@ class UpdateContactDetailsContestedMidHandlerTest {
     @MethodSource("errorScenarios")
     void testHandle(List<String> addressErrors,
                     List<String> emailErrors,
-                    List<String> postalErrors,
                     List<String> expectedErrors) {
 
         FinremCaseData caseData = mock(FinremCaseData.class);
         FinremCallbackRequest callbackRequest =
-            FinremCallbackRequestFactory.create(Long.valueOf(CASE_ID), CONTESTED, UPDATE_CONTACT_DETAILS, caseData);
+            FinremCallbackRequestFactory.create(Long.valueOf(CASE_ID), CONTESTED, UPDATE_CASE_DETAILS_SOLICITOR, caseData);
 
         try (MockedStatic<ContactDetailsValidator> contactValidatorMock = mockStatic(ContactDetailsValidator.class)) {
             contactValidatorMock.when(() -> ContactDetailsValidator.validateCaseDataAddresses(caseData))
                 .thenReturn(new ArrayList<>(addressErrors));
             contactValidatorMock.when(() -> ContactDetailsValidator.validateCaseDataEmailAddresses(caseData))
                 .thenReturn(new ArrayList<>(emailErrors));
-            when(internationalPostalService.validate(caseData))
-                .thenReturn(new ArrayList<>(postalErrors));
 
             GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response =
                 underTest.handle(callbackRequest, AUTH_TOKEN);
 
             assertThat(response.getErrors()).containsExactlyElementsOf(expectedErrors);
-
-            verify(internationalPostalService).validate(caseData);
         }
     }
 }
