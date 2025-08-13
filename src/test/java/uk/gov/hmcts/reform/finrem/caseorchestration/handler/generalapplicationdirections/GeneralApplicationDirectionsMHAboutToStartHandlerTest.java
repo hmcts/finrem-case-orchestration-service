b30c786ca151db33b68173ef99ebb794e8388b18
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-08-13 11:03:53 +0100

    use getFirst()

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/generalapplicationdirections/GeneralApplicationDirectionsMHAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/generalapplicationdirections/GeneralApplicationDirectionsMHAboutToStartHandlerTest.java
@@ -173,7 +173,7 @@ public class GeneralApplicationDirectionsMHAboutToStartHandlerTest {
             getDynamicListElement(CASE_LEVEL_ROLE, CASE_LEVEL_ROLE)
         );
         return DynamicRadioList.builder()
-            .value(dynamicListElements.get(0))
+            .value(dynamicListElements.getFirst())
             .listItems(dynamicListElements)
             .build();
     }
