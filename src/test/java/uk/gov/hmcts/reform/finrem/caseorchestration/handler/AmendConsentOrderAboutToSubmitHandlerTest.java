d191566cb3b8250982a2d3c6d4299f35fc272b0a
Ashley Wong <im@so99y.com>
2025-08-26 15:33:07 +0100

    simplify

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendConsentOrderAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/AmendConsentOrderAboutToSubmitHandlerTest.java
@@ -14,44 +14,32 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseFlagsService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.ConsentOrderService;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
 class AmendConsentOrderAboutToSubmitHandlerTest {
-    private static final String AUTH_TOKEN = "4d73f8d4-2a8d-48e2-af91-11cbaa642345";
 
     @InjectMocks
     private AmendConsentOrderAboutToSubmitHandler handler;
+
     @Mock
     private ConsentOrderService consentOrderService;
+
     @Mock
     private CaseFlagsService caseFlagsService;
 
     @Test
-    void given_case_whenEvent_type_is_amend_consent_order_thenCanHandle() {
-        assertTrue(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONSENTED, EventType.AMEND_CONSENT_ORDER));
-    }
-
-    @Test
-    void given_case_when_wrong_callback_then_case_can_not_handle() {
-        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_START, CaseType.CONSENTED, EventType.AMEND_CONSENT_ORDER));
-    }
-
-    @Test
-    void given_case_when_wrong_casetype_then_case_can_not_handle() {
-        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONTESTED, EventType.AMEND_CONSENT_ORDER));
-    }
-
-    @Test
-    void given_case_when_wrong_eventtype_then_case_can_not_handle() {
-        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, CaseType.CONSENTED, EventType.CLOSE));
+    void testCanHandle() {
+        assertCanHandle(handler, CallbackType.ABOUT_TO_SUBMIT, CaseType.CONSENTED, EventType.AMEND_CONSENT_ORDER);
     }
 
     @Test
@@ -67,11 +55,13 @@ class AmendConsentOrderAboutToSubmitHandlerTest {
     }
 
     private FinremCallbackRequest buildCallbackRequest() {
-        return FinremCallbackRequest
-            .builder()
-            .eventType(EventType.AMEND_CONSENT_ORDER)
-            .caseDetails(FinremCaseDetails.builder().id(123L).caseType(CONSENTED)
-                .data(FinremCaseData.builder().ccdCaseType(CONSENTED).build()).build())
-            .build();
+        FinremCallbackRequest mockedCallbackRequest = mock(FinremCallbackRequest.class);
+        FinremCaseDetails mockedCaseDetails = mock(FinremCaseDetails.class);
+        when(mockedCallbackRequest.getCaseDetails()).thenReturn(mockedCaseDetails);
+
+        FinremCaseData mockedCaseData = spy(FinremCaseData.class);
+        when(mockedCaseDetails.getData()).thenReturn(mockedCaseData);
+        when(mockedCaseDetails.getId()).thenReturn(Long.valueOf(CASE_ID));
+        return mockedCallbackRequest;
     }
 }
