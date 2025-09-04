013da40991389a0e11ff4df5480fc401a5171725
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-09-04 09:04:47 +0100

    DFR-3915: Add NotificationRequestBuilder (#2528)
    * Add NotificationRequestBuilder
    
    * Minor cleanup
    
    * Checkstyle errors

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/mapper/notificationrequest/NotificationRequestBuilderFactory.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.mapper.notificationrequest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationRequestBuilderFactory {
+
+    private final ObjectProvider<NotificationRequestBuilder> builderProvider;
+
+    /**
+     * Creates a new instance of {@link NotificationRequestBuilder} to build
+     * {@link uk.gov.hmcts.reform.finrem.caseorchestration.model.notification.NotificationRequest}.
+     * This method is used to ensure that each NotificationRequest can be built independently without side effects.
+     * @return a new instance of {@link NotificationRequestBuilder}
+     */
+    public NotificationRequestBuilder newInstance() {
+        return builderProvider.getObject();
+    }
+}
