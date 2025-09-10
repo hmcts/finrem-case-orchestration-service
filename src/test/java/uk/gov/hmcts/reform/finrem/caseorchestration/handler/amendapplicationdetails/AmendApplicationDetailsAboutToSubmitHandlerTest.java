fb891fccb39fe18f916e9c1c7a5e28465f8c12a6
Ashley Wong <im@so99y.com>
2025-09-10 17:03:59 +0100

    Add AmendApplicationDetailsAboutToSubmitHandlerTest

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/amendapplicationdetails/AmendApplicationDetailsAboutToSubmitHandlerTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.amendapplicationdetails;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.provider.Arguments;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseFlagsService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnlineFormDocumentService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.express.ExpressCaseService;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.AMEND_CONTESTED_APP_DETAILS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.AMEND_CONTESTED_PAPER_APP_DETAILS;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
+
+@ExtendWith(MockitoExtension.class)
+class AmendApplicationDetailsAboutToSubmitHandlerTest {
+
+    @InjectMocks
+    private AmendApplicationDetailsAboutToSubmitHandler handler;
+    
+    @Mock
+    private OnlineFormDocumentService onlineFormDocumentService;
+
+    @Mock
+    private CaseFlagsService caseFlagsService;
+
+    @Mock
+    private FeatureToggleService featureToggleService;
+
+    @Mock
+    private ExpressCaseService expressCaseService;
+
+    @Test
+    void shouldHandleContestedAndConsentedCaseTypes() {
+        assertCanHandle(handler,
+                Arguments.of(ABOUT_TO_SUBMIT, CONTESTED, AMEND_CONTESTED_PAPER_APP_DETAILS),
+                Arguments.of(ABOUT_TO_SUBMIT, CONTESTED, AMEND_CONTESTED_APP_DETAILS)
+        );
+    }
+}
