362b12f1c19c409fb0e8d746bc5a4244809a5f42
Ashley Wong <im@so99y.com>
2025-09-10 12:14:32 +0100

    Update Organisation

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/EventType.java
@@ -96,6 +96,7 @@ public enum EventType {
     APPROVE_ORDERS("FR_approveOrders"),
     MANAGE_EXPRESS_CASE("FR_manageExpressCase"),
     MANAGE_HEARINGS_MIGRATION("FR_manageHearingsMigration"),
+    UPDATE_ORGANISATION("FR_updateOrganisation"),
     @JsonEnumDefaultValue
     NONE("");
 
