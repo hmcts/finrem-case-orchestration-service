366bb03524946bcc762125bae6c81b842584fa4f
Ashley Wong <im@so99y.com>
2025-09-12 17:00:45 +0100

    update test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/mandatorydatavalidation/ApplicantSolicitorDetailsValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/solicitorcreatecase/mandatorydatavalidation/ApplicantSolicitorDetailsValidatorTest.java
@@ -2,16 +2,24 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler.solicitorcreatecase
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.MockedStatic;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Organisation;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.OrganisationPolicy;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.NullChecker;
 
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_ORG_ID;
@@ -57,7 +65,6 @@ class ApplicantSolicitorDetailsValidatorTest {
         finremCaseData.getContactDetailsWrapper().setApplicantRepresented(YesOrNo.YES);
         finremCaseData.setApplicantOrganisationPolicy(validOrganisationPolicy());
 
-
         List<String> actual = underTest.validate(finremCaseData);
         assertThat(actual).isEmpty();
     }
@@ -73,17 +80,82 @@ class ApplicantSolicitorDetailsValidatorTest {
         assertThat(actual).isEmpty();
     }
 
-    // TODO
-//    if (caseData.isConsentedApplication()) {
-//        if (contactDetailsWrapper.getSolicitorAddress() == null
-//            || !NullChecker.anyNonNull(contactDetailsWrapper.getSolicitorAddress())) {
-//            ret.add("Applicant solicitor's address is required.");
-//        }
-//        validateField(contactDetailsWrapper.getSolicitorEmail(), "email", ret);
-//        validateField(contactDetailsWrapper.getSolicitorPhone(), "phone", ret);
-//        validateField(contactDetailsWrapper.getSolicitorFirm(), "name of your firm", ret);
-//        validateField(contactDetailsWrapper.getSolicitorName(), "name", ret);
-//    }
+    static Stream<Arguments> givenConsentedCaseAndSolicitorMissing_whenValidate_thenReturnAnError() {
+        return Stream.of(
+            Arguments.of(Address.builder().build())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @NullSource
+    void givenConsentedCaseAndSolicitorMissing_whenValidate_thenReturnAnError(Address solicitorAddress) {
+        FinremCaseData finremCaseData = spy(FinremCaseData.class);
+        when(finremCaseData.getApplicantOrganisationPolicy()).thenReturn(validOrganisationPolicy());
+        when(finremCaseData.isConsentedApplication()).thenReturn(true);
+        finremCaseData.getContactDetailsWrapper().setSolicitorAddress(solicitorAddress);
+        if (solicitorAddress != null) {
+            try (MockedStatic<NullChecker> mockedStatic = mockStatic(NullChecker.class)) {
+                mockedStatic.when(() -> NullChecker.anyNonNull(solicitorAddress))
+                    .thenReturn(false);
+
+                List<String> actual = underTest.validate(finremCaseData);
+                assertThat(actual).contains("Applicant solicitor's address is required.");
+                mockedStatic.verify(() -> NullChecker.anyNonNull(solicitorAddress));
+            }
+        } else {
+            List<String> actual = underTest.validate(finremCaseData);
+            assertThat(actual).contains("Applicant solicitor's address is required.");
+        }
+    }
+
+    static Stream<Arguments> givenConsentedCaseAndContactDetailsWrapper_whenValidate_thenReturnErrors() {
+        final String messageTpl = "Applicant solicitor's %s is required.";
+        final String missingAddressMessage = "Applicant solicitor's address is required.";
+        return Stream.of(
+            Arguments.of(
+                ContactDetailsWrapper.builder()
+                    .solicitorEmail(null)
+                    .solicitorPhone("07788666444")
+                    .solicitorFirm("ABC Co Ltd")
+                    .solicitorName("A Cole")
+                    .build(),
+                List.of(format(messageTpl, "email"), missingAddressMessage)
+            ),
+            Arguments.of(
+                ContactDetailsWrapper.builder()
+                    .solicitorEmail("abc@abc.com")
+                    .solicitorPhone(null)
+                    .solicitorFirm("ABC Co Ltd")
+                    .solicitorName("A Cole")
+                    .build(),
+                List.of(format(messageTpl, "phone"), missingAddressMessage)
+            ),
+            Arguments.of(
+                ContactDetailsWrapper.builder()
+                    .solicitorEmail(null)
+                    .solicitorPhone(null)
+                    .solicitorFirm("ABC Co Ltd")
+                    .solicitorName("A Cole")
+                    .build(),
+                List.of(format(messageTpl, "phone"), format(messageTpl, "email"), missingAddressMessage)
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void givenConsentedCaseAndContactDetailsWrapper_whenValidate_thenReturnErrors(
+        ContactDetailsWrapper contactDetailsWrapper, List<String> expectedErrors) {
+
+        FinremCaseData finremCaseData = spy(FinremCaseData.class);
+        when(finremCaseData.getApplicantOrganisationPolicy()).thenReturn(validOrganisationPolicy());
+        when(finremCaseData.isConsentedApplication()).thenReturn(true);
+        finremCaseData.setContactDetailsWrapper(contactDetailsWrapper);
+
+        List<String> actual = underTest.validate(finremCaseData);
+        assertThat(actual).contains(expectedErrors.toArray(new String[0]));
+    }
 
     private static OrganisationPolicy validOrganisationPolicy() {
         return OrganisationPolicy.builder().organisation(Organisation.builder()
