2db3e6da5e48bfd0df8a23f15f8d0a8dab1314fc
James Turnbull <james.turnbull@justice.gov.uk>
2025-09-15 08:43:35 +0100

    Refactor Manage Barrister about to start handler

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/managebarrister/ManageBarristerAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/managebarrister/ManageBarristerAboutToStartHandlerTest.java
@@ -1,146 +1,92 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler.managebarrister;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseAssignedUserRole;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseAssignedUserRolesResource;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseRole;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.CaseAssignedRoleService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Stream;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.APP_SOLICITOR_POLICY;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CASEWORKER_ROLE_FIELD_SHOW_LABEL;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CASE_ROLE;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CASE_ROLE_FOR_FIELD_SHOW;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.RESP_SOLICITOR_POLICY;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.service.CcdServiceTest.AUTH_TOKEN;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ManageBarristerAboutToStartHandlerTest {
+@ExtendWith(MockitoExtension.class)
+class ManageBarristerAboutToStartHandlerTest {
 
-    private static final String USER_ID = "testUserId";
-    public static final String CASE_ID = "1234567890";
-    public static final String CASEWORKER_POLICY = "[CASEWORKER]";
-
-    @Mock
-    private CaseAssignedRoleService caseAssignedRoleService;
+    private static final long CASE_ID = 1234567890L;
 
     @InjectMocks
     private ManageBarristerAboutToStartHandler manageBarristerAboutToStartHandler;
 
-    private CallbackRequest callbackRequest;
-
-    @Before
-    public void setUp() throws Exception {
-        Map<String, Object> caseData = new HashMap<>();
-        callbackRequest = CallbackRequest.builder()
-            .caseDetails(CaseDetails.builder()
-                .id(Long.parseLong(CASE_ID))
-                .data(caseData)
-                .build())
-            .build();
-    }
+    @Mock
+    private CaseAssignedRoleService caseAssignedRoleService;
 
     @Test
-    public void givenHandlerCanHandleCallback_whenCanHandle_thenReturnTrue() {
-        assertThat(manageBarristerAboutToStartHandler.canHandle(
-                CallbackType.ABOUT_TO_START,
-                CaseType.CONTESTED,
-                EventType.MANAGE_BARRISTER),
-            is(true));
+    void testCanHandle() {
+        Assertions.assertCanHandle(manageBarristerAboutToStartHandler, CallbackType.ABOUT_TO_START, CaseType.CONTESTED,
+            EventType.MANAGE_BARRISTER);
     }
 
-    @Test
-    public void givenInvalidCallbackType_whenCanHandle_thenReturnFalse() {
-        assertThat(manageBarristerAboutToStartHandler.canHandle(
-                CallbackType.ABOUT_TO_SUBMIT,
-                CaseType.CONTESTED,
-                EventType.MANAGE_BARRISTER),
-            is(false));
+    @ParameterizedTest
+    @MethodSource
+    void givenUserCaseRole_whenHandle_thenInitialisesEventData(CaseRole caseRole, String expectedCaseRoleLabel) {
+        mockCaseAssignRoleService(caseRole);
+        FinremCallbackRequest callbackRequest = createCallbackRequest();
+
+        var response = manageBarristerAboutToStartHandler.handle(callbackRequest, AUTH_TOKEN);
+
+        FinremCaseData caseData = response.getData();
+        assertThat(caseData.getCurrentUserCaseRole()).isEqualTo(caseRole);
+        assertThat(caseData.getCurrentUserCaseRoleLabel()).isEqualTo(expectedCaseRoleLabel);
+        assertThat(caseData.getBarristerParty()).isNull();
     }
 
-    @Test
-    public void givenInvalidCaseType_whenCanHandle_thenReturnFalse() {
-        assertThat(manageBarristerAboutToStartHandler.canHandle(
-                CallbackType.ABOUT_TO_START,
-                CaseType.CONSENTED,
-                EventType.MANAGE_BARRISTER),
-            is(false));
+    private static Stream<Arguments> givenUserCaseRole_whenHandle_thenInitialisesEventData() {
+        return Stream.of(
+            Arguments.of(CaseRole.APP_SOLICITOR, "APPSOLICITOR"),
+            Arguments.of(CaseRole.RESP_SOLICITOR, "RESPSOLICITOR"),
+            Arguments.of(CaseRole.CASEWORKER, CASEWORKER_ROLE_FIELD_SHOW_LABEL),
+            Arguments.of(CaseRole.INTVR_SOLICITOR_1, "INTVRSOLICITOR1"),
+            Arguments.of(CaseRole.INTVR_SOLICITOR_2, "INTVRSOLICITOR2"),
+            Arguments.of(CaseRole.INTVR_SOLICITOR_3, "INTVRSOLICITOR3"),
+            Arguments.of(CaseRole.INTVR_SOLICITOR_4, "INTVRSOLICITOR4")
+        );
     }
 
-    @Test
-    public void givenInvalidEventType_whenCanHandle_thenReturnFalse() {
-        assertThat(manageBarristerAboutToStartHandler.canHandle(
-                CallbackType.ABOUT_TO_START,
-                CaseType.CONTESTED,
-                EventType.SEND_ORDER),
-            is(false));
-    }
-
-    @Test
-    public void givenUserIsApplicantSolicitor_whenHandle_thenSetCurrentUserCaseRoleToAppSolicitor() {
-        when(caseAssignedRoleService.getCaseAssignedUserRole(
-            callbackRequest.getCaseDetails().getId().toString(), AUTH_TOKEN))
-            .thenReturn(getCaseAssignedUserRolesResource(APP_SOLICITOR_POLICY));
-
-        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>>
-            response = manageBarristerAboutToStartHandler.handle(callbackRequest, AUTH_TOKEN);
-        String expected = APP_SOLICITOR_POLICY
-            .replace("[", "").replace("]","");
-        assertThat(response.getData().get(CASE_ROLE), is(APP_SOLICITOR_POLICY));
-        assertThat(response.getData().get(CASE_ROLE_FOR_FIELD_SHOW), is(expected));
-    }
-
-    @Test
-    public void givenUserIsRespondentSolicitor_whenHandle_thenSetCurrentUserCaseRoleToRespSolicitor() {
-        when(caseAssignedRoleService.getCaseAssignedUserRole(
-            callbackRequest.getCaseDetails().getId().toString(), AUTH_TOKEN))
-            .thenReturn(getCaseAssignedUserRolesResource(RESP_SOLICITOR_POLICY));
-
-        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>>
-            response = manageBarristerAboutToStartHandler.handle(callbackRequest, AUTH_TOKEN);
-
-        String expected = RESP_SOLICITOR_POLICY
-            .replace("[", "").replace("]","");
-        assertThat(response.getData().get(CASE_ROLE), is(RESP_SOLICITOR_POLICY));
-        assertThat(response.getData().get(CASE_ROLE_FOR_FIELD_SHOW), is(expected));
-    }
-
-    @Test
-    public void givenUserIsCaseWorker_whenHandle_thenSetCurrentUserCaseRoleToCaseWorker() {
-        when(caseAssignedRoleService.getCaseAssignedUserRole(
-            callbackRequest.getCaseDetails().getId().toString(), AUTH_TOKEN))
-            .thenReturn(CaseAssignedUserRolesResource.builder().build());
-
-        GenericAboutToStartOrSubmitCallbackResponse<Map<String, Object>>
-            response = manageBarristerAboutToStartHandler.handle(callbackRequest, AUTH_TOKEN);
-
-        assertThat(response.getData().get(CASE_ROLE), is(CASEWORKER_POLICY));
-        assertThat(response.getData().get(CASE_ROLE_FOR_FIELD_SHOW), is(CASEWORKER_ROLE_FIELD_SHOW_LABEL));
-    }
-
-    private CaseAssignedUserRolesResource getCaseAssignedUserRolesResource(String caseRole) {
-        return CaseAssignedUserRolesResource.builder()
+    private void mockCaseAssignRoleService(CaseRole caseRole) {
+        CaseAssignedUserRolesResource caseAssignedUserRolesResource = CaseAssignedUserRolesResource.builder()
             .caseAssignedUserRoles(List.of(CaseAssignedUserRole.builder()
-                .userId(USER_ID)
-                .caseRole(caseRole)
-                .caseDataId(CASE_ID)
+                .userId("testUserId")
+                .caseRole(caseRole.getCcdCode())
+                .caseDataId(String.valueOf(CASE_ID))
                 .build()))
             .build();
+
+        when(caseAssignedRoleService.getCaseAssignedUserRole(String.valueOf(CASE_ID), AUTH_TOKEN))
+            .thenReturn(caseAssignedUserRolesResource);
+    }
+
+    private FinremCallbackRequest createCallbackRequest() {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .build();
+        return FinremCallbackRequestFactory.create(CASE_ID, CaseType.CONTESTED, EventType.MANAGE_BARRISTER, caseData);
     }
 }
