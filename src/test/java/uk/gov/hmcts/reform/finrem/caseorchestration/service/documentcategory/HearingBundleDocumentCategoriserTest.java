5e316396e839ffbed2f4cbc2472694587c88ba18
Ashley Wong <im@so99y.com>
2025-09-26 00:57:40 +0100

    update test case

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcategory/HearingBundleDocumentCategoriserTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcategory/HearingBundleDocumentCategoriserTest.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service.documentcategory;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HearingBundleDocument;
@@ -19,82 +20,96 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
-public class HearingBundleDocumentCategoriserTest {
+@ExtendWith(MockitoExtension.class)
+class HearingBundleDocumentCategoriserTest {
 
+    @InjectMocks
     private HearingBundleDocumentCategoriser hearingBundleDocumentCategoriser;
 
     @Mock
-    FeatureToggleService featureToggleService;
+    private FeatureToggleService featureToggleService;
 
-    @Before
-    public void setUp() {
-        hearingBundleDocumentCategoriser = new HearingBundleDocumentCategoriser(featureToggleService);
+    @BeforeEach
+    void setUp() {
         when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
     }
 
     @Test
-    public void testCategorizeDocuments() {
+    void testCategoriseDocuments() {
         FinremCaseData finremCaseData = buildFinremCaseDataWithBundles();
         hearingBundleDocumentCategoriser.categorise(finremCaseData);
-        HearingUploadBundleHolder hearingUploadBundleHolder = finremCaseData.getFdrHearingBundleCollections().get(0).getValue();
-        assertThat(hearingUploadBundleHolder.getHearingBundleDocuments().get(0).getValue()
-            .getBundleDocuments().getCategoryId(), is(
-            DocumentCategory.FDR_BUNDLE.getDocumentCategoryId()
-        ));
-        assertThat(hearingUploadBundleHolder.getHearingBundleDocuments().get(1).getValue()
-            .getBundleDocuments().getCategoryId(), is(
-            DocumentCategory.FDR_BUNDLE.getDocumentCategoryId()
-        ));
 
+        // FDR bundle
+        HearingUploadBundleHolder fdrBundle =
+            finremCaseData.getFdrHearingBundleCollections().getFirst().getValue();
 
-        HearingUploadBundleHolder hearingUploadBundleHolder1 = finremCaseData.getHearingUploadBundle().get(0).getValue();
-        assertThat(hearingUploadBundleHolder1.getHearingBundleDocuments().get(0).getValue()
-            .getBundleDocuments().getCategoryId(), is(
-            DocumentCategory.HEARING_BUNDLE.getDocumentCategoryId()
-        ));
-        assertThat(hearingUploadBundleHolder1.getHearingBundleDocuments().get(1).getValue()
-            .getBundleDocuments().getCategoryId(), is(
-            DocumentCategory.HEARING_BUNDLE.getDocumentCategoryId()
-        ));
+        assertThat(fdrBundle.getHearingBundleDocuments()).hasSize(3);
+        fdrBundle.getHearingBundleDocuments().subList(0, 1).forEach(doc ->
+            assertThat(doc.getValue().getBundleDocuments().getCategoryId())
+                .isEqualTo(DocumentCategory.FDR_BUNDLE.getDocumentCategoryId())
+        );
+        // Check third document has null bundleDocuments
+        assertThat(fdrBundle.getHearingBundleDocuments().get(2).getValue().getBundleDocuments())
+            .isNull();
+
+        // Hearing bundle
+        HearingUploadBundleHolder hearingBundle =
+            finremCaseData.getHearingUploadBundle().getFirst().getValue();
+
+        assertThat(hearingBundle.getHearingBundleDocuments()).hasSize(3);
+        hearingBundle.getHearingBundleDocuments().subList(0, 1).forEach(doc ->
+            assertThat(doc.getValue().getBundleDocuments().getCategoryId())
+                .isEqualTo(DocumentCategory.HEARING_BUNDLE.getDocumentCategoryId())
+        );
+        // Check third document has null bundleDocuments
+        assertThat(hearingBundle.getHearingBundleDocuments().get(2).getValue().getBundleDocuments())
+            .isNull();
     }
 
     private FinremCaseData buildFinremCaseDataWithBundles() {
-
         HearingUploadBundleCollection fdrHearingUploadBundleCollection =
             getHearingUploadBundleCollection();
 
         HearingUploadBundleCollection hearingUploadBundleCollection =
             getHearingUploadBundleCollection();
 
-        return FinremCaseData.builder().fdrHearingBundleCollections(List.of(fdrHearingUploadBundleCollection))
-            .hearingUploadBundle(List.of(hearingUploadBundleCollection)).build();
+        return FinremCaseData.builder()
+            .fdrHearingBundleCollections(List.of(fdrHearingUploadBundleCollection))
+            .hearingUploadBundle(List.of(hearingUploadBundleCollection))
+            .build();
     }
 
     private static HearingUploadBundleCollection getHearingUploadBundleCollection() {
         List<HearingBundleDocumentCollection> hearingBundleDocumentCollections =
-            Arrays.asList(getHearingBundleDocumentCollection("file2.pdf"), getHearingBundleDocumentCollection("file4.pdf"));
+            Arrays.asList(getHearingBundleDocumentCollection("file2.pdf"),
+                getHearingBundleDocumentCollection("file4.pdf"),
+                getHearingBundleDocumentCollectionWithDeletedBundleDocuments());
 
-        HearingUploadBundleCollection hearingUploadBundleCollection = HearingUploadBundleCollection.builder()
+        return HearingUploadBundleCollection.builder()
             .value(HearingUploadBundleHolder.builder()
                 .hearingBundleDate(LocalDate.of(2020, 1, 1))
                 .hearingBundleDocuments(hearingBundleDocumentCollections)
                 .hearingBundleFdr(YesOrNo.NO)
                 .build())
             .build();
-        return hearingUploadBundleCollection;
     }
 
     private static HearingBundleDocumentCollection getHearingBundleDocumentCollection(String filename) {
-        HearingBundleDocumentCollection hearingBundleDocumentCollection1 = HearingBundleDocumentCollection.builder()
+        return HearingBundleDocumentCollection.builder()
             .value(HearingBundleDocument.builder()
                 .bundleDocuments(CaseDocument.builder().documentFilename(filename).build())
                 .build())
             .build();
-        return hearingBundleDocumentCollection1;
+    }
+
+    private static HearingBundleDocumentCollection getHearingBundleDocumentCollectionWithDeletedBundleDocuments() {
+        return HearingBundleDocumentCollection.builder()
+            .value(HearingBundleDocument.builder()
+                .bundleDocuments(null)
+                .build())
+            .build();
     }
 }
