a4a143405573dc21971b2687f2f4fadcb0daf45d
Ashley Wong <im@so99y.com>
2025-09-26 01:07:41 +0100

    update test case

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcategory/HearingBundleDocumentCategoriserTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/documentcategory/HearingBundleDocumentCategoriserTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service.documentcategory;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
