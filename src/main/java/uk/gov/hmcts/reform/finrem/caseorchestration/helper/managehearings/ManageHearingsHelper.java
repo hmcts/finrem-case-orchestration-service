9eb7857780f7fcab5bada4473554205e17858a32
Hamzah <hamzah.mohammed@justice.gov.uk>
2025-10-08 12:39:38 +0100

    Add ManageHearingsHelper class

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/managehearings/ManageHearingsHelper.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.helper.managehearings;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ManageHearingsWrapper;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.utils.FileUtils.isPdf;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.utils.FileUtils.isWordDocument;
+
+@Slf4j
+@Component
+public class ManageHearingsHelper {
+
+}
