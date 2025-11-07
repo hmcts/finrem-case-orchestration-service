33fc2c9500564969283f168d993fa0bff7396cc8
Ashley Wong <im@so99y.com>
2025-11-07 10:54:18 +0000

    refactoring

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/HearingConsentMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/HearingConsentMidHandlerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
@@ -22,53 +23,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
-class HearingConsentMidHandlerTest extends BaseHandlerTestSetup {
+class HearingConsentMidHandlerTest {
 
     private HearingConsentMidHandler handler;
+
     @Mock
-    private BulkPrintDocumentService service;
-    private static final String FILE_URL = "http://dm:80/documents/kbjh87y8y9JHVKKKJVJ";
-    private static final String FILE_BINARY_URL = "http://dm:80/documents/kbjh87y8y9JHVKKKJVJ/binary";
-    private static final String FILE_NAME = "abc.pdf";
-    public static final String AUTH_TOKEN = "tokien:)";
+    private BulkPrintDocumentService bulkPrintDocumentService;
 
     @BeforeEach
     void setup() {
         FinremCaseDetailsMapper finremCaseDetailsMapper = new FinremCaseDetailsMapper(new ObjectMapper().registerModule(new JavaTimeModule()));
-        handler = new HearingConsentMidHandler(finremCaseDetailsMapper, service);
+        handler = new HearingConsentMidHandler(finremCaseDetailsMapper, bulkPrintDocumentService);
     }
 
     @Test
     void canHandle() {
-        assertTrue(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED));
+        assertCanHandle(handler, CallbackType.MID_EVENT, CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED);
     }
 
     @Test
-    void canNotHandle() {
-        assertFalse(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.LIST_FOR_HEARING_CONSENTED));
-    }
-
-    @Test
-    void canNotHandleWrongEventType() {
-        assertFalse(handler.canHandle(CallbackType.MID_EVENT, CaseType.CONTESTED, EventType.CLOSE));
-    }
-
-    @Test
-    void canNotHandleWrongCallbackType() {
-        assertFalse(handler.canHandle(CallbackType.ABOUT_TO_START, CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED));
-    }
-
-    @Test
-    void givenContestedCase_whenListForHearingbutNoAdditionalUploaded_thenNoCheckPerformed() {
-        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest(EventType.LIST_FOR_HEARING_CONSENTED);
+    void givenContestedCase_whenListForHearingButNoAdditionalUploaded_thenNoCheckPerformed() {
+        FinremCallbackRequest finremCallbackRequest = FinremCallbackRequestFactory
+            .from(Long.valueOf(CASE_ID), CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED);
         FinremCaseData caseData = finremCallbackRequest.getCaseDetails().getData();
 
         List<ConsentedHearingDataWrapper> listForHearings =
@@ -84,16 +71,17 @@ class HearingConsentMidHandlerTest extends BaseHandlerTestSetup {
 
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
 
-        assertTrue(response.getErrors().isEmpty());
-        verify(service, never()).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        assertThat(response.getErrors()).isEmpty();
+        verify(bulkPrintDocumentService, never()).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
     }
 
     @Test
     void givenContestedCase_whenListForHearingAdditionalUploadedButNonEncryptedFileShouldNotGetError() {
-        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest(EventType.LIST_FOR_HEARING_CONSENTED);
+        FinremCallbackRequest finremCallbackRequest = FinremCallbackRequestFactory
+            .from(Long.valueOf(CASE_ID), CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED);
         FinremCaseData caseData = finremCallbackRequest.getCaseDetails().getData();
 
-        CaseDocument caseDocument = caseDocument(FILE_URL, FILE_NAME, FILE_BINARY_URL);
+        CaseDocument caseDocument = caseDocument();
         List<ConsentedHearingDataWrapper> listForHearings =
             Optional.ofNullable(caseData.getListForHearings()).orElse(new ArrayList<>());
 
@@ -106,19 +94,19 @@ class HearingConsentMidHandlerTest extends BaseHandlerTestSetup {
         listForHearings.add(hearingDataWrapper);
         caseData.setListForHearings(listForHearings);
 
-
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
 
         assertTrue(response.getErrors().isEmpty());
-        verify(service).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
     }
 
     @Test
     void givenContestedCase_whenListForHearingAdditionalUploadedButSameDocumentAlreadyExists_thenDoNotCheckDocumentValidity() {
-        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest(EventType.LIST_FOR_HEARING_CONSENTED);
+        FinremCallbackRequest finremCallbackRequest = FinremCallbackRequestFactory
+            .from(Long.valueOf(CASE_ID), CaseType.CONSENTED, EventType.LIST_FOR_HEARING_CONSENTED);
         FinremCaseData caseData = finremCallbackRequest.getCaseDetails().getData();
 
-        CaseDocument caseDocument = caseDocument(FILE_URL, FILE_NAME, FILE_BINARY_URL);
+        CaseDocument caseDocument = caseDocument();
         List<ConsentedHearingDataWrapper> listForHearings =
             Optional.ofNullable(caseData.getListForHearings()).orElse(new ArrayList<>());
 
@@ -134,10 +122,9 @@ class HearingConsentMidHandlerTest extends BaseHandlerTestSetup {
         FinremCaseData caseDataBefore = finremCallbackRequest.getCaseDetailsBefore().getData();
         caseDataBefore.setListForHearings(listForHearings);
 
-
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
 
         assertTrue(response.getErrors().isEmpty());
-        verify(service, never()).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        verify(bulkPrintDocumentService, never()).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
     }
 }
