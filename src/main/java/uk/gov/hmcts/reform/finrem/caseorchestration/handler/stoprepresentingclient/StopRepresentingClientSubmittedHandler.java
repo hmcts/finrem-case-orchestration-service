4e210380e2e6cd1a065b0af4cbd5e57f3b230064
Ashley Wong <im@so99y.com>
2025-11-17 16:05:57 +0000

    805

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientSubmittedHandler.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.stoprepresentingclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+
+import java.util.Arrays;
+
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.STOP_REPRESENTING_CLIENT;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
+
+@Slf4j
+@Service
+public class StopRepresentingClientSubmittedHandler extends FinremCallbackHandler {
+
+    private static final String CONFIRMATION_HEADER = "# Notice of change request submitted";
+
+    public StopRepresentingClientSubmittedHandler(FinremCaseDetailsMapper finremCaseDetailsMapper) {
+        super(finremCaseDetailsMapper);
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return SUBMITTED.equals(callbackType)
+            && Arrays.asList(CONTESTED, CONSENTED).contains(caseType)
+            && STOP_REPRESENTING_CLIENT.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.submitted(callbackRequest));
+        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
+            .confirmationHeader(CONFIRMATION_HEADER)
+            .build();
+    }
+}
