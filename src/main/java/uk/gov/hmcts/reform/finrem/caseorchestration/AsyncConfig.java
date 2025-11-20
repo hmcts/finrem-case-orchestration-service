76bfd1254f1d527fff1cad103aad1d62d9781a4c
Ashley Wong <im@so99y.com>
2025-11-20 12:17:32 +0000

    add AsyncConfig

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/AsyncConfig.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {}
