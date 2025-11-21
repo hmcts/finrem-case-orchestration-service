1c5a77137525601f4d0ffbddd7b887c8d69eabe1
Ashley Wong <im@so99y.com>
2025-11-21 09:51:04 +0000

    fix unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientAboutToStartHandlerTest.java
@@ -45,8 +45,8 @@ class StopRepresentingClientAboutToStartHandlerTest {
     void givenAsApplicantSolicitor_whenHandled_thenSessionWrapperPopulated() {
         when(caseRoleService.getUserCaseRole(CASE_ID, AUTH_TOKEN)).thenReturn(APP_SOLICITOR);
 
-        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(Long.valueOf(CASE_ID),
-            FinremCaseData.builder().build());
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(
+            FinremCaseData.builder().ccdCaseId(CASE_ID).build());
         FinremCaseData finremCaseData = underTest.handle(callbackRequest, AUTH_TOKEN).getData();
         assertThat(finremCaseData.getSessionWrapper()).isNotNull();
         assertThat(finremCaseData.getSessionWrapper().getLoginAsApplicantSolicitor()).isEqualTo(YesOrNo.YES);
