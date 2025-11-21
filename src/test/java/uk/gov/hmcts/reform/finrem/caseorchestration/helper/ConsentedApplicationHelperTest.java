012d6ed3db8cc31e6aa83ad18137f7c50c8e29ac
Ashley Wong <im@so99y.com>
2025-11-21 17:56:23 +0000

    unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/ConsentedApplicationHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/ConsentedApplicationHelperTest.java
@@ -1,9 +1,15 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.helper;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Region;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.AllocatedRegionWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.RegionWrapper;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,26 +28,27 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstant
 import static uk.gov.hmcts.reform.finrem.caseorchestration.OrchestrationConstants.VARIATION_ORDER_LOWERCASE_LABEL_VALUE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigConstant.CONSENTED_NATURE_OF_APPLICATION;
 
-@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
-public class ConsentedApplicationHelperTest {
+@ExtendWith(MockitoExtension.class)
+class ConsentedApplicationHelperTest {
+
+    @InjectMocks
+    private ConsentedApplicationHelper consentedApplicationHelper;
 
     @Test
-    public void isVariationOrder() {
-        CallbackRequest callbackRequest =  callbackRequest();
+    void isVariationOrder() {
+        CallbackRequest callbackRequest = callbackRequest();
         callbackRequest.getCaseDetails().getData()
-            .put(CONSENTED_NATURE_OF_APPLICATION, List.of("Variation Order","Pension document","Lump sum"));
-        ConsentedApplicationHelper helper = new ConsentedApplicationHelper();
-        assertTrue(helper.isVariationOrder(callbackRequest.getCaseDetails().getData()));
+            .put(CONSENTED_NATURE_OF_APPLICATION, List.of("Variation Order", "Pension document", "Lump sum"));
+        assertTrue(consentedApplicationHelper.isVariationOrder(callbackRequest.getCaseDetails().getData()));
     }
 
     @Test
-    public void setConsentVariationOrderLabelField() {
-        CallbackRequest callbackRequest =  callbackRequest();
+    void setConsentVariationOrderLabelField() {
+        CallbackRequest callbackRequest = callbackRequest();
         Map<String, Object> data = callbackRequest.getCaseDetails().getData();
-        data.put(CONSENTED_NATURE_OF_APPLICATION, List.of("Variation Order","Pension document","Lump sum"));
+        data.put(CONSENTED_NATURE_OF_APPLICATION, List.of("Variation Order", "Pension document", "Lump sum"));
 
-        ConsentedApplicationHelper helper = new ConsentedApplicationHelper();
-        helper.setConsentVariationOrderLabelField(callbackRequest.getCaseDetails().getData());
+        consentedApplicationHelper.setConsentVariationOrderLabelField(callbackRequest.getCaseDetails().getData());
 
         assertEquals(VARIATION_ORDER_CAMELCASE_LABEL_VALUE, data.get(CV_ORDER_CAMELCASE_LABEL_FIELD));
         assertEquals(VARIATION_ORDER_LOWERCASE_LABEL_VALUE, data.get(CV_LOWERCASE_LABEL_FIELD));
@@ -49,16 +56,43 @@ public class ConsentedApplicationHelperTest {
     }
 
     @Test
-    public void givenCase_whenEmptyNatureOfApplicationIsEmpty_thenReturnEmptyList() {
-        CallbackRequest callbackRequest =  callbackRequest();
+    void givenCase_whenEmptyNatureOfApplicationIsEmpty_thenReturnEmptyList() {
+        CallbackRequest callbackRequest = callbackRequest();
         Map<String, Object> data = callbackRequest.getCaseDetails().getData();
 
-        ConsentedApplicationHelper helper = new ConsentedApplicationHelper();
-        helper.setConsentVariationOrderLabelField(callbackRequest.getCaseDetails().getData());
+        consentedApplicationHelper = new ConsentedApplicationHelper();
+        consentedApplicationHelper.setConsentVariationOrderLabelField(callbackRequest.getCaseDetails().getData());
 
         assertEquals(CONSENT_ORDER_CAMELCASE_LABEL_VALUE, data.get(CV_ORDER_CAMELCASE_LABEL_FIELD));
         assertEquals(CONSENT_ORDER_LOWERCASE_LABEL_VALUE, data.get(CV_LOWERCASE_LABEL_FIELD));
         assertEquals(CONSENT_OTHER_DOC_LABEL_VALUE, data.get(CV_OTHER_DOC_LABEL_FIELD));
+    }
+
+    @Test
+    void shouldReturnErrorWhenHighCourtSelected() {
+        // given
+        FinremCaseData caseData = buildCaseDataWithRegion(Region.HIGHCOURT);
+
+        // when
+        List<String> result = consentedApplicationHelper.validateRegionList(caseData);
+
+        // then
+        assertEquals(
+            List.of("You cannot select the High Court for a consent application."),
+            result
+        );
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenRegionIsNotHighCourt() {
+        // given
+        FinremCaseData caseData = buildCaseDataWithRegion(Region.MIDLANDS); // any non-HC region
+
+        // when
+        List<String> result = consentedApplicationHelper.validateRegionList(caseData);
+
+        // then
+        assertTrue(result.isEmpty());
     }
 
     private CallbackRequest callbackRequest() {
@@ -68,4 +102,19 @@ public class ConsentedApplicationHelperTest {
                 .data(new HashMap<>()).build())
             .build();
     }
+
+    private FinremCaseData buildCaseDataWithRegion(Region region) {
+        AllocatedRegionWrapper allocatedRegion = AllocatedRegionWrapper.builder()
+            .regionList(region)
+            .build();
+
+        RegionWrapper regionWrapper = RegionWrapper.builder()
+            .allocatedRegionWrapper(allocatedRegion)
+            .build();
+
+        return FinremCaseData.builder()
+            .regionWrapper(regionWrapper)
+            .build();
+    }
+
 }
