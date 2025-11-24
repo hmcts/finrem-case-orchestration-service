5504eb5817a47410c6571a0b4312c5f556cf8405
Ashley Wong <im@so99y.com>
2025-11-24 17:02:24 +0000

    fix

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandlerTest.java
@@ -3,11 +3,22 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler.stoprepresentingcli
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.StopRepresentationWrapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.STOP_REPRESENTING_CLIENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
@@ -33,21 +44,61 @@ class StopRepresentingClientMidHandlerTest {
             Arguments.of(MID_EVENT, CONTESTED, STOP_REPRESENTING_CLIENT));
     }
 
-//    @ParameterizedTest
-//    @ValueSource(booleans = {true, false})
-//    void givenIsHavingClientConsent_whenHandled_thenErrorPopulated(boolean loginAsApplicant) {
-//        FinremCaseData caseData = FinremCaseData.builder()
-//            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
-//                .clientConsentOnAppSolStopRep(loginAsApplicant ? YesOrNo.YES : mock(YesOrNo.class))
-//                .clientConsentOnRespSolStopRep(!loginAsApplicant ? YesOrNo.YES : mock(YesOrNo.class))
-//                .build())
-//            .sessionWrapper(SessionWrapper.builder()
-//                .loginAsApplicantSolicitor(YesOrNo.forValue(loginAsApplicant))
-//                .loginAsRespondentSolicitor(YesOrNo.forValue(!loginAsApplicant))
-//                .build())
-//            .build();
-//
-//        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
-//        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).isEmpty();
-//    }
+    @Test
+    void givenIsHavingClientConsent_whenHandled_thenNoErrorPopulated() {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
+                .stopRepClientConsent(YesOrNo.YES)
+                .build())
+            .build();
+
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
+        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).isEmpty();
+    }
+
+    @Test
+    void givenIsHavingJudicialApproval_whenHandled_thenNoErrorPopulated() {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
+                .stopRepJudicialApproval(YesOrNo.YES)
+                .build())
+            .build();
+
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
+        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).isEmpty();
+    }
+
+    @Test
+    void givenIsNotHavingJudicialApproval_whenHandled_thenErrorPopulated() {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
+                .stopRepClientConsent(mock(YesOrNo.class))
+                .stopRepJudicialApproval(YesOrNo.NO)
+                .build())
+            .build();
+
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
+        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).containsExactly(
+            "You cannot stop representing your client without either client consent or judicial approval. "
+                + "You will need to make a general application to apply to come off record using the next step event 'general application"
+        );
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = YesOrNo.class, names = {"NO"})
+    @NullSource
+    void givenIsNotHavingClientConsent_whenHandled_thenErrorPopulated(YesOrNo stopRepJudicialApproval) {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
+                .stopRepClientConsent(YesOrNo.NO)
+                .stopRepJudicialApproval(stopRepJudicialApproval)
+                .build())
+            .build();
+
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
+        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).containsExactly(
+            "You cannot stop representing your client without either client consent or judicial approval. "
+                + "You will need to make a general application to apply to come off record using the next step event 'general application"
+        );
+    }
 }
