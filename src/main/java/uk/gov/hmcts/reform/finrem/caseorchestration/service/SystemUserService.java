63e0f6df8a1999fac8e0f32e21a36b5660a05ea4
so99y <50691452+so99y@users.noreply.github.com>
2025-12-16 15:29:39 +0000

    Refactor SystemUserService (#2716)
    * Refactor SystemUserService
    
    * Fix
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/SystemUserService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/SystemUserService.java
@@ -3,33 +3,40 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.finrem.caseorchestration.config.SystemUpdateUserConfiguration;
 
-import static uk.gov.hmcts.reform.finrem.caseorchestration.config.CacheConfiguration.SYS_USER_CACHE;
-
+/**
+ * Service class responsible for handling operations related to the system user, such as
+ * retrieving tokens and user identifiers.
+ *
+ * <p>This class is annotated with {@code @Service} to be managed as a Spring bean.
+ * It uses Lombok annotations like {@code @Slf4j} for logging and
+ * {@code @RequiredArgsConstructor(onConstructor_ = {@Autowired})} for constructor injection of final fields.
+ */
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
-@CacheConfig(cacheNames = SYS_USER_CACHE)
 public class SystemUserService {
 
-    private final SystemUpdateUserConfiguration systemUpdateUserConfiguration;
+    private final SystemUserTokenProvider systemUserTokenProvider;
     private final IdamAuthService idamAuthService;
 
-    @Cacheable(cacheNames = {SYS_USER_CACHE})
+    /**
+     * Retrieves the system user token.
+     *
+     * @return The system user's authentication token as a String.
+     */
     public String getSysUserToken() {
-        log.info("Fetching system user token");
-        return idamAuthService.getAccessToken(systemUpdateUserConfiguration.getUserName(), systemUpdateUserConfiguration.getPassword());
+        return systemUserTokenProvider.getSysUserToken();
     }
 
-    @CacheEvict(allEntries = true, cacheNames = {SYS_USER_CACHE})
-    @Scheduled(fixedDelay = 1800000)
-    public void cacheEvict() {
-        log.info("Evicting system user cron cache");
+    /**
+     * Retrieves the Unique Identifier (UID) of the system user by first getting the system
+     * user token and then using it to fetch user information from the IDAM authentication service.
+     *
+     * @return The UID of the system user as a String.
+     */
+    public String getSysUserTokenUid() {
+        return idamAuthService.getUserInfo(systemUserTokenProvider.getSysUserToken()).getUid();
     }
 }
