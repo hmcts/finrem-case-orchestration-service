8e301a1ead51c9fc0f70fd255066436ce9cf136e
Ashley Wong <im@so99y.com>
2025-12-19 22:08:41 +0000

    Add Intervener parties

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ChangeOfRepresentationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/ChangeOfRepresentationRequest.java
@@ -9,6 +9,10 @@ public class ChangeOfRepresentationRequest {
 
     public static final String APPLICANT_PARTY = "Applicant";
     public static final String RESPONDENT_PARTY = "Respondent";
+    public static final String INTERVENER_ONE_PARTY = "Intervener 1";
+    public static final String INTERVENER_TWO_PARTY = "Intervener 2";
+    public static final String INTERVENER_THREE_PARTY = "Intervener 3";
+    public static final String INTERVENER_FOUR_PARTY = "Intervener 4";
 
     RepresentationUpdateHistory current;
     String party;
@@ -17,4 +21,14 @@ public class ChangeOfRepresentationRequest {
     ChangedRepresentative removedRepresentative;
     String by;
     String via;
+
+    public static String getIntervenerPartyByIndex(int index) {
+        return switch (index) {
+            case 1 -> INTERVENER_ONE_PARTY;
+            case 2 -> INTERVENER_TWO_PARTY;
+            case 3 -> INTERVENER_THREE_PARTY;
+            case 4 -> INTERVENER_FOUR_PARTY;
+            default -> null;
+        };
+    }
 }
