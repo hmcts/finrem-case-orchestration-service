12f53fcd0b4f49c6dbc6736fc4f43b3059db0390
Ashley Wong <im@so99y.com>
2026-01-20 16:34:00 +0000

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/notifiers/IntervenerFourPartyListenerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/notifiers/IntervenerFourPartyListenerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.notifications.notifiers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
