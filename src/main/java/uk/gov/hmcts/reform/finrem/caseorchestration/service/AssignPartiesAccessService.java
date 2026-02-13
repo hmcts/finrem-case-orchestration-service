e4bc8c2b0da357a537d6b21ea6cf961cc41257e3
Ashley Wong <im@so99y.com>
2026-02-13 10:07:23 +0000

    WIP

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessService.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AssignPartiesAccessService {
+
+    private final AssignCaseAccessService assignCaseAccessService;
+    private final PrdOrganisationService prdOrganisationService;
+    private final SystemUserService systemUserService;
+
+    public void grantApplicantSolicitor(FinremCaseData finremCaseData) {
+        if (finremCaseData.isApplicantRepresentedByASolicitor()) {
+            finremCaseData
+        } else {
+            log.info("No applicant represented by a solicitor");
+        }
+    }
+
+    public void assignIntervenerRole(FinremCaseData finremCaseData) {
+
+    }
+
+    public void grantRespondentSolicitor(FinremCaseData finremCaseData) {
+        if (finremCaseData.isRespondentRepresentedByASolicitor()) {
+
+        } else {
+            log.info("No applicant represented by a solicitor");
+        }
+    }
+
+    private void grantAccess(Long caseId, String email, String orgId, String caseRole) {
+        Optional<String> userId = prdOrganisationService.findUserByEmail(email, systemUserService.getSysUserToken());
+        if (userId.isPresent()) {
+            assignCaseAccessService.grantCaseRoleToUser(caseId, userId.get(), caseRole, orgId);
+        }
+    }
+}
