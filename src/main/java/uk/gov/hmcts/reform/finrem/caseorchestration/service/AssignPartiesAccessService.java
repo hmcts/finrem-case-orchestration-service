9a770cd9d31fc3aa3d5933639e4f9acf3f249c1d
Ashley Wong <im@so99y.com>
2026-02-13 13:06:34 +0000

    Implement grantRespondentSolicitor

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessService.java
@@ -42,8 +42,8 @@ public class AssignPartiesAccessService {
      *                               while the applicant is marked as represented
      */
     public void grantApplicantSolicitor(FinremCaseData finremCaseData) {
+        String caseId = finremCaseData.getCcdCaseId();
         if (finremCaseData.isApplicantRepresentedByASolicitor()) {
-            String caseId = finremCaseData.getCcdCaseId();
             String appSolicitorEmail = finremCaseData.getAppSolicitorEmail();
             String appOrgId = ofNullable(finremCaseData.getApplicantOrganisationPolicy())
                 .map(OrganisationPolicy::getOrganisation)
@@ -53,20 +53,47 @@ public class AssignPartiesAccessService {
                 ));
             grantAccess(Long.valueOf(caseId), appSolicitorEmail, appOrgId, CaseRole.APP_SOLICITOR.getCcdCode());
         } else {
-            log.info("No applicant represented by a solicitor");
+            log.info("{} - No applicant represented by a solicitor", caseId);
+        }
+    }
+
+    /**
+     * Grants case access to the respondent's solicitor where the respondent is legally represented.
+     *
+     * <p>
+     * This method retrieves the respondent solicitor's email address and organisation identifier
+     * from the provided {@link FinremCaseData}. If the respondent is represented by a solicitor,
+     * the solicitor is granted the {@link CaseRole#APP_SOLICITOR} role on the case.
+     * </p>
+     *
+     * <p>
+     * An {@link IllegalStateException} is thrown if the respondent organisation policy
+     * is missing when representation is indicated.
+     * </p>
+     *
+     * @param finremCaseData the case data containing respondent representation details,
+     *                       solicitor email, organisation policy, and CCD case ID
+     * @throws IllegalStateException if the respondent organisation policy is not present
+     *                               while the respondent is marked as represented
+     */
+    public void grantRespondentSolicitor(FinremCaseData finremCaseData) {
+        String caseId = finremCaseData.getCcdCaseId();
+        if (finremCaseData.isRespondentRepresentedByASolicitor()) {
+            String respondentSolicitorEmail = finremCaseData.getRespondentSolicitorEmail();
+            String appOrgId = ofNullable(finremCaseData.getRespondentOrganisationPolicy())
+                .map(OrganisationPolicy::getOrganisation)
+                .map(Organisation::getOrganisationID)
+                .orElseThrow(() -> new IllegalStateException(
+                    format("%s - Respondent organisation policy is missing", caseId)
+                ));
+            grantAccess(Long.valueOf(caseId), respondentSolicitorEmail, appOrgId, CaseRole.RESP_SOLICITOR.getCcdCode());
+        } else {
+            log.info("{} - No respondent represented by a solicitor", caseId);
         }
     }
 
     public void assignIntervenerRole(FinremCaseData finremCaseData) {
         // TODO
-    }
-
-    public void grantRespondentSolicitor(FinremCaseData finremCaseData) {
-        if (finremCaseData.isRespondentRepresentedByASolicitor()) {
-
-        } else {
-            log.info("No applicant represented by a solicitor");
-        }
     }
 
     private void grantAccess(Long caseId, String email, String orgId, String caseRole) {
