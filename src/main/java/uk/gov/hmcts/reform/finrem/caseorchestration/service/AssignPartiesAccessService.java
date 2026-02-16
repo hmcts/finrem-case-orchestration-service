97c61203d67dd9d57cb6a873f10eabc26dc17d82
Ashley Wong <im@so99y.com>
2026-02-16 11:47:41 +0000

    update tests

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessService.java
@@ -1,17 +1,14 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.service;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseRole;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Organisation;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.OrganisationPolicy;
 
 import java.util.Optional;
-
-import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
 
 @Service
 @Slf4j
@@ -22,78 +19,33 @@ public class AssignPartiesAccessService {
     private final PrdOrganisationService prdOrganisationService;
     private final SystemUserService systemUserService;
 
-    /**
-     * Grants case access to the applicant's solicitor where the applicant is legally represented.
-     *
-     * <p>
-     * This method retrieves the applicant solicitor's email address and organisation identifier
-     * from the provided {@link FinremCaseData}. If the applicant is represented by a solicitor,
-     * the solicitor is granted the {@link CaseRole#APP_SOLICITOR} role on the case.
-     * </p>
-     *
-     * <p>
-     * An {@link IllegalStateException} is thrown if the applicant organisation policy
-     * is missing when representation is indicated.
-     * </p>
-     *
-     * @param finremCaseData the case data containing applicant representation details,
-     *                       solicitor email, organisation policy, and CCD case ID
-     * @throws IllegalStateException if the applicant organisation policy is not present
-     *                               while the applicant is marked as represented
-     */
     public void grantApplicantSolicitor(FinremCaseData finremCaseData) {
         String caseId = finremCaseData.getCcdCaseId();
-        if (finremCaseData.isApplicantRepresentedByASolicitor()) {
+        if (finremCaseData.isApplicantRepresentedByASolicitor()
+            && isOrgIdExists(finremCaseData.getApplicantOrganisationPolicy())) {
             String appSolicitorEmail = finremCaseData.getAppSolicitorEmail();
-            String appOrgId = ofNullable(finremCaseData.getApplicantOrganisationPolicy())
-                .map(OrganisationPolicy::getOrganisation)
-                .map(Organisation::getOrganisationID)
-                .orElseThrow(() -> new IllegalStateException(
-                    format("%s - Applicant organisation policy is missing", caseId)
-                ));
+            String appOrgId = finremCaseData.getApplicantOrganisationPolicy().getOrganisation().getOrganisationID();
             grantAccess(Long.valueOf(caseId), appSolicitorEmail, appOrgId, CaseRole.APP_SOLICITOR.getCcdCode());
         } else {
-            log.info("{} - No applicant represented by a solicitor", caseId);
+            log.info("{} - No applicant represented by a solicitor or organisation policy missing", caseId);
         }
     }
 
-    /**
-     * Grants case access to the respondent's solicitor where the respondent is legally represented.
-     *
-     * <p>
-     * This method retrieves the respondent solicitor's email address and organisation identifier
-     * from the provided {@link FinremCaseData}. If the respondent is represented by a solicitor,
-     * the solicitor is granted the {@link CaseRole#APP_SOLICITOR} role on the case.
-     * </p>
-     *
-     * <p>
-     * An {@link IllegalStateException} is thrown if the respondent organisation policy
-     * is missing when representation is indicated.
-     * </p>
-     *
-     * @param finremCaseData the case data containing respondent representation details,
-     *                       solicitor email, organisation policy, and CCD case ID
-     * @throws IllegalStateException if the respondent organisation policy is not present
-     *                               while the respondent is marked as represented
-     */
     public void grantRespondentSolicitor(FinremCaseData finremCaseData) {
         String caseId = finremCaseData.getCcdCaseId();
-        if (finremCaseData.isRespondentRepresentedByASolicitor()) {
+        if (finremCaseData.isRespondentRepresentedByASolicitor()
+            && isOrgIdExists(finremCaseData.getRespondentOrganisationPolicy())) {
             String respondentSolicitorEmail = finremCaseData.getRespondentSolicitorEmail();
-            String appOrgId = ofNullable(finremCaseData.getRespondentOrganisationPolicy())
-                .map(OrganisationPolicy::getOrganisation)
-                .map(Organisation::getOrganisationID)
-                .orElseThrow(() -> new IllegalStateException(
-                    format("%s - Respondent organisation policy is missing", caseId)
-                ));
+            String appOrgId = finremCaseData.getRespondentOrganisationPolicy().getOrganisation().getOrganisationID();
             grantAccess(Long.valueOf(caseId), respondentSolicitorEmail, appOrgId, CaseRole.RESP_SOLICITOR.getCcdCode());
         } else {
-            log.info("{} - No respondent represented by a solicitor", caseId);
+            log.info("{} - No respondent represented by a solicitor or organisation policy missing", caseId);
         }
     }
 
-    public void assignIntervenerRole(FinremCaseData finremCaseData) {
-        // TODO
+    private boolean isOrgIdExists(OrganisationPolicy organisationPolicy) {
+        return organisationPolicy.getOrganisation() != null &&
+            StringUtils.isNotBlank(organisationPolicy.getOrganisation().getOrganisationID());
     }
 
     private void grantAccess(Long caseId, String email, String orgId, String caseRole) {
---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/AssignPartiesAccessServiceTest.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseRole;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID_IN_LONG;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_ORG_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_SYSTEM_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_USER_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.organisationPolicy;
+
+@ExtendWith(MockitoExtension.class)
+class AssignPartiesAccessServiceTest {
+
+    @Mock
+    private AssignCaseAccessService assignCaseAccessService;
+
+    @Mock
+    private PrdOrganisationService prdOrganisationService;
+
+    @Mock
+    private SystemUserService systemUserService;
+
+    @InjectMocks
+    private AssignPartiesAccessService assignPartiesAccessService;
+
+    @BeforeEach
+    void setUp() {
+        when(systemUserService.getSysUserToken()).thenReturn(TEST_SYSTEM_TOKEN);
+    }
+
+    @Test
+    void givenRegisteredEmail_whenGrantApplicantSolicitorInvoked_thenAssignAppSolicitorToCase() {
+        FinremCaseData caseData = mock(FinremCaseData.class);
+        when(caseData.getAppSolicitorEmail()).thenReturn(TEST_SOLICITOR_EMAIL);
+        when(caseData.getCcdCaseId()).thenReturn(CASE_ID);
+        when(caseData.isApplicantRepresentedByASolicitor()).thenReturn(true);
+        when(caseData.getApplicantOrganisationPolicy()).thenReturn(organisationPolicy(TEST_ORG_ID));
+
+        when(prdOrganisationService.findUserByEmail(TEST_SOLICITOR_EMAIL, TEST_SYSTEM_TOKEN)).thenReturn(
+            Optional.of(TEST_USER_ID)
+        );
+
+        // Act
+        assignPartiesAccessService.grantApplicantSolicitor(caseData);
+
+        verify(assignCaseAccessService).grantCaseRoleToUser(CASE_ID_IN_LONG, TEST_USER_ID, CaseRole.APP_SOLICITOR.getCcdCode(),
+            TEST_ORG_ID);
+        verifyNoMoreInteractions(assignCaseAccessService);
+    }
+
+    @Test
+    void givenEmailNotFound_whenGrantApplicantSolicitorInvoked_thenShouldNotAssignAppSolicitorToCase() {
+        FinremCaseData caseData = mock(FinremCaseData.class);
+        when(caseData.getAppSolicitorEmail()).thenReturn(TEST_SOLICITOR_EMAIL);
+        when(caseData.getCcdCaseId()).thenReturn(CASE_ID);
+        when(caseData.isApplicantRepresentedByASolicitor()).thenReturn(true);
+        when(caseData.getApplicantOrganisationPolicy()).thenReturn(organisationPolicy(TEST_ORG_ID));
+
+        when(prdOrganisationService.findUserByEmail(TEST_SOLICITOR_EMAIL, TEST_SYSTEM_TOKEN)).thenReturn(
+            Optional.empty()
+        );
+
+        // Act
+        assignPartiesAccessService.grantApplicantSolicitor(caseData);
+
+        verify(assignCaseAccessService, never()).grantCaseRoleToUser(anyLong(), anyString(), anyString(), anyString());
+    }
+}
