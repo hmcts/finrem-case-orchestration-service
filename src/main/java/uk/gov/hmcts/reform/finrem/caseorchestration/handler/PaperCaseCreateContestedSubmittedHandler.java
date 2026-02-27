1cbecb866c149156e763ca689f3abf386a32ddd0
Ashley Wong <im@so99y.com>
2026-02-27 13:11:22 +0000

    WIP

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/PaperCaseCreateContestedSubmittedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/PaperCaseCreateContestedSubmittedHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
@@ -39,21 +40,66 @@ public class PaperCaseCreateContestedSubmittedHandler extends FinremCallbackHand
                                                                               String userAuthorisation) {
         log.info(CallbackHandlerLogger.submitted(callbackRequest));
 
-        FinremCaseData finremCaseData = callbackRequest.getCaseDetails().getData();
+        String setSupplementaryDataError = setSupplementaryData(callbackRequest, userAuthorisation);
+        String assignApplicantSolicitorError = grantApplicantSolicitor(callbackRequest);
 
-        createCaseService.setSupplementaryData(callbackRequest, userAuthorisation);
-        grantApplicantSolicitor(finremCaseData);
-
-        return response(finremCaseData);
+        return submittedResponse(toConfirmationHeader(!StringUtils.isAllBlank(setSupplementaryDataError,
+                assignApplicantSolicitorError)),
+            toConfirmationBody(setSupplementaryDataError, assignApplicantSolicitorError));
     }
 
-    private void grantApplicantSolicitor(FinremCaseData caseData) {
+    private String toConfirmationHeader(boolean withError) {
+        return "# Paper Case Created%s".formatted(withError ? " with error" : "");
+    }
 
-        executeWithRetrySafely(log,
-            () -> assignPartiesAccessService.grantApplicantSolicitor(caseData),
-            caseData.getCcdCaseId(),
-            "granting respondent solicitor",
-            3
-        );
+    private String toConfirmationBody(String setSupplementaryDataError,
+                                      String assignApplicantSolicitorError) {
+
+        StringBuilder body = new StringBuilder("<ul>");
+
+        if (setSupplementaryDataError != null && !setSupplementaryDataError.isBlank()) {
+            body.append("<li>").append(setSupplementaryDataError).append("</li>");
+        }
+
+        if (assignApplicantSolicitorError != null && !assignApplicantSolicitorError.isBlank()) {
+            body.append("<li>").append(assignApplicantSolicitorError).append("</li>");
+        }
+
+        body.append("</ul>");
+
+        return body.toString();
+    }
+
+    private String setSupplementaryData(FinremCallbackRequest request, String userAuthorisation) {
+        try {
+            executeWithRetry(log,
+                () -> createCaseService.setSupplementaryData(request, userAuthorisation),
+                request.getCaseDetails().getCaseIdAsString(),
+                "setting supplementary data",
+                3
+            );
+            return null;
+        } catch (Exception ex) {
+            return "There was a problem setting supplementary data.";
+        }
+    }
+
+    private String grantApplicantSolicitor(FinremCallbackRequest request) {
+        String appSolEmail = request.getCaseDetails().getData().getAppSolicitorEmailIfRepresented();
+        if (StringUtils.isBlank(appSolEmail)) {
+            return null;
+        }
+        try {
+            executeWithRetry(log,
+                () -> assignPartiesAccessService.grantApplicantSolicitor(request.getCaseDetails().getData()),
+                request.getCaseDetails().getCaseIdAsString(),
+                "granting respondent solicitor",
+                3
+            );
+            return null;
+        } catch (Exception ex) {
+            return "There was a problem granting access to application solicitor %s"
+                .formatted(appSolEmail);
+        }
     }
 }
