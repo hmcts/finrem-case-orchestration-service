66ebbff0276b8a3230d1fd7cd1f2f0a78275981b
Ashley Wong <im@so99y.com>
2026-03-05 17:06:18 +0000

    update FormerApplicantBarristerListener & FormerRespondentBarristerListener

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/notifiers/FormerApplicantBarristerListener.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/notifications/notifiers/FormerApplicantBarristerListener.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.notifications.notifiers;
 
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Barrister;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.BarristerCollectionItem;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.notifications.service.EmailService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.InternationalPostalService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.NotificationService;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 public class FormerApplicantBarristerListener extends EmailNotificationOnlyListener {
@@ -28,6 +33,16 @@ public class FormerApplicantBarristerListener extends EmailNotificationOnlyListe
 
     @Override
     protected boolean shouldSendEmailNotification(SendCorrespondenceEvent event) {
-        return event.getCaseDetailsBefore() != null;
+        if (event.getCaseDetailsBefore() != null) {
+            Barrister barrister = event.getBarrister(); // target barrister is set in the event
+            FinremCaseData caseDataBefore = event.getCaseDetailsBefore().getData();
+            return ofNullable(caseDataBefore.getBarristerCollectionWrapper()
+                .getApplicantBarristers()
+            ).orElseThrow(IllegalStateException::new)
+                .stream()
+                .map(BarristerCollectionItem::getValue)
+                .anyMatch(b -> b.equals(barrister));
+        }
+        return false;
     }
 }
