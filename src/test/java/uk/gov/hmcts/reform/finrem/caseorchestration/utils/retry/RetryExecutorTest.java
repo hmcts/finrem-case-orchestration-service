d5f0da91abd3be6bbace7c213eee68cbcd0abff0
Ashley Wong <im@so99y.com>
2026-03-19 18:03:48 +0000

    update unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/retry/RetryExecutorTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/retry/RetryExecutorTest.java
@@ -45,6 +45,53 @@ class RetryExecutorTest {
     }
 
     @Nested
+    class RunWithRetryTest {
+
+        @ParameterizedTest(name = "should retry on status {0} and succeed")
+        @ValueSource(ints = {500, 503, 504})
+        void shouldRetryOnRetryableStatus(int status) throws Exception {
+            AtomicInteger counter = new AtomicInteger();
+
+            retryExecutor.runWithRetry(() -> {
+                if (counter.getAndIncrement() < DEFAULT_MAX_ATTEMPTS - 1) {
+                    throw feignException(status, "Retryable Error");
+                }
+            }, "send", CASE_ID);
+
+            assertThat(counter.get()).isEqualTo(DEFAULT_MAX_ATTEMPTS);
+        }
+
+        @ParameterizedTest(name = "should retry once on first status {0} then succeed")
+        @ValueSource(ints = {504}) // can expand if needed
+        void shouldRetryOnceThenSucceed(int status) throws Exception {
+            AtomicInteger counter = new AtomicInteger();
+
+            retryExecutor.runWithRetry(() -> {
+                if (counter.getAndIncrement() == 0) {
+                    throw feignException(status, "Retryable Error");
+                }
+            }, "send", CASE_ID);
+
+            assertThat(counter.get()).isEqualTo(2);
+        }
+
+        @ParameterizedTest(name = "should not retry on non-retryable exception")
+        @ValueSource(ints = {0}) // dummy parameter to allow parameterized test
+        void shouldNotRetryOnNonRetryableException(int ignored) {
+            AtomicInteger counter = new AtomicInteger();
+
+            assertThatThrownBy(() ->
+                retryExecutor.supplyWithRetry(() -> {
+                    counter.incrementAndGet();
+                    throw new RuntimeException("Not retryable");
+                }, "send", CASE_ID)
+            ).isInstanceOf(RuntimeException.class);
+
+            assertThat(counter.get()).isEqualTo(1);
+        }
+    }
+
+    @Nested
     class SupplyWithRetryTest {
 
         @ParameterizedTest(name = "should retry on status {0} and succeed")
