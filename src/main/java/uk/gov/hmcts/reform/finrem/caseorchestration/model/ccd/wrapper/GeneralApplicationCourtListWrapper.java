a227ef4a113d2649d0b94be6b74426e91c543a16
Hamzah <hamzah.mohammed@justice.gov.uk>
2026-03-26 14:36:16 +0000

    return null for getLondonCourt due to interface.

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/GeneralApplicationCourtListWrapper.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/GeneralApplicationCourtListWrapper.java
@@ -20,6 +20,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.HumberCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.KentSurreyCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.LancashireCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.LiverpoolCourt;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.LondonCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.ManchesterCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NewportCourt;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.NorthWalesCourt;
@@ -74,6 +75,10 @@ public class GeneralApplicationCourtListWrapper implements CourtListWrapper {
 
     @JsonProperty("generalApplicationDirections_highCourtList")
     private HighCourt generalApplicationDirectionsHighCourtList;
+
+    // This is only required for the consented side and should not be used elsewhere.
+    @Override
+    public LondonCourt getLondonCourt() { return null; }
 
     @JsonIgnore
     @Override
