d6485de8d379a0cb188b5e2bc8f48b6d134ed947
Ashley Wong <im@so99y.com>
2026-03-26 15:17:17 +0000

    check postcode

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler.stoprepresentingclient;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
@@ -9,14 +10,19 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandle
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.StopRepresentationWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.stoprepresentingclient.RepresentativeInContext;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.stoprepresentingclient.StopRepresentingClientService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Optional.ofNullable;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.STOP_REPRESENTING_CLIENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
@@ -29,8 +35,14 @@ public class StopRepresentingClientMidHandler extends FinremCallbackHandler {
     private static final String ERROR_MESSAGE = "You cannot stop representing your client without either client consent or judicial approval. "
         + "You will need to make a general application to apply to come off record using the next step event 'general application";
 
-    public StopRepresentingClientMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper) {
+    private static final String POSTCODE_FIELD_IS_REQUIRED = "%s's postcode field is required";
+
+    private final StopRepresentingClientService stopRepresentingClientService;
+
+    public StopRepresentingClientMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                            StopRepresentingClientService stopRepresentingClientService) {
         super(finremCaseDetailsMapper);
+        this.stopRepresentingClientService = stopRepresentingClientService;
     }
 
     @Override
@@ -47,15 +59,13 @@ public class StopRepresentingClientMidHandler extends FinremCallbackHandler {
 
         FinremCaseData finremCaseData = callbackRequest.getCaseDetails().getData();
 
-        List<String> errors = new ArrayList<>();
+        List<String> errors = new ArrayList<>(calculatePostcodeMissingErrors(finremCaseData, userAuthorisation));
+
         if (isNotHavingClientConsent(finremCaseData) && isNotHavingJudicialApproval(finremCaseData)) {
             errors.add(ERROR_MESSAGE);
         }
 
-        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder()
-            .data(finremCaseData)
-            .errors(errors)
-            .build();
+        return response(finremCaseData, null, errors);
     }
 
     private boolean isNotHavingClientConsent(FinremCaseData finremCaseData) {
@@ -64,5 +74,74 @@ public class StopRepresentingClientMidHandler extends FinremCallbackHandler {
 
     private boolean isNotHavingJudicialApproval(FinremCaseData finremCaseData) {
         return !YesOrNo.isYes(finremCaseData.getStopRepresentationWrapper().getStopRepJudicialApproval());
+    }
+
+    private List<String> calculatePostcodeMissingErrors(FinremCaseData finremCaseData, String userAuthorisation) {
+        RepresentativeInContext representativeInContext = stopRepresentingClientService.buildRepresentation(finremCaseData,
+            userAuthorisation);
+
+        StopRepresentationWrapper wrapper = finremCaseData.getStopRepresentationWrapper();
+        List<String> errors = new ArrayList<>();
+        if (isPostcodeMissingInMasterServiceAddress(wrapper)) {
+            errors.add(POSTCODE_FIELD_IS_REQUIRED.formatted(resolveMasterParty(representativeInContext)));
+        }
+        check(isPostcodeMissingInExtraAddress1(wrapper), wrapper.getExtraClientAddr1Id(), errors);
+        check(isPostcodeMissingInExtraAddress2(wrapper), wrapper.getExtraClientAddr2Id(), errors);
+        check(isPostcodeMissingInExtraAddress3(wrapper), wrapper.getExtraClientAddr3Id(), errors);
+        check(isPostcodeMissingInExtraAddress4(wrapper), wrapper.getExtraClientAddr4Id(), errors);
+
+        return errors;
+    }
+
+    private void check(boolean condition, String id, List<String> errors) {
+        if (condition) {
+            ExtraAddrType.describe(id)
+                .ifPresent(desc -> errors.add(POSTCODE_FIELD_IS_REQUIRED.formatted(desc)));
+        }
+    }
+
+    private boolean isPostcodeMissingInMasterServiceAddress(StopRepresentationWrapper wrapper) {
+        return isPostcodeMissing(wrapper.getClientAddressForService());
+    }
+
+    private boolean isPostcodeMissingInExtraAddress1(StopRepresentationWrapper wrapper) {
+        return isExtraFieldPostcodeMissing(wrapper.getExtraClientAddr1Id(), wrapper.getExtraClientAddr1());
+    }
+
+    private boolean isPostcodeMissingInExtraAddress2(StopRepresentationWrapper wrapper) {
+        return isExtraFieldPostcodeMissing(wrapper.getExtraClientAddr2Id(), wrapper.getExtraClientAddr2());
+    }
+
+    private boolean isPostcodeMissingInExtraAddress3(StopRepresentationWrapper wrapper) {
+        return isExtraFieldPostcodeMissing(wrapper.getExtraClientAddr3Id(), wrapper.getExtraClientAddr3());
+    }
+
+    private boolean isPostcodeMissingInExtraAddress4(StopRepresentationWrapper wrapper) {
+        return isExtraFieldPostcodeMissing(wrapper.getExtraClientAddr4Id(), wrapper.getExtraClientAddr4());
+    }
+
+    private boolean isPostcodeMissing(Address address) {
+        String postcode = ofNullable(address).map(Address::getPostCode).orElse("");
+        return StringUtils.isBlank(postcode);
+    }
+
+    private boolean isExtraFieldPostcodeMissing(String id, Address address) {
+        if (StringUtils.isBlank(id)) {
+            return false;
+        }
+        return isPostcodeMissing(address);
+    }
+
+    private String resolveMasterParty(RepresentativeInContext representativeInContext) {
+        if (representativeInContext.isApplicantRepresentative()) {
+            return "Applicant";
+        }
+        if (representativeInContext.isRespondentRepresentative()) {
+            return "Respondent";
+        }
+        if (representativeInContext.isIntervenerRepresentative()) {
+            return "Intervener %s".formatted(representativeInContext.intervenerIndex());
+        }
+        return null;
     }
 }
--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/stoprepresentingclient/StopRepresentingClientMidHandlerTest.java
@@ -6,19 +6,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.StopRepresentationWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.stoprepresentingclient.RepresentativeInContext;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.stoprepresentingclient.StopRepresentingClientService;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_USER_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.STOP_REPRESENTING_CLIENT;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
@@ -31,10 +39,13 @@ class StopRepresentingClientMidHandlerTest {
     private StopRepresentingClientMidHandler underTest;
     @Mock
     private FinremCaseDetailsMapper finremCaseDetailsMapper;
+    @Mock
+    private StopRepresentingClientService stopRepresentingClientService;
 
     @BeforeEach
-    public void setup() {
-        underTest = new StopRepresentingClientMidHandler(finremCaseDetailsMapper);
+    void setup() {
+        underTest = new StopRepresentingClientMidHandler(finremCaseDetailsMapper,
+            stopRepresentingClientService);
     }
 
     @Test
@@ -49,6 +60,7 @@ class StopRepresentingClientMidHandlerTest {
         FinremCaseData caseData = FinremCaseData.builder()
             .stopRepresentationWrapper(StopRepresentationWrapper.builder()
                 .stopRepClientConsent(YesOrNo.YES)
+                .clientAddressForService(createValidAddress())
                 .build())
             .build();
 
@@ -61,6 +73,7 @@ class StopRepresentingClientMidHandlerTest {
         FinremCaseData caseData = FinremCaseData.builder()
             .stopRepresentationWrapper(StopRepresentationWrapper.builder()
                 .stopRepJudicialApproval(YesOrNo.YES)
+                .clientAddressForService(createValidAddress())
                 .build())
             .build();
 
@@ -74,6 +87,7 @@ class StopRepresentingClientMidHandlerTest {
             .stopRepresentationWrapper(StopRepresentationWrapper.builder()
                 .stopRepClientConsent(mock(YesOrNo.class))
                 .stopRepJudicialApproval(YesOrNo.NO)
+                .clientAddressForService(createValidAddress())
                 .build())
             .build();
 
@@ -92,6 +106,7 @@ class StopRepresentingClientMidHandlerTest {
             .stopRepresentationWrapper(StopRepresentationWrapper.builder()
                 .stopRepClientConsent(YesOrNo.NO)
                 .stopRepJudicialApproval(stopRepJudicialApproval)
+                .clientAddressForService(createValidAddress())
                 .build())
             .build();
 
@@ -101,4 +116,61 @@ class StopRepresentingClientMidHandlerTest {
                 + "You will need to make a general application to apply to come off record using the next step event 'general application"
         );
     }
+
+    static Stream<Arguments> givenPostcodeMissingInMasterServiceAddress_whenHandled_thenErrorPopulated() {
+        return Stream.of(
+            Arguments.of(createEmptyPostcodeAddress(), createApplicantRepresentativeInContext(), "Applicant"),
+            Arguments.of(createNullPostcodeAddress(), createApplicantRepresentativeInContext(), "Applicant"),
+            Arguments.of(createEmptyPostcodeAddress(), createRespondentRepresentativeInContext(), "Respondent"),
+            Arguments.of(createNullPostcodeAddress(), createRespondentRepresentativeInContext(), "Respondent")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void givenPostcodeMissingInMasterServiceAddress_whenHandled_thenErrorPopulated(Address clientServiceAddress,
+                                                                                   RepresentativeInContext representativeInContext,
+                                                                                   String expectedPartyInError) {
+        FinremCaseData caseData = FinremCaseData.builder()
+            .stopRepresentationWrapper(StopRepresentationWrapper.builder()
+                .stopRepClientConsent(YesOrNo.YES)
+                .clientAddressForService(clientServiceAddress)
+                .build())
+            .build();
+
+        when(stopRepresentingClientService.buildRepresentation(caseData, AUTH_TOKEN)).thenReturn(
+            representativeInContext);
+
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(caseData);
+        assertThat(underTest.handle(request, AUTH_TOKEN).getErrors()).containsExactly(
+            "%s's postcode field is required".formatted(expectedPartyInError)
+        );
+    }
+
+    private static Address createValidAddress() {
+        Address address = mock(Address.class);
+        when(address.getPostCode()).thenReturn("AB1 DDE");
+        return address;
+    }
+
+    private static Address createEmptyPostcodeAddress() {
+        Address address = mock(Address.class);
+        when(address.getPostCode()).thenReturn("");
+        return address;
+    }
+
+    private static Address createNullPostcodeAddress() {
+        Address address = mock(Address.class);
+        when(address.getPostCode()).thenReturn(null);
+        return address;
+    }
+
+    private static RepresentativeInContext createApplicantRepresentativeInContext() {
+        return new RepresentativeInContext(TEST_USER_ID, true, false, null, null);
+    }
+
+    private static RepresentativeInContext createRespondentRepresentativeInContext() {
+        return new RepresentativeInContext(TEST_USER_ID, false, true, null, null);
+    }
+
 }
