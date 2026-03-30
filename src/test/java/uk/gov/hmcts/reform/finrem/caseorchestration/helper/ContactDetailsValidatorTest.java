853bd95e6a85c311062324b45ce8b08bb504c12f
Hamzah <hamzah.mohammed@justice.gov.uk>
2026-03-30 13:43:33 +0100

    refactor unit tests to work accordingly

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/ContactDetailsValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/helper/ContactDetailsValidatorTest.java
@@ -264,7 +264,7 @@ class ContactDetailsValidatorTest {
     @Test
     void shouldNotReturnErrorWhenApplicantAddressIsEmpty() {
         FinremCaseData caseData = createConsentedCaseData("SW1A 1AA", null, "E1 6AN", "EC1A 1BB", null, null);
-        caseData.getContactDetailsWrapper().setApplicantAddress(new Address()); // Empty address object
+        caseData.getContactDetailsWrapper().setApplicantAddress(new Address());
         List<String> errors = ContactDetailsValidator.validateCaseDataAddresses(caseData);
         assertThat(errors).isEmpty();
     }
@@ -272,7 +272,7 @@ class ContactDetailsValidatorTest {
     @Test
     void shouldNotReturnErrorWhenRespondentAddressIsEmpty() {
         FinremCaseData caseData = createConsentedCaseData("SW1A 1AA", "E1 6AN", "EC1A 1BB", "B1 1BB", null, null);
-        caseData.getContactDetailsWrapper().setRespondentAddress(new Address()); // Empty address object
+        caseData.getContactDetailsWrapper().setRespondentAddress(new Address());
         List<String> errors = ContactDetailsValidator.validateCaseDataAddresses(caseData);
         assertThat(errors).isEmpty();
     }
@@ -280,7 +280,7 @@ class ContactDetailsValidatorTest {
     @Test
     void shouldNotReturnErrorWhenApplicantAddressIsNull() {
         FinremCaseData caseData = createConsentedCaseData("SW1A 1AA", null, "E1 6AN", "EC1A 1BB", null, null);
-        caseData.getContactDetailsWrapper().setApplicantAddress(null); // Null address object
+        caseData.getContactDetailsWrapper().setApplicantAddress(null);
         List<String> errors = ContactDetailsValidator.validateCaseDataAddresses(caseData);
         assertThat(errors).isEmpty();
     }
@@ -288,7 +288,7 @@ class ContactDetailsValidatorTest {
     @Test
     void shouldNotReturnErrorWhenRespondentAddressIsNull() {
         FinremCaseData caseData = createConsentedCaseData("SW1A 1AA", "E1 6AN", "EC1A 1BB", "B1 1BB", null, null);
-        caseData.getContactDetailsWrapper().setRespondentAddress(null); // Null address object
+        caseData.getContactDetailsWrapper().setRespondentAddress(null);
         List<String> errors = ContactDetailsValidator.validateCaseDataAddresses(caseData);
         assertThat(errors).isEmpty();
     }
