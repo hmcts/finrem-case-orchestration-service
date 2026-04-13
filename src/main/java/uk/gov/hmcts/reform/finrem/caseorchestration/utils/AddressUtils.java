16a63dc3db7778988e8502a0b47e675b9dac5b22
dawudgovuk <dawud.rahman@justice.gov.uk>
2026-04-13 17:09:06 +0100

    DFR-4589-Update-Contact-Details-Automatic-Assigment-base-Review

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/AddressUtils.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/AddressUtils.java
@@ -4,33 +4,62 @@ import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
 
+import static java.util.Objects.isNull;
+
 /**
  * Utility class for comparing Address objects.
  */
 @UtilityClass
 public class AddressUtils {
+
+    private static final String[] ADDRESS_FIELDS = {
+        "addressLine1",
+        "addressLine2",
+        "addressLine3",
+        "county",
+        "country",
+        "postTown",
+        "postCode"
+    };
+
     /**
      * Checks if any field in the address has changed between two Address objects.
      * Nulls and blanks are treated as empty strings, and comparison is case-insensitive and trimmed.
+     * Applicant and Respondent Address are not requested when represented by solicitor so null's are allowed.
      *
      * @param oldAddress the original address
      * @param newAddress the new address
      * @return true if any field has changed, false otherwise
      */
     public static boolean hasChange(Address oldAddress, Address newAddress) {
-        if (oldAddress == newAddress) {
+        // Applicant or Respondent details if they are both represented by Solicitors can be null,
+        // so we should not consider that as a change
+        if (isNull(oldAddress) && isNull(newAddress)) {
             return false;
         }
-        if (oldAddress == null || newAddress == null) {
+        if (oldAddress == null ^ newAddress == null) {
             return true;
         }
-        return !equalsIgnoreCaseAndTrim(oldAddress.getAddressLine1(), newAddress.getAddressLine1())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getAddressLine2(), newAddress.getAddressLine2())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getAddressLine3(), newAddress.getAddressLine3())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getCounty(), newAddress.getCounty())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getCountry(), newAddress.getCountry())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getPostTown(), newAddress.getPostTown())
-            || !equalsIgnoreCaseAndTrim(oldAddress.getPostCode(), newAddress.getPostCode());
+
+        for (String field : ADDRESS_FIELDS) {
+            if (!equalsIgnoreCaseAndTrim(getAddressFieldValue(oldAddress, field), getAddressFieldValue(newAddress, field))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String getAddressFieldValue(Address address, String fieldName) {
+        return switch (fieldName) {
+            case "addressLine1" -> address.getAddressLine1();
+            case "addressLine2" -> address.getAddressLine2();
+            case "addressLine3" -> address.getAddressLine3();
+            case "county" -> address.getCounty();
+            case "country" -> address.getCountry();
+            case "postTown" -> address.getPostTown();
+            case "postCode" -> address.getPostCode();
+            default -> throw new IllegalArgumentException("Unknown address field: " + fieldName);
+        };
     }
 
     private static boolean equalsIgnoreCaseAndTrim(String s1, String s2) {
