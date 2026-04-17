d8aa26f0a9a8ba83e2e29827deaefba704d7ff34
Ashley Wong <im@so99y.com>
2026-04-17 18:07:13 +0100

    tidy up

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/managehearings/ManageHearingsSubmittedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/managehearings/ManageHearingsSubmittedHandlerTest.java
@@ -15,18 +15,9 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToSt
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CfcCourt;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Court;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Region;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.RegionLondonFrc;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingMode;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.HearingType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.ManageHearingsAction;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.hearings.Hearing;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.managehearings.hearings.ManageHearingsCollectionItem;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.DefaultCourtListWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ManageHearingsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.notifications.notifiers.NotificationParty;
 import uk.gov.hmcts.reform.finrem.caseorchestration.notifications.notifiers.SendCorrespondenceEvent;
@@ -37,7 +28,6 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryErrorHandle
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryExecutor;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.ThrowingRunnable;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -84,7 +74,7 @@ class ManageHearingsSubmittedHandlerTest {
     void givenSinglePartyToBeNotified_whenHandleAddHearingAction_thenPublishSendCorrespondenceEvent(NotificationParty notificationParty) {
         // Arrange
         UUID hearingID = UUID.randomUUID();
-        FinremCallbackRequest callbackRequest = buildCallbackRequest(hearingID, hearingID, ManageHearingsAction.ADD_HEARING);
+        FinremCallbackRequest callbackRequest = buildCallbackRequest(ManageHearingsAction.ADD_HEARING);
 
         SendCorrespondenceEvent event = mock(SendCorrespondenceEvent.class);
         when(event.getCaseId()).thenReturn(CASE_ID);
@@ -120,7 +110,7 @@ class ManageHearingsSubmittedHandlerTest {
     void givenMultiplePartiesToBeNotified_whenHandleAddHearingAction_thenPublishSendCorrespondenceEvent() {
         // Arrange
         UUID hearingID = UUID.randomUUID();
-        FinremCallbackRequest callbackRequest = buildCallbackRequest(hearingID, hearingID, ManageHearingsAction.ADD_HEARING);
+        FinremCallbackRequest callbackRequest = buildCallbackRequest(ManageHearingsAction.ADD_HEARING);
 
         SendCorrespondenceEvent event = mock(SendCorrespondenceEvent.class);
         when(event.getCaseId()).thenReturn(CASE_ID);
@@ -161,7 +151,7 @@ class ManageHearingsSubmittedHandlerTest {
     void shouldHandleSubmittedCallbackForVacateHearing() {
         // Arrange
         UUID hearingID = UUID.randomUUID();
-        FinremCallbackRequest callbackRequest = buildCallbackRequest(hearingID, hearingID, ManageHearingsAction.ADJOURN_OR_VACATE_HEARING);
+        FinremCallbackRequest callbackRequest = buildCallbackRequest(ManageHearingsAction.ADJOURN_OR_VACATE_HEARING);
 
         // Act
         GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response =
@@ -176,34 +166,11 @@ class ManageHearingsSubmittedHandlerTest {
         verify(manageHearingsCorresponder).buildAdjournedOrVacatedHearingCorrespondenceEventIfNeeded(callbackRequest, AUTH_TOKEN);
     }
 
-    private FinremCallbackRequest buildCallbackRequest(UUID hearingID, UUID hearingItemId, ManageHearingsAction action) {
+    private FinremCallbackRequest buildCallbackRequest(ManageHearingsAction action) {
         FinremCaseData finremCaseData = FinremCaseData.builder()
             .manageHearingsWrapper(ManageHearingsWrapper
                 .builder()
                 .manageHearingsActionSelection(action)
-                .workingHearingId(hearingID)
-                .hearings(List.of(
-                    ManageHearingsCollectionItem
-                        .builder()
-                        .id(hearingItemId)
-                        .value(Hearing
-                            .builder()
-                            .hearingType(HearingType.APPEAL_HEARING)
-                            .hearingDate(LocalDate.now())
-                            .hearingTime("10:00 AM")
-                            .hearingTimeEstimate("2 hours")
-                            .hearingMode(HearingMode.IN_PERSON)
-                            .additionalHearingInformation("Additional Info")
-                            .hearingCourtSelection(Court.builder()
-                                .region(Region.LONDON)
-                                .londonList(RegionLondonFrc.LONDON)
-                                .courtListWrapper(DefaultCourtListWrapper.builder()
-                                    .cfcCourtList(CfcCourt.BROMLEY_COUNTY_COURT_AND_FAMILY_COURT)
-                                    .build())
-                                .build())
-                            .build())
-                        .build()
-                ))
                 .build())
             .build();
 
