839ab2726f1f48b26795294228182353c294daef
dawudgovuk <dawud.rahman@justice.gov.uk>
2026-04-23 22:27:58 +0100

    DFR-4967-Boost-Parametized-Unit-Test-dfr-4303

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/AddressUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/utils/AddressUtilsTest.java
@@ -3,7 +3,12 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.Address;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,28 +19,6 @@ class AddressUtilsTest {
     @Nested
     @DisplayName("hasChange")
     class HasChange {
-        @Test
-        void shouldReturnFalseWhenAddressesAreSame() {
-            Address a1 = Address.builder()
-                    .addressLine1("123 Main St")
-                    .addressLine2("Apt 4")
-                    .addressLine3("Block B")
-                    .county("County")
-                    .country("UK")
-                    .postTown("London")
-                    .postCode("E1 1AA")
-                    .build();
-            Address a2 = Address.builder()
-                    .addressLine1("123 Main St")
-                    .addressLine2("Apt 4")
-                    .addressLine3("Block B")
-                    .county("County")
-                    .country("UK")
-                    .postTown("London")
-                    .postCode("E1 1AA")
-                    .build();
-            assertFalse(AddressUtils.hasChange(a1, a2));
-        }
 
         @Test
         void shouldReturnTrueWhenAnyFieldIsDifferent() {
@@ -52,13 +35,6 @@ class AddressUtilsTest {
         }
 
         @Test
-        void shouldReturnTrueIfOneAddressIsNull() {
-            Address a1 = Address.builder().addressLine1("A").build();
-            assertTrue(AddressUtils.hasChange(a1, null));
-            assertTrue(AddressUtils.hasChange(null, a1));
-        }
-
-        @Test
         void shouldReturnFalseWhenFieldsContainsWhitespace() {
             Address a1 = Address.builder().addressLine1("123 Main St ").build();
             Address a2 = Address.builder().addressLine1("123 Main St").build();
@@ -70,14 +46,142 @@ class AddressUtilsTest {
             assertFalse(AddressUtils.hasChange(null, null));
         }
 
-        @Test
-        void shouldReturnTrueIfOneFieldIsNullAndOtherIsNot() {
-            Address a1 = Address.builder().addressLine1(null).build();
-            Address a2 = Address.builder().addressLine1(" ").build();
+        @ParameterizedTest(name = "shouldReturnTrueWhen {0} IsDifferent")
+        @MethodSource("addressFieldChangeProvider")
+        void shouldReturnTrueWhenFieldIsDifferent(String ignoredField, Address a1, Address a2) {
+            assertTrue(AddressUtils.hasChange(a1, a2));
+        }
+
+        static Stream<Arguments> addressFieldChangeProvider() {
+            return Stream.of(
+                //  Address line1
+                Arguments.of("AddressLine1",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("124 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA")),
+                //  Address line2
+                Arguments.of("AddressLine2",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 5", "Block B", "County", "UK", "London", "E1 1AA")),
+                //  Addresss line3
+                Arguments.of("AddressLine3",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block C", "County", "UK", "London", "E1 1AA")),
+                //  Address country
+                Arguments.of("County",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "OtherCounty", "UK", "London", "E1 1AA")),
+                //  Address country
+                Arguments.of("Country",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "France", "London", "E1 1AA")),
+                //  Address post town
+                Arguments.of("PostTown",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "Manchester", "E1 1AA")),
+                //  Address post code
+                Arguments.of("PostCode",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E2 2BB")),
+                //  Address line1 case
+                Arguments.of("AddressLine1",
+                    buildAddress("main street", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("MAIN STREET", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA")),
+                //  Address line2 case
+                Arguments.of("AddressLine2",
+                    buildAddress("123 Main St", "apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "APT 4", "Block B", "County", "UK", "London", "E1 1AA")),
+                //  Address line3 case
+                Arguments.of("AddressLine3",
+                    buildAddress("123 Main St", "Apt 4", "block b", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "BLOCK B", "County", "UK", "London", "E1 1AA")),
+                //  Address county case
+                Arguments.of("County",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "county", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "COUNTY", "UK", "London", "E1 1AA")),
+                //  Address country case
+                Arguments.of("Country",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "uk", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA")),
+                //  Address post town case
+                Arguments.of("PostTown",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "london", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "LONDON", "E1 1AA")),
+                //  Address post code case
+                Arguments.of("PostCode",
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "e1 1aa"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("addressWhitespaceProvider")
+        void shouldReturnFalseWhenFieldsContainOnlyWhitespace(Address a1, Address a2) {
             assertFalse(AddressUtils.hasChange(a1, a2));
-            Address a3 = Address.builder().addressLine1("A").build();
-            assertTrue(AddressUtils.hasChange(a1, a3));
+        }
+
+        static Stream<Arguments> addressWhitespaceProvider() {
+            return Stream.of(
+                Arguments.of(
+                    buildAddress("123 Main St ", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA")
+                ),
+                Arguments.of(
+                    buildAddress("123 Main St", "Apt 4 ", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA")
+                )
+            );
+        }
+
+        @Test
+        void shouldReturnFalseWhenAddressesAreSame() {
+            Address a1 = buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA");
+            Address a2 = buildAddress("123 Main St", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA");
+            assertFalse(AddressUtils.hasChange(a1, a2));
+        }
+
+        @Test
+        void shouldReturnTrueIfOneAddressIsNull() {
+            Address a1 = buildAddress("A", null, null, null, null, null, null);
+            assertTrue(AddressUtils.hasChange(a1, null));
+            assertTrue(AddressUtils.hasChange(null, a1));
+        }
+
+        @ParameterizedTest
+        @MethodSource("addressNullFieldProvider")
+        void shouldReturnTrueIfOneFieldIsNullAndOtherIsNot(Address a1, Address a2, boolean expected) {
+            if (expected) {
+                assertTrue(AddressUtils.hasChange(a1, a2));
+            } else {
+                assertFalse(AddressUtils.hasChange(a1, a2));
+            }
+        }
+
+        static Stream<Arguments> addressNullFieldProvider() {
+            return Stream.of(
+                Arguments.of(
+                    buildAddress(null, "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress(" ", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    false
+                ),
+                Arguments.of(
+                    buildAddress(null, "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    buildAddress("A", "Apt 4", "Block B", "County", "UK", "London", "E1 1AA"),
+                    true
+                )
+            );
+        }
+
+        private static Address buildAddress(String line1, String line2, String line3, String county,
+                                            String country, String postTown, String postCode) {
+            return Address.builder()
+                .addressLine1(line1)
+                .addressLine2(line2)
+                .addressLine3(line3)
+                .county(county)
+                .country(country)
+                .postTown(postTown)
+                .postCode(postCode)
+                .build();
         }
     }
 }
-
