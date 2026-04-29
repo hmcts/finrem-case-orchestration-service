8faf13b21cc65e932a0b19e525ccd04549463bc7
Ashley Wong <im@so99y.com>
2026-04-29 14:24:10 +0100

    revert

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/FinremCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/FinremCallbackHandler.java
@@ -143,15 +143,15 @@ public abstract class FinremCallbackHandler implements CallbackHandler<FinremCas
     private FinremCallbackRequest mapToFinremCallbackRequest(CallbackRequest callbackRequest) {
         FinremCaseDetails finremCaseDetails = finremCaseDetailsMapper.mapToFinremCaseDetails(callbackRequest.getCaseDetails());
         FinremCaseDetails finremCaseDetailsBefore = null;
-        String caseID = finremCaseDetails.getCaseIdAsString();
+        String caseIc = finremCaseDetails.getCaseIdAsString();
         if (callbackRequest.getCaseDetailsBefore() != null) {
             finremCaseDetailsBefore = finremCaseDetailsMapper.mapToFinremCaseDetails(callbackRequest.getCaseDetailsBefore());
             if (nonNull(finremCaseDetailsBefore.getData())
                 && isNull(finremCaseDetailsBefore.getData().getCcdCaseId())) {
-                finremCaseDetailsBefore.getData().setCcdCaseId(caseID);
+                finremCaseDetailsBefore.getData().setCcdCaseId(caseIc);
             }
         }
-        finremCaseDetails.getData().setCcdCaseId(caseID);
+        finremCaseDetails.getData().setCcdCaseId(caseIc);
 
         return FinremCallbackRequest.builder()
             .caseDetails(finremCaseDetails)
