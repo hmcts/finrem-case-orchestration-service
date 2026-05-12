e99afad511997cdfa91ce47f09144b9c0909b891
Ashley Wong <im@so99y.com>
2026-05-12 11:01:15 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/creategeneralemail/GeneralEmailMidHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/creategeneralemail/GeneralEmailMidHandlerTest.java
@@ -3,115 +3,145 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler.creategeneralemail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils;
-import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
-import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralEmailWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.BulkPrintDocumentService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.evidencemanagement.EvidenceManagementDownloadService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID_IN_LONG;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.MID_EVENT;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.CREATE_GENERAL_EMAIL;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONTESTED;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandleAnyCaseType;
 
 @ExtendWith(MockitoExtension.class)
 class GeneralEmailMidHandlerTest {
 
+    private static final int MAX_FILE_SIZE = 2 * 1024 * 1024;
+    private static final byte[] PDF_BYTES = {1};
+    private static final byte[] PDF_BYTES_EXCEEDS_FILE_SIZE = new byte[MAX_FILE_SIZE + 1];
+
+    private final CaseDocument attachment = caseDocument("a.doc");
+
+    @InjectMocks
     private GeneralEmailMidHandler handler;
 
     @Mock
     private BulkPrintDocumentService bulkPrintDocumentService;
     @Mock
-    private FinremCaseDetailsMapper finremCaseDetailsMapper;
-    @Mock
     private EvidenceManagementDownloadService evidenceManagementDownloadService;
 
     @BeforeEach
-    void setup() {
-        handler =  new GeneralEmailMidHandler(finremCaseDetailsMapper, evidenceManagementDownloadService, bulkPrintDocumentService);
+    void setUp() {
+        lenient().when(evidenceManagementDownloadService.getByteArray(attachment, AUTH_TOKEN)).thenReturn(PDF_BYTES);
     }
 
     @Test
-    void testCanHandle() {
-        assertCanHandle(handler,
-            Arguments.of(MID_EVENT, CONTESTED, CREATE_GENERAL_EMAIL),
-            Arguments.of(MID_EVENT, CONSENTED, CREATE_GENERAL_EMAIL)
+    void shouldHandleAllCaseTypes() {
+        assertCanHandleAnyCaseType(handler, MID_EVENT, EventType.CREATE_GENERAL_EMAIL);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void givenEmailWithAttachment_whenHandle_thenShouldValidateEncryptionOnAttachment(boolean withAttachment) {
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .generalEmailWrapper(GeneralEmailWrapper.builder().generalEmailUploadedDocument(withAttachment
+                ? attachment : null).build())
+            .build();
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG, finremCaseData);
+
+        handler.handle(request, AUTH_TOKEN);
+
+        verify(bulkPrintDocumentService, times(withAttachment ? 1 : 0))
+            .validateEncryptionOnUploadedDocument(attachment, CASE_ID, List.of(), AUTH_TOKEN);
+    }
+
+    @Test
+    void givenEncryptionDetectedOnAttachment_whenHandle_thenPopulateError() {
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .generalEmailWrapper(GeneralEmailWrapper.builder().generalEmailUploadedDocument(attachment).build())
+            .build();
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG, finremCaseData);
+
+        mockEncryptionDetected();
+
+        var response = handler.handle(request, AUTH_TOKEN);
+
+        assertAll(
+            () -> verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(eq(attachment), eq(CASE_ID), anyList(),
+                eq(AUTH_TOKEN)),
+            () -> assertThat(response.getErrors()).containsOnly("ENCRYPTION_DETECTED")
         );
     }
 
     @Test
-    void givenACcdCallbackCallbackCreateGeneralEmailMidHandler() {
-        FinremCallbackRequest callbackRequest = buildFinremCallbackRequest();
+    void givenFileSizeExceeds_whenHandle_thenPopulateError() {
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .generalEmailWrapper(GeneralEmailWrapper.builder().generalEmailUploadedDocument(attachment).build())
+            .build();
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG, finremCaseData);
 
-        when(evidenceManagementDownloadService.getByteArray(any(CaseDocument.class), eq(AUTH_TOKEN))).thenReturn(new byte[1]);
+        when(evidenceManagementDownloadService.getByteArray(attachment, AUTH_TOKEN)).thenReturn(PDF_BYTES_EXCEEDS_FILE_SIZE);
 
-        handler.handle(callbackRequest, AUTH_TOKEN);
-        verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        var response = handler.handle(request, AUTH_TOKEN);
+
+        assertAll(
+            () -> verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(eq(attachment), eq(CASE_ID), anyList(),
+                eq(AUTH_TOKEN)),
+            () -> verify(evidenceManagementDownloadService).getByteArray(attachment, AUTH_TOKEN),
+            () -> assertThat(response.getErrors()).containsOnly( "You attached a document which exceeds the size limit: 2MB")
+        );
     }
 
     @Test
-    void shouldReturnErrorWhenFileUploadedExceeds2MB() {
-        FinremCallbackRequest callbackRequest = buildFinremCallbackRequest();
+    void givenFileSizeExceedsAndEncryptionDetected_whenHandle_thenPopulateErrors() {
+        FinremCaseData finremCaseData = FinremCaseData.builder()
+            .generalEmailWrapper(GeneralEmailWrapper.builder().generalEmailUploadedDocument(attachment).build())
+            .build();
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG, finremCaseData);
 
-        when(evidenceManagementDownloadService.getByteArray(any(CaseDocument.class), eq(AUTH_TOKEN))).thenReturn(new byte[2 * 1024 * 1024 + 1]);
-        doNothing().when(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
+        when(evidenceManagementDownloadService.getByteArray(attachment, AUTH_TOKEN)).thenReturn(PDF_BYTES_EXCEEDS_FILE_SIZE);
+        mockEncryptionDetected();
 
-        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(callbackRequest, AUTH_TOKEN);
-        verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
-        assertThat(response.getErrors()).containsExactly("You attached a document which exceeds the size limit: 2MB");
+        var response = handler.handle(request, AUTH_TOKEN);
+
+        assertAll(
+            () -> verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(eq(attachment), eq(CASE_ID), anyList(),
+                eq(AUTH_TOKEN)),
+            () -> verify(evidenceManagementDownloadService).getByteArray(attachment, AUTH_TOKEN),
+            () -> assertThat(response.getErrors()).containsExactly("ENCRYPTION_DETECTED",
+                "You attached a document which exceeds the size limit: 2MB")
+        );
     }
 
-    @Test
-    void shouldReturnMultipleErrorsWhenFileUploadedExceeds2mbAndWithEncryptionValidationError() {
-        FinremCallbackRequest callbackRequest = buildFinremCallbackRequest();
-
-        String expectedErrorMessage = "Encryption validation failed";
-
-        when(evidenceManagementDownloadService.getByteArray(any(CaseDocument.class), eq(AUTH_TOKEN))).thenReturn(new byte[2 * 1024 * 1024 + 1]);
+    private void mockEncryptionDetected() {
+        List<String> errors = new ArrayList<>();
         doAnswer(invocation -> {
-            List<String> errorList = invocation.getArgument(2);
-            errorList.add(expectedErrorMessage);
+            ((List<String>) invocation.getArguments()[2]).add("ENCRYPTION_DETECTED");
             return null;
         }).when(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(
-            any(CaseDocument.class), anyString(), anyList(), anyString()
-        );
-        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(callbackRequest, AUTH_TOKEN);
-        verify(bulkPrintDocumentService).validateEncryptionOnUploadedDocument(any(), any(), any(), any());
-        assertThat(response.getErrors()).containsExactly(expectedErrorMessage, "You attached a document which exceeds the size limit: 2MB");
-    }
-
-    private FinremCallbackRequest buildFinremCallbackRequest() {
-        FinremCaseData caseData = FinremCaseData.builder()
-            .generalEmailWrapper(GeneralEmailWrapper.builder()
-                .generalEmailCreatedBy("Test")
-                .generalEmailBody("Email body")
-                .generalEmailRecipient("Applicant@tedt.com")
-                .generalEmailUploadedDocument(TestSetUpUtils.caseDocument())
-                .build())
-            .build();
-        FinremCaseDetails caseDetails = FinremCaseDetails.builder().id(123L).caseType(CaseType.CONSENTED).data(caseData).build();
-        return FinremCallbackRequest.builder().eventType(CREATE_GENERAL_EMAIL).caseDetails(caseDetails).build();
+            attachment, CASE_ID, errors, AUTH_TOKEN);
     }
 }
