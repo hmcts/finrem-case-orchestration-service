ae7f15e267136e0be52f017af3414c9bc358a08d
Joe Wilkinson <Joe.Wilkinson@justice.gov.uk>
2026-05-14 13:05:31 +0100

    DFR-5056: Remove unused import in PreviousOrganisationCollectionItem.java

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/PreviousOrganisationCollectionItem.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/PreviousOrganisationCollectionItem.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
