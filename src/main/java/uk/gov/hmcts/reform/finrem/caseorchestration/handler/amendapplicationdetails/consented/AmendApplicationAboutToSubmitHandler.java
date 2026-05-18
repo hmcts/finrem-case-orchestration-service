e2dd94212bfe659fde3a5e8c531f0ab1701262fa
so99y <50691452+so99y@users.noreply.github.com>
2026-05-18 09:35:21 +0100

    DFR-4250 CT & CS: Amend Application Details -Auto Assign Applicant Solicitor (#2913)
    * refactoring
    
    * WIP
    
    * update error message
    
    * revoke logic
    
    * Extract common logic from Dawud's branch - DFR-4589
    
    * WIP
    
    * Rename
    
    * Moving package
    
    * refactor
    
    * log enhancement
    
    * canHandle
    
    * wip
    
    * wip
    
    * wip
    
    * update test case
    
    * EOL
    
    * WIP
    
    * WIP
    
    * update test case
    
    * update test case
    
    * refactor
    
    * refactor
    
    * JavaDoc
    
    * Fix missing case id in before finremCaseDetails
    
    * refactor
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * tidy up
    
    * resolved comment
    
    * resolved comment
    
    * remove check null
    
    * remove check null
    
    * refactor
    
    * resolved comment
    
    * check null
    
    * checkstyle
    
    * wrong label
    
    * resolved comment
    
    * revert
    
    * revert
    
    * revert
    
    * Resolved
    
    * missing unit tests
    
    * missing unit tests
    
    * fix
    
    * fix
    
    * missing unit tests
    
    * enhance readability
    
    * making use of runWithRetryWithHandler
    
    * DFR-4983 CT & CS: Amend Application Details - Validate applicant solicitor email in organisation (#2922)
    
    * DFR-4983
    
    * unit test
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/amendapplicationdetails/consented/AmendApplicationAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/amendapplicationdetails/consented/AmendApplicationAboutToSubmitHandler.java
@@ -1,10 +1,13 @@
-package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.amendapplicationdetails.consented;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.solicitorcreatecase.mandatorydatavalidation.CreateCaseMandatoryDataValidator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ContactDetailsValidator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
@@ -85,7 +88,7 @@ public class AmendApplicationAboutToSubmitHandler extends FinremCallbackHandler 
                 .data(caseData).build();
         }
 
-        return GenericAboutToStartOrSubmitCallbackResponse.<FinremCaseData>builder().data(caseData).errors(errors).build();
+        return response(caseData, null, errors);
     }
 
     private void updateLatestConsentOrder(FinremCallbackRequest callbackRequest) {
