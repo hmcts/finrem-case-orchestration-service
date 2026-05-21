01c2d238ee9ba8e99c7bd317f0d39f10a8c65cd9
al-hmcts <166380454+al-hmcts@users.noreply.github.com>
2026-05-21 14:47:05 +0100

    DFR-5093 - Fix use of new OM without Instantiated Modules (#2951)
    * DFR-5093 Updated Services to use existing ObjectMapper bean that is configured to use correct localDate deserialisation.
    
    * DFR-5093 Updated AddedSolicitorService and RemovedSolicitorService to use the correct objectMapper bean also.
    
    ---------
    
    Co-authored-by: Paul Trelease <paul.trelease@justice.gov.uk>

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/TestObjectMapperFactory.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+public final class TestObjectMapperFactory {
+
+    private TestObjectMapperFactory() {
+    }
+
+    public static ObjectMapper createObjectMapper() {
+        return JsonMapper.builder()
+            .addModule(new JavaTimeModule())
+            .addModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES))
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT)
+            .build();
+    }
+}
