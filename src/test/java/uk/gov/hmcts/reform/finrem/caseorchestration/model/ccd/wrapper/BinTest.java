d771467e1b190c7861d1552d304bce78d2b3c0b3
Ashley Wong <im@so99y.com>
2026-05-21 17:29:56 +0100

    add BinTest

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/model/ccd/wrapper/BinTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.DynamicListElement;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
+
+class BinTest {
+
+    @Test
+    void shouldInitialiseBinAndAddCaseDocument() {
+        Bin bin = Bin.builder().build();
+
+        CaseDocument doc = caseDocument("fileA");
+
+        bin.binCaseDocument(doc);
+
+        assertAll(
+            () -> assertNotNull(bin.getFileUrlsToBeDeleted())
+        );
+
+        List<DynamicListElement> items = bin.getFileUrlsToBeDeleted().getListItems();
+
+        assertThat(items)
+            .hasSize(1)
+            .extracting(DynamicListElement::getCode)
+            .containsExactly(doc.getDocumentUrl());
+    }
+
+    @Test
+    void shouldAppendMultipleCaseDocuments() {
+        Bin bin = Bin.builder().build();
+
+        CaseDocument doc1 = caseDocument("fileA");
+        CaseDocument doc2 = caseDocument("fileB");
+
+        bin.binCaseDocument(doc1);
+        bin.binCaseDocument(doc2);
+
+        assertAll(
+            () -> assertNotNull(bin.getFileUrlsToBeDeleted())
+        );
+
+        List<DynamicListElement> items = bin.getFileUrlsToBeDeleted().getListItems();
+
+        assertThat(items)
+            .hasSize(2)
+            .extracting(DynamicListElement::getCode)
+            .containsExactly(
+                doc1.getDocumentUrl(),
+                doc2.getDocumentUrl()
+            );
+    }
+
+    @Test
+    void shouldClearBin() {
+        Bin bin = Bin.builder().build();
+
+        CaseDocument doc = caseDocument("fileA");
+
+        bin.binCaseDocument(doc);
+
+        assertAll(
+            () -> assertNotNull(bin.getFileUrlsToBeDeleted())
+        );
+
+        bin.clearBin();
+
+        assertThat(bin.getFileUrlsToBeDeleted()).isNull();
+    }
+}
