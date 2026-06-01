08a813fbf376fb223a46a69674e28f338cdc0dc0
Ashley Wong <im@so99y.com>
2026-06-01 16:01:28 +0100

    Add event UPLOAD_PENSION_DOCUMENTS - mid event

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/uploadpensiondocument/consented/UploadPensionDocumentMidHandler.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.uploadpensiondocument.consented;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+
+@Slf4j
+@Service
+public class UploadPensionDocumentMidHandler extends FinremCallbackHandler {
+
+    public UploadPensionDocumentMidHandler(FinremCaseDetailsMapper finremCaseDetailsMapper) {
+        super(finremCaseDetailsMapper);
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.MID_EVENT.equals(callbackType)
+            && CaseType.CONSENTED.equals(caseType)
+            && EventType.UPLOAD_PENSION_DOCUMENTS.equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.midEvent(callbackRequest));
+
+        FinremCaseData finremCaseData = callbackRequest.getFinremCaseData();
+
+        return response(finremCaseData);
+    }
+}
