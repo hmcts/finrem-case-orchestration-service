49698c42a80ace6e6bfcc97109b7fa7bd05e2b37
Ashley Wong <im@so99y.com>
2026-06-03 10:39:47 +0100

    add GenericInputFields

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/FinremCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/FinremCallbackHandler.java
@@ -17,6 +17,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.Bin;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.CaseDataMetricsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GeneralApplicationWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.GenericInputFields;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ManageCaseDocumentsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.StopRepresentationWrapper;
 
@@ -251,6 +252,7 @@ public abstract class FinremCallbackHandler implements CallbackHandler<FinremCas
      */
     private static List<Class<?>> getClassesWithTemporaryFieldAnnotation() {
         return List.of(
+            GenericInputFields.class,
             StopRepresentationWrapper.class,
             CaseDataMetricsWrapper.class,
             ContactDetailsWrapper.class,
