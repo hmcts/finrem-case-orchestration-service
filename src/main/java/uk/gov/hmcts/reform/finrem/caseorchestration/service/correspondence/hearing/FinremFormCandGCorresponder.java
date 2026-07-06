31a58167c60adc40b98bc687004ca26da8921d92
Ashley Wong <im@so99y.com>
2026-07-06 16:43:35 +0100

    Fix FinremFormCandGCorresponderTest

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/hearing/FinremFormCandGCorresponder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/hearing/FinremFormCandGCorresponder.java
@@ -29,8 +29,7 @@ public class FinremFormCandGCorresponder extends FinremHearingCorresponder {
 
     @Override
     public List<CaseDocument> getCaseDocuments(FinremCaseDetails caseDetails) {
-        String caseId = caseDetails.getId() == null ? "noId" : caseDetails.getId().toString();
-        return getHearingCaseDocuments(caseDetails.getData(), caseId);
+        return getHearingCaseDocuments(caseDetails.getData());
     }
 
     @Override
@@ -43,10 +42,9 @@ public class FinremFormCandGCorresponder extends FinremHearingCorresponder {
             documentHelper.getCaseDocumentsAsBulkPrintDocuments(caseDocuments, caseDetails.getCaseType(), authorisationToken));
     }
 
-    private List<CaseDocument> getHearingCaseDocuments(FinremCaseData caseData, String caseId) {
+    private List<CaseDocument> getHearingCaseDocuments(FinremCaseData caseData) {
         List<CaseDocument> caseDocuments = new ArrayList<>();
 
-        log.info("Fetching Contested Paper Case bulk print document for Case ID: {}", caseId);
         ListForHearingWrapper listForHearingWrapper = caseData.getListForHearingWrapper();
         Optional.ofNullable(listForHearingWrapper.getFormC()).ifPresent(caseDocuments::add);
         Optional.ofNullable(listForHearingWrapper.getFormG()).ifPresent(caseDocuments::add);
