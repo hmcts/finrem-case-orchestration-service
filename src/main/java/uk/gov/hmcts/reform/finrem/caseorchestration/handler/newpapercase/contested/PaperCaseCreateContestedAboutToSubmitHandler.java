ff65877d4e41164c65d13b4c67f6b770af6c8185
Ashley Wong <im@so99y.com>
2026-07-09 17:16:55 +0100

    extends FinremAboutToSubmitCallbackHandler

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/newpapercase/contested/PaperCaseCreateContestedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/newpapercase/contested/PaperCaseCreateContestedAboutToSubmitHandler.java
@@ -6,7 +6,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
-import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremAboutToSubmitCallbackHandler;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.handler.solicitorcreatecase.mandatorydatavalidation.ApplicantSolicitorDetailsValidator;
 import uk.gov.hmcts.reform.finrem.caseorchestration.helper.ContactDetailsValidator;
@@ -33,7 +33,7 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CCDConfigCo
 
 @Slf4j
 @Service
-public class PaperCaseCreateContestedAboutToSubmitHandler extends FinremCallbackHandler {
+public class PaperCaseCreateContestedAboutToSubmitHandler extends FinremAboutToSubmitCallbackHandler {
 
     private final CaseFlagsService caseFlagsService;
     private final IdamService idamService;
