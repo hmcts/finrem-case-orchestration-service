008b97a94d5763ba496a4ffdb2394f7896ec2ba3
Ravi Doki <ravi.doki@hmcts.net>
2026-07-10 18:01:16 +0100

    DFR-5043 - FS added

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.service.globalsearch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
 
 import java.util.Map;
--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchServiceTest.java
@@ -13,8 +13,6 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
