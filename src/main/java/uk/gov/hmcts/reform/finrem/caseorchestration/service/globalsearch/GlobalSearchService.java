d42be62346b57eee920eff0826fe17792bb27c0c
Ravi Doki <ravi.doki@hmcts.net>
2026-07-15 10:19:12 +0100

    DFR-5043 - java docs

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/globalsearch/GlobalSearchService.java
@@ -7,6 +7,10 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService
 
 import java.util.Map;
 
+/**
+ * Service responsible for populating global search related fields on case data.
+ * It checks the feature toggle before mutating the provided case data map.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,13 @@ public class GlobalSearchService {
 
     private final FeatureToggleService featureToggleService;
 
+    /**
+     * Sets fields required for global search on the provided case data map.
+     * No changes are made if the global search feature is disabled.
+     *
+     * @param caseData
+     *
+     */
     public void setGlobalSearchDataByMap(Map<String, Object> caseData) {
         if (featureToggleService.isGlobalSearchEnabled()) {
             log.info("setGlobalSearchDataByMap::Received request to set global search fields for case with CCD ID: {}", caseData.get("ccdCaseId"));
