680b026771896ed7706bff137277f9fa164fdecf
Ashley Wong <im@so99y.com>
2026-08-18 17:51:42 +0100

    fix unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/hwfacceptedandissue/consented/HwfAcceptedAndIssueSubmittedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/hwfacceptedandissue/consented/HwfAcceptedAndIssueSubmittedHandlerTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.finrem.caseorchestration.handler.hwfacceptedandissue
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,18 +12,31 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackReques
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignPartiesAccessService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.UserNotFoundInOrganisationApiException;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.assigntojudge.IssueApplicationConsentCorresponder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.hwf.HwfCorrespondenceService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryErrorHandler;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryExecutor;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.ThrowingRunnable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID_IN_LONG;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.TEST_RESP_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.getThrowingRunnableCaptor;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.mockRunWithRetryWithHandlerInvokesFirstErrorHandler;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.runSafely;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.HWF_ACCEPTED_AND_ISSUE;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
@@ -38,6 +52,15 @@ class HwfAcceptedAndIssueSubmittedHandlerTest {
 
     @Mock
     private RetryExecutor retryExecutor;
+
+    @Mock
+    private HwfCorrespondenceService hwfNotificationsService;
+
+    @Mock
+    private IssueApplicationConsentCorresponder issueApplicationConsentCorresponder;
+
+    @Mock
+    private AssignPartiesAccessService assignPartiesAccessService;
 
     @Test
     void testCanHandle() {
@@ -150,5 +173,126 @@ class HwfAcceptedAndIssueSubmittedHandlerTest {
             () -> assertThat(response.getConfirmationHeader()).isNull(),
             () -> assertThat(response.getConfirmationBody()).isNull()
         );
+    }
+
+    @Test
+    void givenCase_whenHandled_shouldSendHwfCorrespondence() {
+        // Arrange
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.fromId(CASE_ID_IN_LONG);
+
+        // Act
+        handler.handle(callbackRequest, AUTH_TOKEN);
+
+        ArgumentCaptor<ThrowingRunnable> runnableCaptor = getThrowingRunnableCaptor();
+        verify(retryExecutor)
+            .runWithRetryWithHandler(
+                runnableCaptor.capture(),
+                eq("sending HWF correspondence"),
+                eq(CASE_ID),
+                any(RetryErrorHandler.class)
+            );
+        runSafely(runnableCaptor.getValue());
+        verify(hwfNotificationsService).sendCorrespondence(callbackRequest.getCaseDetails(), AUTH_TOKEN);
+    }
+
+    @Test
+    void givenCase_whenHandled_shouldSendIssueApplicationCorrespondence() {
+        // Arrange
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.fromId(CASE_ID_IN_LONG);
+
+        // Act
+        handler.handle(callbackRequest, AUTH_TOKEN);
+
+        ArgumentCaptor<ThrowingRunnable> runnableCaptor = getThrowingRunnableCaptor();
+        verify(retryExecutor)
+            .runWithRetryWithHandler(
+                runnableCaptor.capture(),
+                eq("sending issue application correspondence"),
+                eq(CASE_ID),
+                any(RetryErrorHandler.class)
+            );
+        runSafely(runnableCaptor.getValue());
+        verify(issueApplicationConsentCorresponder).sendCorrespondence(callbackRequest.getCaseDetails(), AUTH_TOKEN);
+    }
+
+    @Test
+    void givenCaseWithRepresentedRespondentSolicitor_whenHandled_shouldGrantRespondentSolicitor()
+        throws UserNotFoundInOrganisationApiException {
+        // Arrange
+        FinremCaseData caseData = spy(FinremCaseData.builder()
+            .contactDetailsWrapper(ContactDetailsWrapper.builder()
+                .consentedRespondentRepresented(YesOrNo.YES)
+                .build())
+            .build());
+        when(caseData.getRespondentSolicitorEmail()).thenReturn(TEST_RESP_SOLICITOR_EMAIL);
+
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG,
+            caseData);
+
+        // Act
+        handler.handle(callbackRequest, AUTH_TOKEN);
+
+        ArgumentCaptor<ThrowingRunnable> runnableCaptor = getThrowingRunnableCaptor();
+        verify(retryExecutor)
+            .runWithRetryWithHandler(
+                runnableCaptor.capture(),
+                eq("granting respondent solicitor"),
+                eq(CASE_ID),
+                any(RetryErrorHandler.class)
+            );
+        runSafely(runnableCaptor.getValue());
+        verify(assignPartiesAccessService).grantRespondentSolicitor(caseData);
+    }
+
+    @Test
+    void givenCaseWithoutRespondentSolicitorEmail_whenHandled_shouldGrantRespondentSolicitor()
+        throws UserNotFoundInOrganisationApiException {
+        // Arrange
+        FinremCaseData caseData = FinremCaseData.builder()
+            .contactDetailsWrapper(ContactDetailsWrapper.builder()
+                .consentedRespondentRepresented(YesOrNo.YES)
+                .build())
+            .build();
+
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG,
+            caseData);
+
+        // Act
+        handler.handle(callbackRequest, AUTH_TOKEN);
+
+        verify(retryExecutor, never())
+            .runWithRetryWithHandler(
+                any(ThrowingRunnable.class),
+                eq("granting respondent solicitor"),
+                eq(CASE_ID),
+                any(RetryErrorHandler.class)
+            );
+        verify(assignPartiesAccessService, never()).grantRespondentSolicitor(caseData);
+    }
+
+    @Test
+    void givenRespondentUnrepresented_whenHandled_shouldGrantRespondentSolicitor()
+        throws UserNotFoundInOrganisationApiException {
+        // Arrange
+        FinremCaseData caseData = FinremCaseData.builder()
+            .contactDetailsWrapper(ContactDetailsWrapper.builder()
+                .consentedRespondentRepresented(YesOrNo.NO)
+                .build())
+            .build();
+
+        FinremCallbackRequest callbackRequest = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG,
+            caseData);
+
+        // Act
+        handler.handle(callbackRequest, AUTH_TOKEN);
+
+        verify(retryExecutor, never())
+            .runWithRetryWithHandler(
+                any(ThrowingRunnable.class),
+                eq("granting respondent solicitor"),
+                eq(CASE_ID),
+                any(RetryErrorHandler.class)
+            );
+        verify(assignPartiesAccessService, never()).grantRespondentSolicitor(caseData);
     }
 }
