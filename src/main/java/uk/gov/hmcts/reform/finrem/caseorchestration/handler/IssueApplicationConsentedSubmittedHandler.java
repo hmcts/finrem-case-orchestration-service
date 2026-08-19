fbb31a1e72b9ba83561d2262df7c0471407c5e07
Ashley Wong <im@so99y.com>
2026-08-19 17:20:14 +0100

    Introduce AbstractDefaultIssueDateSubmittedHandler

--- src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedSubmittedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedSubmittedHandler.java
@@ -1,99 +1,30 @@
 package uk.gov.hmcts.reform.finrem.caseorchestration.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.consented.AbstractDefaultIssueDateSubmittedHandler;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignPartiesAccessService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.assigntojudge.IssueApplicationConsentCorresponder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.evidencemanagement.EvidenceManagementDeleteService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryExecutor;
 
 @Slf4j
 @Service
-public class IssueApplicationConsentedSubmittedHandler extends FinremCallbackHandler {
+public class IssueApplicationConsentedSubmittedHandler extends AbstractDefaultIssueDateSubmittedHandler {
 
-    private final IssueApplicationConsentCorresponder issueApplicationConsentCorresponder;
-    private final AssignPartiesAccessService assignPartiesAccessService;
-    private final RetryExecutor retryExecutor;
-
-    @Autowired
     public IssueApplicationConsentedSubmittedHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                                     EvidenceManagementDeleteService evidenceManagementDeleteService,
+                                                     RetryExecutor retryExecutor,
                                                      IssueApplicationConsentCorresponder issueApplicationConsentCorresponder,
-                                                     AssignPartiesAccessService assignPartiesAccessService,
-                                                     RetryExecutor retryExecutor) {
-        super(finremCaseDetailsMapper);
-        this.issueApplicationConsentCorresponder = issueApplicationConsentCorresponder;
-        this.assignPartiesAccessService = assignPartiesAccessService;
-        this.retryExecutor = retryExecutor;
+                                                     AssignPartiesAccessService assignPartiesAccessService) {
+        super(finremCaseDetailsMapper, evidenceManagementDeleteService,
+            retryExecutor, issueApplicationConsentCorresponder, assignPartiesAccessService);
     }
 
     @Override
-    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
-        return CallbackType.SUBMITTED.equals(callbackType)
-            && CaseType.CONSENTED.equals(caseType)
-            && EventType.ISSUE_APPLICATION.equals(eventType);
-    }
-
-    @Override
-    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
-                                                                              String userAuthorisation) {
-        log.info(CallbackHandlerLogger.submitted(callbackRequest));
-        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
-        FinremCaseData caseData = caseDetails.getData();
-
-        String assignRespondentSolicitorError = grantRespondentSolicitor(caseData);
-        String sendCorrespondenceError = sendCorrespondence(caseDetails, userAuthorisation);
-        boolean isHavingErrors = !StringUtils.isAllBlank(assignRespondentSolicitorError, sendCorrespondenceError);
-
-        if (isHavingErrors) {
-            return submittedResponse(
-                toConfirmationHeader("Application Issued with Errors"),
-                toConfirmationBody(assignRespondentSolicitorError, sendCorrespondenceError));
-        } else {
-            return submittedResponse();
-        }
-    }
-
-    private String grantRespondentSolicitor(FinremCaseData caseData) {
-        ContactDetailsWrapper contactDetailsWrapper = caseData.getContactDetailsWrapper();
-        String respSolEmail = YesOrNo.isYes(contactDetailsWrapper.getConsentedRespondentRepresented())
-            ? caseData.getRespondentSolicitorEmail() : null;
-        if (StringUtils.isBlank(respSolEmail)) {
-            return null;
-        }
-        try {
-            retryExecutor.runWithRetry(() -> assignPartiesAccessService
-                .grantRespondentSolicitor(caseData),
-                "granting respondent solicitor",
-                caseData.getCcdCaseId()
-            );
-            return null;
-        } catch (Exception ex) {
-            log.error("Error granting respondent solicitor", ex);
-            return "There was a problem granting access to respondent solicitor: %s".formatted(respSolEmail);
-        }
-    }
-
-    private String sendCorrespondence(FinremCaseDetails caseDetails, String userAuthorisation) {
-        try {
-            retryExecutor.runWithRetry(() -> issueApplicationConsentCorresponder
-                .sendCorrespondence(caseDetails, userAuthorisation),
-                "sending correspondence",
-                caseDetails.getCaseIdAsString()
-            );
-            return null;
-        } catch (Exception ex) {
-            log.error("Error sending correspondence", ex);
-            return "There was a problem sending correspondence.";
-        }
+    protected EventType supportedEventType() {
+        return EventType.ISSUE_APPLICATION;
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/consented/AbstractDefaultIssueDateSubmittedHandler.java
@@ -0,0 +1,128 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.consented;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.CallbackHandlerLogger;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremSubmittedCallbackHandler;
+import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.FinremCaseDetailsMapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.wrapper.ContactDetailsWrapper;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.AssignPartiesAccessService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.correspondence.assigntojudge.IssueApplicationConsentCorresponder;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.evidencemanagement.EvidenceManagementDeleteService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.utils.retry.RetryExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public abstract class AbstractDefaultIssueDateSubmittedHandler extends FinremSubmittedCallbackHandler {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected final IssueApplicationConsentCorresponder issueApplicationConsentCorresponder;
+
+    protected final AssignPartiesAccessService assignPartiesAccessService;
+
+    protected AbstractDefaultIssueDateSubmittedHandler(FinremCaseDetailsMapper finremCaseDetailsMapper,
+                                                       EvidenceManagementDeleteService evidenceManagementDeleteService,
+                                                       RetryExecutor retryExecutor,
+                                                       IssueApplicationConsentCorresponder issueApplicationConsentCorresponder,
+                                                       AssignPartiesAccessService assignPartiesAccessService) {
+        super(finremCaseDetailsMapper, evidenceManagementDeleteService, retryExecutor);
+        this.issueApplicationConsentCorresponder = issueApplicationConsentCorresponder;
+        this.assignPartiesAccessService = assignPartiesAccessService;
+    }
+
+    protected abstract EventType supportedEventType();
+
+    protected String getConfirmationHeader() {
+        return "Application Issued with Errors";
+    }
+
+    /**
+     * A unit of post-issue work that may fail. Implementations should return {@code null}
+     * (or a blank string) on success, and a human-readable error message on failure.
+     */
+    @FunctionalInterface
+    protected interface SubmittedTask {
+        String execute(FinremCaseDetails caseDetails, String userAuthorisation);
+    }
+
+    /**
+     * Hook for subclasses to contribute additional tasks to run after the standard
+     * "grant respondent solicitor" and "send correspondence" tasks. Each task's returned
+     * error (if any) is folded into the overall error check and confirmation body.
+     *
+     * @return list of additional tasks to execute; empty by default
+     */
+    protected List<SubmittedTask> additionalTasks() {
+        return List.of();
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, CaseType caseType, EventType eventType) {
+        return CallbackType.SUBMITTED.equals(callbackType)
+            && CaseType.CONSENTED.equals(caseType)
+            && supportedEventType().equals(eventType);
+    }
+
+    @Override
+    public GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> handle(FinremCallbackRequest callbackRequest,
+                                                                              String userAuthorisation) {
+        log.info(CallbackHandlerLogger.submitted(callbackRequest));
+        FinremCaseDetails caseDetails = callbackRequest.getCaseDetails();
+        FinremCaseData caseData = caseDetails.getData();
+
+        List<String> errors = new ArrayList<>();
+        errors.add(grantRespondentSolicitor(caseData));
+        errors.add(sendIssueApplicationCorrespondence(caseDetails, userAuthorisation));
+        additionalTasks().forEach(task -> errors.add(task.execute(caseDetails, userAuthorisation)));
+
+        boolean isHavingErrors = !StringUtils.isAllBlank(errors.toArray(new String[0]));
+
+        if (isHavingErrors) {
+            return submittedResponse(
+                toConfirmationHeader(getConfirmationHeader()),
+                toConfirmationBody(errors.toArray(new String[0])));
+        } else {
+            return submittedResponse();
+        }
+    }
+
+    private String sendIssueApplicationCorrespondence(FinremCaseDetails caseDetails, String userAuthorisation) {
+        AtomicReference<String> error = new AtomicReference<>();
+        retryExecutor.runWithRetryWithHandler(() -> issueApplicationConsentCorresponder
+                .sendCorrespondence(caseDetails, userAuthorisation),
+            "sending issue application correspondence",
+            caseDetails.getCaseIdAsString(),
+            (exception, actionName, caseId1) ->
+                error.set("There was a problem sending issue application correspondence. Please send it manually."));
+        return error.get();
+    }
+
+    private String grantRespondentSolicitor(FinremCaseData caseData) {
+        ContactDetailsWrapper contactDetailsWrapper = caseData.getContactDetailsWrapper();
+        String respSolEmail = YesOrNo.isYes(contactDetailsWrapper.getConsentedRespondentRepresented())
+            ? caseData.getRespondentSolicitorEmail() : null;
+        if (StringUtils.isBlank(respSolEmail)) {
+            return null;
+        }
+
+        AtomicReference<String> error = new AtomicReference<>();
+        retryExecutor.runWithRetryWithHandler(() -> assignPartiesAccessService.grantRespondentSolicitor(caseData),
+            "granting respondent solicitor",
+            caseData.getCcdCaseId(),
+            (exception, actionName, caseId1) ->
+                error.set("There was a problem granting access to respondent solicitor: %s".formatted(respSolEmail)));
+        return error.get();
+    }
+}
