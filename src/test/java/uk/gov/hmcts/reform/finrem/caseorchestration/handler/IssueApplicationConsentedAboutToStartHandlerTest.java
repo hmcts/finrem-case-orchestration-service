73b0e8bf54531b875872bc9b20f0c8a410c66ba1
Ashley Wong <im@so99y.com>
2026-08-20 12:11:46 +0100

    update about-to-start tests

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/IssueApplicationConsentedAboutToStartHandlerTest.java
@@ -5,24 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.finrem.caseorchestration.controllers.GenericAboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
-import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.consented.IssueApplicationAboutToStartHandlerContractTest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
 
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.ISSUE_APPLICATION;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
 
 @ExtendWith(MockitoExtension.class)
-class IssueApplicationConsentedAboutToStartHandlerTest {
+class IssueApplicationConsentedAboutToStartHandlerTest extends IssueApplicationAboutToStartHandlerContractTest {
 
     @InjectMocks
     private IssueApplicationConsentedAboutToStartHandler handler;
@@ -35,27 +27,13 @@ class IssueApplicationConsentedAboutToStartHandlerTest {
         assertCanHandle(handler, ABOUT_TO_START, CONSENTED, ISSUE_APPLICATION);
     }
 
-    @Test
-    void givenConsentedCase_whenUseIssueApplication_thenDefaultIssueDateSetToCurrentDate() {
-        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
-        handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        verify(onStartDefaultValueService).defaultIssueDate(finremCallbackRequest);
+    @Override
+    protected FinremCallbackHandler handler() {
+        return handler;
     }
 
-    @Test
-    void givenConsentedCase_whenUseIssueApplicationAndIssueDateEnteredManually_thenHandle() {
-        FinremCallbackRequest finremCallbackRequest = buildCallbackRequest();
-        finremCallbackRequest.getCaseDetails().getData().setIssueDate(LocalDate.of(2000,10,10));
-        GenericAboutToStartOrSubmitCallbackResponse<FinremCaseData> response = handler.handle(finremCallbackRequest, AUTH_TOKEN);
-        assertEquals(LocalDate.of(2000,10,10), response.getData().getIssueDate());
-    }
-
-    private FinremCallbackRequest buildCallbackRequest() {
-        return FinremCallbackRequest
-            .builder()
-            .eventType(EventType.ISSUE_APPLICATION)
-            .caseDetails(FinremCaseDetails.builder().id(123L).caseType(CONSENTED)
-                .data(new FinremCaseData()).build())
-            .build();
+    @Override
+    protected OnStartDefaultValueService onStartDefaultValueService() {
+        return onStartDefaultValueService;
     }
 }
