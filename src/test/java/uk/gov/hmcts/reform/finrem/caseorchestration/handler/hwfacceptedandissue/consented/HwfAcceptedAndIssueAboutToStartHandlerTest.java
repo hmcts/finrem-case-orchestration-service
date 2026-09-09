0bc8f6c005a521b9250c439ef5d00d946bf7b137
Ashley W <50691452+so99y@users.noreply.github.com>
2026-09-09 13:00:29 +0100

    DFR-5309 CS: Create new HWF Accepted and Issue event- #3466 (#3069)
    * Add event FR_HWFAcceptedAndIssue
    
    * Add about-to-start handler
    
    * Grant respondent solicitor automatically and send issue application correspondence.
    
    * Copying about-to-submit from IssueApplicationConsentedAboutToSubmitHandler
    
    * Move generateCoverSheetService to IssueApplicationService
    
    * indentation
    
    * refactor
    
    * unit test
    
    * increase code coverage
    
    * Remove duplication
    
    * Extends FinremAboutToSubmitCallbackHandler
    
    * fix unit test
    
    * fix unit test
    
    * checkstyle
    
    * fix unit test
    
    * Remove unwanted comments.
    
    ---------
    
    Co-authored-by: Ashley Wong <im@so99y.com>

---
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/handler/hwfacceptedandissue/consented/HwfAcceptedAndIssueAboutToStartHandlerTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.handler.hwfacceptedandissue.consented;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.FinremCallbackRequestFactory;
+import uk.gov.hmcts.reform.finrem.caseorchestration.handler.FinremCallbackRequest;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseData;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnStartDefaultValueService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID_IN_LONG;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.ccd.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType.HWF_ACCEPTED_AND_ISSUE;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseType.CONSENTED;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.test.Assertions.assertCanHandle;
+
+@ExtendWith(MockitoExtension.class)
+class HwfAcceptedAndIssueAboutToStartHandlerTest {
+
+    @InjectMocks
+    private HwfAcceptedAndIssueAboutToStartHandler handler;
+    @Mock
+    private OnStartDefaultValueService onStartDefaultValueService;
+
+    @Test
+    void testCanHandle() {
+        assertCanHandle(handler, ABOUT_TO_START, CONSENTED, HWF_ACCEPTED_AND_ISSUE);
+    }
+
+    @Test
+    void shouldPopulateIssueDate_whenHandled() {
+        FinremCaseData finremCaseData = FinremCaseData.builder().build();
+        FinremCallbackRequest request = FinremCallbackRequestFactory.from(CASE_ID_IN_LONG, finremCaseData);
+
+        var response = handler.handle(request, AUTH_TOKEN);
+
+        verify(onStartDefaultValueService).defaultIssueDate(request);
+        assertThat(response.getData()).isEqualTo(finremCaseData);
+    }
+}
