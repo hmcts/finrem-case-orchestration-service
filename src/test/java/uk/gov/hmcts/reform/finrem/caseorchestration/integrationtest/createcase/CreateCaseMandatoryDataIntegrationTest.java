28d1596b7738a8e218c5e0407a90af37956ec132
Ravi Doki <ravi.doki@hmcts.net>
2026-09-21 13:43:04 +0100

    DFR-5693 - Location changes

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/integrationtest/createcase/CreateCaseMandatoryDataIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/integrationtest/createcase/CreateCaseMandatoryDataIntegrationTest.java
@@ -24,6 +24,7 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.IdamService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.OnlineFormDocumentService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.SelectedCourtService;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.caselocation.CaseManagementLocationService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.express.ExpressCaseService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.globalsearch.GlobalSearchService;
 import uk.gov.hmcts.reform.finrem.caseorchestration.service.noc.nocworkflows.UpdateRepresentationWorkflowService;
@@ -45,7 +46,8 @@ import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TO
     CallbackDispatchService.class,
     GlobalSearchService.class,
     SolicitorCreateContestedAboutToSubmitHandler.class,
-    ObjectMapper.class, FinremCaseDetailsMapper.class
+    ObjectMapper.class, FinremCaseDetailsMapper.class,
+    CaseManagementLocationService.class
 })
 class CreateCaseMandatoryDataIntegrationTest {
 
