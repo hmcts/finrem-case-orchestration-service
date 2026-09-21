bca7d5147f163ede788a88a70dd108d0700a3440
Hamzah <hamzah.mohammed@justice.gov.uk>
2026-09-21 16:26:06 +0100

    Add abstract class to handle documents and categorise updating disclosure docs

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/casedocuments/UpdatingDisclosureHandler.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.finrem.caseorchestration.service.casedocuments;
+
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocumentParty;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocumentType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.UploadCaseDocument;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.YesOrNo;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.document.CaseDocumentCollectionType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.service.FeatureToggleService;
+
+public abstract class UpdatingDisclosureHandler extends PartyDocumentsHandler {
+
+    public UpdatingDisclosureHandler(CaseDocumentCollectionType caseDocumentCollectionType,
+                                     CaseDocumentParty party, FeatureToggleService featureToggleService) {
+        super(caseDocumentCollectionType, party, featureToggleService);
+    }
+
+    @Override
+    protected boolean canHandleDocument(UploadCaseDocument uploadCaseDocument) {
+
+        CaseDocumentType caseDocumentType = uploadCaseDocument.getCaseDocumentType();
+        return uploadCaseDocument.getCaseDocumentFdr().equals(YesOrNo.NO)
+            && (caseDocumentType.equals(CaseDocumentType.UPDATING_DISCLOSURE));
+    }
+}
