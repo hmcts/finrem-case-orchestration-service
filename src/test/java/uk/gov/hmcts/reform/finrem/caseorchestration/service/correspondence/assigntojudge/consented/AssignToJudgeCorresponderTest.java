479b9d334ba065099b63b03ab732b4fff3d62251
Ashley Wong <im@so99y.com>
2026-09-22 16:12:43 +0100

    doc generation unit test

--- src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/consented/AssignToJudgeCorresponderTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/caseorchestration/service/correspondence/assigntojudge/consented/AssignToJudgeCorresponderTest.java
@@ -7,8 +7,10 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.finrem.caseorchestration.helper.DocumentHelper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.mapper.notificationrequest.FinremNotificationRequestMapper;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.EventType;
+import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.CaseDocument;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.ccd.FinremCaseDetails;
 import uk.gov.hmcts.reform.finrem.caseorchestration.model.notification.NotificationRequest;
 import uk.gov.hmcts.reform.finrem.caseorchestration.notifications.notifiers.NotificationParty;
@@ -19,8 +21,10 @@ import uk.gov.hmcts.reform.finrem.caseorchestration.service.noc.solicitors.Check
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -31,6 +35,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.TestConstants.CASE_ID_IN_LONG;
+import static uk.gov.hmcts.reform.finrem.caseorchestration.TestSetUpUtils.caseDocument;
 import static uk.gov.hmcts.reform.finrem.caseorchestration.notifications.domain.EmailTemplateNames.FR_ASSIGNED_TO_JUDGE;
 
 @ExtendWith(MockitoExtension.class)
@@ -104,6 +109,7 @@ class AssignToJudgeCorresponderTest {
                         events.getFirst().getEmailNotificationRequest()
                     ),
                     () -> assertEquals(AUTH_TOKEN, events.getFirst().getAuthToken()),
+                    () -> assertThat(events.getFirst().getDocumentsToPost()).isEmpty(),
 
                     // Respondent event
                     () -> assertEquals(eventType.name(), events.get(1).getEventId()),
@@ -119,9 +125,78 @@ class AssignToJudgeCorresponderTest {
                     ),
                     () -> assertEquals(AUTH_TOKEN, events.get(1).getAuthToken()),
                     () -> assertEquals("tracker-valid-id", // respondent event should be the first tracker id
-                        events.get(1).getNotificationTrackerId())
+                        events.get(1).getNotificationTrackerId()),
+                    () -> assertThat(events.get(1).getDocumentsToPost()).isEmpty()
                 );
             }
+        );
+    }
+
+    @Test
+    void shouldGenerateDocument_whenBuildSendCorrespondenceEvents() {
+        // Arrange
+        FinremCaseDetails finremCaseDetails = mock(FinremCaseDetails.class);
+        when(finremCaseDetails.getId()).thenReturn(CASE_ID_IN_LONG);
+
+        EventType eventType = EventType.ISSUE_APPLICATION;
+
+        when(checkSolicitorIsDigitalService.isApplicantSolicitorDigital(CASE_ID)).thenReturn(true);
+        when(checkSolicitorIsDigitalService.isRespondentSolicitorDigital(CASE_ID)).thenReturn(true);
+
+        NotificationRequest applicantNotificationRequest = mock(NotificationRequest.class);
+        when(finremNotificationRequestMapper
+            .getNotificationRequestForApplicantSolicitor(finremCaseDetails, false)
+        ).thenReturn(applicantNotificationRequest);
+
+        NotificationRequest respondentNotificationRequest = mock(NotificationRequest.class);
+        when(finremNotificationRequestMapper
+            .getNotificationRequestForRespondentSolicitor(finremCaseDetails, false)
+        ).thenReturn(respondentNotificationRequest);
+
+        CaseDocument applicantDoc = caseDocument("applicantDoc");
+        when(assignedToJudgeDocumentService.generateAssignedToJudgeNotificationLetter(finremCaseDetails, AUTH_TOKEN,
+                DocumentHelper.PaperNotificationRecipient.APPLICANT)).thenReturn(applicantDoc);
+        CaseDocument respondentDoc = caseDocument("respondentDoc");
+        when(assignedToJudgeDocumentService.generateAssignedToJudgeNotificationLetter(finremCaseDetails, AUTH_TOKEN,
+            DocumentHelper.PaperNotificationRecipient.RESPONDENT)).thenReturn(respondentDoc);
+
+        // Act
+        List<SendCorrespondenceEvent> events = assignToJudgeCorresponder.buildSendCorrespondenceEvents(eventType, finremCaseDetails, AUTH_TOKEN);
+
+        // Verify
+        assertAll(
+            () -> verifyNoInteractions(notificationAuditService),
+            // Applicant event
+            () -> assertEquals(eventType.name(), events.getFirst().getEventId()),
+            () -> assertEquals(finremCaseDetails, events.getFirst().getCaseDetails()),
+            () -> assertEquals(
+                List.of(NotificationParty.APPLICANT),
+                events.getFirst().getNotificationParties()
+            ),
+            () -> assertEquals(FR_ASSIGNED_TO_JUDGE, events.getFirst().getEmailTemplate()),
+            () -> assertEquals(
+                applicantNotificationRequest,
+                events.getFirst().getEmailNotificationRequest()
+            ),
+            () -> assertEquals(AUTH_TOKEN, events.getFirst().getAuthToken()),
+            () -> assertNull(events.getFirst().getNotificationTrackerId()),
+            () -> assertThat(events.getFirst().getDocumentsToPost()).containsOnly(applicantDoc),
+
+            // Respondent event
+            () -> assertEquals(eventType.name(), events.get(1).getEventId()),
+            () -> assertEquals(finremCaseDetails, events.get(1).getCaseDetails()),
+            () -> assertEquals(
+                List.of(NotificationParty.RESPONDENT),
+                events.get(1).getNotificationParties()
+            ),
+            () -> assertEquals(FR_ASSIGNED_TO_JUDGE, events.get(1).getEmailTemplate()),
+            () -> assertEquals(
+                respondentNotificationRequest,
+                events.get(1).getEmailNotificationRequest()
+            ),
+            () -> assertEquals(AUTH_TOKEN, events.get(1).getAuthToken()),
+            () -> assertNull(events.get(1).getNotificationTrackerId()),
+            () -> assertThat(events.get(1).getDocumentsToPost()).containsOnly(respondentDoc)
         );
     }
 }
